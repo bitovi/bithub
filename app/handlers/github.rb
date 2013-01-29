56a8b4c6320c21f7cb19172e9ff444a254155c28
Nikica Jokic <neektza@gmail.com>
2013-01-29 15:35:11 +0100

    Rescue block for Github:34

--- app/handlers/github.rb
+++ app/handlers/github.rb
@@ -31,7 +31,12 @@ module Handler
     
     def filter_old(feed_events)
       feed_events.each do |e|
-        e['hash_key'] = Digest::MD5.hexdigest(e['id'].to_s + feed.to_s)
+        begin
+          e['hash_key'] = Digest::MD5.hexdigest(e['id'].to_s + feed.to_s)
+        rescue TypeError => e
+          @log.error e
+          @log.error "FEED: #{feed} | ID: #{e['id']}"
+        end
       end
       super(feed_events)
     end
