b19b5880f28dd4f083ef8302ed6ccbfe246a841d
Nikica Jokic <neektza@gmail.com>
2013-02-26 17:04:09 +0100

    Created the app with a PG template.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bithub::Application.config.secret_token = '0d57936a0cbc952a7559a2ae9976efa296860047b598e89224e2bd758cc8e1acc1f2cc39e2f8e9ab7c45a8b96eaaaee07a3fd29b9a406b93baf69fca11917df6'
