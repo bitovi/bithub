56783d9c95b2cc39ebea0550e886c3ad37e1c2ec
Veljko Dragsic <veljko@kset.org>
2013-03-04 13:33:31 +0100

    Insert default rule via migration.

---
+++ db/migrate/20130304122257_insert_prepopulate_rules.rb
@@ -0,0 +1,14 @@
+class InsertPrepopulateRules < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      INSERT INTO rules (required_tags, authorship_value, award_value, upvote_value, created_at, updated_at)
+        VALUES ('{}', 0, 0, 0, NOW(), NOW());
+      SQL
+  end
+
+  def down
+    execute <<-SQL
+      DELETE FROM rules WHERE required_tags='{}';
+    SQL
+  end
+end
