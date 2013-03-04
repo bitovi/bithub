b8fa294e09b2e00abd2f07320f897da75cc9fbf9
Veljko Dragsic <veljko@kset.org>
2013-03-04 17:00:43 +0100

    Static best_match method on Rule model.

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,4 +1,25 @@
 class Rule < ActiveRecord::Base
   attr_accessible :required_tags, :authorship_value, :upvote_value, :award_value
   has_many :events
+
+  def self.best_match(tags)
+
+    # Try to find exact match
+    match = Rule.where("required_tags = ?", tags.to_postgres_array(true)).first
+
+    # otherwise try to find best match
+    if not match
+      score = -1 # will match default rule created by migrations
+
+      self.find_each do |rule|
+        if (count = (tags & rule.required_tags).count) > score
+          match = rule
+          score = count
+        end
+      end
+    end
+
+    return match
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130304111726) do
+ActiveRecord::Schema.define(:version => 20130304122257) do
 
   create_table "activities", :force => true do |t|
     t.integer  "applies_to_id",    :null => false
