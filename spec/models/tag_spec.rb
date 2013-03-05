2bf49246fb1904571260b6efb146443c69299036
Nikica Jokic <neektza@gmail.com>
2013-03-05 16:08:07 +0100

    Some tag_spec modifications.

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,16 +2,10 @@ require 'spec_helper'
 
 describe Tag do
   describe ".find_or_create" do
-    context "when there is no tag in the system" do
-      it "creates the tag and returns it" do
-        new_or_found_tag = Tag.find_or_create("some_tag")
-        found_tag = Tag.where({:name => "some_tag"}).first
-        expect(found_tag).to eql(new_or_found_tag)
-      end
-    end
-
-    context "when the tag is already in the system" do
-      it "just returns the tag"
+    it "creates or finds the tag and returns it" do
+      new_or_found_tag = Tag.find_or_create("some_tag")
+      found_tag = Tag.where({:name => "some_tag"}).first
+      expect(new_or_found_tag).to eql(found_tag)
     end
   end
 end
