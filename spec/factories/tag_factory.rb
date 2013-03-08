8bf7c948fe5c51a691979218373f56d6a86d63e4
Nikica Jokic <neektza@gmail.com>
2013-03-08 18:58:07 +0100

    Imporved the tag factory (only finds, then builds, instead of creating the object if it doesn't exist).

--- spec/factories/tag_factory.rb
+++ spec/factories/tag_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 
   factory :tag, :aliases => [:category, :feed] do
-    initialize_with { Tag.find_or_create_with_like_by_name(name)}
+    initialize_with { Tag.find_by_name(name) || new({name: name}) } # should only initialize, not create, but this is ok for now
     name "a_tag"
     display_name "A tag"
   end
