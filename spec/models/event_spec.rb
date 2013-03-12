f80a235a253da6619b5dff8866ae8bbe9ce52119
Nikica Jokic <neektza@gmail.com>
2013-03-12 14:05:51 +0100

    Event spec formatting and test names.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -49,7 +49,7 @@ describe Event do
         expect(event.tags.count).to eq(event.meta[:tags].count)        
       end
     end
-    
+
     describe "#determine_author" do
       context "when there is an author in the system" do
         it "associates it with a github event" do
@@ -59,7 +59,6 @@ describe Event do
           ghe.save!
           expect(ghe.author).to eq(usr)
         end
-
         it "associates it with a twitter event" do
           twe = build(:twitter_tweet)
           usr = create(:user)
@@ -68,7 +67,6 @@ describe Event do
           expect(twe.author).to eq(usr)
         end
       end
-
       context "when there is no author in the system" do
         it "creates it and associates it with the event" do
           generic_event = build(:event_wo_author)
@@ -77,7 +75,6 @@ describe Event do
           expect(generic_event.author).to be
         end
       end
-
     end
 
     describe "#determine_all" do
@@ -88,7 +85,6 @@ describe Event do
         feed = Tag.find_by_name(event.meta[:feed])
         category = Tag.find_by_name(event.meta[:category])
         rule = Rule.best_match(event.meta[:tags])
-        
         expect(event.feed).to eq(feed)
         expect(event.category).to eq(category)
         expect(event.rule).to eq(rule)
@@ -97,19 +93,13 @@ describe Event do
       end
     end
 
-    context "when grouping forum events" do
+    describe "#process_forums" do
       before :each do
         @starter = build(:forum_thread_starter)
         @reply1 = build(:forum_child)
         @reply2 = build(:forum_child)
       end
-
-      it "there should be a thread starter without children" do
-        @starter.process_forums.save!
-        expect(@starter.parent).to be_nil
-      end
-
-      it "there should be thread with 2 replies" do
+      it "groups a thread starter with 2 replies" do
         @reply1.process_forums.save!
         @starter.process_forums.save!
         @reply2.process_forums.save!
@@ -117,69 +107,68 @@ describe Event do
         expect(@reply1.reload.parent_id).to eql(@starter.id)
         expect(@reply2.reload.parent_id).to eql(@starter.id)
       end
-
-      it "there should be two replies grouped without thread" do
+      it "groups replies without a thread starter" do
         @reply1.process_forums.save!
         @reply2.process_forums.save!
         expect(@reply1.children.count).to eql(1)
       end
     end
 
-    context "when grouping github issues" do
-      before :each do
-        @issue = build(:github_issue)
-        @issue_comment1 = build(:github_issue_comment)
-        @issue_comment2 = build(:github_issue_comment)
+    describe "#process_github" do
+      context "when grouping issue comments" do
+        before :each do
+          @issue = build(:github_issue)
+          @issue_comment1 = build(:github_issue_comment)
+          @issue_comment2 = build(:github_issue_comment)
+        end
+        it "groups issue comments with an issue" do
+          @issue_comment1.process_github.save!
+          @issue.process_github.save!
+          @issue_comment2.process_github.save!
+          expect(@issue.reload.children.count).to eql(2)
+          expect(@issue_comment1.reload.parent_id).to eql(@issue.id)
+          expect(@issue_comment2.reload.parent_id).to eql(@issue.id)
+        end
+        it "groups comments when there is no issue" do
+          @issue_comment1.process_github.save!
+          @issue_comment2.process_github.save!
+          expect(@issue_comment2.reload.parent_id).to eql(@issue_comment1.id)        
+        end
       end
-
-      it "there should be issue with 2 comments" do
-        @issue_comment1.process_github.save!
-        @issue.process_github.save!
-        @issue_comment2.process_github.save!
-        expect(@issue.reload.children.count).to eql(2)
-        expect(@issue_comment1.reload.parent_id).to eql(@issue.id)
-        expect(@issue_comment2.reload.parent_id).to eql(@issue.id)
-      end
-
-      it "there should be 2 grouped comments without issue" do
-        @issue_comment1.process_github.save!
-        @issue_comment2.process_github.save!
-        expect(@issue_comment2.reload.parent_id).to eql(@issue_comment1.id)        
-      end
-    end
-
-    context "when grouping commit comments" do
-      before :each do
-        @push = build(:github_push)
-        @commit_comment1 = build(:github_commit_comment1)
-        @commit_comment2 = build(:github_commit_comment2)
-      end
-          
-      it "there should be push event with 2 commit comments" do
-        @commit_comment1.process_github.save!
-        @push.process_github.save!
-        @commit_comment2.process_github.save!
-        expect(@push.reload.children.count).to eql(2)
-        expect(@commit_comment1.reload.parent_id).to eql(@push.id)
-        expect(@commit_comment2.reload.parent_id).to eql(@push.id)
+      context "when grouping commit comments" do
+        before :each do
+          @push = build(:github_push)
+          @commit_comment1 = build(:github_commit_comment1)
+          @commit_comment2 = build(:github_commit_comment2)
+        end
+        it "groups a push event with 2 commit comments" do
+          @commit_comment1.process_github.save!
+          @push.process_github.save!
+          @commit_comment2.process_github.save!
+          expect(@push.reload.children.count).to eql(2)
+          expect(@commit_comment1.reload.parent_id).to eql(@push.id)
+          expect(@commit_comment2.reload.parent_id).to eql(@push.id)
+        end
       end
     end
 
-    context "when grouping (re)tweets" do
-      before :each do
-        @tweet = build(:twitter_tweet)
-        @retweet1 = build(:twitter_retweet1)
-        @retweet2 = build(:twitter_retweet2)
-      end
-
-      it "there should be tweet with 2 retweets" do
-        @retweet1.process_twitter.save!
-        @tweet.process_twitter.save!
-        @retweet2.process_twitter.save!
-        expect(@tweet.reload.children.count).to eql(2)
-        expect(@retweet1.reload.parent_id).to eql(@tweet.id)
-        expect(@retweet2.reload.parent_id).to eql(@tweet.id)
+    describe "#process_twitter" do
+      context "when grouping (re)tweets" do
+        before :each do
+          @tweet = build(:twitter_tweet)
+          @retweet1 = build(:twitter_retweet1)
+          @retweet2 = build(:twitter_retweet2)
+        end
+        it "groups a tweet with 2 retweets" do
+          @retweet1.process_twitter.save!
+          @tweet.process_twitter.save!
+          @retweet2.process_twitter.save!
+          expect(@tweet.reload.children.count).to eql(2)
+          expect(@retweet1.reload.parent_id).to eql(@tweet.id)
+          expect(@retweet2.reload.parent_id).to eql(@tweet.id)
+        end
       end
     end
+
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,39 +2,37 @@ require 'spec_helper'
 
 describe User do
 
-  context "upon creation" do
-    before :each do
-      #
+  context "upon creation / first sign in" do
+    describe "#get_all_events" do
     end
-    
-    describe "#sum_points" do
-      it "calculates total points" do
-        rule = create(:rule, authorship_value: 100, award_value: 1000)
-        rule_reply = create(:rule, authorship_value: 10)
+  end
 
-        author = create(:user, name: "Author")
-        author_reply = create(:user, name: "Reply author")
-        admin = create(:user, name: "Admin")
+  describe "#sum_points" do
+    it "calculates total points" do
+      rule = create(:rule, authorship_value: 100, award_value: 1000)
+      rule_reply = create(:rule, authorship_value: 10)
 
-        event = create(:event_determined, rule: rule, author: author)
-        event_reply = create(:event_determined, rule: rule_reply, author: author_reply, parent: event)
-        event_other = create(:event_determined)
+      author = create(:user, name: "Author")
+      author_reply = create(:user, name: "Reply author")
+      admin = create(:user, name: "Admin")
 
-        activity_upvote = Activity.create_upvote(admin, event)
-        activity_upvote = Activity.create_upvote(admin, event_reply)
-        activity_stake = Activity.create_stake(admin, event, 25)
-        activity_award = Activity.create_award(admin, event_reply)
-        activity_stake_other = Activity.create_stake(author_reply, event_other, 20)        
+      event = create(:event_determined, rule: rule, author: author)
+      event_reply = create(:event_determined, rule: rule_reply, author: author_reply, parent: event)
+      event_other = create(:event_determined)
 
-        sum = event_reply.rule.authorship_value
-            + event_reply.activities.upvotes.sum('value')
-            + event_reply.activities.awards.sum('value')
-            - author_reply.activities.fullfilled_stakes.sum('value')
+      activity_upvote = Activity.create_upvote(admin, event)
+      activity_upvote = Activity.create_upvote(admin, event_reply)
+      activity_stake = Activity.create_stake(admin, event, 25)
+      activity_award = Activity.create_award(admin, event_reply)
+      activity_stake_other = Activity.create_stake(author_reply, event_other, 20)        
 
-        expect(author_reply.sum_points).to eq(sum)
-      end
+      sum = event_reply.rule.authorship_value
+      + event_reply.activities.upvotes.sum('value')
+      + event_reply.activities.awards.sum('value')
+      - author_reply.activities.fullfilled_stakes.sum('value')
+
+      expect(author_reply.sum_points).to eq(sum)
     end
-    
   end
 
 end
