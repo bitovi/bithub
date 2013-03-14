69300f69bf00a9c33982c536a2f7dd1448edda3e
Veljko Dragsic <veljko@kset.org>
2013-03-14 17:11:24 +0100

    Activities on User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,53 +6,67 @@ class User < ActiveRecord::Base
   attr_accessible :address, :city, :email, :name, :postal, :email, :remember_me
 
   has_many :stakes, :foreign_key => "actor_id", :dependent => :destroy, :class_name => "Stake"
+  has_many :upvotes, :foreign_key => "actor_id", :dependent => :destroy, :class_name => "Upvote"
   has_many :authored_events, :foreign_key => "author_id", :class_name => "Event"
   has_many :identities, :dependent => :destroy
 
   validates :email, :uniqueness => true
   
-  # Received awards
-  has_many :awards, :finder_sql => proc {
-    "SELECT a.* FROM events AS e activities AS a" +
-    "WHERE e.id = a.applies_to_id" +
-    "AND a.identificator = award" +
-    "AND e.author_id = #{id}"
-  }
-
-  # Received upvotes
-  has_many :upvotes, :finder_sql => proc {
-    "SELECT a.* FROM events AS e activities AS a" +
-    "WHERE e.id = a.applies_to_id" +
-    "AND a.identificator = upvote" +
-    "AND e.author_id = #{id}"
-  }
-
-  # Events that the user awarded
-  has_many :events_awarded, :finder_sql => proc { 
-    "SELECT e.* FROM events AS e activities AS a" +
-    "WHERE e.id = a.applies_to_id" +
-    "AND a.identificator = award" +
-    "AND a.actor_id = #{id}"
-  }  
-   
-  # Events that the user upvoted
-  has_many :events_upvoted, :finder_sql => proc { 
-    "SELECT e.* FROM events AS e, activities AS a" +
-    "WHERE e.id = a.applies_to_id" +
-    "AND a.identificator = upvote" +
-    "AND a.actor_id = #{id}"
+  # type, actor_id, applies_to, value, ts
+  has_many :activities, :finder_sql => proc { <<-SQL
+    SELECT 
+       'authorship' AS type,
+        events.id AS event_id,
+        events.title AS event_title, 
+        rules.authorship_value AS value, 
+        events.origin_ts AS ts
+      FROM events 
+        LEFT JOIN rules ON events.rule_id=rules.id
+        WHERE events.author_id=#{id}
+    UNION
+    SELECT 
+        'award' AS type, 
+        awards.applies_to_id AS event_id,
+        events.title AS event_title, 
+        awards.value AS value,
+        awards.updated_at AS ts
+      FROM awards
+        LEFT JOIN events ON events.id=awards.applies_to_id
+        WHERE events.author_id=#{id}
+    UNION
+    SELECT 
+        'stake' AS type, 
+        stakes.applies_to_id,
+        events.title AS event_title,
+        stakes.value AS value,
+        stakes.updated_at AS ts
+      FROM stakes 
+        LEFT JOIN events ON events.id=stakes.applies_to_id
+        WHERE stakes.actor_id=#{id} AND fullfilled='t'
+    UNION
+    SELECT 
+        'internal' AS type,
+        NULL,
+        NULL,
+        value AS value,
+        updated_at AS ts
+      FROM internal WHERE receiver_id=#{id}
+    ORDER BY ts DESC;
+  SQL
   }
 
   def sum_points
     sum = 0
     authored_events.each do |ev|
-      sum += ev.rule.authorship_value                             # authorships
-          +  ev.upvotes.sum('value')                              # upvotes
-          +  ev.awards.sum('value')                               # awards
+      sum += ev.rule.authorship_value             # authorships
+          +  ev.upvotes.sum('value')              # upvotes
+          +  ev.awards.sum('value')               # awards
           -  self.stakes.fullfilled.sum('value')  # subtract fullfilled stakes
     end
     sum
   end
+
+  
 
   def self.top(n=10)
 
