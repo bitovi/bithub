2bb8bc52bb130446cfb34b5d5659aafcc5a85269
Nikica Jokic <neektza@gmail.com>
2013-03-25 18:58:12 +0100

    Trying to deref muster.

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'muster'
+
+puts "MUSTER VERSION: #{Muster::VERSION}"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
