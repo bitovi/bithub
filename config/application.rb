be08394f65f9ec20f93b45f6d4dd1e212a01a82d
Nikica Jokic <neektza@gmail.com>
2013-03-25 20:33:52 +0100

    Bundler.setup before Muster.

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,16 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-puts "MUSTER VERSION: #{Muster::VERSION}"
-
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+  Bundler.setup
+end
+
+if defined?(Muster)
+  puts "MUSTER VERSION: #{Muster::VERSION}"
 end
 
 module Bithub
