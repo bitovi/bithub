65129bb77ad230ec593ceacc41b561d64e2c4085
Veljko Dragsic <veljko@kset.org>
2013-04-03 13:52:16 +0200

    Uid as sequence in identity factory

--- spec/factories/identity_factory.rb
+++ spec/factories/identity_factory.rb
@@ -1,15 +1,13 @@
 FactoryGirl.define do
   factory :identity do
-    uid '123'
+    sequence(:uid)
     provider 'some_feed'
 
     trait :twitter do
-      uid '123456'
       provider 'twitter'
     end
 
     trait :github do
-      uid '456789'
       provider 'github'
     end
 
