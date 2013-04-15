0d20e8895dd74a1f8e24c141a3af2f28b0c11d97
Veljko Dragsic <veljko@kset.org>
2013-04-15 21:40:20 +0200

    async exchange declaration

--- app/crawler.rb
+++ app/crawler.rb
@@ -44,46 +44,47 @@ AMQP.start($mq_cs) do |connection, open_ok|
   Signal.trap("TERM", &stop)
 
   channel = AMQP::Channel.new(connection)
-  exchange = channel.direct("e.events.preproc")
+  channel.direct("e.events.preproc") do |exchange|
 
-  # --- Streams
-  $log.info "Registering to Twitter's public stream"
-  Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:public_feed], false)
-
-  $log.info "Registering @canjs user stream"
-  Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:canjs], true)
-
-  $log.info "Registering @jquerypp user stream"
-  Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:jquerypp], true)
-
-  $log.info "Registering @funcunit user stream"
-  Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:funcunit], true)
+    # --- Streams
+    $log.info "Registering to Twitter's public stream"
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:public_feed], false)
+    
+    $log.info "Registering @canjs user stream"
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:canjs], true)
+    
+    $log.info "Registering @jquerypp user stream"
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:jquerypp], true)
+    
+    $log.info "Registering @funcunit user stream"
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:funcunit], true)
   
-  $log.info "Registering @javascriptmvc user stream"
-  Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:javascriptmvc], true)
-  
-  $log.info "Registering @donejs user stream"
-  Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:donejs], true)
+    $log.info "Registering @javascriptmvc user stream"
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:javascriptmvc], true)
+    
+    $log.info "Registering @donejs user stream"
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:donejs], true)
 
-  Exceptional.rescue do
-    # --- Pollers
-    $log.info "Registering Github"
-    EM.add_periodic_timer(6, &Handler::Github.handler($log, exchange))
-    
-    $log.info "Registering Disqus"
-    EM.add_periodic_timer(11, &Handler::Disqus.handler($log, exchange))
-    
-    $log.info "Registering Forums"
-    forum_endpoints = {
-      questions: 'https://forum.javascriptmvc.com/feed/filter/questions',
-      all: 'https://forum.javascriptmvc.com/feed'
-    }
-    EM.add_periodic_timer(25, &Handler::Forums.handler($log, exchange, forum_endpoints))
-    
-    $log.info "Registering Blog"
-    EM.add_periodic_timer(31, &Handler::Blog.handler($log, exchange))
-    
-    $log.info "Registering Community site"
-    EM.add_periodic_timer(46, &Handler::CommunitySite.handler($log, exchange))
+    Exceptional.rescue do
+      # --- Pollers
+      $log.info "Registering Github"
+      EM.add_periodic_timer(6, &Handler::Github.handler($log, exchange))
+      
+      $log.info "Registering Disqus"
+      EM.add_periodic_timer(11, &Handler::Disqus.handler($log, exchange))
+      
+      $log.info "Registering Forums"
+      forum_endpoints = {
+        questions: 'https://forum.javascriptmvc.com/feed/filter/questions',
+        all: 'https://forum.javascriptmvc.com/feed'
+      }
+      EM.add_periodic_timer(25, &Handler::Forums.handler($log, exchange, forum_endpoints))
+      
+      $log.info "Registering Blog"
+      EM.add_periodic_timer(31, &Handler::Blog.handler($log, exchange))
+      
+      $log.info "Registering Community site"
+      EM.add_periodic_timer(46, &Handler::CommunitySite.handler($log, exchange))
+    end
   end
 end
