b38f3886103872623d34b3284f167bed39a1f382
Nikica Jokic <neektza@gmail.com>
2013-04-15 17:33:14 +0200

    Config for deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,8 +23,8 @@ set(:default_stage, 'staging')
 
 server "69.164.216.88", :app, :web, :db, :primary => true
 
-namespace :deploy do
-  task(:start) { run "#{try_sudo} service bithub-#{application}-#{app_env} start" }
-  task(:stop) { run "#{try_sudo} service bithub-#{application}-#{app_env} stop" }
-  task(:restart) { run "#{try_sudo} service bithub-#{application}-#{app_env} restart" }
-end
+# namespace :deploy do
+#   task(:start) { run "#{try_sudo} service bithub-#{application}-#{app_env} start" }
+#   task(:stop) { run "#{try_sudo} service bithub-#{application}-#{app_env} stop" }
+#   task(:restart) { run "#{try_sudo} service bithub-#{application}-#{app_env} restart" }
+# end
