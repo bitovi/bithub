d9657153457b869a8a8f4cda37dda59f93fd592c
Nikica Jokic <neektza@gmail.com>
2013-04-15 15:05:13 +0200

    RabbitMQ URI updated.

--- app/crawler.rb
+++ app/crawler.rb
@@ -18,7 +18,7 @@ require 'handlers'
 require 'string'
 
 # Connection string
-$mq_cs = ENV['CLOUDAMQP_URL']
+$mq_cs = ENV['RABBITMQ_URI']
 
 # Exceptional
 Exceptional::Config.load("config/exceptional.yml")
