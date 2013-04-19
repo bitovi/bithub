1985c050fe324e7c612390f19c9da4c6e341b604
Nikica Jokic <neektza@gmail.com>
2013-04-19 14:27:20 +0200

    Publishing to live service.

--- app/listener.rb
+++ app/listener.rb
@@ -12,28 +12,31 @@ $log.add(Log4r::StdoutOutputter.new('console', {
 # Message queue (RabbitMQ) connection and event loop
 AMQP.start(ENV['RABBITMQ_URI']) do |connection, open_ok|
   $log.info "Connected to AMQP broker on #{connection.settings[:host]}:#{connection.settings[:port]}"
-  
+
   stop = proc { $log.info "Terminating the listener"; connection.close { EM.stop }}
   Signal.trap("INT",  &stop)
   Signal.trap("TERM", &stop)
 
   channel = AMQP::Channel.new(connection)
 
-  channel.direct("e.events") do |exchange|  
-    queue = channel.queue("q.events.web").bind(exchange)
-    queue.subscribe do |metadata, payload|
+  channel.direct("e.events") do |web_exchange|  
+    channel.direct("e.events.liveservice") do |liveservice_exchange|
+      queue = channel.queue("q.events.web").bind(web_exchange)
 
-      event_hash = ActiveSupport::JSON.decode(payload)
-      meta = event_hash.delete('meta')
-      
-      begin
-        ev = Event.new_with_checks(event_hash, meta)
-        ev.save!
-      rescue ActiveRecord::RecordInvalid => invalid
-        $log.info "Save failed | META: #{meta}"
-        $log.info invalid.record.errors.messages.to_yaml
+      queue.subscribe do |metadata, payload|
+        event_hash = ActiveSupport::JSON.decode(payload)
+        meta = event_hash.delete('meta')
+
+        begin
+          ev = Event.new_with_checks(event_hash, meta)
+          ev.save!
+          liveservice_exchange.publish(ev)
+        rescue ActiveRecord::RecordInvalid => invalid
+          $log.info "Save failed | META: #{meta}"
+          $log.info invalid.record.errors.messages.to_yaml
+        end
+        ev.connection.close
       end
-
     end
   end
 end
