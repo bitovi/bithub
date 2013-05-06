11b9a79055ee24e5c3c8378485e1f42e36db35cf
Nikica Jokic <neektza@gmail.com>
2013-05-06 18:18:18 +0200

    Query logic analyzer full spec coverage.

--- lib/query_logic_analyzer.rb
+++ lib/query_logic_analyzer.rb
@@ -17,7 +17,7 @@ class QueryLogicAnalizer
 
   # =========> Tag based params
 
-  def apply_tag_based_params_to_scope(scope, params)
+  def apply_tag_based_params_to_scope!(scope, params)
     taggables = pluck_and_process_tag_based_params(params)
     if taggables
       scope = scope.tagged_with(taggables[:any], :any => true) if taggables[:any]
@@ -27,17 +27,35 @@ class QueryLogicAnalizer
   end
 
   def pluck_and_process_tag_based_params(params)
-    Hash[params.select{|qi| tag_based_query_item?(qi)}.map{|el| process_query_logic_for_tag_based_items(qi)}]
+    process_tag_based_params(pluck_tag_based_params(params))
+  end
+
+  def pluck_tag_based_params(params)
+    params.select{|k,v| qi=[k,v]; tag_based_query_item?(qi)}
+  end
+
+  def process_tag_based_params(params)
+    hash = Hash.new
+    params.each do |qi|
+      merge_with_existing_keys(hash, process_query_logic_for_tag_based_items(qi))
+    end
+    hash
+  end
+
+  def tag_based_query_item?(query_item)
+    qi_name, * = query_item 
+    TAG_FIELD_NAMES.include?(qi_name.to_s)
   end
 
   def process_query_logic_for_tag_based_items(query_item)
+    *, qi_value = query_item 
     case
     when or_query?(query_item)
-      [:any, extract_alternatives(qi)]
+      [:any, extract_alternatives(query_item)]
     when and_query?(query_item)
-      [:all, extract_conditions(query_item) ]
+      [:all, extract_conjuctions(query_item)]
     else equals_query?(query_item)
-      [:all, Array.wrap(val)]
+      [:all, Array.wrap(qi_value)]
     end
   end
 
@@ -50,15 +68,23 @@ class QueryLogicAnalizer
   end
 
   def pluck_and_process_regular_params(params)
-    Hash[params.select{|qi| regular_and_valid_query_item?(qi)}.map{|qi| process_query_logic(qi)}]
+    process_regular_params(pluck_regular_params(params))
+  end
+
+  def pluck_regular_params(params)
+    params.select{|k,v| qi=[k,v]; regular_and_valid_query_item?(qi)}
+  end
+
+  def process_regular_params(params)
+    Hash[params.map{|qi| process_query_logic_for_regular_query_items(qi)}]
   end
 
   def regular_and_valid_query_item?(query_item)
     qi_name, * = query_item
-    @model.has_an_attribute?(qi_name) && !taggable_query_item?(qi_name)
+    @model.has_an_attribute?(qi_name) and !tag_based_query_item?(query_item)
   end
 
-  def process_query_logic(query_item)
+  def process_query_logic_for_regular_query_items(query_item)
     qi_name, qi_value = query_item
     case
     when between_query?(query_item)
@@ -75,16 +101,16 @@ class QueryLogicAnalizer
   def extract_range(query_item)
     qi_name, qi_value = query_item
     lower_str, higher_str = qi_value.split(DELIMITERS[:between])
-    type = @model.columns_hash[qi_name].type
+    column_type = @model.columns_hash[qi_name.to_s].type
     case
-    when type == :datetime || type == :date
+    when column_type == :datetime || column_type  == :date
       lower = (!lower_str || lower_str.blank?) ?  Date.new(0) + 1.year : DateTime.parse(lower_str)
       higher = (!higher_str || higher_str.blank?) ? DateTime.tomorrow : DateTime.parse(higher_str)
-    when type == :integer
+    when column_type == :integer
       lower = (lower_str && !lower_str.blank?) ? lower_str.to_i : -(2**(0.size * 8 -2)) # Platform MIN_INT
       higher = higher_str ? higher_str.to_i : (2**(0.size * 8 -2) -1) # Platform MAX_INT
     else
-      klass = Object.const_get(type.capitalize)
+      klass = Object.const_get(column_type.capitalize)
       lower = klass.new(lower_str)
       higher = klass.new(higher_str)
     end
@@ -104,11 +130,6 @@ class QueryLogicAnalizer
 
   # =========> Checking methods
 
-  def tag_based?(query_item)
-    qi_name, * = query_item 
-    TAG_FIELD_NAMES.include?(qi_name)
-  end
-
   def between_query?(query_item)
     *, qi_value = query_item
     qi_value.include?(DELIMITERS[:between])
@@ -126,5 +147,17 @@ class QueryLogicAnalizer
 
   def equals_query?(query_item)
     true
+  end
+
+  # ========> Helper methods
+
+  def merge_with_existing_keys(hash, qi)
+    qi_key, qi_val = qi
+    if hash[qi_key]
+      hash[qi_key] += qi_val
+    else
+      hash[qi_key] = qi_val
+    end
+    hash
   end
 end
--- spec/libs/query_logic_analyzer_spec.rb
+++ spec/libs/query_logic_analyzer_spec.rb
@@ -2,6 +2,58 @@ require 'spec_helper'
 
 describe QueryLogicAnalizer do
   let(:qla) { QueryLogicAnalizer.new(Event) }
+  let(:lower_date_limit_str) { "2013-1-1" }
+  let(:higher_date_limit_str) { "2013-5-1" }
+  let(:date_range) { DateTime.parse(lower_date_limit_str)..DateTime.parse(higher_date_limit_str) }
+
+  describe "#pluck_regular_params" do
+    it "plucks regular query items from the params" do
+      params = { title: "Some title", origin_date: "2013-1-1", feed: "github", category: "code" }
+      expect(qla.pluck_regular_params(params)).to eq({title: "Some title", origin_date: "2013-1-1"})
+    end
+  end
+
+  describe "#process_regular_params" do
+    it "processes regular query items" do
+      params = { id: "1:10", origin_date: "2013-1-1:2013-5-1"}
+      expect(qla.process_regular_params(params)).to eq({id: 1..10, origin_date: date_range})
+    end
+  end
+
+  describe "#pluck_tag_based_params" do
+    it "selects tag based query items from the params" do
+      params = { id: 1, title: "Some title", tag: "canjs", feed: "twitter,github", category: "chat|code" }
+      expect(qla.pluck_tag_based_params params).to eq({tag: "canjs", feed: "twitter,github", category: "chat|code"})
+    end
+  end
+
+  describe "#process_tag_based_params" do
+    it "processes tag based query items" do
+      params = { feed: "twitter,github", category: "chat|code", tag: "canjs" }
+      expect(qla.process_tag_based_params params).to eq({all: ["twitter", "github", "canjs"], any: ["chat", "code"]})
+    end
+  end
+
+  describe "#regular_and_valid_query_item?" do
+    it "asserts that the query item's name appers in the model attributes" do
+      query_item = ["title", "Some title"]
+      expect(qla.regular_and_valid_query_item? query_item).to eq(true)
+    end
+
+    it "asserts that the query item's name is not of the taggable type" do
+      query_item = ["feed", "github"]
+      expect(qla.regular_and_valid_query_item? query_item).to eq(false)
+    end
+  end
+  
+  describe "#tag_based_query_item?" do
+    it "asserts that the query item's name is of the taggable type" do
+      taggable_query_item = ["feed", "twitter,github"]
+      regular_query_item = ["title", "Some title"]
+      expect(qla.tag_based_query_item? taggable_query_item).to eq(true)
+      expect(qla.tag_based_query_item? regular_query_item).to eq(false)
+    end
+  end
 
   describe "#and_query?" do
     it "checks whether a query item is of 'AND' type" do
@@ -24,13 +76,6 @@ describe QueryLogicAnalizer do
     end
   end
 
-  describe "#tag_based_query_item?" do
-    it "checks whether a query item is tag based" do
-      query_item = ["feed", "twitter,github"]
-      expect(qla.tag_based? query_item).to eq(true)
-    end
-  end
-
   describe "#extract_conjuctions" do
     it "extracts ANDs from a query item"
   end
@@ -44,10 +89,8 @@ describe QueryLogicAnalizer do
 
   describe "#extract_range" do
     it "constructs a date range when given dates" do
-      lower = "2013-1-1"; higher = "2013-5-1"
-      query_item = ["origin_date", "#{lower}:#{higher}"]
-      range = DateTime.parse(lower)..DateTime.parse(higher)
-      expect(qla.extract_range query_item).to eq(range)
+      query_item = ["origin_date", "#{lower_date_limit_str}:#{higher_date_limit_str}"]
+      expect(qla.extract_range query_item).to eq(date_range)
     end
     
     it "constructs an integer range when given integers" do
