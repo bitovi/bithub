afa63657cc4bf5d537ec7ce1b670854b60b0f0db
Nikica Jokic <neektza@gmail.com>
2013-05-06 02:10:04 +0200

    Added specs for User and Event controllers.

---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Api::EventsController do
+  describe "#index" do
+    it "applies muster query params to a scope"
+    it "applies regular params to a scope"
+    it "applies taggable query params to a scope"
+    it "calculates upvotes by calling a method"
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Api::UsersController do
+  describe "#index" do
+    it "applies muster query segments to the scope"
+    it "applies regular params to scope"
+    it "applies taggables to scope"
+  end
+end
