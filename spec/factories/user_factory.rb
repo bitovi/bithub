6c1afaeff17140bc45682041b55813f7113fa8f6
Nikica Jokic <neektza@gmail.com>
2013-05-10 20:27:01 +0200

    Associations in factories fixed.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,10 +4,11 @@ FactoryGirl.define do
     sequence(:name) {|n| "User ##{n}" }
     sequence(:email) {|n| "user#{n}@bitovi.com" }
 
-    after :build do |user|
-      user.identities << FactoryGirl.create(:identity_from_github, user: user)
-      user.identities << FactoryGirl.create(:identity_from_twitter, user: user)
+    trait :with_ident do
+      after :build do |user|
+        user.identities << FactoryGirl.create(:identity, user: user, provider: 'github')
+        user.identities << FactoryGirl.create(:identity, user: user, provider: 'twitter')
+      end
     end
   end
-
 end
