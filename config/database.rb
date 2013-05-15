64f441133199c526baecfad9a8ff577b9f99a03e
Veljko Dragsic <veljko@kset.org>
2013-05-15 20:23:40 +0200

    Capistrano tasks for managing db backups.

---
+++ config/database.rb
@@ -0,0 +1,82 @@
+set(:db_backups_path, '/backups/dbsnapshots/')
+
+namespace :db do
+
+  desc "Determine a name for the backup file"
+  task :backup_name, :roles => :db, :only => { :primary => true } do
+    backup_time = Time.now.strftime("%Y%m%d-%H%M%S")
+    set :backup_file, File.join(db_backups_path, backup_time + '.backup')
+  end
+
+  desc "Backup database"
+  task :backup, :roles => :db, :only => {:primary => true} do
+    backup_name
+
+    run("cat #{current_path}/config/database.yml") { |channel, stream, data| @environment_info = YAML.load(data)[rails_env] }
+    dbuser = @environment_info['username']
+    dbpass = @environment_info['password']
+    environment_database = @environment_info['database']
+    dbhost = @environment_info['host']
+
+    run "pg_dump -W -Fc -c -U #{dbuser} #{environment_database} > #{backup_file}" do |ch, stream, out |
+      ch.send_data "#{dbpass}\n" if out=~ /^Password:/
+    end
+  end
+
+  desc "List database backups"
+  task :list_backups, :roles => :db, :only => {:primary => true} do
+    run("ls -lh #{db_backups_path}/*.backup") do |channel, stream, data|
+
+      data.split(/\r?\n/).each do |row|
+        attrs = row.split()
+        name = File.basename(attrs[8], ".backup")
+        puts "Version: #{name} [#{attrs[4]}] [#{attrs[5]} #{attrs[6]} #{attrs[7]}]"
+      end
+
+    end
+  end
+
+  desc "Restore database to selected version"
+  task :restore, :roles => :db, :only => {:primary => true} do
+    if not exists? :version
+      puts "Specify which version to restore. Hint: use \"cap db:restore -s version=__version__\""
+    else
+      dbname = (app_env == 'prod') ? 'bithub' : 'bithub_' + app_env
+      backup_path = File.join(db_backups_path, version + '.backup')
+      run("pg_restore -c -d #{dbname} #{backup_path}")
+    end
+  end
+
+  desc "Download backup"
+  task :download, :roles => :db, :only => {:primary => true} do
+    if not exists? :version
+      puts "Specify which version to restore. Hint: use \"cap db:download -s version=__version__\""
+    else
+      src = File.join(db_backups_path, version + ".backup")      
+      dest = ((exists? :path) ? path : './') + version + ".backup"
+      top.download(src, dest, :via => :scp, &block)
+    end
+  end
+
+  desc "Upload backup"
+  task :upload, :roles => :db, :only => {:primary => true} do
+    if not exists? :backup_file
+      puts "Please provide path to backup file. Hint: user \"cap db:upload -s backup_file=__path_to_backup_file__\""
+    else
+      src = backup_file
+      dest = File.join(db_backups_path, File.basename(backup_file))
+      top.upload(src, dest, :via => :scp, &block)
+    end
+  end
+
+  desc "Sync your production database to your local workstation"
+  task :download_backup, :roles => :db, :only => {:primary => true} do
+    backup_name
+    dump
+    get "#{backup_file}.bz2", "/tmp/#{application}.sql.bz2"
+    development_info = YAML.load_file("config/database.yml")['development']
+    run_str = "PGPASSWORD=#{development_info['password']} bzcat /tmp/#{application}.sql.bz2 | psql -U #{development_info['username']} -h #{development_info['host']} #{development_info['database']}"
+    %x!#{run_str}!
+  end
+
+end
--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,7 @@ set(:default_stage, 'prod')
 set(:shared_children, shared_children + %w{public/uploads})
 
 namespace :deploy do
+
   desc "Zero-downtime restart of Unicorn"
   task :restart, :except => { :no_release => true } do
     run "kill -s USR2 `cat #{shared_path}/pids/unicorn.pid`"
@@ -44,51 +45,13 @@ namespace :deploy do
     run "sudo /usr/bin/service bithub-listener stop"
   end
 
+  desc "Recreate Upstart configuration"
   task(:recreate_upstart_conf) do
     run "#{current_path}/bin/foreman export --app bithub-listener --user #{user} --env #{current_path}/.env_#{app_env} --procfile #{current_path}/Procfile.#{app_env} upstart /etc/init"
   end
 
   task :symlink_uploads do
     run "ln -nfs #{shared_path}/uploads  #{current_path}/public/uploads"
-  end
-end
-
-namespace :db do
-
-  desc "Determine a name for the dump file"
-  task :backup_name, :roles => :db, :only => { :primary => true } do
-    backup_time = Time.now.strftime("%Y%m%d-%H%M%S")
-    set :backup_file, "/backups/#{app_env}-dbsnapshots/#{backup_time}.backup"
-  end
-
-  desc "Backup PostgreSQL database to /backups/prod-snapshots/"
-  task :dump, :roles => :db, :only => {:primary => true} do
-    backup_name
-
-    run("cat #{current_path}/config/database.yml") { |channel, stream, data| @environment_info = YAML.load(data)[rails_env] }
-    dbuser = @environment_info['username']
-    dbpass = @environment_info['password']
-    environment_database = @environment_info['database']
-    dbhost = @environment_info['host']
-
-    run("cat #{current_path}/config/database.yml") { |channel, stream, data| @environment_info = YAML.load(data)[rails_env] }
-    run "pg_dump -W -Fc -c -U #{dbuser} #{environment_database} > #{backup_file}" do |ch, stream, out |
-      ch.send_data "#{dbpass}\n" if out=~ /^Password:/
-    end
-  end
-
-
-  desc "Sync your production database to your local workstation"
-  task :download_dump, :roles => :db, :only => {:primary => true} do
-    backup_name
-    dump
-    get "#{backup_file}.bz2", "/tmp/#{application}.sql.bz2"
-    development_info = YAML.load_file("config/database.yml")['development']
-    run_str = "PGPASSWORD=#{development_info['password']} bzcat /tmp/#{application}.sql.bz2 | psql -U #{development_info['username']} -h #{development_info['host']} #{development_info['database']}"
-    %x!#{run_str}!
-  end
-
-  task :upload_dump, :roles => :db, :only => {:primary => true} do
   end
 
 end
