2b9953b8f888bc29cd40adff2d61426eeac7aa9b
Nikica Jokic <neektza@gmail.com>
2013-05-16 17:39:52 +0200

    Replaced nastiness with a ScopeApplier lib.

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,6 +1,4 @@
 class Api::EventsController < Api::ApiController
-  TAG_FIELDS = ['tag', 'feed', 'category']
-  DELIMITERS = { :and => ',', :or => '|', :between => ':' }
   respond_to :json
 
   def index
@@ -11,7 +9,7 @@ class Api::EventsController < Api::ApiController
       render :json => { :count => scope.count(muster_query[:count]) }
     else
       scope = apply_upvote_calculation_to_scope(scope, params)
-      scope = apply_order_to_scope(scope, muster_query)
+      scope = scope_applier.apply_order_to_scope(scope, muster_query)
       @events = EventDecorator.decorate_collection(scope.all)
       render :index
     end
@@ -34,36 +32,31 @@ class Api::EventsController < Api::ApiController
 
   def update
     @event = Event.find(params[:id])
-    if Event.update(params[:event])
+    if @event.update_from_bithub!(params[:event])
       render :status => 200
     else
       render :json => @event.errors.messages, :status => 500
     end
   end
 
-
-  # =======> SCOPE BUILDING
-
-  private
+  private # SCOPE BUILDING
   def build_scope(muster_query, params)
-    scope = Event.scoped ; logic_analyzer = QueryLogicAnalyzer.new(Event)
-    scope = ScopeApplier.apply_muster_query_to_scope(scope, muster_query)
-    scope = ScopeApplier.apply_regular_params_to_scope(logic_analyzer, scope, params)
-    scope = ScopeApplier.apply_tag_based_params_to_scope(logic_analyzer, scope, params)
-  end
-
-  def apply_order_to_scope(scope, muster_query)
-    if !muster_query[:order].blank?
-      attribute, direction = muster_query[:order].first.split
-      attribute = "total_upvotes" if attribute == "upvotes" # total_upvotes => calculated field
-      scope = scope.order("#{attribute} #{direction}")
-    end
-    scope
+    scope = Event.scoped;
+    scope = scope_applier.apply_muster_query_to_scope(scope, muster_query)
+    scope = scope_applier.apply_regular_params_to_scope(scope, params)
+    scope = scope_applier.apply_tag_based_params_to_scope(scope, params)
   end
   
   def apply_upvote_calculation_to_scope(scope, params)
-    logic_analyzer = QueryLogicAnalyzer.new(Event)
     taggables = logic_analyzer.pluck_and_process_tag_based_params(params)
     scope = scope.select_with_upvotes(taggables && !taggables[:any])
+  end
+  
+  def logic_analyzer
+    QueryLogicAnalyzer.new(Event)
+  end
+
+  def scope_applier
+    ScopeApplier.new(QueryLogicAnalyzer.new(Event)) 
   end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,8 +8,8 @@ class Api::UsersController < Api::ApiController
     if !muster_query[:count].blank?
       render :json => { :count => scope.count(muster_query[:count]) }
     else
-      scope = apply_score_calculation_to_scope(scope)
-      scope = apply_order_to_scope(scope, muster_query)
+      scope = apply_score_calculation_to_scope(scope, )
+      scope = scope_applier.apply_order_to_scope(scope, muster_query)
       @users = UserDecorator.decorate_collection(scope.all)
       render :index
     end
@@ -35,24 +35,22 @@ class Api::UsersController < Api::ApiController
     end
   end
 
-  private
+  private # SCOPE BUILDING
   def build_scope(muster_query, params)
-    scope = User.scoped ; logic_analyzer = QueryLogicAnalyzer.new(User)
-    scope = ScopeApplier.apply_muster_query_to_scope(scope, muster_query)
-    scope = ScopeApplier.apply_regular_params_to_scope(logic_analyzer, scope, params)
-    scope
+    scope = User.scoped
+    scope = scope_applier.apply_muster_query_to_scope(scope, muster_query)
+    scope = scope_applier.apply_regular_params_to_scope(scope, params)
   end
   
   def apply_score_calculation_to_scope(scope)
-    scope = scope.select_with_score(true)
+    scope = scope.select_with_score
   end
-  
-  def apply_order_to_scope(scope, muster_query)
-    if !muster_query[:order].blank?
-      attribute, direction = muster_query[:order].first.split
-      attribute = "total_score" if attribute == "score" # total_score => calculated field
-      scope = scope.order("#{attribute} #{direction}")
-    end
-    scope
+
+  def logic_analyzer
+    QueryLogicAnalyzer.new(User)
+  end
+
+  def scope_applier
+    ScopeApplier.new(QueryLogicAnalyzer.new(User)) 
   end
 end
