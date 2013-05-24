83733528bc3cbe77a5808477b2c9077aa07b847a
Nikica Jokic <neektza@gmail.com>
2013-05-24 22:28:00 +0200

    Return 406 if already upvoted instead of 500.

--- app/controllers/api/event_activities_controller.rb
+++ app/controllers/api/event_activities_controller.rb
@@ -10,42 +10,37 @@ class Api::EventActivitiesController < Api::ApiController
 
   def create_upvote
     event = Event.find(params[:event_id])
-
     if Upvote.where({:applies_to_id => event, :actor_id => current_user}).length == 0      
       upvote = Upvote.create_upvote(current_user, event)
       render :json => upvote
     else
-      render :json => {:error => t('errors.messages.already_upvoted')}, :status => 500
+      render :json => {:error => t('errors.messages.already_upvoted')}, :status => 406
     end
   end
 
   def create_anteup
     event = Event.find(params[:event_id])
-
     if Anteup.where({:applies_to_id => event, :actor_id => current_user}).length == 0      
       anteup = Anteup.create_anteup(current_user, event, params[:value])
       render :json => anteup
     else
-      render :json => {:error => t('errors.messages.already_anteuped')}, :status => 500
+      render :json => {:error => t('errors.messages.already_anteuped')}, :status => 406
     end
   end
 
   def create_award
     event = Event.find(params[:event_id])
-
     if Award.where({:applies_to_id => event, :actor_id => current_user}).length == 0      
       award = Award.create_award(current_user, event)
       render :json => award
     else
-      render :json => {:error => t('errors.messages.already_awarded')}, :status => 500
+      render :json => {:error => t('errors.messages.already_awarded')}, :status => 406
     end
   end
 
 
   private
-
   def show_errors(e)
     render :json => {:error => e.message}, :status => 500
   end
-
 end
