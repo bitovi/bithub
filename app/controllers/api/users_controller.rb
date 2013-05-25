5ac3a90453d5435fc4a31dc5e2663d759ee41309
Nikica Jokic <neektza@gmail.com>
2013-05-25 17:02:02 -0400

    Access logic_analyzer via instance method
    Said instance method caches and initializes correctly, instead of accessing it through a @var.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -55,6 +55,6 @@ class Api::UsersController < Api::ApiController
   end
 
   def scope_applier
-    @scope_applier ||= ScopeApplier.new(QueryLogicAnalyzer.new(User)) 
+    @scope_applier ||= ScopeApplier.new(logic_analyzer) 
   end
 end
