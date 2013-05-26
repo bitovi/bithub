5432d0811b82e2b8a95b567fae12c767511a3266
Veljko Dragsic <veljko@kset.org>
2013-05-26 20:30:50 +0200

    Twitter handlers uncommented.

--- app/crawler.rb
+++ app/crawler.rb
@@ -52,22 +52,22 @@ AMQP.start($mq_cs) do |connection, open_ok|
 
     # --- Streams
     $log.info "Registering to Twitter's public stream"
-    #Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:public_feed], false)
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:public_feed], false)
     
     $log.info "Registering @canjs user stream"
-    #Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:canjs], true)
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:canjs], true)
     
     $log.info "Registering @jquerypp user stream"
-    #Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:jquerypp], true)
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:jquerypp], true)
     
     $log.info "Registering @funcunit user stream"
-    #Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:funcunit], true)
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:funcunit], true)
   
     $log.info "Registering @javascriptmvc user stream"
-    #Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:javascriptmvc], true)
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:javascriptmvc], true)
     
     $log.info "Registering @donejs user stream"
-    #Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:donejs], true)
+    Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:donejs], true)
 
     # --- Pollers
     $feeds[:github][:events].each do |project, repo|
