fc0a517099426e6f1996e3d4012f9c0221f1ce3b
Nikica Jokic <neektza@gmail.com>
2013-06-10 00:51:35 +0200

    Github and Twitter search API wrapper.

--- lib/third_party_user_informer.rb
+++ lib/third_party_user_informer.rb
@@ -1,28 +1,22 @@
 class ThirdPartyUserInformer
 
-  def initialize(env)
-    env ||= ENV
+  def initialize
     Twitter.configure do |config|
-      config.consumer_key = env['TWITTER_CONSUMER_KEY']
-      config.consumer_secret = env['TWITTER_CONSUMER_SECRET']
-      config.oauth_token = env['TWITTER_OAUTH_TOKEN']
-      config.oauth_token_secret = env['TWITTER_OAUTH_TOKEN_SECRET']
+      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
+      config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
     end
 
-    @github = Github.new(:client_id => env['GITHUB_CLIENT_ID'], :client_secret => env['GITHUB_CLIENT_SECRET'])
-    @github.authorize_url(:redirect_uri => 'http://bithub.dev/api/auth/google/callback')
-    token = @github.get_token()
-
-    Github.configure do |config|
-      config.oauth_token = token
-    end
+    @github = Github.new basic_auth: 'neektza:ahn8Choo'
   end
 
-  def fetch_from_twitter(user_query)
-    Twitter.user_search(user_query)
+  def from_twitter(q)
+    Twitter.user_search(q)
   end
 
-  def fetch_from_github
-    @github.users.followers.following 'neektza'
+  def from_github(q)
+    res = @github.search.users(q)
+    res.users
   end
 end
