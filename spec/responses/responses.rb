78ce6e768758afca40d519b406a5bc03392c774e
Veljko Dragsic <veljko@proton>
2013-07-08 19:02:01 +0200

    load_raw method on Response module.

--- spec/responses/responses.rb
+++ spec/responses/responses.rb
@@ -5,11 +5,15 @@ module Response
 
   @root = './spec/responses/'
 
-  def self.load(feed, type)
-    
+  def self.load(feed, type)    
     @path = File.join(@root, feed, type.snake_case + '.json')
-    JSON.parse( IO.read(@path) )
-    
+    JSON.parse( IO.read(@path) )    
+  end
+
+  def self.load_raw(feed, type)    
+    @path = File.join(@root, feed, type.snake_case + '.json')
+    file = File.open(@path, "rb")
+    file.read
   end
   
 end
