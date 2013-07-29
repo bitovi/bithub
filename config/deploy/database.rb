bc6fe308b36c6a8c060855b611e0137b2b406efb
Veljko Dragsic <veljko@proton>
2013-07-29 20:29:07 +0200

    Cap task for syncing local db with prod fixed

--- config/deploy/database.rb
+++ config/deploy/database.rb
@@ -74,7 +74,7 @@ namespace :db do
   desc "Sync staging with production"
   task :sync_with_prod, :roles => :db, :only => {:primary => true} do
     dbname = 'bithub_staging' unless dbname
-    run "pg_dump -Fc -w -h 69.164.216.88 bithub | pg_restore -c -d bithub_staging"
+    run "pg_dump -Fc -w -h 69.164.216.88 bithub | pg_restore -c -n public -d bithub_staging"
   end
 
   task :pass_var, :roles => :db, :only => {:primary => true} do
@@ -103,7 +103,7 @@ namespace :db do
     end
 
     # restore to local db
-    run_locally "pg_restore -Fc -c -d #{restore_db} #{dest}"
+    run_locally "pg_restore -Fc -c -n public -d #{restore_db} #{dest}"
   end
 
 end
