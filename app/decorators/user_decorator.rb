496170dd75820717ce8b6e8e1d6efc1e9129a86d
Nikica Jokic <neektza@gmail.com>
2013-08-09 18:19:25 +0200

    User decorator updates:
    * roles method for plucking role names a user has
    * removed '/bithub' prefix from image url

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,13 +1,18 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
+
+  def roles
+    source.roles.pluck(:name)
+  end
+
   def avatar_url
     if !source.props['gravatar_url'].blank?
       source.props['gravatar_url']
     elsif source.identities && source.identities.map{|ident| ident.source_data['image'] if ident.source_data}.first
       source.identities.map{|ident| ident.source_data['image']}.first
     else
-      '/bithub/assets/images/icon-user.png'
+      '/assets/images/icon-user.png'
     end
   end
 
