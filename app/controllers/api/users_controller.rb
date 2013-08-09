5ec7566d8428b4489eb7029e6e3df7005e6e1e98
Nikica Jokic <neektza@gmail.com>
2013-08-09 18:34:02 +0200

    Added auth filters to UserController.
    ... for add_role and remove_role.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::UsersController < Api::ApiController
+  before_filter :authenticate_user!, :only => ['add_role', 'remove_role']
   respond_to :json
 
   rescue_from ActiveRecord::RecordNotFound, with: :show_404
