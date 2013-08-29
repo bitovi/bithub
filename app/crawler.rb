ff58a33e8190ad1b26574dda5d2fe6bdc50ac88f
Nikica Jokic <neektza@gmail.com>
2013-08-29 13:46:29 +0200

    Registering handlers in phases.
    Added 1s pause between handler registration.

--- app/crawler.rb
+++ app/crawler.rb
@@ -31,9 +31,6 @@ $log.add(Log4r::StdoutOutputter.new('console', {
 # Load feeds config 
 $feeds = YAML::load_file('config/feeds.yml')
 
-$timer = 20
-def next_timer; $timer += 10; end
-
 # Event loop
 AMQP.start($mq_cs) do |connection, open_ok|
   puts "Connected to AMQP broker on #{connection.settings[:host]}:#{connection.settings[:port]}"
@@ -65,25 +62,52 @@ AMQP.start($mq_cs) do |connection, open_ok|
     Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:donejs], true)
 
     # --- Pollers
+    phase = 1; shift_phase = lambda {phase+=1}
+
+    # --- Github
     $feeds[:github][:events].each do |project, repo|
-      $log.info "Registering Github handler for \"#{project}\" at \"#{repo[:endpoint]}\""
-      EM.add_periodic_timer($timer, &Handler::Github.handler($log, exchange, repo[:endpoint]))
+      EM.add_timer(phase) do
+        $log.info "Registering Github handler for \"#{project}\" at \"#{repo[:endpoint]}\""
+        EM.add_periodic_timer(60, &Handler::Github.handler($log, exchange, $feeds[:github][:token], repo[:endpoint]))
+      end
+      shift_phase.call
     end
 
-    $log.info "Registering Disqus"
-    EM.add_periodic_timer($timer, &Handler::Disqus.handler($log, exchange))
 
-    $log.info "Registering Forums"
+    # --- Disqus
+    EM.add_timer(phase) do
+      $log.info "Registering Disqus"
+      EM.add_periodic_timer(15, &Handler::Disqus.handler($log, exchange, $feeds[:disqus][:api_key]))
+    end
+    shift_phase.call
+
+
+    # --- Forums
     forum_endpoints = {
       questions: 'https://forum.javascriptmvc.com/feed/filter/questions',
       all: 'https://forum.javascriptmvc.com/feed'
     }
-    EM.add_periodic_timer($timer, &Handler::Forums.handler($log, exchange, forum_endpoints))
 
-    $log.info "Registering Blog"
-    EM.add_periodic_timer($timer, &Handler::Blog.handler($log, exchange))
+    EM.add_timer(phase) do
+      $log.info "Registering Forums"
+      EM.add_periodic_timer(30, &Handler::Forums.handler($log, exchange, forum_endpoints))
+    end
+    shift_phase.call
 
-    $log.info "Registering Community site"
-    EM.add_periodic_timer($timer, &Handler::CommunitySite.handler($log, exchange))
+
+    # --- Blog
+    EM.add_timer(phase) do
+      $log.info "Registering Blog"
+      EM.add_periodic_timer(300, &Handler::Blog.handler($log, exchange))
+    end
+    shift_phase.call
+
+
+    # --- Old community site
+    EM.add_timer(phase) do
+      $log.info "Registering Community site"
+      EM.add_periodic_timer(300, &Handler::CommunitySite.handler($log, exchange))
+    end
+    shift_phase.call
   end
 end
