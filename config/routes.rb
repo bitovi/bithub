853b13b2498e41cf630b5de72d7629422aec0fc1
Nikica Jokic <neektza@gmail.com>
2013-09-06 17:31:40 +0200

    Routes for achievement endpoint, actually.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Bithub::Application.routes.draw do
     get '/api/auth/logout', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
-  namespace :api, :defaults => { :format => 'json' } do
+  namespace :api, :defaults => { :formats => [:json] } do
     match '/auth/session' => 'auth/session_info#current_session'
 
     resources :events, :except => [:new, :edit] do
@@ -40,6 +40,7 @@ Bithub::Application.routes.draw do
     end
     
     resources :rewards
+    resources :achievements, :only => [:index, :create, :update, :destroy]
     resources :countries, :only => :index
 
     root :to => "api#home"
