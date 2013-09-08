b09e98a0931b8944eef5fc93226104080901d519
Nikica Jokic <neektza@gmail.com>
2013-09-08 13:44:28 +0200

    Revert "Migration for renaming thread_updated_ts."
    This reverts commit 6d96c46600aa135c5a97432ac11b69bc1ea32084.

--- db/migrate/20130905120435_rename_thread_updated_at.rb
+++
@@ -1,9 +0,0 @@
-class RenameThreadUpdatedAt < ActiveRecord::Migration
-  def up
-    rename_column :events, :thread_updated_at, :thread_updated_ts
-  end
-
-  def down
-    rename_column :events, :thread_updated_ts, :thread_updated_at
-  end
-end
