22002a6f7ecaf734ff39de09ab0e240b68c86731
Nikica Jokic <neektza@gmail.com>
2013-09-17 18:26:21 +0200

    Updated event factory to match new specs.

--- spec/factories/event_factory.rb
+++ spec/factories/event_factory.rb
@@ -1,3 +1,14 @@
+issue_source_data = {
+  payload: {
+    issue: {
+      title: "Someone found a bug!",
+      body: "The description",
+      state: "closed",
+      labels: [{name: 'bug'}, {name: 'question'}]
+    }
+  }
+}
+
 FactoryGirl.define do
 
   factory :props, class:Hash do
@@ -79,115 +90,59 @@ FactoryGirl.define do
 
       trait :tweet do
         title "A hashtag #canjs and a @canjs mention."
-        props({
-          :feed => 'twitter',
-          :tweet_id => "100",
-          :type => "status_event",
-          :origin_author_id => "123456",
-          :origin_author_username => "some_user"
-        })
       end
 
-      trait :retweet1 do
+      trait :retweet do
         title "RT: A hashtag #canjs and a @canjs mention."
-        props({
-          :tweet_id => "101",
-          :retweeted_id => "100",
-          :type => "status_event"
-        })
       end
 
-      trait :retweet2 do
-        title "RT: A hashtag #canjs and a @canjs mention."
-        props({
-          :tweet_id => "102",
-          :retweeted_id => "100",
-          :type => "status_event"
-        })
-      end
-
-      factory :twitter_tweet, traits: [:tweet, :with_determined_rule]
-      factory :twitter_retweet1, traits: [:retweet1, :with_determined_rule]
-      factory :twitter_retweet2, traits: [:retweet2, :with_determined_rule]
+      factory :twitter_tweet, traits: [:with_determined_rule, :tweet]
+      factory :twitter_retweet, traits: [:with_determined_rule, :retweet]
     end
 
     ### Github event
 
     factory :github_event do
-      title "Some generic title"
       association :feed, factory: :tag, name: 'github'
-
-      trait :issue do
+      with_determined_rule
+        
+      factory :github_issue do
         title "raised issue #1"
-        body "I'm awesome because I raised an issue."
         association :category, factory: :tag, name: 'bug'
-        tag_list ['github','issues_event','issue','canjs','bug']
-        props({
-          :feed => 'github',
-          :type => "issues_event",
-          :issue_id => "111",
-          :origin_author_id => "456789",
-          :origin_author_username => "some_user"
-        })
+        tag_list %w(github issues_event issue canjs bug)
+      
+        trait :with_source_data do
+          source_data(issue_source_data)
+        end
       end
 
-      trait :issue_comment do
+      factory :github_push do
+        title "pushed commits"
+        association :category, factory: :tag, name: "code"
+        tag_list %w(github push_event code canjs)
+      end
+
+      factory :github_pull_request do
+        title "requested a pull"
+        association :category, factory: :tag, name: "code"
+        tag_list %w(github pull_request_event code canjs)
+      end
+
+      factory :github_issue_comment do
         title "commented on issue #1"
         association :category, factory: :tag, name: "comment"
-        tag_list ['github','issue_comment_event','comment','canjs']
-        sequence(:body) {|n| "Here's a comment no. ##{n} to your issue" }
-        props({
-          :type => "issue_comment_event",
-          :issue_id => "111"
-        })
+        tag_list %w(github issue_comment_event comment canjs)
+        
+        trait :with_source_data do
+          source_data(issue_source_data)
+        end
       end
 
-      trait :push do
-        title "pushed"
-        body ""
-        association :category, factory: :tag, name: "code"
-        tag_list ['github', 'push_event', 'code', 'canjs']
-
-        sd_map = {
-          # payload: push_event_payload
-        }
-        source_data sd_map
-
-        props({
-          :type => "push_event",
-          :feed => "github",
-          :commits => "3sdaf4s,43a2aa8,295aa54",
-        })
-      end
-
-      # how to generate commit hashes? (to avoid c/p)
-      trait :commit_comment1 do
-        title "commented on a commit 43a2aa8"
-        body "This is an awesome comment"
+      factory :github_commit_comment do
+        title "commented on a commit 4b2342hh"
         association :category, factory: :tag, name: "comment"
-        tag_list ['github','commit_comment_event','comment','canjs']
-        props({
-          :type => "commit_comment_event",
-          :commit_id => "43a2aa8",
-        })
+        tag_list %w(github commit_comment_event comment canjs)
       end
-
-      trait :commit_comment2 do
-        title "commented on a commit 295aa54"
-        body "This is an awesome comment"
-        association :category, factory: :tag, name: "comment"
-        tag_list ['github','commit_comment_event','comment','canjs']
-        props({
-          :type => "commit_comment_event",
-          :commit_id => "295aa54",
-        })
-      end
-
-      factory :github_issue, traits: [:issue, :with_determined_rule]
-      factory :github_issue_comment, traits: [:issue_comment, :with_determined_rule]
-      factory :github_push, traits: [:push, :with_determined_rule]
-      factory :github_commit_comment1, traits: [:commit_comment1, :with_determined_rule]
-      factory :github_commit_comment2, traits: [:commit_comment2, :with_determined_rule]
     end
   end
 end
