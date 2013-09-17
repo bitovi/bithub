cc915094f2f723400d6198eba50f115fdcf850f2
Nikica Jokic <neektza@gmail.com>
2013-09-17 21:22:13 +0200

    Kept only the parts needed from processors.

--- app/processors/github.rb
+++ app/processors/github.rb
@@ -1,8 +1,31 @@
-require 'lib/hash'
-require 'lib/string'
-require 'app/processors/github/event_types'
+require "#{Rails.root}/lib/hash"
+require "#{Rails.root}/lib/string"
 
-module EventProcessor
+push_event = lambda do |event|
+  if m = (event['payload']['commits'].map{|c| c['message']}.join(' ')).match(/#(\d*)/)
+    issue_nmb = m[1]
+  end
+
+  event_hash = {
+    :title => "pushed to #{event['repo']['name']}",
+    :body => event['payload']['body'],
+    :url => "http://github.com/#{event['repo']['name']}/commit/#{event['payload']['head']}",
+    :meta => {
+      :commits => event['payload']['commits'].map{|c| c['sha']}.join(','),
+      :commit_shas => event['payload']['commits'].map{|c| c['sha']}.join(','),
+      :repo_name => event['repo']['name'],
+      :referenced_issue_number => issue_nmb
+    }
+  }
+
+  event_hash
+end
+
+EVENT_TYPES = {
+  "PushEvent" => push_event
+}
+
+module Processors
   class Github
     class NotValidEventException < Exception; end
     attr_reader :feed
