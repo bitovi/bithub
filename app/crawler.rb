7a061a1dea32a9fa06d466017a0840e0856472db
Nikica Jokic <neektza@gmail.com>
2013-09-19 22:03:47 +0200

    Updated query intervals and made crawler declare some queues to be sure.

--- app/crawler.rb
+++ app/crawler.rb
@@ -39,26 +39,26 @@ AMQP.start($mq_cs) do |connection, open_ok|
   Signal.trap("TERM", &stop)
 
   channel = AMQP::Channel.new(connection)
-  channel.fanout("e.events.preproc") do |exchange|
 
-    # # --- Streams
-    # $log.info "Registering to Twitter's public stream"
-    # Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:public_feed], false)
+  channel.fanout("e.events.preproc") do |preproc_exchange|
+    # --- Streams
+    $log.info "Registering to Twitter's public stream"
+    Handler::Twitter.connect($log, preproc_exchange, $feeds[:twitter][:streams][:public_feed], false)
     
-    # $log.info "Registering @canjs user stream"
-    # Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:canjs], true)
+    $log.info "Registering @canjs user stream"
+    Handler::Twitter.connect($log, preproc_exchange, $feeds[:twitter][:streams][:canjs], true)
     
-    # $log.info "Registering @jquerypp user stream"
-    # Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:jquerypp], true)
+    $log.info "Registering @jquerypp user stream"
+    Handler::Twitter.connect($log, preproc_exchange, $feeds[:twitter][:streams][:jquerypp], true)
     
-    # $log.info "Registering @funcunit user stream"
-    # Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:funcunit], true)
+    $log.info "Registering @funcunit user stream"
+    Handler::Twitter.connect($log, preproc_exchange, $feeds[:twitter][:streams][:funcunit], true)
   
-    # $log.info "Registering @javascriptmvc user stream"
-    # Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:javascriptmvc], true)
+    $log.info "Registering @javascriptmvc user stream"
+    Handler::Twitter.connect($log, preproc_exchange, $feeds[:twitter][:streams][:javascriptmvc], true)
     
-    # $log.info "Registering @donejs user stream"
-    # Handler::Twitter.connect($log, exchange, $feeds[:twitter][:streams][:donejs], true)
+    $log.info "Registering @donejs user stream"
+    Handler::Twitter.connect($log, preproc_exchange, $feeds[:twitter][:streams][:donejs], true)
 
     # --- Pollers
     phase = 1; shift_phase = lambda {phase+=1}
@@ -69,51 +69,51 @@ AMQP.start($mq_cs) do |connection, open_ok|
         EM.add_timer(phase) do
           $log.info "Registering Github events handler for \"#{project}\" at \"#{repo[:events]}\""
           # or to make something bold in console log with "\033[1mFOOBAR\033[0m ?!
-          EM.add_periodic_timer(3, &Handler::Github.handler($log, exchange, $feeds[:github][:token], repo[:events]))
+          EM.add_periodic_timer(10, &Handler::Github.handler($log, preproc_exchange, $feeds[:github][:token], repo[:events]))
         end
       end
       shift_phase.call
     end
 
-    # # --- Disqus
-    # EM.add_timer(phase) do
-    #   $log.info "Registering Disqus"
-    #   EM.add_periodic_timer(15, &Handler::Disqus.handler($log, exchange, $feeds[:disqus][:api_key]))
-    # end
-    # shift_phase.call
+    # --- Disqus
+    EM.add_timer(phase) do
+      $log.info "Registering Disqus"
+      EM.add_periodic_timer(30, &Handler::Disqus.handler($log, preproc_exchange, $feeds[:disqus][:api_key]))
+    end
+    shift_phase.call
 
 
-    # # --- Forums
-    # forum_endpoints = {
-    #   questions: 'https://forum.javascriptmvc.com/feed/filter/questions',
-    #   all: 'https://forum.javascriptmvc.com/feed'
-    # }
+    # --- Forums
+    forum_endpoints = {
+      questions: 'https://forum.javascriptmvc.com/feed/filter/questions',
+      all: 'https://forum.javascriptmvc.com/feed'
+    }
 
-    # EM.add_timer(phase) do
-    #   $log.info "Registering Forums"
-    #   EM.add_periodic_timer(30, &Handler::Forums.handler($log, exchange, forum_endpoints))
-    # end
-    # shift_phase.call
-
-
-    # # --- Blog
-    # EM.add_timer(phase) do
-    #   $log.info "Registering Blog"
-    #   EM.add_periodic_timer(300, &Handler::Blog.handler($log, exchange))
-    # end
-    # shift_phase.call
+    EM.add_timer(phase) do
+      $log.info "Registering Forums"
+      EM.add_periodic_timer(120, &Handler::Forums.handler($log, preproc_exchange, forum_endpoints))
+    end
+    shift_phase.call
 
 
-    # # --- Old community site
-    # EM.add_timer(phase) do
-    #   $log.info "Registering Community site"
-    #   EM.add_periodic_timer(300, &Handler::CommunitySite.handler($log, exchange))
-    # end
-    # shift_phase.call
+    # --- Blog
+    EM.add_timer(phase) do
+      $log.info "Registering Blog"
+      EM.add_periodic_timer(600, &Handler::Blog.handler($log, preproc_exchange))
+    end
+    shift_phase.call
 
+
+    # --- Old community site
+    EM.add_timer(phase) do
+      $log.info "Registering Community site"
+      EM.add_periodic_timer(600, &Handler::CommunitySite.handler($log, preproc_exchange))
+    end
+    shift_phase.call
   end
 
-  channel.fanout("e.issues") do |exchange|
+  channel.fanout("e.issues") do |issues_exchange|
+    queue = channel.queue("q.issues.web").bind(issues_exchange)
 
     # --- Pollers
     phase = 1; shift_phase = lambda {phase+=1}
@@ -122,8 +122,7 @@ AMQP.start($mq_cs) do |connection, open_ok|
     $feeds[:github][:repos].each do |project, repo|
       if repo[:issues]
         $log.info "Registering Github issues handler for \"#{project}\" at \"#{repo[:issues]}\""
-        EM.add_periodic_timer(20, &Handler::GithubIssues.handler($log, exchange, $feeds[:github][:token], repo[:issues]))
-        
+        EM.add_periodic_timer(60, &Handler::GithubIssues.handler($log, issues_exchange, $feeds[:github][:token], repo[:issues]))
       end
       shift_phase.call
     end    
