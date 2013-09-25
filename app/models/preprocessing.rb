4079487d347a7ee23033f2231a1e27937a871025
Nikica Jokic <neektza@gmail.com>
2013-09-25 13:04:08 +0200

    Move :origin_author_id and :origin_author_feed to props.

--- app/models/preprocessing.rb
+++ app/models/preprocessing.rb
@@ -10,7 +10,9 @@ module Preprocessing
       category: args.delete(:category),
       project: args.delete(:project),
       feed: args.delete(:feed),
-      tags: args.delete(:tags)
+      tags: args.delete(:tags),
+      origin_author_id: args.delete(:origin_author_id),
+      origin_author_feed: args.delete(:origin_author_feed)
     }
     self.props[:location] = args.delete(:location)
     self.props[:scheduled_for] = DateTime.parse(args.delete(:datetime)) if args[:datetime] && args[:datetime].present?
