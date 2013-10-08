d72aec22d6620353123a19f59310dc5ad61693f6
Nikica Jokic <neektza@gmail.com>
2013-10-08 13:38:41 +0200

    Exclude IRC on greates + task for putting feed into props.

--- app/models/event.rb
+++ app/models/event.rb
@@ -50,7 +50,7 @@ class Event < ActiveRecord::Base
   scope :not_parents, lambda { where("id NOT IN (SELECT parent_id FROM events WHERE parent_id IS NOT NULL)") }
   scope :not_children, lambda { where("parent_id IS NULL") }
   scope :with_state, lambda {|state| where("props ? 'state'").where("props -> 'state' = :val", val: state) }
-  scope :no_irc, lambda { tagged_with("irc", exclude: true) }
+  scope :no_irc, lambda { where("props -> 'feed' <> 'irc'") }
 
   after_create do
     author.reward_if_eligible if author
