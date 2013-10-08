5494cf86d7f444017cb08d841220d19bd1a9e318
Nikica Jokic <neektza@gmail.com>
2013-10-08 16:21:26 +0200

    Check if there's source_data in decorator.

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -71,7 +71,7 @@ class EventDecorator < Draper::Decorator
       source.props[:target] = source.source_data['target']['screen_name'] if tag_list.include?('follow_event')
     end
 
-    if source.feed && source.feed.name == 'github'
+    if source.feed && source.source_data && source.feed.name == 'github'
       source.props[:repo_name] = source.source_data['repo']['name']
     else
       source.props[:repo_name] = ''
