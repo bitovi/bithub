40336aec53dd487ef34aca190c49be5c023b8bec
Veljko Dragsic <veljko@kset.org>
2013-10-18 16:18:22 +0200

    Method for determining category based on tags.

--- app/models/category_determination_rule.rb
+++ app/models/category_determination_rule.rb
@@ -3,4 +3,21 @@ class CategoryDeterminationRule < ActiveRecord::Base
   serialize :scorings, ActiveRecord::Coders::Hstore
 
   validates_uniqueness_of :name
+
+  def self.determine_category(tags)
+    match_best(CategoryDeterminationRule.all, tags)
+  end
+
+  def self.match_best(rules, tags)
+    best = calculate_scores(rules, tags).max {|a,b| a[:score] <=> b[:score]}
+    best[:score] > 0 ? best[:name] : nil
+  end
+
+  def self.calculate_scores(rules, tags)
+    rules.map do |rule|
+      score = (rule.scorings.keys & tags).reduce(0) {|score, key| score += rule.scorings[key].to_i}
+      {:name => rule.name, :score => score}
+    end    
+  end
+
 end
