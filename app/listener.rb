73ecd2b7fefc8df3a1bf21fef55bbfcbe6114e1e
Veljko Dragsic <veljko@kset.org>
2013-10-30 15:29:39 +0100

    Listener handles arrays of events from MQ.
    Former-commit-id: 3e412192ecc4f38625a356a8fb6c985f67065fcc

--- app/listener.rb
+++ app/listener.rb
@@ -39,22 +39,26 @@ AMQP.start(ENV['RABBITMQ_URI']) do |connection, open_ok|
       queue = channel.queue("q.events").bind(input_exchange)
 
       queue.subscribe do |metadata, payload|
-        if event_hash = ActiveSupport::JSON.decode(payload)
-          meta = event_hash.delete('meta')
 
-          begin
-            ev = Event.new_from_crawler(event_hash, meta)
-            ev.save!
-            ev.bump_thread
-            liveservice_exchange.publish(ActiveSupport::JSON.encode(ev))
-          rescue ActiveRecord::RecordInvalid => invalid
-            $log.info "Invalid record: #{invalid}"
-          rescue ActiveRecord::RecordNotUnique => duplicate 
-            $log.info "Duplicate record: #{duplicate}"
-          ensure
-            ev.connection.close if ev && ev.connection
+        if events = ActiveSupport::JSON.decode(payload)
+          events.each do |event_hash|
+            meta = event_hash.delete('meta')
+
+            begin
+              ev = Event.new_from_crawler(event_hash, meta)
+              ev.save!
+              ev.bump_thread
+              liveservice_exchange.publish(ActiveSupport::JSON.encode(ev))
+            rescue ActiveRecord::RecordInvalid => invalid
+              $log.info "Invalid record: #{invalid}"
+            rescue ActiveRecord::RecordNotUnique => duplicate 
+              $log.info "Duplicate record: #{duplicate}"
+            ensure
+              ev.connection.close if ev && ev.connection
+            end
           end
         end
+
       end
     end
   end
@@ -62,71 +66,76 @@ AMQP.start(ENV['RABBITMQ_URI']) do |connection, open_ok|
   channel.direct("e.issues") do |issues_exchange|
     queue = channel.queue("q.issues").bind(issues_exchange)
     queue.subscribe do |metadata, payload|
-      issue_hash = ActiveSupport::JSON.decode(payload)
 
-      if (i = Event.issues_by_issue_id(issue_hash['source_data']['id']).first)
-        issue = i.top_level_parent
-        if (issue.props['content_digest'] != issue_hash['content_digest'])
-          $log.info "Issue with ID=#{issue_hash['source_data']['id']} changed. Updating"
-          issue.title = issue_hash['source_data']['title']
-          issue.body = issue_hash['source_data']['body']
-          issue.props['labels'] = issue_hash['source_data']['labels'].map {|l| l['name']}.join(',')
-          issue.props['state'] = issue_hash['source_data']['state']
-          issue.props['content_digest'] = issue_hash['content_digest']
+      if issues = ActiveSupport::JSON.decode(payload)
+
+        issues.each do |issue_hash|
+          if (i = Event.issues_by_issue_id(issue_hash['source_data']['id']).first)
+            issue = i.top_level_parent
+            if (issue.props['content_digest'] != issue_hash['content_digest'])
+              $log.info "Issue with ID=#{issue_hash['source_data']['id']} changed. Updating"
+              issue.title = issue_hash['source_data']['title']
+              issue.body = issue_hash['source_data']['body']
+              issue.props['labels'] = issue_hash['source_data']['labels'].map {|l| l['name']}.join(',')
+              issue.props['state'] = issue_hash['source_data']['state']
+              issue.props['content_digest'] = issue_hash['content_digest']
           
-          issue.determine_tags
-          issue.determine_category
+              issue.determine_tags
+              issue.determine_category
+              
+              begin
+                issue.save!
+              rescue Exception => e
+                puts e.message
+                puts e.backtrace.inspect
+              end
+              
+            end
+          else
+            $log.info "Issue with ID=#{issue_hash['source_data']['id']} doesn't exist. Creating"
+            issue = Event.new
+            action = (issue_hash['source_data']['state'] == 'open') ? 'opened' : 'closed'
+            
+            issue.hash_key = Digest::MD5.hexdigest("github:issue:#{issue_hash['source_data']['id']}")
+            issue.title = issue_hash['source_data']['title']
+            issue.body = issue_hash['source_data']['body']
+            issue.url = issue_hash['source_data']['url']
+            issue.source_data = issue_hash['source_data']
+            issue.feed = Tag.find_or_create_by_name('github')
+            
+            created_at = (issue_hash['source_data']['created_at']) ? issue_hash['source_data']['created_at'] : (fail NoTimestampsException);
+            t = Time.parse(created_at).utc
 
-          begin
-            issue.save!
-          rescue Exception => e
-            puts e.message
-            puts e.backtrace.inspect
+            issue.origin_ts = t
+            issue.origin_date = t
+            issue.thread_updated_at = t
+            issue.thread_updated_date = t
+            
+            issue.props = {
+              repo_name: repo_name(issue_hash['source_data']['url']),
+              issue_id: issue_hash['source_data']['id'],
+              issue_number: issue_hash['source_data']['number'],
+              labels: issue_hash['source_data']['labels'].map {|l| l['name']}.join(','),
+              state: issue_hash['source_data']['state'],
+              action: action,
+              feed: 'github',
+              type: 'issues_event',
+              origin_author_id: issue_hash['source_data']['user']['id']
+            }
+            
+            issue.determine
+
+            begin
+              issue.save!
+            rescue Exception => e
+              puts e.message
+              puts e.backtrace.inspect
+            end
           end
-
+          
         end
-      else
-        $log.info "Issue with ID=#{issue_hash['source_data']['id']} doesn't exist. Creating"
-        issue = Event.new
-        action = (issue_hash['source_data']['state'] == 'open') ? 'opened' : 'closed'
-
-        issue.hash_key = Digest::MD5.hexdigest("github:issue:#{issue_hash['source_data']['id']}")
-        issue.title = issue_hash['source_data']['title']
-        issue.body = issue_hash['source_data']['body']
-        issue.url = issue_hash['source_data']['url']
-        issue.source_data = issue_hash['source_data']
-        issue.feed = Tag.find_or_create_by_name('github')
-
-        created_at = (issue_hash['source_data']['created_at']) ? issue_hash['source_data']['created_at'] : (fail NoTimestampsException);
-        t = Time.parse(created_at).utc
-
-        issue.origin_ts = t
-        issue.origin_date = t
-        issue.thread_updated_at = t
-        issue.thread_updated_date = t
-        
-        issue.props = {
-          repo_name: repo_name(issue_hash['source_data']['url']),
-          issue_id: issue_hash['source_data']['id'],
-          issue_number: issue_hash['source_data']['number'],
-          labels: issue_hash['source_data']['labels'].map {|l| l['name']}.join(','),
-          state: issue_hash['source_data']['state'],
-          action: action,
-          feed: 'github',
-          type: 'issues_event',
-          origin_author_id: issue_hash['source_data']['user']['id']
-        }
-
-        issue.determine
-
-        begin
-          issue.save!
-        rescue Exception => e
-          puts e.message
-          puts e.backtrace.inspect
-        end
-
       end
+
     end
   end
 
