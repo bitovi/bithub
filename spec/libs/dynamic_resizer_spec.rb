8725e5d58fe42ca6853ec465e9d35fbdfc1130f9
Veljko Dragsic <veljko@kset.org>
2013-11-04 16:12:43 +0100

    Tests for DynamicResizer lib.
    Former-commit-id: 564611f64b513286782f90d1b895d8bfdb79f80e

---
+++ spec/libs/dynamic_resizer_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe DynamicResizer do
+  
+  let(:origin_filename) { "foo_bar.jpg" }
+  let(:width) { 400 }
+  let(:height) { 300 }  
+  let(:width_limit) { 1024 }
+  let(:height_limit) { 768 }
+  let(:filename) { width.to_s + "x" + height.to_s + "_" + origin_filename }
+  let(:req_path) { "/public/images/" + filename }
+
+  subject(:image) do
+    DynamicResizer.new(req_path, {:width_limit => width_limit, :height_limit => height_limit})
+  end
+  
+  describe "#parse_filename" do
+    it "extract correct values from filename to props" do
+      image.stub(:is_filepath_valid? => true)
+      props = image.parse_filename(image.filename)
+
+      expect(props[:filename]).to eq filename
+      expect(props[:width]).to eq width
+      expect(props[:height]).to eq height
+      expect(props[:origin_filename]).to eq origin_filename
+    end
+  end
+
+  describe "#is_geometry_valid?" do
+    it "validate width and height to be greater than 0" do
+      expect(image.is_geometry_valid?(400,300)).to be
+      expect(image.is_geometry_valid?(-400,300)).to be_false      
+      expect(image.is_geometry_valid?(0,0)).to be_false      
+    end
+    it "validate width and height to be less smaller than limit" do
+      expect(image.is_geometry_valid?(400,300)).to be
+      expect(image.is_geometry_valid?(width_limit+1, height_limit)).to be_false      
+    end
+  end
+
+end  
