f787003fefbe21fb0b2bc6629616a6c6a5b795fe
Veljko Dragsic <veljko@kset.org>
2013-11-14 13:53:42 +0100

    More expects on "Raising an issue ..." integration test.

--- spec/integration_testing/github_spec.rb
+++ spec/integration_testing/github_spec.rb
@@ -23,35 +23,49 @@ describe "Handling Github issues" do
   default_options :pass  => $rabbitmq[:pass] || "guest"
   default_options :vhost => $rabbitmq[:vhost] || "/"
   default_timeout 60
+  
   amqp_before do
     @channel = AMQP::Channel.new
-    @exchange = @channel.fanout("e.events.liveservice") # !!! change to fanout later !!!
+    @exchange = @channel.fanout("e.events.liveservice")
   end
   
   it "raises an issue with label 'bug'" do
 
     # raise an issue
-    identifier = Helpers.unique_string
-    data = {:title => identifier, :body => "foobar", :labels => ['bug']}
+    data = {
+      :title => Helpers.unique_string,
+      :body => "Raising an issue with label 'bug'.",
+      :labels => ['bug']
+    }
     issue = Github::Issue.new $user1[:username], $user1[:password], $repo1[:name], data
 
-    # check is it successful; TODO: stop test in a case of failure
+    # check if request was successful
     expect(issue.last_response.status).to eq 201
 
     # listen on MQ for new event and check response
     @queue = @channel.queue("q.events.testing.github", :auto_delete => true)
     @queue.bind(@exchange).subscribe do |payload|
       event = Yajl::Parser.parse(payload, :symbolize_keys => true)
+
+      # match event on the MQ
       if event[:title].include?(issue.title)
-        expect(event[:body]).to eq issue.body
-        #expect(event[:tags]).to include *(issue.labels.map {|l| l[:name]})
-        #expect(event[:category]).to eq "bug"
-        #expect(event[:author]).to eq "bug"
-        #expect(event[:feed]).to eq "github"
+
+        # fetch event via Bithub API
+        api_event = Bithub::Event.new 'http://bithub.dev', {:id => event[:id]}
+
+        # examine
+        expect(api_event.body).to include("Raising an issue") # response body is wrapped within <p>
+        expect(api_event.tags).to include("bug","github","issues_event")
+        expect(api_event.feed).to eq "github"
+        expect(api_event.category).to eq "bug"
+
+        #expect(api_event.author).to eq "foobar"
         # check author points
-        
-        done        
+
+        # stop listening
+        done
       end
+      
     end
 
   end
@@ -72,12 +86,12 @@ describe "Handling Github issues" do
     @queue = @channel.queue("q.events.testing.github", :auto_delete => true)
     @queue.bind(@exchange).subscribe do |payload|
       event = Yajl::Parser.parse(payload, :symbolize_keys => true)
-
+      
       if event[:props][:type] == 'push_event' && event[:title].include?($repo1[:name])
         api_event = Bithub::Event.new 'http://bithub.dev', {:id => event[:id]}
         
         expect(api_event.children.length).to eq 1
-                         
+        
         #expect(event[:body]).to eq issue.body
         #expect(event[:tags]).to include *(issue.labels.map {|l| l[:name]})
         #expect(event[:category]).to eq "bug"
@@ -85,7 +99,7 @@ describe "Handling Github issues" do
         #expect(event[:feed]).to eq "github"
         # check author points
         
-        done        
+        done
       end
     end
   end
