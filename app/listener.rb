7b710ee2bc2d15095b767d723864bffd9dbf91d8
Veljko Dragsic <veljko@kset.org>
2013-11-14 23:07:04 +0100

    Listener sends saved issues to MQ.
    Former-commit-id: 47358e9b2ecd4f0e38380d58238505bfedecb347

--- app/listener.rb
+++ app/listener.rb
@@ -49,75 +49,80 @@ AMQP.start(ENV['RABBITMQ_URI']) do |connection, open_ok|
   end
 
   channel.direct("e.issues") do |issues_exchange|
-    queue = channel.queue("q.issues").bind(issues_exchange)
-    queue.subscribe do |metadata, payload|
+    channel.fanout("e.events.liveservice") do |liveservice_exchange|
 
-      issue_hash = ActiveSupport::JSON.decode(payload)
-      issue_id = get_issue_id(issue_hash)
+      queue = channel.queue("q.issues").bind(issues_exchange)
+      queue.subscribe do |metadata, payload|
 
-      if (i = Event.issues_by_issue_id(issue_id).first)
-        $log.info "Updating issue with ID=#{issue_id}."
+        issue_hash = ActiveSupport::JSON.decode(payload)
+        issue_id = get_issue_id(issue_hash)
 
-        issue = i.top_level_parent
-        if (issue.props['content_digest'] != issue_hash['content_digest'])
-          $log.info "Issue with ID=#{issue_hash['source_data']['id']} changed. Updating"
+        if (i = Event.issues_by_issue_id(issue_id).first)
+          $log.info "Updating issue with ID=#{issue_id}."
+
+          issue = i.top_level_parent
+          if (issue.props['content_digest'] != issue_hash['content_digest'])
+            $log.info "Issue with ID=#{issue_hash['source_data']['id']} changed. Updating"
+            issue.title = issue_hash['source_data']['title']
+            issue.body = issue_hash['source_data']['body']
+            issue.props['labels'] = issue_hash['source_data']['labels'].map {|l| l['name']}.join(',')
+            issue.props['state'] = issue_hash['source_data']['state']
+            issue.props['content_digest'] = issue_hash['content_digest']
+
+            issue.determine_tags
+            issue.determine_category
+
+            begin
+              issue.save!
+              liveservice_exchange.publish(ActiveSupport::JSON.encode(issue))
+            rescue Exception => e
+              puts e.message
+              puts e.backtrace.inspect
+            end
+
+          end
+        else
+          $log.info "Creating issue with ID=#{issue_id}."
+
+          issue = Event.new
+          action = (issue_hash['source_data']['state'] == 'open') ? 'opened' : 'closed'
+
+          issue.hash_key = Digest::MD5.hexdigest("github:issue:#{issue_hash['source_data']['id']}")
           issue.title = issue_hash['source_data']['title']
           issue.body = issue_hash['source_data']['body']
-          issue.props['labels'] = issue_hash['source_data']['labels'].map {|l| l['name']}.join(',')
-          issue.props['state'] = issue_hash['source_data']['state']
-          issue.props['content_digest'] = issue_hash['content_digest']
+          issue.url = issue_hash['source_data']['url']
+          issue.source_data = issue_hash['source_data']
+          issue.feed = Tag.find_or_create_by_name('github')
 
-          issue.determine_tags
-          issue.determine_category
+          created_at = issue_hash.andand['source_data'].andand['created_at']
+          t = Time.parse(created_at).utc
+
+          issue.origin_ts = t
+          issue.origin_date = t
+          issue.thread_updated_at = t
+          issue.thread_updated_date = t
+
+          issue.props = {
+            repo_name: repo_name(issue_hash['source_data']['url']),
+            issue_id: issue_hash['source_data']['id'],
+            issue_number: issue_hash['source_data']['number'],
+            labels: issue_hash['source_data']['labels'].map {|l| l['name']}.join(','),
+            state: issue_hash['source_data']['state'],
+            action: action,
+            feed: 'github',
+            type: 'issues_event',
+            origin_author_id: issue_hash['source_data']['user']['id']
+          }
+
+          issue.determine
 
           begin
             issue.save!
+            liveservice_exchange.publish(ActiveSupport::JSON.encode(issue))
           rescue Exception => e
             puts e.message
             puts e.backtrace.inspect
           end
-
-        end
-      else
-        $log.info "Creating issue with ID=#{issue_id}."
-
-        issue = Event.new
-        action = (issue_hash['source_data']['state'] == 'open') ? 'opened' : 'closed'
-
-        issue.hash_key = Digest::MD5.hexdigest("github:issue:#{issue_hash['source_data']['id']}")
-        issue.title = issue_hash['source_data']['title']
-        issue.body = issue_hash['source_data']['body']
-        issue.url = issue_hash['source_data']['url']
-        issue.source_data = issue_hash['source_data']
-        issue.feed = Tag.find_or_create_by_name('github')
-
-        created_at = issue_hash.andand['source_data'].andand['created_at']
-        t = Time.parse(created_at).utc
-
-        issue.origin_ts = t
-        issue.origin_date = t
-        issue.thread_updated_at = t
-        issue.thread_updated_date = t
-
-        issue.props = {
-          repo_name: repo_name(issue_hash['source_data']['url']),
-          issue_id: issue_hash['source_data']['id'],
-          issue_number: issue_hash['source_data']['number'],
-          labels: issue_hash['source_data']['labels'].map {|l| l['name']}.join(','),
-          state: issue_hash['source_data']['state'],
-          action: action,
-          feed: 'github',
-          type: 'issues_event',
-          origin_author_id: issue_hash['source_data']['user']['id']
-        }
-
-        issue.determine
-
-        begin
-          issue.save!
-        rescue Exception => e
-          puts e.message
-          puts e.backtrace.inspect
         end
       end
     end
