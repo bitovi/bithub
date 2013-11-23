606d59fd9c47535372675d52cc27c6081781b1da
Veljko Dragsic <veljko@kset.org>
2013-11-23 20:35:11 +0100

    Paths to tag config updated.
    Former-commit-id: c4685f328d0ce019cd18b5fe06042bd16bd500e9

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-projects = YAML::load_file('config/tag_aliases.yml')
+projects = YAML::load_file('config/tags_definition.yml')
 categories = YAML::load_file('config/categories.yml')
 projects.each {|t, opts| Tag.create({name: t, display_name: opts['display_name'], aliases: opts['aliases']}) }
 categories.each {|c, opts| Tag.create({name: c, display_name: opts['display_name']}) }
