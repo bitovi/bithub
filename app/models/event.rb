0e62b728e9f4010184f9b122a195833e308388d9
Nikica Jokic <neektza@gmail.com>
2013-11-25 14:55:53 +0100

    Don't show events that have 'digest' in props on greatest.

--- app/models/event.rb
+++ app/models/event.rb
@@ -51,7 +51,7 @@ class Event < ActiveRecord::Base
   scope :not_parents, lambda { where("id NOT IN (SELECT parent_id FROM events WHERE parent_id IS NOT NULL)") }
   scope :not_children, lambda { where("parent_id IS NULL") }
   scope :with_state, lambda {|state| where("props ? 'state'").where("props -> 'state' = :val", val: state) }
-  scope :no_irc, lambda { where("props -> 'feed' <> 'irc'") }
+  scope :no_irc_nor_digest, lambda { where("props -> 'feed' <> 'irc' AND props -> 'category' <> 'digest'") }
 
   after_create do
     author.reward_if_eligible if author
