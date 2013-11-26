fec92dd9f31e8260d05e6d311d5ab356a10d4a39
Veljko Dragsic <veljko@kset.org>
2013-11-26 18:17:37 +0100

    Revert "Changed determination to always acces props with string-keys."
    This reverts commit b3ecd56733a446b7476fad1721b87ae9ec522af6.

--- app/models/determination.rb
+++ app/models/determination.rb
@@ -1,7 +1,7 @@
 module Determination
   class DeterminationException < Exception; end
 
-  PROPS_TO_TAGS = %w(feed type project tags category)
+  PROPS_TO_TAGS = [:feed, :type, :project, :tags, :category]
   ATTRS_FOR_TAGGING = [:url, :title, :body]
 
   def determine(custom_props = nil)
@@ -17,6 +17,7 @@ module Determination
 
   def determine_tags    
     # cast some magic
+    self.props.symbolize_keys!
 
     tags =
       (taggify_props + taggify_content + taggify_labels)
@@ -25,22 +26,23 @@ module Determination
       .map {|t| t.snake_case}
     
     self.tag_list = ActsAsTaggableOn::TagList.new(tags) unless tags.empty?
+
     self
   end
 
   def determine_feed
-    feed_name = self.props['feed']
-    fail DeterminationException, ":feed missing from props" if feed_name.nil?
-    self.feed = Tag.find_or_create_by_name(feed_name)
+    f_raw = (self.props[:feed] || self.props['feed'])
+    fail DeterminationException, ":feed missing from props" if f_raw.nil?
+    self.feed = Tag.find_or_create_by_name(f_raw)
     self
   end
 
   def determine_category
-    if (category_name = self.props['category'] || CategoryDeterminationRule.determine_category(self.tag_list))
-      category_name &&= category_name.snake_case
-      self.tag_list.add(category_name)
-      self.props['category'] = category_name
-      self.category = Tag.find_or_create_by_name(category_name)
+    if (category = (self.props[:category] || self.props['category']) || CategoryDeterminationRule.determine_category(self.tag_list))
+      category = category.snake_case
+      self.tag_list.add(category)
+      self.props['category'] = category
+      self.category = Tag.find_or_create_by_name(category)
     end
     self
   end
@@ -51,10 +53,11 @@ module Determination
   end
 
   def determine_author
-    uid = self.props['origin_author_id'] 
-    if f = self.props['origin_author_feed'] # Coming from Bithub
+    p = ActiveSupport::HashWithIndifferentAccess.new(self.props)
+    uid = p[:origin_author_id] 
+    if f = p[:origin_author_feed] # Coming from Bithub
       ident = Identity.find_or_create_with_provider_and_uid(f, uid)
-    else f = self.props['feed'] # Coming from crawler
+    else f = p[:feed] # Coming from crawler
       ident = Identity.find_by_provider_and_uid(f, uid)
     end
     self.author = ident.user if ident && ident.user
@@ -62,8 +65,8 @@ module Determination
   end
 
   def clean_props_after_categorization
-    self.props.delete('tags')
-    self.props.delete('origin_author_feed')
+    self.props.delete(:tags)
+    self.props.delete(:origin_author_feed)
   end
 
   def taggify_props
@@ -77,9 +80,9 @@ module Determination
   end
 
   def taggify_labels
-    if self.props['labels']
+    if self.props[:labels]
       search_tags = Tag.to_name_aliases_hash(:label)
-      input = self.props['labels']
+      input = self.props[:labels]
       Tagger::Engine.new(search_tags).find_tags(input)
     else
       []
