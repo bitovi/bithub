a714762e2cbf9005205485937a4c75dffa62d651
Nikica Jokic <neektza@gmail.com>
2013-11-27 19:19:57 +0100

    Added (cached) total_score to user.
    Methods for updating the cached score added - which are
    called after creating/destroying an update or an award.
    One or two methods were renamed to better describe what they're
    intended for.
    
    This commit resolves #197 but tests are needed to confirm
    that it indeed does that.

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -10,13 +10,16 @@ class Upvote < ActiveRecord::Base
 
   after_create :bust_event_cache
 
-  after_save :update_upvotes
-  after_destroy :update_upvotes
-  after_touch :update_upvotes
+  after_destroy :update_upvotes_in_associated_event
+  after_destroy :decrease_score_in_associated_user
 
   def self.create_based_on_rule(actor, applies_to)
-    upvote = Upvote.create!({actor: actor, applies_to: applies_to, value: applies_to.rule.upvote_value})
-    applies_to.author.reward_if_eligible if applies_to.author
+    ActiveRecord::Base.trasaction do
+      upvote = Upvote.create!({actor: actor, applies_to: applies_to, value: applies_to.rule.upvote_value})
+      upvote.update_upvotes_in_associated_event!
+      upvote.increase_score_in_associated_user!
+      upvote.reward_associated_user_if_eligible
+    end
     upvote
   end
 
@@ -28,7 +31,21 @@ class Upvote < ActiveRecord::Base
     self.applies_to.parent.parent.touch if self.applies_to.parent && self.applies_to.parent.parent
   end
 
-  def update_upvotes
+  def update_upvotes_in_associated_event
     self.applies_to.update_attribute(:total_upvotes, self.applies_to.upvotes.sum('value'))
+  end
+    
+  def increase_score_in_associated_user!
+    self.applies_to.author.total_score += self.value
+    self.applies_to.author.save!
+  end
+
+  def decrease_score_in_associated_user!
+    self.applies_to.author.total_score -= self.value
+    self.applies_to.author.save!
+  end
+
+  def reward_associated_user_if_eligible
+    self.applies_to.author.reward_if_eligible if applies_to.author
   end
 end
