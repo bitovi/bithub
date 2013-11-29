7df7bee4670bc9e6eb5df19f708a54ee264da25f
Veljko Dragsic <veljko@kset.org>
2013-11-29 15:02:09 +0100

    Total upvotes defaults to 0 in migration.
    Former-commit-id: a8d76d5c4d1d8e41484726d7e05dad2d904fdb67

--- db/migrate/20131126102241_add_total_upvotes_to_events.rb
+++ db/migrate/20131126102241_add_total_upvotes_to_events.rb
@@ -1,5 +1,5 @@
 class AddTotalUpvotesToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :total_upvotes, :integer
+    add_column :events, :total_upvotes, :integer, :default => 0
   end
 end
