ed3ee6bb402d221d63b2daf9732c5f3d0b212d22
Nikica Jokic <neektza@gmail.com>
2013-12-09 17:40:40 +0100

    AR models that represent matviews.
    Former-commit-id: 9dcad4ef50a5c6eb0160e68ef566dd86ddd3c090

---
+++ app/models/pagination.rb
@@ -0,0 +1,42 @@
+class Pagination < ActiveRecord::Base
+  self.table_name = :pagination
+
+  scope :by_category, lambda {|c| where(:category => c)}
+
+
+  def self.grouped_db
+    select_command = <<-SQL
+      "date",
+      (select coalesce(sum(cnt),0) from pagination ip where ip.category = 'chat' and ip."date" = pagination."date") as chat,
+      (select coalesce(sum(cnt),0) from pagination ip where ip.category = 'digest' and ip."date" = pagination."date") as digest,
+      (select coalesce(sum(cnt),0) from pagination ip where ip.category <> 'digest' and ip.category <> 'chat' and ip."date" = pagination."date") as else
+    SQL
+
+    self.select(select_command).group("\"date\"").order("\"date\" desc")
+  end
+  
+  def self.grouped
+    grouped_rows = {}
+    self.all.each do |row|
+      type = row.category.to_sym
+
+      if grouped_rows[row.date].nil?
+        grouped_rows[row.date] = {chat: 0, digest: 0, other: 0}
+      end
+
+      if type == :chat
+        grouped_rows[row.date][:chat] += row.cnt
+      elsif type == :digest
+        grouped_rows[row.date][:digest] += row.cnt
+      else
+        grouped_rows[row.date][:other] += row.cnt
+      end
+    end
+
+    grouped_rows
+  end
+
+  def self.refresh
+    ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW \"#{self.table_name}\";")
+  end
+end
