c51948cdf04391cfbff42eb05794c7f8a39c4ff5
Veljko Dragsic <veljko@kset.org>
2013-12-11 14:49:52 +0100

    Grouping in Pagination model counts every category.

--- app/models/pagination.rb
+++ app/models/pagination.rb
@@ -3,37 +3,15 @@ class Pagination < ActiveRecord::Base
 
   scope :by_category, lambda {|c| where(:category => c)}
 
-
-  def self.grouped_db
-    select_command = <<-SQL
-      "date",
-      (select coalesce(sum(cnt),0) from pagination ip where ip.category = 'chat' and ip."date" = pagination."date")::int as chat,
-      (select coalesce(sum(cnt),0) from pagination ip where ip.category = 'digest' and ip."date" = pagination."date")::int as digest,
-      (select coalesce(sum(cnt),0) from pagination ip where ip.category <> 'digest' and ip.category <> 'chat' and ip."date" = pagination."date")::int as other
-    SQL
-
-    self.select(select_command).group("\"date\"").order("\"date\" desc")
-  end
-  
   def self.grouped
-    grouped_rows = {}
-    self.all.each do |row|
-      type = row.category.to_sym
-
-      if grouped_rows[row.date].nil?
-        grouped_rows[row.date] = {chat: 0, digest: 0, other: 0}
-      end
-
-      if type == :chat
-        grouped_rows[row.date][:chat] += row.cnt
-      elsif type == :digest
-        grouped_rows[row.date][:digest] += row.cnt
-      else
-        grouped_rows[row.date][:other] += row.cnt
-      end
+    grouped = {}
+    
+    self.order('"date" desc').each do |row|
+      grouped[row.date] = Hash.new(0) unless grouped[row.date]
+      grouped[row.date][row.category] += row.cnt
     end
 
-    grouped_rows
+    grouped
   end
 
   def self.refresh
