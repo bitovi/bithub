eeeb9d3d918688bc3d055ffe9660eaf6dd0618b4
Veljko Dragsic <veljko@kset.org>
2013-12-13 20:13:59 +0100

    Pagination grouping by raw SQL query.
    Former-commit-id: cbfa5cc08a3af07e6e61dbfe92ea52c97125515b

--- app/models/pagination.rb
+++ app/models/pagination.rb
@@ -2,29 +2,31 @@ class Pagination < ActiveRecord::Base
   self.table_name = :pagination
 
   def self.grouped(tags = nil)
-    grouped = []
     tags = tags.is_a?(Array) ? tags : []
-    
-    self
-      .select("ts::date AS date, category, COUNT(*) AS cnt")
-      .where("#{tags.to_postgres_array} <@ tags")
-      .group("date, category")
-      .order("date DESC")
-      .each do |row|
-      
-        if grouped.last && (grouped.last[:date] == row.date)
-          grouped.last[row.category] = row.cnt
-        else
-          grouped.push({:date => row.date, row.category.to_sym => row.cnt })
-        end
-      
-      end
 
-    grouped
+    query = "SELECT p.date, hstore(array_agg(p.category)::text[], array_agg(p.cnt)::text[]) AS counts FROM 
+               (SELECT ts::date AS date, category, COUNT(*) AS cnt 
+                  FROM pagination AS p 
+                  WHERE #{tags.to_postgres_array} <@ tags 
+                  GROUP BY date, category
+                  ORDER BY date DESC) AS p
+               GROUP BY p.date
+               ORDER BY p.date DESC
+               LIMIT 20 OFFSET 0;"
+
+    ActiveRecord::Base.connection.execute(query).map do |row|
+      {'date' => row['date']}.merge Hash[ from_hstore(row['counts']).map {|k,v| [k,v.to_i]} ]
+    end
   end
 
   def self.refresh
     ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW \"#{self.table_name}\";")
   end
-end
 
+  private
+
+  def self.from_hstore str
+    ActiveRecord::Coders::Hstore.load(str)
+  end
+  
+end
