c09643cea08fc25805ec4d28c74eda1f7b685b0d
Nikica Jokic <neektza@gmail.com>
2013-12-19 18:43:47 +0100

    Event persistence works!

--- app/domain/events/dispatcher.rb
+++ app/domain/events/dispatcher.rb
@@ -11,48 +11,46 @@ module Events
   class Dispatcher
     include Events::Mappings
 
-    def initialize#(event_persistor, entity_persistor)
-      @logger = Log4r::Logger.new('Dispatcher')
-      @logger.add(Log4r::StdoutOutputter.new('console', {
-        :formatter => Log4r::PatternFormatter.new(:pattern => "[#{Process.pid}:%l] %d :: %m")
-      }))
+    def initialize(event_persistor) #, entity_persistor)
+      initialize_logger
+      @ep = event_persistor
     end
 
-    # def process(payload)
-    #   @event_persistor.persist(build_self(payload))
+    def persist(payload)
+      event_class = subtype(payload).to_s
+      @logger.debug event_class.inspect
 
-    #   @entities = []
-    #   @entities += entities_to_update
-    #   @entities += entities_to_create
+      full_name, feed, type = event_class.match(/Event::(.*)::(.*)/).to_a
+      @ep.new({
+        feed: feed,
+        type: type,
+        content_digest: payload['content_digest'],
+        source_data: payload['source_data'],
+        source_json: payload['source_json'],
+        extracted: payload['extracted'],
+      })
 
-    #   @entities.each do |e|
-    #     @entity_persistor.create(e) if e.create?
-    #     @entity_persistor.update(e) if e.update?
-    #   end
+      # @entities = []
+      # @entities += entities_to_update
+      # @entities += entities_to_create
+
+      # @entities.each do |e|
+      #   @entity_persistor.create(e) if e.create?
+      #   @entity_persistor.update(e) if e.update?
+      # end
+    end
+    
+    # def entities_to_update
+    #   subtype.entities_to_update(payload)
     # end
 
-    def process(original_hash)
-      build_self(original_hash)
-      original_hash
-    end
+    # def entities_to_create
+    #   subtype.entities_to_create(payload)
+    # end
 
-    # Delegation
-    def build_self(payload)
-      @logger.debug "EVENT SUBTYPE: #{subtype(payload)}"
-      #@event_persistor.new(attr_hash)
-    end
-
-    def entities_to_update
-      subtype.entities_to_update(payload)
-    end
-
-    def entities_to_create
-      subtype.entities_to_create(payload)
-    end
-
-    def dispatch(payload)
-      subtype(payload)
-    end
+    # def subtype_name(payload)
+    #   subtype(payload).to_s.gsub(/Events::.*::/, '')
+    # end
 
     def subtype(payload)
       meta = (payload['meta'] || payload[:meta])
@@ -76,5 +74,11 @@ module Events
       end
     end
 
+    def initialize_logger
+      @logger = Log4r::Logger.new('Dispatcher')
+      @logger.add(Log4r::StdoutOutputter.new('console', {
+        :formatter => Log4r::PatternFormatter.new(:pattern => "[#{Process.pid}:%l] %d :: %m")
+      }))
+    end
   end
 end
