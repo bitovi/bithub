d68504f21d9170bfe46f7a8ea6481f0beb6bd35c
Nikica Jokic <neektza@gmail.com>
2013-12-20 13:46:25 +0100

    Entities API prototype.

--- app/domain/entities/entity.rb
+++ app/domain/entities/entity.rb
@@ -1,11 +1,28 @@
 module Entities
-  class Entity
+  class Dispatcher
 
-    def find_event_by_origin_uid(uid)
-      query = {
-        props: { origin_author_id: uid }
-      }
+    def initialize(entity_persistor)
+      @ep = entity_persistor
     end
 
+    def judge(payload)
+      entity_class = Entities.const_get(payload.feed).const_get(payload.type)
+
+
+      entity = @ep.new(attrs)
+
+      Builder.new.extract_attrs(payload)
+      Determinator.new.determine(entity_instance)
+
+      Grouper.new(entity_instance)
+
+      entity_instance
+      
+      if e.fetch
+        if e.isDirty?
+
+        end
+      end
+    end
   end
 end
