83c7b51e76e4bf6195cc7277bb7d54e42b98fa46
Nikica Jokic <neektza@gmail.com>
2014-01-08 03:35:36 +0100

    Twitter events OOP.
    Former-commit-id: bfbd39b038cd4f6719b8e5f58a959fad22abb3ac

--- app/domain/events/feeds/twitter/types/follow.rb
+++ app/domain/events/feeds/twitter/types/follow.rb
@@ -1,23 +1,49 @@
 module Events
   module Twitter
-    module Follow
 
-      class Processor
-        def process(original_hash, processed)
-          processed.deep_merge({
-            extracted: {
-              title: "followed @#{original_hash['target']['screen_name']}",
-              # hash_key: Digest::MD5.hexdigest(original_hash['source']['id_str'] + original_hash['target']['id_str'] + @feed.to_s),
-            },
-            meta: {
-              type: 'follow_event',
-              origin_author_name: original_hash['source']['screen_name'],
-              origin_author_id: original_hash['source']['id'],
-            }
-          })
-        end
+    class Follow
+      include Constructable
+
+      def content_digest
+        Digest::MD5.hexdigest(source_id + target + self.class.name)
       end
 
+      def source
+        source_data.andand[:source]
+      end
+
+      def target
+        source_data.andand[:target]
+      end
+
+      def source_id
+        source.andand[:id]
+      end
+
+      def target_id
+        target.andand[:id]
+      end
+
+      def target_screen_name
+        target.andand[:screen_name]
+      end
+
+      def source_screen_name
+        source.andand[:screen_name]
+      end
     end
+
   end
 end
+
+# processed.deep_merge({
+#   extracted: {
+#     title: "followed @#{original_hash['target']['screen_name']}",
+#     # hash_key: Digest::MD5.hexdigest(original_hash['source']['id_str'] + original_hash['target']['id_str'] + @feed.to_s),
+#   },
+#   meta: {
+#     type: 'follow_event',
+#     origin_author_name: original_hash['source']['screen_name'],
+#     origin_author_id: original_hash['source']['id'],
+#   }
+# })
