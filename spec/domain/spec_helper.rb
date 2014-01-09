9f05bd9ad63c3c4e7c4e7d26c04e578f847a984c
Nikica Jokic <neektza@gmail.com>
2014-01-10 00:21:01 +0100

    All domain tests running.

--- spec/domain/spec_helper.rb
+++ spec/domain/spec_helper.rb
@@ -2,9 +2,18 @@ require 'spec_helper'
 
 # Domain logic is in app/domain
 $:.unshift(File.expand_path(File.join('app', 'domain')))
+
 require 'app/domain/events/payload'
 require 'app/domain/events/processor'
+
 require 'app/domain/entities/grouper'
 require 'app/domain/entities/determinator'
 require 'app/domain/entities/normalizer'
 require 'app/domain/entities/procurer'
+
+require 'app/domain/queries/query_logic_analyzer'
+require 'app/domain/queries/scope_applier'
+
+require 'app/domain/fifo_filter'
+require 'app/domain/tagger'
+require 'app/domain/dispatcher'
