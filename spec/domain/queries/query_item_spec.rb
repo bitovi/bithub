810a39aed14863dd52a2e8d20c72c40992146b95
Nikica Jokic <neektza@gmail.com>
2014-01-12 23:00:30 +0100

    Added a pending test for query_item.
    Former-commit-id: b13125e91069c722524e8e92971b359549fbce13

--- spec/domain/queries/query_item_spec.rb
+++ spec/domain/queries/query_item_spec.rb
@@ -3,14 +3,18 @@ require 'domain/spec_helper'
 describe QueryItem do
   let(:model) { double() }
 
-  describe ".negation?" do
+  describe "#value" do
+    it "should cast query values to appropriate type (which it has to read from column info)"
+  end
+
+  describe "#negation?" do
     it "asserts that the query item is negated and is native" do
       model = double(); model.stub(:has_an_attribute?) { true }
       expect(QueryItem.new(model, ["title", "!Some title"]).negation?).to eq(true)
     end
   end
 
-  describe ".regular_and_valid?" do
+  describe "#regular_and_valid?" do
     it "confirms that non-tag-based items are regular" do
       model = double(); model.stub(:has_an_attribute?) { true }
       expect(QueryItem.new(model, ["title", "Some title"]).regular_and_valid?).to eq(true)
@@ -22,7 +26,7 @@ describe QueryItem do
     end
   end
 
-  describe ".tag_based?" do
+  describe "#tag_based?" do
     let(:model) { m = double(); m.stub(:tag_based_attrs) { %w(feed type category project) }; m }
 
     it "confrims that attributes that are stored as tags are of the taggable type" do
@@ -34,7 +38,7 @@ describe QueryItem do
     end
   end
 
-  describe ".native?" do
+  describe "#native?" do
     let(:model) { m = double(); m.stub(:has_an_attribute?) { true }; m }
 
     it "confirms that regular items are indeed native" do
@@ -51,35 +55,35 @@ describe QueryItem do
     end
   end
 
-  describe ".and_value?" do
+  describe "#and_value?" do
     it "checks whether a query item is of 'AND' type" do
       expect(QueryItem.new(model, ["name", "nikica,veljko"]).and_value?).to eq(true)
     end
   end
 
-  describe ".or_value?" do
+  describe "#or_value?" do
     it "checks whether a query item is of 'OR' type" do
       expect(QueryItem.new(model, ["name", "nikica|veljko"]).or_value?).to eq(true)
     end
   end
 
-  describe ".between_value?" do
+  describe "#between_value?" do
     it "checks whether a query item is of 'BETWEEN' type" do
       expect(QueryItem.new(model, ["age", "19:28"]).between_value?).to eq(true)
     end
   end
 
-  describe ".extract_conjuctions" do
+  describe "#extract_conjuctions" do
     it "extracts ANDs from a query item"
   end
 
-  describe ".extract_alternatives" do
+  describe "#extract_alternatives" do
     it "extracts ORs from a query item" do
       expect(QueryItem.new(model, ["feed", "twitter|github"]).extract_alternatives).to eq(["twitter", "github"])
     end
   end
 
-  describe ".extract_range" do
+  describe "#extract_range" do
     let(:lower_date_limit_str) { "2013-1-1" }
     let(:higher_date_limit_str) { "2013-5-1" }
     let(:date_range) { DateTime.parse(lower_date_limit_str)..DateTime.parse(higher_date_limit_str) }
