f5b9d99ab3cfd65e97440552824de54702ea0402
Mihael Konjevic <konjevic@gmail.com>
2014-01-14 13:01:53 -0600

    Correctly calculate date ranges for event list

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,14 +29,14 @@ class User < ActiveRecord::Base
   has_many :awards, :through => :events
 
   has_many :identities, :dependent => :destroy
-  
+
   has_many :achievements, :dependent => :destroy
   has_many :rewards, :through => :achievements
 
   before_save :calculate_avatar_url
 
   scope :only_not_null_names, lambda { where("name <> '' and name IS NOT NULL") }
-  
+
   after_update :check_and_award_points_for_completing_profile
 
   def activities
@@ -47,13 +47,13 @@ class User < ActiveRecord::Base
     end
 
     self.awards.select(['awards.*', 'events.title']).all.each do |a|
-      activities.push({:type => 'award', :id => a.id, :event_id => a.applies_to_id, :title => a.title, :value => a.value, :created_at => a.created_at})  
+      activities.push({:type => 'award', :id => a.id, :event_id => a.applies_to_id, :title => a.title, :value => a.value, :created_at => a.created_at})
     end
 
     self.upvotes.select(['upvotes.*', 'events.title']).all.each do |u|
       activities.push({:type => 'upvote', :id => u.id, :title => u.title, :value => u.value, :created_at => u.created_at})
     end
-    
+
     self.anteups.select(['anteups.*', 'events.title']).all.each do |u|
       activities.push({:type => 'anteup', :id => u.id, :title => u.title, :value => u.value, :created_at => u.created_at})
     end
@@ -73,7 +73,7 @@ class User < ActiveRecord::Base
     activities += self.internals.all
     activities
   end
-  
+
   def actions
     actions = []
     actions += self.awards_as_actor.all
@@ -139,7 +139,7 @@ class User < ActiveRecord::Base
     elsif already_linked_to_current_user?
       self
     else
-      self.identities << identity 
+      self.identities << identity
       self.delay.snatch_all_and_destroy(other_user)
       self.save!
     end
@@ -212,7 +212,7 @@ class User < ActiveRecord::Base
     end
   end
 
-  def already_linked_to_current_user(identity)
+  def already_linked_to_current_user?(identity)
     self.identities.include?(identity)
   end
 
