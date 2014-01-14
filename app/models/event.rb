67cba62d00013cbf0318b1105e42e7b51220ae95
Mihael Konjevic <konjevic@gmail.com>
2014-01-14 13:01:53 -0600

    Correctly calculate date ranges for event list

--- app/models/event.rb
+++ app/models/event.rb
@@ -74,9 +74,17 @@ class Event < ActiveRecord::Base
         end_date   = v.last
       end
 
-      if end_date.nil?
-        end_date = Date.parse(start_date) + 1.day
+      if start_date.is_a?(String)
+        start_date = Date.parse(start_date)
       end
+
+      if end_date.nil?
+        end_date = start_date
+      elsif end_date.is_a?(String)
+        end_date = Date.parse(end_date)
+      end
+
+      end_date = end_date + 1.day - 1.second
 
       args = [params[:clientTz] || 'UTC', start_date, end_date]
       scope = scope.where("thread_updated_at AT TIME ZONE 'UTC' AT TIME ZONE ? BETWEEN ? AND ?", *args)
