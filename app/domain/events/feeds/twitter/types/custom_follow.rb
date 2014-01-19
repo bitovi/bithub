ada5f6a7655584a5ffba0674f9f77b152d903cb4
Nikica Jokic <neektza@gmail.com>
2014-01-19 11:10:58 -0600

    CustomWatch and CustomFollow events.

---
+++ app/domain/events/feeds/twitter/types/custom_follow.rb
@@ -0,0 +1,53 @@
+module Events
+  module Twitter
+
+    class CustomFollow
+      include Constructable
+
+      def initialize(account, identity)
+        @account = Account.new(account)
+        @identity = OAuthIdentity.new(identity)
+      end
+
+      def source_data
+        { identity: @identity, target: @account }
+      end
+
+      def content_digest
+        seed = @identity.uid.to_s + @account.id.to_s
+        calc_digest(seed)
+      end
+
+      def origin_author_id
+        @identity.uid
+      end
+
+      def origin_author_name
+        @identity.nickname
+      end
+
+      def title
+        "followed @#{screen_name}"
+      end
+      
+      def origin_timestamp
+        2.years.ago
+      end
+
+      def taggify_target_screen_name
+        [@account.screen_name]
+      end
+
+      class Account
+        def initialize(acct)
+          @data = acct
+        end
+
+        def screen_name
+          @data.andand[:screen_name]
+        end
+      end
+    end
+
+  end
+end
