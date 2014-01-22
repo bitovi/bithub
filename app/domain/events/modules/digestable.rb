90eefdfb3c8f450ffaa8657b032bb55670311890
Nikica Jokic <neektza@gmail.com>
2014-01-21 16:46:46 -0800

    Digestable module added.
    Former-commit-id: 8b8a5243efef17e08de0df0025f39499a7d0445a

---
+++ app/domain/events/modules/digestable.rb
@@ -0,0 +1,16 @@
+module Events
+  module Digestable
+    def content_digest
+      if respond_to? :origin_id
+        @digest ||= calc_digest(origin_id.to_s)
+      else
+        fail InvalidDigestSeed
+      end
+    end
+
+
+    def calc_digest(seed)
+      Digest::MD5.hexdigest(seed + self.class.name)
+    end
+  end
+end
