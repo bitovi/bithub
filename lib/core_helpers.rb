fe5b41702a50fbd35b708e03797c2ce104fe8983
Veljko Dragsic <veljko@kset.org>
2014-01-23 20:45:34 +0100

    symobolize_keys helper fixed.

--- lib/core_helpers.rb
+++ lib/core_helpers.rb
@@ -1,16 +1,23 @@
 module CoreHelpers
-  def symbolize_keys(hash)
-    hash.inject({}){|result, (key, value)|
-      new_key = case key
-                when String then key.to_sym
-                else key
-                end
-      new_value = case value
-                  when Hash then symbolize_keys(value)
-                  else value
+  def symbolize_keys(x)
+    if x.is_a? Hash
+      x.inject({}) do |acc, (key, value)|
+        new_key = case key
+                  when String then key.to_sym
+                  else key
                   end
-      result[new_key] = new_value
-      result
-    }
+        new_value = case value
+                    when Hash then symbolize_keys(value)
+                    when Array then symbolize_keys(value)
+                    else value
+                    end
+        acc[new_key] = new_value
+        acc
+      end
+    elsif x.is_a? Array
+      x.map {|el| symbolize_keys(el)}
+    else
+      x
+    end
   end
 end
