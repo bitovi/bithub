0eb09002e0e8aac6fc3d7ee8fd19bc910c0fbb13
Nikica Jokic <neektza@gmail.com>
2014-01-24 12:14:45 -0800

    Watch event/entity implemented.

--- app/domain/entities/feeds/github/types/watch.rb
+++ app/domain/entities/feeds/github/types/watch.rb
@@ -2,12 +2,40 @@ module Entities
   module Github
 
     class Watch < Protocol
+      include Events::Github::Accessors::Standard
 
       Relationships = {
         upstream: [],
         downstream: [],
         references: [],
       }
+      
+      def find
+        @payload.origin_id && find_by_actor_and_repo_name.first
+      end
+
+      def build
+        {
+          title: "started watching #{@payload.repo_name}",
+          origin_ts: @payload.origin_ts,
+          props: {
+            origin_author_id: @payload.origin_author_id,
+            origin_author_name: @payload.origin_author_name,
+          }
+        }
+      end
+
+      def find_by_origin_id
+        Entity.tagged_with(%w(github watch))
+        .where("props -> origin_id = #{@payload.origin_id}")
+      end
+
+      def find_by_actor_and_repo_name
+        Entity.tagged_with(%w(github watch))
+        .where("props -> origin_author_id = #{@payload.origin_author_id}")
+        .where("props -> repo_name = #{@payload.repo_name}")
+      end
+
 
       def relationships
         Entities::Github::Watch::Relationships
