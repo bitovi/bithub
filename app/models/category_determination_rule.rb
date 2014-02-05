7a5b6b3b3b0329fc2c1f10920a7ec5e8ca184b17
Nikica Jokic <neektza@gmail.com>
2014-02-05 16:15:16 +0100

    Category determination rules + specs fixed.
    Former-commit-id: 31f766a5b3ac69c02e046687fae64d35322a23a0

--- app/models/category_determination_rule.rb
+++ app/models/category_determination_rule.rb
@@ -4,13 +4,14 @@ class CategoryDeterminationRule < ActiveRecord::Base
 
   validates_uniqueness_of :name
 
-  def self.best_match(tags)
-    rules = CategoryDeterminationRule.all
-    best = calculate_scores(rules, tags).max {|a,b| a[:score] <=> b[:score]}
+  def self.best_match(tags, rules = nil)
+    rules ||= CategoryDeterminationRule.all
+    best = calculate_scores(tags, rules).max {|a,b| a[:score] <=> b[:score]}
     (best && (best[:score] > 0)) ? best[:name] : nil
   end
 
-  def self.calculate_scores(rules, tags)
+  def self.calculate_scores(tags, rules = nil)
+    rules ||= CategoryDeterminationRule.all
     rules.map do |rule|
       score = (rule.scorings.keys & tags).reduce(0) {|score, key| score += rule.scorings[key].to_i}
       {:name => rule.name, :score => score}
