0953bdd166f115536fac18fd08e32eb2cd998054
Veljko Dragsic <veljko@kset.org>
2014-02-06 20:30:41 +0100

    Minor updates on Bithub event and entity.
    Former-commit-id: efa78052f3df29083e7c1e4f6906afb2956f5ad4

--- app/domain/entities/feeds/bithub/types/post.rb
+++ app/domain/entities/feeds/bithub/types/post.rb
@@ -19,8 +19,8 @@ module Entities
             project: @payload.project,
             tags: @payload.category,
             origin_author_id: @payload.origin_author_id,
-            origin_author_feed: @payload.origin_author_feed
-            #origin_author_name: @payload.origin_author_name
+            origin_author_feed: @payload.origin_author_feed,
+            origin_author_name: @payload.origin_author_name
           }
         })
       end
@@ -29,6 +29,7 @@ module Entities
         @instance.title = @payload.title
         @instance.body = @payload.body
         @instance.url = @payload.url
+        @instance.image = @payload.image
         @instance.props[:scheduled_for] = @payload.scheduled_for
         @instance.props[:location] = @payload.location
         # TODO tags?
