4b419e7086805193dd19b140a4af2734657694a4
Veljko Dragsic <veljko@kset.org>
2014-02-06 21:48:50 +0100

    Github fork entity.

---
+++ app/domain/entities/feeds/github/types/fork.rb
@@ -0,0 +1,42 @@
+module Entities
+  module Github
+
+    class Fork < Protocol
+      include Events::Github::Accessors::Standard
+
+      def find
+        @payload.origin_id && find_by_actor_and_repo_name.first
+      end
+
+      def build
+        Entity.new({
+          title: "forked #{@payload.repo_name}",
+          origin_ts: @payload.origin_ts,
+          origin_id: @payload.fork_id.to_s,
+          props: {
+            origin_author_id: @payload.origin_author_id,
+            origin_author_name: @payload.origin_author_name,
+            repo_name: @payload.repo_name,
+          }
+        })
+      end
+
+      def find_by_origin_id
+        Entity
+        .feed('github')
+        .type('fork')
+        .where(origin_id: @payload.origin_id)
+      end
+
+      def find_by_actor_and_repo_name
+        Entity
+        .feed('github')
+        .type('fork')
+        .where("props -> 'origin_author_id' = '#{@payload.origin_author_id}'")
+        .where("props -> 'repo_name' = '#{@payload.repo_name}'")
+      end
+
+    end
+
+  end
+end
--- app/domain/events/feeds/github/types/fork.rb
+++ app/domain/events/feeds/github/types/fork.rb
@@ -3,13 +3,17 @@ module Events
 
     class Fork < Protocol
       include Events::Github::Accessors::Standard
+
+      def fork_id
+        origin_event_id
+      end
+
+      def content_digest
+        seed = origin_author_name + repo_name
+        calc_digest(seed)
+      end
+
     end
 
   end
 end
-
-# processed.deep_merge({
-#   extracted: {
-#     :title => "forked #{original_hash['repo']['name']}"
-#   }
-# })
