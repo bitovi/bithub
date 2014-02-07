d9e535be5c3936d0d847b62a8515c83785ddc305
Nikica Jokic <neektza@gmail.com>
2014-02-07 12:31:57 +0100

    Leftover references to origin_* methods fixed.
    Former-commit-id: 6a5f25380f1986b9a7f70d1bee31c7f269231fdb

--- app/domain/entities/feeds/github/types/fork.rb
+++ app/domain/entities/feeds/github/types/fork.rb
@@ -33,7 +33,7 @@ module Entities
         Entity
         .feed('github')
         .type('fork')
-        .where("props -> 'origin_author_id' = '#{@payload.origin_author_id}'")
+        .where("props -> 'origin_author_id' = '#{@payload.actor_id}'")
         .where("props -> 'repo_name' = '#{@payload.repo_name}'")
       end
 
