4625c2e7e68218e678c82868908d64f9302de2f3
Mihael Konjevic <konjevic@gmail.com>
2014-02-13 23:29:11 +0100

    Work on the login / merging workflow
    Former-commit-id: 4cd6b9b58dc1a611cab675f077d0acfc446a79db

--- app/controllers/api/auth/omniauth_callbacks_controller.rb
+++ app/controllers/api/auth/omniauth_callbacks_controller.rb
@@ -1,5 +1,30 @@
 class Api::Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
+  class AccountLinker
+    def initialize(current_user, identity)
+    end
+    def not_merging?
+      merging_state == :not_merge
+    end
+
+    def valid_merge?
+      merging_state == :valid_merge
+    end
+
+    def merging_user
+      User.where('name ILIKE ?', '%brian%').first
+    end
+
+    def offending_identities
+      User.where('name ILIKE ?', '%brian%').first.identities
+    end
+
+    def merging_state
+      :invalid_merge
+    end
+  end
+
+
   def github
     oauthorize "github"
   end
@@ -12,6 +37,11 @@ class Api::Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksControll
     oauthorize "meetup"
   end
 
+  def link_identity
+    # do the linking magic
+    render :template => 'special/close_oauth_popup.html'
+  end
+
   def passthru
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
   end
@@ -19,17 +49,37 @@ class Api::Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksControll
   private
 
   def oauthorize(kind)
-    begin
-      if (@user = Accounts::AccountManager.new(current_user).find_or_create_user(kind, env["omniauth.auth"]))
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => kind
-        session["devise.#{kind.downcase}_data"] = env["omniauth.auth"]
-        sign_in @user, :event => :authentication
-        render :template => 'special/close_oauth_popup.html'
-      else
-        render :json => { message: 'error' }, :status => 500
-      end
-    rescue User::OtherUserAlreadyLinked => e
-      render :template => 'special/identity_linking_error.html', :status => 406
-    end
+    @sites = HashWithIndifferentAccess.new({
+      github: 'GitHub',
+      twitter: 'Twitter',
+      meetup: 'Meetup'
+    })
+
+    session["devise.#{kind.downcase}_data"] = env["omniauth.auth"]
+    session["current_oauth_data"] = env["omniauth.auth"]
+
+    @identity       = Identity.new_from_oauth(env['omniauth.auth'])
+    @account_linker = AccountLinker.new(current_user, @identity)
+
+    render :template => "oauth/account_linker.html.erb", :layout => false
+
+
+
+
+
+
+
+    #begin
+    #  if (@user = Accounts::AccountManager.new(current_user).find_or_create_user(kind, env["omniauth.auth"]))
+    #    flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => kind
+    #    session["devise.#{kind.downcase}_data"] = env["omniauth.auth"]
+    #    sign_in @user, :event => :authentication
+    #    render :template => 'special/close_oauth_popup.html'
+    #  else
+    #    render :json => { message: 'error' }, :status => 500
+    #  end
+    #rescue User::OtherUserAlreadyLinked => e
+    #  render :template => 'special/identity_linking_error.html', :status => 406
+    #end
   end
 end
