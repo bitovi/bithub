4523837caff15030e9136d8dd4cf3a391deb62d3
Mihael Konjevic <konjevic@gmail.com>
2014-02-14 19:28:37 +0100

    Fix stuff for the oauth

--- app/controllers/api/auth/omniauth_callbacks_controller.rb
+++ app/controllers/api/auth/omniauth_callbacks_controller.rb
@@ -34,6 +34,10 @@ class Api::Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksControll
     @identity = Identity.find_or_create_with_oauth_data(oauth_data)
     @manager = Accounts::AccountManager.new(kind, @identity, current_user)
 
+    @manager.linker.determine_state.merging_state
+
+    Rails.logger.info "OAUTH #{oauth_data.inspect}"
+
     if @manager.linking_or_merging?
       session["devise.#{kind.downcase}_data"] = oauth_data
       session["current_oauth_data"] = oauth_data
