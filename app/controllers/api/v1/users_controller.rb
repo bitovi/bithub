428f6c813d30a9bdd533c7f72b0a7654b2f14f57
Nikica Jokic <neektza@gmail.com>
2014-02-14 22:16:42 +0100

    Fixed user reference in controller.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -38,7 +38,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     filtered_params = params.select {|param| User.accessible_attributes.include?(param)}
 
     if u.update_attributes(filtered_params)
-      @user.calculate_avatar_url; @user.save
+      u.calculate_avatar_url; u.save
       @user = UserDecorator.decorate(u)
       render :show
     else
