18ed24be7e9505b14ae1cddf637bda190b7255af
Nikica Jokic <neektza@gmail.com>
2014-02-14 22:23:16 +0100

    Wrong user's total points get updated.
    Former-commit-id: 73b655e8ffb758a422e0b0ed11199e4abdeb1791

--- app/domain/users/activities_and_entities_snatcher.rb
+++ app/domain/users/activities_and_entities_snatcher.rb
@@ -8,7 +8,7 @@ module Users
         if current_user && other_user
           ActivitiesAndEntitiesSnatcher.new(current_user, other_user).execute
           DuplicateInternalsCleaner.new(current_user).execute
-          other_user.update_total_score
+          current_user.update_total_score
         end
       end
     end
