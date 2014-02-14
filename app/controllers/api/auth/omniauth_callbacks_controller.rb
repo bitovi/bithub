2d508c3bb402cb1037c5e8c2742c31e70b4551db
Nikica Jokic <neektza@gmail.com>
2014-02-14 19:56:46 +0100

    Take all identities when merging.
    Former-commit-id: c655aaf988a82c2df1f7b0b4f0b0463370629432

--- app/controllers/api/auth/omniauth_callbacks_controller.rb
+++ app/controllers/api/auth/omniauth_callbacks_controller.rb
@@ -33,6 +33,7 @@ class Api::Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksControll
 
     @identity = Identity.find_or_create_with_oauth_data(oauth_data)
     @manager = Accounts::AccountManager.new(kind, @identity, current_user)
+    @manager.linker.determine_state
 
     if @manager.linking_or_merging?
       session["devise.#{kind.downcase}_data"] = oauth_data
