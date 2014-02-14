de050361f442a4846991b2da21fccc2377a382fe
Mihael Konjevic <konjevic@gmail.com>
2014-02-14 17:39:23 +0100

    Get to the point where auth popup renders
    Former-commit-id: f9fe041a3179a09e6beac495d6a0d5c38095fc4d

--- app/domain/accounts/account_manager.rb
+++ app/domain/accounts/account_manager.rb
@@ -1,13 +1,13 @@
 module Accounts
   class AccountManager
-    extend Forwardable
 
-    def_delegators :@account_linker, :not_merging?, :valid_merge?, :merging_state, :merging_user, :offending_identities
+    attr_reader :account_linker
 
     def initialize(provider, identity, current_user = nil)
       @provider = provider
       @identity = identity
       @current_user = current_user
+      @account_linker = AccountLinker.new(@current_user, @identity)
     end
 
     def linking_or_merging?
@@ -23,8 +23,36 @@ module Accounts
     end
 
     def link_and_merge
-      (@account_linker ||= AccountLinker.new(@current_user, @identity)).determine_state.link
+      account_linker.determine_state.link
     end
 
+    def determine_state
+      account_linker.determine_state
+      self
+    end
+
+    def not_merging?
+      account_linker.not_merging?
+    end
+
+    def valid_merge?
+      account_linker.valid_merge?
+    end
+
+    def invalid_merge?
+      account_linker.invalid_merge?
+    end
+
+    def merging_state
+      account_linker.merging_state
+    end
+
+    def merging_user
+      account_linker.merging_user
+    end
+
+    def offending_identities
+      account_linker.offending_identities
+    end
   end
 end
