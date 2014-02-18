a9593ec2c392e33a7c366eb65c261c0ea0fdc6cc
Veljko Dragsic <veljko@kset.org>
2014-02-18 19:08:09 +0100

    User activities materialized view and AR model.

---
+++ app/models/user_activity.rb
@@ -0,0 +1,9 @@
+class UserActivity < ActiveRecord::Base
+  self.table_name = :user_activities
+
+  def self.refresh
+    ActiveRecord::Base.connection.execute("REFRESH MATERIALIZED VIEW \"#{self.table_name}\";")
+  end
+
+
+end
