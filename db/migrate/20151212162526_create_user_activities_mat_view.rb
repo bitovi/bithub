c48948115de986308ebbee72f780c03d468a787e
Veljko Dragsic <veljko@kset.org>
2014-02-18 20:18:17 +0100

    Activities matview attr renamed.

--- db/migrate/20151212162526_create_user_activities_mat_view.rb
+++ db/migrate/20151212162526_create_user_activities_mat_view.rb
@@ -2,18 +2,18 @@ class CreateUserActivitiesMatView < ActiveRecord::Migration
   def up
     execute <<-SQL
 CREATE MATERIALIZED VIEW user_activities AS
-SELECT 'Entity' AS model, entities.id AS id, ownerships.owner_id AS user_id, entities.title AS title, total_upvotes + scoring_rules.authorship_value AS value, entities.origin_ts AS ts, entities.cached_tag_list AS tags
+SELECT 'Entity' AS model_name, entities.id AS id, ownerships.owner_id AS user_id, entities.title AS title, total_upvotes + scoring_rules.authorship_value AS value, entities.origin_ts AS ts, entities.cached_tag_list AS tags
 	FROM entities
 		JOIN ownerships ON ownerships.entity_id = entities.id
 		JOIN scoring_rules ON scoring_rules.id = entities.scoring_rule_id
 UNION
-SELECT 'Internal' AS model, internals.id AS id, internals.receiver_id AS user_id, internals.comment AS title, internals.value AS value, internals.created_at AS ts, '' AS tags
+SELECT 'Internal' AS model_name, internals.id AS id, internals.receiver_id AS user_id, internals.comment AS title, internals.value AS value, internals.created_at AS ts, '' AS tags
 	FROM internals
 UNION
-SELECT 'Anteup' AS model, anteups.id AS id, anteups.actor_id AS user_id, '' AS title, anteups.value AS value, anteups.created_at AS ts, '' AS tags
+SELECT 'Anteup' AS model_name, anteups.id AS id, anteups.actor_id AS user_id, '' AS title, anteups.value AS value, anteups.created_at AS ts, '' AS tags
 	FROM anteups
 UNION
-SELECT 'Upvote' AS model, upvotes.id AS id, upvotes.actor_id AS user_id, entities.title AS title, 0 AS value, upvotes.created_at AS ts, '' AS tags
+SELECT 'Upvote' AS model_name, upvotes.id AS id, upvotes.actor_id AS user_id, entities.title AS title, 0 AS value, upvotes.created_at AS ts, '' AS tags
 	FROM upvotes
 		JOIN entities ON upvotes.applies_to_id = entities.id
 SQL
