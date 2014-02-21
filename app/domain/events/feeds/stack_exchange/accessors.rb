bf6700f099873c8ec2c5ed6f8c32f198916a04c5
Veljko Dragsic <veljko@kset.org>
2014-02-21 18:52:18 +0100

    StackExchange Event.
    Former-commit-id: 0d47a5ef6d7ed489381fe7cc4c0bbffe88337e8c

---
+++ app/domain/events/feeds/stack_exchange/accessors.rb
@@ -0,0 +1,64 @@
+module Events
+  module StackExchange
+    module Accessors
+
+      module Standard
+        def link
+          source_data.andand[:link]
+        end
+
+        def body
+          source_data.andand[:body]
+        end
+
+        def body_markdown
+          source_data.andand[:body_markdown]
+        end
+
+        def owner
+          source_data.andand[:body_markdown]
+        end
+
+        def origin_author_id
+          owner.andand[:user_id]
+        end
+
+        def origin_author_name
+          owner.andand[:display_name]
+        end
+
+        def origin_author_avatar_url
+          owner.andand[:profile_image]
+        end
+
+        def origin_author_url
+          owner.andand[:link]
+        end
+
+        def origin_ts
+          creation_date
+        end
+
+        # def last_activity_date
+        #   unix_ts_to_time source_dataa.andand[:last_activity_date]
+        # end
+
+        # def last_edit_date
+        #   unix_ts_to_time source_dataa.andand[:last_edit_date]
+        # end
+
+        def creation_date
+          unix_ts_to_time source_dataa.andand[:creation_date]
+        end
+
+        private
+
+        def unix_ts_to_time(ts)
+          Time.at(ts.to_i).utc
+        end
+
+      end
+
+    end
+  end
+end
