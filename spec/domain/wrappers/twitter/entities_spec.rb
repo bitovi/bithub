f66f6b9e77fe4a782266f0964768b10cedb688ba
Nikica Jokic <neektza@gmail.com>
2014-02-22 02:33:30 +0100

    A few pending tests for Twitter wrappers + a minor fix.

--- spec/domain/wrappers/twitter/entities_spec.rb
+++ spec/domain/wrappers/twitter/entities_spec.rb
@@ -18,30 +18,35 @@ describe Wrappers::Twitter::Entities do
 
   describe "#urls" do
     it "should respond with 'urls' from raw data" do
-      urls = raw_entities['urls'].map {|url| url.symbolize_keys!}
+      urls = raw_entities['urls'].andand.map {|url| url.andand.symbolize_keys!}
       expect(entities.urls).to eq urls
     end
   end
   
   describe "#symbols" do
     it "should respond with 'symbols' from raw data" do
-      symbols = raw_entities['symbols'].map {|sym| sym.symbolize_keys!}
+      symbols = raw_entities['symbols'].andand.map {|sym| sym.andand.symbolize_keys!}
       expect(entities.symbols).to eq symbols
     end
   end
   
   describe "#hashtags" do
     it "should respond with 'hashtags' from raw data" do
-      hashtags = raw_entities['hashtags'].map {|ht| ht.symbolize_keys!}
+      hashtags = raw_entities['hashtags'].andand.map {|ht| ht.andand.symbolize_keys!}
       expect(entities.hashtags).to eq hashtags
     end
   end
 
   describe "#user_mentions" do
     it "should respond with 'user_mentions' from raw data" do
-      user_mentions = raw_entities['user_mentions'].map {|um| um.symbolize_keys!}
+      user_mentions = raw_entities['user_mentions'].andand.map {|um| um.andand.symbolize_keys!}
       expect(entities.user_mentions).to eq user_mentions
     end
+  end
+
+  describe "#retweet?" do
+    it "should determine if the tweet is a retweet"
+    it "should be able to access the original tweet's data"
   end
 
 end
