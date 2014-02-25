45ba793f169eb1ba553b80b715abe838bdbec769
Nikica Jokic <neektza@gmail.com>
2014-02-25 03:32:16 +0100

    Only set the current user as the host.
    Former-commit-id: ef4e35a92df5595425b1e874091ed223dba7c511

--- app/models/user.rb
+++ app/models/user.rb
@@ -89,8 +89,7 @@ class User < ActiveRecord::Base
   def collect_hosted_entities
     if (ident = identities.where(provider: 'meetup').first)
       Entity.feed('meetup').type('event').origin_host(ident.uid).find_each do |entity|
-        hosts = entity.hosts
-        entity.event_hosts = (hosts << self)
+        entity.event_hosts = self
       end
     end
   end
