ec2ed8e37f22159fcdd036463dfceefff80839d2
Nikica Jokic <neektza@gmail.com>
2014-02-25 15:33:54 +0100

    has and maybe_has for DataAccessible.

--- app/domain/wrappers/data_accessible.rb
+++ app/domain/wrappers/data_accessible.rb
@@ -2,14 +2,37 @@ module Wrappers
   module DataAccessible
 
     def data_accessors(*key_names)
-      key_names.each do |kn|
+      key_names.each do |key_name|
         instance_eval do
-          define_method(kn) do
-            @data.andand[kn]
+          define_method(key_name) do
+            @data.fetch(key_name)
           end
         end
       end
     end
+    
+    # Will fail if data is not available
+    def has(*key_names)
+      key_names.each do |key_name|
+        instance_eval do
+          define_method(key_name) do
+            @data.fetch(key_name)
+          end
+        end
+      end
+    end
+    
+    # Will return nil if data is not available
+    def maybe_has(*key_names)
+      key_names.each do |key_name|
+        instance_eval do
+          define_method(key_name) do
+            @data[key_name]
+          end
+        end
+      end
+    end
+
   end
 
 end
