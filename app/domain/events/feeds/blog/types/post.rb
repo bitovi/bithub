cc3f0a8f9adf1a2461d98b4befce523fa7c71625
Nikica Jokic <neektza@gmail.com>
2014-02-26 13:30:08 +0100

    Forum and Blog Post now delegate to RSS item when possible.

--- app/domain/events/feeds/blog/types/post.rb
+++ app/domain/events/feeds/blog/types/post.rb
@@ -1,28 +1,31 @@
 module Events
   module Blog
+
     class Post < Protocol
+      extend Forwardable
+
+      def_delegators :@item,
+        :title, :link, :pub_date,
+        :category
 
       def digest_seed
        link + self.class.name
       end
 
-      def link
-        source_data.andand[:link]
-      end
-
-      def title
-        source_data.andand[:title]
-      end
-
       def description
-        Sanitize.clean(source_data.andand[:description], Sanitize::Config::RELAXED)
+        Sanitizer.clean(@item.description)
       end
 
       def origin_timestamp
         Time.strptime(source_data.andand[:published], "%e %b %Y").utc
       end
 
+      def wrap_reponse_parts
+        @item = Wrappers::Rss::Item.new(source_data)
+      end
+
       alias_method :body, :description
+      alias_method :origin_id, :link
     end
   end
 end
