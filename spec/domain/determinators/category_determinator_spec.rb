ca79e06d2f46f5b6835cd2933837f837566563eb
Nikica Jokic <neektza@gmail.com>
2014-02-27 17:43:54 +0100

    CategoryDeterminator extracted + specs.
    Former-commit-id: dc767cce4628cd44e3cf570dee1f142656780705

--- spec/domain/determinators/category_determinator_spec.rb
+++ spec/domain/determinators/category_determinator_spec.rb
@@ -1,45 +1,44 @@
-# require 'domain/spec_helper'
+require 'domain/spec_helper'
 
-# describe Determinators::CategoryDeterminator do
-  
-#   context "upon determination" do
+describe Determinators::CategoryDeterminator do
 
-#     before(:all) { 
-#       @rule1 = build(:category_determination_rule, name: "foo", scorings: {"foo" => 1})
-#       @rule2 = build(:category_determination_rule, name: "foobarbaz", scorings: {"foo" => 1, "bar" =>1, "baz" =>1})
-#       @rule3 = build(:category_determination_rule, name: "foobar", scorings: {"foo" => 1, "bar" => 1})
-#     }
+  let (:rule1) { FactoryGirl.build(:category_determination_rule, name: "foo", scorings: {"foo" => 1}) }
+  let (:rule2) { FactoryGirl.build(:category_determination_rule, name: "foobarbaz", scorings: {"foo" => 1, "bar" =>1, "baz" =>1}) }
+  let (:rule3) { FactoryGirl.build(:category_determination_rule, name: "foobar", scorings: {"foo" => 1, "bar" => 1}) }
 
-#     describe "#calculate_scores" do
-#       it "returns hash with rule/category names and calculated scores" do
-#         rules = [@rule1, @rule2, @rule3]
-#         tags = ['foo', 'bar', 'baz']
-#         result = [{:name => "foo", :score => 1},
-#                   {:name => "foobarbaz", :score => 3},
-#                   {:name => "foobar", :score => 2}]
-#         expect(CategoryDeterminationRule.calculate_scores(tags, rules)).to eq(result)
-#       end
-#     end
+  let(:rules) {[ rule1, rule2, rule3 ]}
 
-#     describe "#best_match" do
+  describe "#category_scores" do
+    it "returns hash with rule/category names and calculated scores" do
+      d = Determinators::CategoryDeterminator.new(['foo', 'bar', 'baz'], rules)
 
-#       it "matches rule with highest score and returns it's name" do
-#         rules =  [@rule1, @rule2, @rule3]
-#         tags = ["foo","bar","baz"]
-#         expect(CategoryDeterminationRule.best_match(tags, rules)).to eq(@rule2.name)
-#       end
+      result = [
+        {:name => "foo", :score => 1},
+        {:name => "foobarbaz", :score => 3},
+        {:name => "foobar", :score => 2}
+      ]
 
-#       it "returns nil if there is no match with score higher than 0" do
-#         expect(CategoryDeterminationRule.best_match(["more","tags"], [@rule2])).to eq(nil)
-#       end
+      expect(d.category_scores).to eq(result)
+    end
+  end
 
-#       it "returns first match if more rules achieve the same score" do
-#         rules =  [@rule1, @rule2, @rule3]
-#         tags = ["foo","bar"]
-#         expect(CategoryDeterminationRule.best_match(tags, rules)).to eq(@rule2.name)
-#       end
+  describe "#best_match" do
 
-#     end
+    it "matches rule with highest score and returns it's name" do
+      d = Determinators::CategoryDeterminator.new(%w(foo bar baz), rules)
+      expect(d.best_match).to eq(rule2.name)
+    end
 
-#   end
-# end
+    it "returns nil if there is no match with score higher than 0" do
+      d = Determinators::CategoryDeterminator.new(%w(more tags), rules)
+      expect(d.best_match).to eq nil
+    end
+
+    it "returns first match if more rules achieve the same score" do
+      d = Determinators::CategoryDeterminator.new(%w(foo bar), rules)
+      expect(d.best_match).to eq rule2.name
+    end
+
+  end
+
+end
