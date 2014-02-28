0fa4472823b367dddaa151df7211f5bd71a0ea1a
Nikica Jokic <neektza@gmail.com>
2014-02-28 17:00:55 +0100

    Entities unlinker specs + minor refactoring.

--- app/domain/users/entities_unlinker.rb
+++ app/domain/users/entities_unlinker.rb
@@ -1,4 +1,7 @@
 module Users
+
+  IdentData = Struct.new(:uid, :provider)
+
   class EntitiesUnlinker
 
     class UnlinkingJob < Struct.new(:user_id)
@@ -6,11 +9,14 @@ module Users
         EntitiesUnlinker.new(user_id).unlink
       end
     end
+    
+    def async_unlink
+      Delayed::Job.enqueue UnlinkingJob.new(@user_id)
+    end
 
-    def initialize(uid, user_id, provider)
-      @uid = uid
+    def initialize(ident_data, user_id)
+      @uid, @provider = ident_data.values
       @user_id = user_id
-      @provider = provider
     end
 
     def unlink
@@ -18,17 +24,33 @@ module Users
       UserActivity.refresh
     end
     
-    def async_unlink
-      Delayed::Job.enqueue UnlinkingJob.new(@user_id)
+    def unlink_entities
+      return if not(user_owns_identity?)
+
+      entities_to_unlink.reduce(true) do |acc, e|
+        acc && ownerships_to_destroy(e).reduce(true) do |acc, o|
+          acc && o.destroy
+        end
+      end
     end
 
-    def unlink_entities
-      Entity.origin_author(@uid).find_each do |e|
-        e.ownerships
-        .where(ownership_type: 'author')
-        .where(owner_id: @user_id)
-        .destroy_all
-      end
+    private
+
+    def entities_to_unlink
+      Entity.origin_author(@uid).all
+    end
+
+    def ownerships_to_destroy(entity)
+      entity
+      .ownerships
+      .where(ownership_type: 'author')
+      .where(owner_id: @user_id)
+      .all
+    end
+
+    def user_owns_identity?
+      user = User.find_by_id(@user_id)
+      user && user.identities.where(uid: @uid, provider: @provider).present?
     end
 
   end
