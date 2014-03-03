05fe4a73a03c148c66bc7ac32893df25493865ed
Veljko Dragsic <veljko@kset.org>
2014-03-03 16:37:23 +0100

    Cap3 console tasks.
    Former-commit-id: c8f2e08564d8bcb01e107122ee94b6fb725c7485

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{bithub@testing.bithub.com}
+#role :app, %w{bithub@testing.bithub.com}
 #role :web, %w{bithub@testing.bithub.com}
-role :db,  %w{bithub@testing.bithub.com}
+#role :db,  %w{bithub@testing.bithub.com}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,8 @@ role :db,  %w{bithub@testing.bithub.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-#server 'testing.bithub.com', user: 'bithub', roles: %w{web}, my_property: :my_value
+
+server 'testing.bithub.com', user: fetch(:user), roles: %w{app db} #, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
