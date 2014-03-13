272b8f96c7a13131653efe11bfe77bc01b3d3326
Veljko Dragsic <veljko@kset.org>
2014-03-13 21:52:55 +0100

    Drop schema upon destroying tenant.
    Former-commit-id: af2384b251835923b506ee78847d7c6c75664b55

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -12,11 +12,16 @@ class Tenant < ActiveRecord::Base
   belongs_to :country
 
   after_create :create_schema
+  after_destroy :drop_schema
 
   private
 
   def create_schema
     Apartment::Database.create(self.login)
+  end
+
+  def drop_schema
+    Apartment::Database.drop(self.login)
   end
 
 end
