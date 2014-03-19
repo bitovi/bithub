72a119b611c03a6136e1c70b2f57c164db5a6d97
Veljko Dragsic <veljko@kset.org>
2014-03-19 22:14:05 +0100

    Renaming brand also renames psql schema.

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -6,7 +6,10 @@ class Brand < ActiveRecord::Base
 
   has_many :accounts
 
+  validates :name, format: { with: /\A[-_0-9a-zA-Z]+\z/, message: "invalid characters" }
+
   after_create :create_tenant
+  after_save :update_tenant
   after_destroy :destroy_tenant
 
   private
@@ -17,6 +20,15 @@ class Brand < ActiveRecord::Base
 
   def destroy_tenant
     Apartment::Database.drop(name)
+  end
+
+  def update_tenant
+    # update schema name
+    if name = self.changes["name"]
+      old_name = name.first
+      new_name = name.second
+      ActiveRecord::Base.connection.execute("ALTER SCHEMA \"#{old_name}\" RENAME TO \"#{new_name}\"")
+    end
   end
 
 end
