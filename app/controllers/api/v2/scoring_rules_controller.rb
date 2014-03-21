87a8cc99bd4eae8025ca04f1dc6eb85d3db33c2d
Veljko Dragsic <veljko@kset.org>
2014-03-21 19:24:16 +0100

    ScoringRule controller.

--- app/controllers/api/v2/scoring_rules_controller.rb
+++ app/controllers/api/v2/scoring_rules_controller.rb
@@ -1,3 +1,50 @@
 class Api::V2::ScoringRulesController < Api::V2::BaseController
+  respond_to :json
+
+  rescue_from ActiveRecord::RecordNotFound, with: :show_404
+  rescue_from ActiveRecord::RecordInvalid, with: :show_406
+
+  def index
+    @rules = ScoringRule.all
+    render :index
+  end
+
+  def show
+    @rule = ScoringRule.find(params[:id])
+    render :show
+  end
+
+  def create
+    @rule = ScoringRule.new(rule_params)
+    if @rule.save
+      render :show
+    else
+      render :json => msg_hash(@rule, 'create'), :status => 406
+    end
+  end
+
+  def update
+    @rule = ScoringRule.find(params[:id])
+    if @rule.update_attributes(rule_params)
+      render :show
+    else
+      render :json => msg_hash(@rule, 'update'), :status => 406
+    end
+  end
+
+  def destroy
+    @rule = ScoringRule.find(params[:id])
+    if @rule.destroy
+      render :json => msg_hash(@rule, 'destroy', 'success')
+    else
+      render :json => msg_hash(@rule, 'destroy'), :status => 406
+    end
+  end
+
+  private
+
+  def rule_params
+    params.require(:rule).permit(:authorship_value, :upvote_value, :award_value, :priority, :valid_until, required_tags: [])
+  end
 
 end
