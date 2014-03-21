53f767c7a5ec2817deb2aa1259ab4c9e8480cd11
Nikica Jokic <neektza@gmail.com>
2014-03-21 15:01:32 +0100

    Minor config updates.

--- services/crawler2/fetchers/github/client.rb
+++ services/crawler2/fetchers/github/client.rb
@@ -1,9 +1,12 @@
+require 'octokit'
+
 module Fetchers
   module Github
 
     module Client
       def initialize(cfg)
         @config = cfg
+
         @client = Octokit::Client.new(access_token: @config.fetch(:token))
         @repo = @config.fetch(:repo)
       end
--- services/crawler2/fetchers/twitter/tweet_search.rb
+++ services/crawler2/fetchers/twitter/tweet_search.rb
@@ -6,12 +6,8 @@ module Fetchers
     class TweetSearch
       include Client
       
-      def initialize(cfg)
-        super(cfg)
-        @terms = @config.fetch(:terms).join(',')
-      end
-
       def fetch
+        @terms = @config.fetch(:terms).join(',')
         @client.search(terms, :result_type => "recent").take(10).to_a
       end
     end
