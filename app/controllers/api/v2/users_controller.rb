1bc9d53f9ae778de7d3c38743169ecadb32fb6cd
Veljko Dragsic <veljko@kset.org>
2014-03-24 00:03:22 +0100

    User v2 controller copied.

---
+++ app/controllers/api/v2/users_controller.rb
@@ -0,0 +1,111 @@
+class Api::V2::UsersController < Api::V2::BaseController
+  before_filter :authenticate_user!, except: [:index, :show]
+  respond_to :json
+
+  rescue_from ActiveRecord::RecordNotFound, with: :show_404
+  rescue_from ActiveRecord::RecordInvalid, with: :show_406
+  rescue_from CanCan::AccessDenied, with: :show_401
+
+  def index
+    if params[:cached] == "true"
+      @users = Leaderboard.all
+      render :index_cached
+    else
+      muster_query = request.env['muster.query']
+      scope = build_scope(muster_query, params)
+      if !muster_query[:count].blank?
+        render :json => { :count => scope.count(muster_query[:count]) }
+      else
+        scope = scope_applier.apply_order_to_scope
+        @users = UserDecorator.decorate_collection(scope.result.all)
+        render :index
+      end
+    end
+  end
+
+  def show
+    @user = UserDecorator.decorate(User.find(params[:id]))
+    render :show
+  end
+
+  def update
+    if params[:countryISO]
+      country = Country.where({:iso => params[:countryISO]}).first
+      params[:country] = country ? country : nil
+    end
+
+    u = User.find(params[:id])
+    filtered_params = params.select {|param| User.accessible_attributes.include?(param)}
+
+    if u.update_attributes(filtered_params)
+      u.calculate_avatar_url; u.save
+      @user = UserDecorator.decorate(u)
+      render :show
+    else
+      render :json => msg_hash(u, 'update'), :status => 406
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    authorize! :destroy_user, user, :message => "No rights to destroy user."
+    if user && user.destroy
+      @user = UserDecorator.decorate(user)
+      render :show
+    else
+      render :json => msg_hash(u, 'destroy'), :status => 406
+    end
+  end
+
+  def from_github
+    res = user_apis.from_github(params[:user])
+    render :json => res
+  end
+
+  def from_twitter
+    res = user_apis.from_twitter(params[:user])
+    render :json => res
+  end
+
+  def add_role
+    authorize! :manage_roles, User, :message => "No rights to manage user roles!"
+    user = User.find(params[:id])
+    if user && user.add_role(params[:role])
+      @user = UserDecorator.decorate(user)
+      render :show
+    else
+      render :json => msg_hash(u, 'role_management'), :status => 406
+    end
+  end
+
+  def remove_role
+    authorize! :manage_roles, User, :message => "No rights to manage user roles!"
+    user = User.find(params[:id])
+    if user && user.remove_role(params[:role])
+      @user = UserDecorator.decorate(user)
+      render :show
+    else
+      render :json => msg_hash(u, 'role_management'), :status => 406
+    end
+  end
+
+  private # SCOPE BUILDING
+  def build_scope(muster_query, params)
+    scope = User.scoped
+    scope = scope.only_not_null_names
+    scope = scope_applier(scope).apply_muster_query_to_scope(muster_query)
+    scope = scope_applier(scope).apply_regular_params_to_scope
+  end
+
+  def logic_analyzer
+    @logic_analyzer ||= QueryLogic::Query.new(User, params)
+  end
+
+  def scope_applier(current_scope = nil)
+    @scope_applier ||= ScopeApplier.new(current_scope || User.scoped, logic_analyzer)
+  end
+
+  def user_apis
+    @user_apis ||= Accounts::ThirdPartyUserInformer.new
+  end
+end
