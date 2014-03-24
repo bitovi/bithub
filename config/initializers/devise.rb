31e1f6f57c4cdd97d750a14d07a1724492fa1d4b
Veljko Dragsic <veljko@kset.org>
2014-03-24 15:46:47 +0100

    Revert "/api/auth endpoints moved to /api/v2/auth"
    This reverts commit f8cfd75b730f97f097b8f3458e5c266ce2cd03bc.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,6 +7,8 @@ require 'omniauth-stackexchange'
 Devise.setup do |config|
   config.secret_key = '2aa637d41eb2b387b2bb19211702de5bafd653317bc8df921ffa1279c6f73c81418e49aa1fea745a22f322439311b9cd4b92320a12cadab54a6069b4763272f5'
 
+  config.omniauth_path_prefix = '/api/auth'
+
   config.omniauth :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET']
   config.omniauth :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
   config.omniauth :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
