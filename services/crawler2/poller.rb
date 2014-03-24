8ce0beebfba7e3f6790f620ca97131ca891df516
Nikica Jokic <neektza@gmail.com>
2014-03-24 20:06:08 +0100

    Streamer,Poller,FeedSupervisor all work.

--- services/crawler2/poller.rb
+++ services/crawler2/poller.rb
@@ -1,14 +1,26 @@
 class Poller
   include Celluloid
 
-  def initialize(endpoint, interval)
-    @endpoint = endpoint
-    @interval = interval
-    every(interval) { poll }
+  def initialize(name, cfg, fetcher_class)
+    @feed_name, @endpoint_name = name; @config = cfg
+    @fetcher = fetcher_class.new(@config)
+
+    poll
   end
 
   def poll
-    puts "Polling #{@endpoint}"
+    Celluloid.logger.info "Polling #{@feed_name}:#{@endpoint_name}"
+    every(x_seconds) { fetch }
   end
 
+  def fetch
+    resp = @fetcher.fetch
+    Celluloid.logger.info "POLLED #{@feed_name}:#{@endpoint_name} ---> #{resp.length}"
+    resp
+  end
+
+  private
+  def x_seconds
+    @config.fetch(:interval) { 60 }
+  end
 end
