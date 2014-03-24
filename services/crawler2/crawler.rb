90ee4d037d9ee856bea4df8c071abbc9f1fc2004
Nikica Jokic <neektza@gmail.com>
2014-03-24 20:06:08 +0100

    Streamer,Poller,FeedSupervisor all work.
    Former-commit-id: 8ce0beebfba7e3f6790f620ca97131ca891df516

--- services/crawler2/crawler.rb
+++ services/crawler2/crawler.rb
@@ -7,13 +7,13 @@ $:.unshift(File.join(RootDir, 'services'))
 require 'bundler/setup'
 require 'rubygems'
 require 'celluloid'
+require 'celluloid/io'
 require 'bunny'
 
 require 'core_ext'
 require 'logger_factory'
 
 require_relative 'publisher'
-require_relative 'generic_producer'
 require_relative 'feed_supervisor'
 
 # log4r logger
@@ -23,9 +23,10 @@ Celluloid.logger = logger
 
 class Crawler < Celluloid::SupervisionGroup
   supervise Publisher, as: :Publisher
-  supervise GenericProducer, as: :GenericProducer
-  supervise FeedSupervisor, as: :TwitterSupervisor, args: [:twitter]
   supervise FeedSupervisor, as: :MeetupSupervisor, args: [:meetup]
+  supervise FeedSupervisor, as: :TwitterSupervisor, args: [:twitter]
+  supervise FeedSupervisor, as: :GithubSupervisor, args: [:github]
+  # supervise FeedSupervisor, as: :FacebookSupervisor, args: [:facebook]
 end
 
 Crawler.run
