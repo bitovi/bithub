e93d25339641a5dbb91b4693693962aa0635cc76
Veljko Dragsic <veljko@kset.org>
2014-03-25 17:33:58 +0100

    BaseController logic extracted to module.

--- app/controllers/api/v2/base_controller.rb
+++ app/controllers/api/v2/base_controller.rb
@@ -1,26 +1,9 @@
 class Api::V2::BaseController < ActionController::Base
+  respond_to :json
+
+  include Api::V2::BaseHelpers
 
   def home
     render :text => "Bithub API v2"
   end
-
-  def show_401(exception)
-    render json: { message: exception.message }, status: 401
-  end
-
-  def show_404(exception)
-    render json: exception, status: 404
-  end
-
-  def show_406(exception)
-    render json: exception, status: 406
-  end
-
-  def msg_hash(ar_obj, t_action, t_outcome = "error")
-      resp_hash = {}; t_key = ar_obj.class.to_s.downcase + "s"
-      resp_hash[:message] = t("api.#{t_key}.#{t_action}.#{t_outcome}")
-      resp_hash[:errors] = ar_obj.errors.full_messages if !ar_obj.errors.blank?
-      resp_hash
-  end
-
 end
