5964fcd14bd6d4889a5d7dea63ad15c85d6eba5e
Veljko Dragsic <veljko@kset.org>
2014-03-27 13:18:07 +0100

    Brand identities model and migration.

---
+++ db/migrate/001160_create_brand_identities.rb
@@ -0,0 +1,22 @@
+class CreateBrandIdentities < ActiveRecord::Migration
+  def up
+    create_table :brand_identities do |t|
+      t.string :provider
+      t.string :uid
+      t.column :source_data, :json
+
+      t.references :brand
+      t.timestamps
+    end
+
+    execute <<-SQL
+      ALTER TABLE brand_identities
+        ADD CONSTRAINT brand_identities_unique_uid_provider_combination
+        UNIQUE (provider, uid);
+      SQL
+  end
+
+  def down
+    drop_table :brand_identities
+  end
+end
