a770b620da358ac3675c75d3f03e8df947cad54c
Veljko Dragsic <veljko@kset.org>
2014-03-27 19:11:13 +0100

    Account omniauth identities associated with Brand.
    Former-commit-id: 1c3ba4d7d9a2288e834593ac475202cc98266c17

--- app/models/brand_identity.rb
+++ app/models/brand_identity.rb
@@ -1,7 +1,6 @@
 class BrandIdentity < ActiveRecord::Base
-  attr_accessible :provider, :uid, :source_data
+  attr_accessible :provider, :uid, :source_data, :brand
 
   belongs_to :brand
   serialize :source_data, JSON
-
 end
