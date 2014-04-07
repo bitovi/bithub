8f5cc7fbf80d70fb55bc69c9d64b3018a81213f3
Nikica Jokic <neektza@gmail.com>
2014-04-07 15:06:15 +0200

    Moved digest_queue and processor to crawler2/
    Former-commit-id: 62f23d75bb8f4736f220924fb99e3af90a93fa38

---
+++ services/crawler2/digest_set.rb
@@ -0,0 +1,36 @@
+require 'digest/md5'
+require 'redis'
+require 'andand'
+
+class DigestSet
+  def initialize(initial_world = {})
+    @redis = Redis.new(:url => "redis://localhost/0")
+    unless initial_world.empty?
+      initial_world.each {|brand, digests| add_many brand, digests}
+    end
+  end
+
+  def reject_old(events, brand)
+    new_events = events.reject {|e| seen? brand, e.fetch(:content_digest) }
+    new_events.each {|e| add brand, e.fetch(:content_digest) }
+    new_events
+  end
+  
+  def add_many(brand, digests)
+    digests.map{|d| add(brand, d)}.reduce{|acc, x| acc && x}
+  end
+
+  def test(brand, digest)
+    @redis.sismember("digests:#{brand}", digest)
+  end
+  
+  def seen(brand)
+    @redis.smembers("digests:#{brand}")
+  end
+
+  def add(brand, digest)
+    @redis.sadd("digests:#{brand}", digest)
+  end
+
+  alias_method :seen?, :test
+end
