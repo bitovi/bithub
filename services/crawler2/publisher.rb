8e8ff84a557f15bb5b617d8bb7fcfc93b46f92e6
Nikica Jokic <neektza@gmail.com>
2014-04-07 15:19:45 +0200

    Dummy code for filtering already present messages.
    .. added to publisher.
    
    
    Former-commit-id: 767f4c089338da4c9b415a70f3ba3fc6fb915c6c

--- services/crawler2/publisher.rb
+++ services/crawler2/publisher.rb
@@ -1,19 +1,37 @@
 require 'bunny'
+require_relative 'digest_set'
 
 class Publisher
   include Celluloid
 
   def initialize
-    @conn = Bunny.new
-    @conn.start
-    @chan = @conn.create_channel
+    @rabbit = Bunny.new
+    @rabbit.start
+    @chan = @rabbit.create_channel
 
     # Exchange and queue
     @x = @chan.topic("x.events", :auto_delete => true)
     @q = @chan.queue("q.events", :auto_delete => true).bind(@x)
+
+    @filter = DigestSet.new
   end
 
-  def publish(msg, routing_key = nil)
-    @x.publish(msg, routing_key: routing_key)
+  def publish(brand, events)
+    Celluloid.logger.info "-----------> Publishing with routing_key:#{brand}"
+    events.each do |e|
+      Celluloid.logger.info e.text
+    end
+    
+    # reject_old(brand, process(events)).each do |e|
+    #   @x.publish(e, routing_key: brand)
+    # end
+  end
+
+  def process(events)
+    events.map{|e| ResponseProcessor.new(e).extract_raw}
+  end
+
+  def reject_old(brand, events)
+    @filter.reject_old(brand, events)
   end
 end
