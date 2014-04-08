7d59dd433c658131d8ad7fac52529aeea7151fc0
Veljko Dragsic <veljko@kset.org>
2014-04-08 21:00:12 +0200

    FeedConfig moved to public schema, added brand_name attr.

--- db/migrate/001011_create_feed_config.rb
+++ db/migrate/001011_create_feed_config.rb
@@ -1,6 +1,7 @@
 class CreateFeedConfig < ActiveRecord::Migration
   def change
     create_table :feed_configs do |t|
+      t.string :brand_name
       t.string :feed_name
       t.column :config, :json
 
