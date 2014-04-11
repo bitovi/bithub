618bd9aaafd8eaaacfc8eb9a7d0e2c16bc5d4410
Nikica Jokic <neektza@gmail.com>
2014-04-11 15:01:14 +0200

    Unconmmented facebook pages polling.

--- services/crawler/feed_supervisors/facebook.rb
+++ services/crawler/feed_supervisors/facebook.rb
@@ -10,17 +10,17 @@ module FeedSupervisors
     end
 
     def boot
-      # Celluloid.logger.info "Booting Facebook supervisor for #{@brand_name}"
-      # @endpoints = SupervisionGroup.new
-      # pages.each do |page|
-      #   fetcher = Fetchers::Facebook::PageFeed.new(client = ::Koala::Facebook::API.new(page.fetch(:token)))
-      #   @endpoints.supervise_as(actor_name('pages', page.fetch(:id)), Poller, *[@brand_name, fetcher])
-      # end
+      Celluloid.logger.info "Booting Facebook supervisor for #{@brand_name}"
+      @endpoints = SupervisionGroup.new
+      pages.each do |page|
+        fetcher = Fetchers::Facebook::PageFeed.new(client = ::Koala::Facebook::API.new(page.fetch(:token)))
+        @endpoints.supervise_as(actor_name('pages', page.fetch(:id)), Poller, *[@brand_name, fetcher])
+      end
     end
 
     private
     def pages
-      Celluloid::Configurator[:configurator].feed_config(@brand_name, feed_name).fetch(:pages)
+      Celluloid::Actor[:configurator].feed_config(@brand_name, :facebook).fetch(:pages)
     end
     
     def actor_name(endpoint_type, endpoint_id)
