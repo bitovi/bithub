5232be4a8def3301985f263894973ca865de3131
Nikica Jokic <neektza@gmail.com>
2014-04-15 16:23:25 +0200

    Facebook->Status event + spec (initial).
    Former-commit-id: 6bc5fa49a5dc3bdedfb6e8686006e7c5753cecb9

--- app/domain/events/feeds/facebook/types/status.rb
+++ app/domain/events/feeds/facebook/types/status.rb
@@ -2,6 +2,20 @@ module Events
   module Facebook
 
     class Status < Protocol
+      extend Forwardable
+
+      def_delegators :@status, :id, :type, :link
+
+      attr_accessor :user
+
+      def digest_seed
+        id + self.class.name
+      end
+
+      def wrap_response
+        @status ||= Wrappers::Facebook::Status.new(source_data)
+        self
+      end
     end
 
   end
