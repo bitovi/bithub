b4ec9236c4867cc01fddeca42d6dc25f2e7cdc8b
Veljko Dragsic <veljko@kset.org>
2014-04-15 14:51:00 +0200

    Events filtering in crawler, github publishing works.

--- services/crawler/feed_supervisors/github.rb
+++ services/crawler/feed_supervisors/github.rb
@@ -4,10 +4,9 @@ module FeedSupervisors
   class Github
     include Celluloid
 
-    def initialize(brand_name, cfg)
+    def initialize(brand_name)
       @brand_name = brand_name
-      @config = cfg
-      @client = ::Github.new(oauth_token: @config.fetch(:token))
+      @client = ::Github.new(oauth_token: config.fetch(:token))
       boot
     end
 
@@ -16,27 +15,58 @@ module FeedSupervisors
       @endpoints = SupervisionGroup.new
 
       repos.each do |repo_name|
-        @endpoints.supervise_as(actor_name('repo_activity', repo_name)          , Poller , *[@brand_name, Fetchers::Github::RepoActivity.new(@client, {user_repo: repo_name})])
-        @endpoints.supervise_as(actor_name('issues', repo_name)                 , Poller , *[@brand_name, Fetchers::Github::RepoIssues.new(@client, {user_repo: repo_name})])
-        @endpoints.supervise_as(actor_name('issues_comments', repo_name)        , Poller , *[@brand_name, Fetchers::Github::RepoPullRequests.new(@client, {user_repo: repo_name})])
-        @endpoints.supervise_as(actor_name('pull_requests', repo_name)          , Poller , *[@brand_name, Fetchers::Github::RepoIssuesComments.new(@client, {user_repo: repo_name})])
-        @endpoints.supervise_as(actor_name('pull_requests_comments', repo_name) , Poller , *[@brand_name, Fetchers::Github::RepoPullRequestsComments.new(@client, {user_repo: repo_name})])
+        @endpoints.supervise_as \
+          actor_name('repo_activity', repo_name),
+          Poller,
+          *[@brand_name, Fetchers::Github::RepoActivity.new(@client, {user_repo: repo_name}), {interval: 7}]
+
+        @endpoints.supervise_as \
+          actor_name('issues', repo_name),
+          Poller,
+          *[@brand_name, Fetchers::Github::RepoIssues.new(@client, {user_repo: repo_name})]
+
+        @endpoints.supervise_as \
+          actor_name('issues_comments', repo_name),
+          Poller,
+          *[@brand_name, Fetchers::Github::RepoPullRequests.new(@client, {user_repo: repo_name})]
+
+        @endpoints.supervise_as \
+          actor_name('pull_requests', repo_name),
+          Poller ,
+          *[@brand_name, Fetchers::Github::RepoIssuesComments.new(@client, {user_repo: repo_name})]
+
+        @endpoints.supervise_as \
+          actor_name('pull_requests_comments', repo_name),
+          Poller,
+          *[@brand_name, Fetchers::Github::RepoPullRequestsComments.new(@client, {user_repo: repo_name})]
       end
 
       orgs.each do |org_name|
-        @endpoints.supervise_as(actor_name(org_name , 'org_activity') , Poller , *[Fetchers::Github::OrgActivity.new(@client, {org_name: org_name})])
+        @endpoints.supervise_as \
+          actor_name(org_name , 'org_activity'),
+          Poller,
+          *[@brand_name, Fetchers::Github::OrgActivity.new(@client, {org_name: org_name})]
       end
     end
 
     private
+
+    def config
+      Celluloid::Actor[:configurator].feed_config(@brand_name, :github)
+    end
+
+    def token
+      config.fetch(:token)
+    end
+
     def repos
-      @config.fetch(:repos)
+      config.fetch(:repos)
     end
 
     def orgs
-      @config.fetch(:orgs)
+      config.fetch(:orgs)
     end
-    
+
     def actor_name(endpoint_type, endpoint_id)
       "#{@brand_name}_github_#{endpoint_type}_#{endpoint_id}".to_sym
     end
@@ -47,4 +77,3 @@ module FeedSupervisors
 
   end
 end
-
