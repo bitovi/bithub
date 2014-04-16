9c68baa776792226f55a27dcc059688996568c6d
Veljko Dragsic <veljko@kset.org>
2014-04-16 15:55:59 +0200

    Intervals setup.
    Former-commit-id: ed4901de9bc5bacc0017a96c547118e2df5cd820

--- services/crawler/feed_supervisors/facebook.rb
+++ services/crawler/feed_supervisors/facebook.rb
@@ -15,7 +15,7 @@ module FeedSupervisors
 
       pages.each do |page|
         fetcher = Fetchers::Facebook::PageFeed.new(client = ::Koala::Facebook::API.new(page.fetch(:token)))
-        @endpoints.supervise_as(actor_name('pages', page.fetch(:id)), Poller, *[@brand_name, fetcher, {interval: 10}])
+        @endpoints.supervise_as(actor_name('pages', page.fetch(:id)), Poller, *[@brand_name, fetcher, {interval: 30}])
       end
     end
 
--- services/crawler/feed_supervisors/github.rb
+++ services/crawler/feed_supervisors/github.rb
@@ -18,34 +18,34 @@ module FeedSupervisors
         @endpoints.supervise_as \
           actor_name('repo_activity', repo_name),
           Poller,
-          *[@brand_name, Fetchers::Github::RepoActivity.new(@client, {user_repo: repo_name})]
+          *[@brand_name, Fetchers::Github::RepoActivity.new(@client, {user_repo: repo_name}), {interval: 15}]
 
         @endpoints.supervise_as \
           actor_name('issues', repo_name),
           Poller,
-          *[@brand_name, Fetchers::Github::RepoIssues.new(@client, {user_repo: repo_name})]
+          *[@brand_name, Fetchers::Github::RepoIssues.new(@client, {user_repo: repo_name}), {interval: 600}]
 
         @endpoints.supervise_as \
           actor_name('issues_comments', repo_name),
           Poller,
-          *[@brand_name, Fetchers::Github::RepoPullRequests.new(@client, {user_repo: repo_name})]
+          *[@brand_name, Fetchers::Github::RepoPullRequests.new(@client, {user_repo: repo_name}), {interval: 600}]
 
         @endpoints.supervise_as \
           actor_name('pull_requests', repo_name),
           Poller ,
-          *[@brand_name, Fetchers::Github::RepoIssuesComments.new(@client, {user_repo: repo_name})]
+          *[@brand_name, Fetchers::Github::RepoIssuesComments.new(@client, {user_repo: repo_name}), {interval: 300}]
 
         @endpoints.supervise_as \
           actor_name('pull_requests_comments', repo_name),
           Poller,
-          *[@brand_name, Fetchers::Github::RepoPullRequestsComments.new(@client, {user_repo: repo_name})]
+          *[@brand_name, Fetchers::Github::RepoPullRequestsComments.new(@client, {user_repo: repo_name}), {interval: 300}]
       end
 
       orgs.each do |org_name|
         @endpoints.supervise_as \
           actor_name(org_name , 'org_activity'),
           Poller,
-          *[@brand_name, Fetchers::Github::OrgActivity.new(@client, {org_name: org_name})]
+          *[@brand_name, Fetchers::Github::OrgActivity.new(@client, {org_name: org_name}), {interval: 30}]
       end
     end
 
