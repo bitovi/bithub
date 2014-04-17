181836eb72ea073e7e6820de862221aaff29d901
Veljko Dragsic <veljko@kset.org>
2014-04-17 19:13:07 +0200

    Logging in listener.
    Former-commit-id: 2f85b697599987784b2e6e1d189a21a4ee5d4e96

--- lib/logger_factory.rb
+++ lib/logger_factory.rb
@@ -3,19 +3,22 @@ require 'log4r/yamlconfigurator'
 require 'log4r/outputter/rollingfileoutputter'
 require 'log4r/outputter/datefileoutputter'
 
+$root_dir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
 class LoggerFactory
-  ROOT_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
   def initialize(name, env = 'development')
     @name = name; @env = env
     filename = (env == 'development' || env == 'test') ? 'log4r_local.yml' : 'log4r.yml'
-    @config_path = File.join(ROOT_DIR, 'config', filename)
+
+    @config_path = File.join($root_dir, 'config', filename)
 
     logger_config_data = YAML.load_file(@config_path)
     log_cfg = Log4r::YamlConfigurator
     log_cfg["ENV"] = @env
     log_cfg["COMPONENT_NAME"] = @name
     log_cfg.decode_yaml(logger_config_data['log4r_config'])
+
     @loggers = Log4r::Logger
   end
 
@@ -26,8 +29,9 @@ class LoggerFactory
   def ar_logger
     @loggers['active_record']
   end
-  
+
   def ac_logger
     @loggers['action_controller']
   end
+
 end
