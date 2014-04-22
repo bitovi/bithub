897f17a8d58c54cce0a5e21f3200dfee92118582
Veljko Dragsic <veljko@kset.org>
2014-04-22 22:05:33 +0200

    Meetup identity builder.
    Former-commit-id: 8b39b438d5b02b1e5cccf0442f3475d6b0fb2d1a

--- app/domain/identities/builders/meetup.rb
+++ app/domain/identities/builders/meetup.rb
@@ -2,10 +2,22 @@ module Identities
   module Builders
     class Meetup < Base
 
+      MEETUP_API_DOMAIN = 'secure.meetup.com'
+
       def initialize(args)
         super
-        @conn = create_meetup_client
+        @http = create_https_client
+        boot
         self
+      end
+
+      def boot
+        oauth_credentials = oauth.fetch(:credentials)
+        @data[:credentials] = {
+            access_token: oauth_credentials.fetch(:token),
+            refresh_token: oauth_credentials.fetch(:refresh_token),
+            expires_at: oauth_credentials.fetch(:expires_at)
+        }
       end
 
       def build
@@ -14,7 +26,25 @@ module Identities
       end
 
       def sync_groups
-        @data[:groups] = fetch_groups
+        # rmeetup response lacks of #to_hash
+        @data[:groups] = fetch_groups.map do |g|
+          {
+            id: g.id,
+            link: g.link,
+            urname: g.urlname,
+            timezone: g.timezone
+          }
+        end
+      end
+
+      def refresh_credentials
+        if new_credentials = fetch_credentials
+          @data[:credentials] = {
+            access_token: new_credentials[:access_token],
+            refresh_token: new_credentials[:refresh_token],
+            expires_at: Time.now.to_i + new_credentials['expires_in']
+          }
+        end
       end
 
       # Accessors
@@ -23,18 +53,62 @@ module Identities
         @data[:groups]
       end
 
+      def uid
+        oauth.fetch(:uid)
+      end
+
       def access_token
-        oauth.fetch(:credentials).fetch(:token)
+        credentials.fetch(:access_token)
+      end
+
+      def refresh_token
+        credentials.fetch(:refresh_token)
+      end
+
+      def credentials
+        @data.fetch(:credentials)
       end
 
       private
 
-      def fetch_groups
+      def fetch_access_token
+        domain = args[:domain] || DISQUS_API_DOMAIN
+
+        http = Net::HTTP.new domain, 443
+        http.use_ssl = true
 
       end
 
-      def create_meetup_client
+      def fetch_groups
+        client = RMeetup::Client.new :access_token => access_token
+        client.fetch :groups, :member_id => uid
+      end
 
+      def fetch_credentials
+        params = {
+          grant_type: 'refresh_token',
+          client_id: ENV['MEETUP_KEY'],
+          client_secret: ENV['MEETUP_SECRET'],
+          refresh_token: refresh_token
+        }
+
+        response = @http.post "/oauth2/access", URI.encode_www_form(params)
+
+        if response.code == "200"
+          HashWithIndifferentAccess.new(JSON.parse(response.body))
+        else
+          Rails.logger.error "Meetup, refreshing access tokens failed with #{response.code} #{response.body.inspect}"
+          nil
+        end
+      end
+
+      def create_https_client(args={})
+        domain = args[:domain] || MEETUP_API_DOMAIN
+
+        http = Net::HTTP.new domain, 443
+        http.use_ssl = true
+
+        http
       end
 
     end
