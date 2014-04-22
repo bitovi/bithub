1bc2968b58a9c876528a0c5d533b9e3c957b375e
Nikica Jokic <neektza@gmail.com>
2014-04-22 12:08:49 +0200

    RSS scraping.

--- services/crawler/fetchers/all.rb
+++ services/crawler/fetchers/all.rb
@@ -1,15 +1,13 @@
 require_relative 'protocol'
 
+require_relative 'rss/rss'
+
 require_relative 'github/org_activity'
 require_relative 'github/repo_activity'
 require_relative 'github/repo_issues'
 require_relative 'github/repo_issues_comments'
 require_relative 'github/repo_pull_requests'
 require_relative 'github/repo_pull_requests_comments'
-
-# require_relative 'meetup/open_events'
-# require_relative 'meetup/events'
-# require_relative 'meetup/rsvps'
 
 require_relative 'twitter/tweet_search'
 require_relative 'facebook/page_feed'
