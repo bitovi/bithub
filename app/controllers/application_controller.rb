6cceb31459f623cac29cd7354b12d4745b03a33a
Veljko Dragsic <veljko@kset.org>
2014-04-24 16:52:45 +0200

    No one should see this, ...ever!
    Former-commit-id: aa5d3eda4645e1fd435329b65265844cd8383bc8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_filter :redirect_to_subdomain
+
+  def redirect_to_subdomain
+
+    if current_account
+      subdomain = current_account.brand.name
+
+      if request.host.split('.').first != subdomain
+        redirect_to "http://#{subdomain}.#{request.host}/login?acc=#{current_account.id}"
+      end
+
+    elsif params[:acc].present?
+      sign_in(:account, Account.find(params[:acc].to_i))
+      redirect_to "/admin"
+    end
+
+  end
 end
