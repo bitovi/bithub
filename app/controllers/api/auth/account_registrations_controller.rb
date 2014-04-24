d81942bc8e43d58108ceb02be8ba0121a1129d88
Veljko Dragsic <veljko@kset.org>
2014-04-24 17:12:12 +0200

    Redirect upon registration.

--- app/controllers/api/auth/account_registrations_controller.rb
+++ app/controllers/api/auth/account_registrations_controller.rb
@@ -3,7 +3,11 @@ class Api::Auth::AccountRegistrationsController < Devise::RegistrationsControlle
   protected
 
   def after_sign_up_path_for(resource)
-    '/admin'
+    subdomain = resource.brand.name
+    key = 'some really long key that we will use for this'
+    crypt = ActiveSupport::MessageEncryptor.new(key)
+
+    "http://#{subdomain}.#{request.host}/login?acc=#{crypt.encrypt_and_sign(resource.id)}"
   end
 
   def after_inactive_sign_up_path_for(resource)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,19 @@
 class ApplicationController < ActionController::Base
-  # before_filter :redirect_to_subdomain
+  before_filter :redirect_to_subdomain
 
-  # def redirect_to_subdomain
+  def redirect_to_subdomain
 
-  #   key = 'some really long key that we will use for this'
-  #   crypt = ActiveSupport::MessageEncryptor.new(key)
+    key = 'some really long key that we will use for this'
+    crypt = ActiveSupport::MessageEncryptor.new(key)
 
-  #   if current_account
-  #     subdomain = current_account.brand.name
+    if params[:acc].present?
+      account = Account.find(crypt.decrypt_and_verify(params[:acc]).to_i)
+      subdomain = account.brand.name
+      if subdomain == request.host.split('.').first
+        sign_in(:account, account)
+        redirect_to "/admin"
+      end
+    end
 
-  #     if request.host.split('.').first != subdomain
-  #       crypt = ActiveSupport::MessageEncryptor.new(key)
-  #       redirect_to "http://#{subdomain}.#{request.host}/login?acc=#{crypt.encrypt_and_sign(current_account.id)}"
-  #     end
-
-  #   elsif params[:acc].present?
-  #     account = Account.find(crypt.decrypt_and_verify(params[:acc]).to_i)
-  #     subdomain = account.brand.name
-  #     if subdomain == request.host.split('.').first
-  #       sign_in(:account, account)
-  #       redirect_to "/admin"
-  #     end
-  #   end
-
-  # end
+  end
 end
