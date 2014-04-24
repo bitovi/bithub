9568381c7ef8e8d48b5a4a60c6e2d4c5b8097720
Mihael Konjevic <konjevic@gmail.com>
2014-04-24 16:59:36 +0200

    Encrypt redirect data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,24 @@ class ApplicationController < ActionController::Base
 
   def redirect_to_subdomain
 
+    key = 'some really long key that we will use for this'
+    crypt = ActiveSupport::MessageEncryptor.new(key) 
+
     if current_account
       subdomain = current_account.brand.name
 
       if request.host.split('.').first != subdomain
-        redirect_to "http://#{subdomain}.#{request.host}/login?acc=#{current_account.id}"
+        crypt = ActiveSupport::MessageEncryptor.new(key) 
+        redirect_to "http://#{subdomain}.#{request.host}/login?acc=#{crypt.encrypt_and_sign(current_account.id)}"
       end
 
     elsif params[:acc].present?
-      sign_in(:account, Account.find(params[:acc].to_i))
-      redirect_to "/admin"
+      account = Account.find(crypt.decrypt_and_verify(params[:acc]).to_i)
+      subdomain = account.brand.name
+      if subdomain == request.host.split('.').first
+        sign_in(:account, account)
+        redirect_to "/admin"
+      end
     end
 
   end
