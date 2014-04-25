163b12a7d444a1b0f39b62817566e645ae011198
Mihael Konjevic <konjevic@gmail.com>
2014-04-25 17:49:34 +0200

    Allow oauth from main domain
    Former-commit-id: 7f37aee558614a50d5a5e18d488737a67c9f1af9

--- app/controllers/api/auth/account_sessions_controller.rb
+++ app/controllers/api/auth/account_sessions_controller.rb
@@ -7,4 +7,5 @@ class Api::Auth::AccountSessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource_or_scope)
     '/login'
   end
+
 end
--- app/models/feed_config.rb
+++ app/models/feed_config.rb
@@ -85,9 +85,11 @@ module ConfigBuilders
   end
 
   class Rss < Generic
-    {
-      urls : feed_config.andand.urls || []
-    }
+    def config
+      {
+        urls: feed_config.andand.urls || []
+      }
+    end
   end
 
 end
