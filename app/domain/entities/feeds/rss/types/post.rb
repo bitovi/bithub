89408fddda6562c9eb1027b920e8f31f523a93ab
Nikica Jokic <neektza@gmail.com>
2014-04-25 18:11:31 +0200

    RSS fixes.
    Former-commit-id: b7e8239eed7b92e399edb44257c7774b1904d7f7

--- app/domain/entities/feeds/rss/types/post.rb
+++ app/domain/entities/feeds/rss/types/post.rb
@@ -12,7 +12,7 @@ module Entities
           title: @event.title,
           body: @event.description,
           url: @event.link,
-          origin_ts: @event.pub_date,
+          origin_ts: @event.pub_date || Time.now.utc,
         })
       end
 
