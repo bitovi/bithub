85253cbc98b457f9f50cbceea07055b344bf4081
Veljko Dragsic <veljko@kset.org>
2014-04-28 18:37:59 +0200

    Stackexchange tests fixed.
    Former-commit-id: 9f0398b06f758ffe073fbb3e08e76e6f6523fb6e

--- spec/domain/events/feeds/stack_exchange/types/answer_spec.rb
+++ spec/domain/events/feeds/stack_exchange/types/answer_spec.rb
@@ -1,20 +1,20 @@
 require 'domain/events/spec_helper'
 
-describe Events::StackExchange::Answer do
+describe Events::Stackexchange::Answer do
 
   let(:raw_answer) do
     raw_data(response_path: 'stackexchange/question.json')['answers'].first
   end
-  
+
   subject(:answer_event) do
-    Events::StackExchange::Answer.new(raw_answer)
+    Events::Stackexchange::Answer.new(raw_answer)
   end
 
   describe "#digest_seed" do
     it "should construct the digest_seed from answer_id, last_activity date or creation date and class name" do
       seed =  raw_answer['answer_id'].to_s
       seed += Time.at(raw_answer['last_activity_date'] || raw_answer['creation_date']).utc.to_s
-      seed += "Events::StackExchange::Answer"
+      seed += "Events::Stackexchange::Answer"
 
       expect(answer_event.digest_seed).to eq seed
     end
