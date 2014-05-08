60b7b02e9f478f676cddb7cb7e0d2bd485c9cf79
Nikica Jokic <neektza@gmail.com>
2014-05-08 16:48:14 +0200

    Tests for interval setting for Poller.

--- spec/services/crawler/poller_spec.rb
+++ spec/services/crawler/poller_spec.rb
@@ -1,13 +1,33 @@
+require 'celluloid/test'
 require 'no_rails_spec_helper'
 require 'services/crawler/poller'
 
+module Fetchers
+  module Github
+    class RepoIssues
+      def fetch
+        []
+      end
+    end
+  end
+end
+
 describe Poller do
+  before { Celluloid.boot }
+  after { Celluloid.shutdown }
 
   describe "#set_interval" do
-    it "sets a new polling interval"
+    it "changes the polling interval" do
+      fetcher = Fetchers::Github::RepoIssues.new
+      poller = Poller.new("nikica", fetcher, { :interval => 30 })
+      expect{ poller.interval=35 }.to change{ poller.interval }.from(30).to(35)
+    end
   end
 
   describe "#feed_name" do
-    it "extracts the feed_name from the fetcher name"
+    it "extracts the feed_name from the fetcher name" do
+      fetcher = Fetchers::Github::RepoIssues.new
+      expect(Poller.new("nikica", fetcher).feed_name).to eq "github"
+    end
   end
 end
