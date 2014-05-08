9b804a5f4f559984e43c3265d0af44d9fd1cb8fd
Nikica Jokic <neektza@gmail.com>
2014-05-08 18:32:53 +0200

    Removed requires of vcr and webmock
    Former-commit-id: e0f7684a2add0736bad5db837e5517fb75a4ee31

--- spec/no_rails_spec_helper.rb
+++ spec/no_rails_spec_helper.rb
@@ -3,17 +3,15 @@ $:.unshift PROJECT_ROOT
 
 require 'rspec'
 require 'rspec/mocks'
-require 'webmock/rspec'
-require 'vcr'
 
 require 'celluloid'
 require 'celluloid/io'
 
 require 'lib/core_helpers'
 
-VCR.configure do |c|
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
-  c.hook_into :webmock
-end
+# VCR.configure do |c|
+#   c.cassette_library_dir = 'fixtures/vcr_cassettes'
+#   c.hook_into :webmock
+# end
 
 Celluloid.logger.level = Logger::ERROR
