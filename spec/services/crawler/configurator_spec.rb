421a0dd35e1ba700d1ba46a90484e7de02ea533a
Nikica Jokic <neektza@gmail.com>
2014-05-08 17:58:53 +0200

    Configurator spec, only testing config_path method

--- spec/services/crawler/configurator_spec.rb
+++ spec/services/crawler/configurator_spec.rb
@@ -1,7 +1,17 @@
+require 'celluloid/test'
 require 'no_rails_spec_helper'
 require 'services/crawler/configurator'
 
 describe Configurator do
+  before { Celluloid.boot }
+  after { Celluloid.shutdown }
+
+  describe "#config_file_path" do
+    it "tells the Configurator where to look for static config file" do
+      expect(Configurator.new(:environment => 'development').config_file_path).to\
+        eq File.expand_path('config/services/crawler/development.yml')
+    end
+  end
 
   describe "#all_brands" do
     it "should fetch all feed configs from the web component"
