a7515107bd7b16d3d72e5a08dedbc085b486edeb
Veljko Dragsic <veljko@kset.org>
2014-05-09 16:24:02 +0200

    Reduced identities in user response.

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -53,6 +53,12 @@ class Identity < ActiveRecord::Base
     source_data['name']
   end
 
+  def profile_url
+    if urls = source_data['urls']
+      urls['GitHub'] || urls['Twitter'] || urls['public_profile']
+    end
+  end
+
   def identifier
     nickname || name || email
   end
@@ -70,7 +76,7 @@ class Identity < ActiveRecord::Base
   def self.find_or_create_with_oauth_data(oauth_data)
     self.find_or_create_with_provider_and_uid(oauth_data['provider'], oauth_data['uid'], oauth_data['info'])
   end
-  
+
   def self.find_or_init_with_oauth_data(oauth_data)
     self.find_or_init_with_provider_and_uid(oauth_data['provider'], oauth_data['uid'], oauth_data['info'])
   end
