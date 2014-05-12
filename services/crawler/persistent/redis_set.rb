b67383f80b0af7671528c928b162d79cf5c9a4a5
Nikica Jokic <neektza@gmail.com>
2014-05-12 18:11:05 +0200

    Updated specs for DigestSet now test RedisSet.

--- services/crawler/persistent/redis_set.rb
+++ services/crawler/persistent/redis_set.rb
@@ -3,28 +3,34 @@ require 'redis'
 require 'andand'
 
 class RedisSet
-  def initialize(initial_world = {})
+  def initialize(initial_world = [])
     @redis = Redis.new(:url => ENV['REDIS_URL'])
 
     unless initial_world.empty?
-      initial_world.each {|event| add_many event}
+      initial_world.each {|elem| add_many elem}
     end
   end
 
-  def add_many(events)
-    events.map {|e| add(e)}.reduce{|acc, x| acc && x}
+  def add_many(data_set)
+    data_set
+    .map{|e| add(e)}
+    .reduce(true){|acc, e| acc && e}
   end
 
-  def test(event)
-    @redis.sismember key(event), value(event)
+  def test(data_elem)
+    @redis.sismember key(data_elem), value(data_elem)
   end
 
   def members(key)
     @redis.smembers key
   end
 
-  def add(event)
-    @redis.sadd key(event), value(event)
+  def add(data_elem)
+    (r = @redis.sadd key(data_elem), value(data_elem)) == 0 ? false : r
+  end
+  
+  def colon_separated(path)
+    path.join ':'
   end
   
   alias_method :seen?, :test
