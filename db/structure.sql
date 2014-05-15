677aa688af933e333ad3e8fb8100befdb5156364
Veljko Dragsic <veljko@kset.org>
2014-05-15 14:22:55 +0200

    Entity model test fixed.

--- db/structure.sql
+++ db/structure.sql
@@ -1671,7 +1671,7 @@ ALTER TABLE ONLY achievements
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO "$user",public;
+SET search_path TO "public","public";
 
 INSERT INTO schema_migrations (version) VALUES ('0');
 
