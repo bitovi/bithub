b0fa625e760c4b39d0fcabe93268ed358dc1ea5b
Veljko Dragsic <veljko@kset.org>
2014-05-19 18:30:41 +0200

    ScoringRule controller.

--- app/models/scoring_rule.rb
+++ app/models/scoring_rule.rb
@@ -16,13 +16,17 @@ class ScoringRule < ActiveRecord::Base
 
   acts_as_list
 
-  def invalidate!
-    if entities.count == 0
-      destroy!
-    else
-      valid_until = Time.now
-      save!
-    end
+  def required_tags=(tags)
+    tags = Tagger.list_to_name_weight_hash tags
+    write_attribute(:required_tags, tags)
   end
 
+  def invalidate
+    if entities.count == 0
+      destroy
+    else
+      valid_until = Time.now
+      save
+    end
+  end
 end
