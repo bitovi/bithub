7fb3eb575de289aed4398fd6322a4a654a69f9a8
Veljko Dragsic <veljko@kset.org>
2014-05-20 19:59:30 +0200

    Create 'category_name' tag upon after_save on CategoryDeterminationRule.
    Former-commit-id: b6ddcb2b4440556610c40f3182bd6a456f8bd201

--- app/models/category_determination_rule.rb
+++ app/models/category_determination_rule.rb
@@ -10,8 +10,16 @@ class CategoryDeterminationRule < ActiveRecord::Base
 
   acts_as_list
 
+  after_save :create_tag
+
   def required_tags=(tags)
     tags = Tagger.list_to_name_weight_hash tags
     write_attribute(:required_tags, tags)
+  end
+
+  private
+
+  def create_tag
+    Tag.register category_name, 'categories'
   end
 end
