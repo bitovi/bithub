1fb3cd50676d1f203e8d237ff48bf107f6821ca5
Veljko Dragsic <veljko@kset.org>
2014-05-21 14:53:03 +0200

    Second chance for Travis.

--- services/crawler/configurator.rb
+++ services/crawler/configurator.rb
@@ -6,7 +6,7 @@ class Configurator
 
   def initialize(opts)
     @env = opts.fetch(:environment)
-    reload unless @env == 'test'
+    reload unless ENV['TRAVIS']
     Celluloid.logger.debug "All brands: #{@all_brands}"
   end
   attr_reader :all_brands
