76d24af49b74767b07abfcc04d93f9d4c2e10ed0
Nikica Jokic <neektza@gmail.com>
2014-05-26 15:26:36 +0200

    Added Funnel model + Entity#from_funnel

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -107,6 +107,14 @@ class Entity < ActiveRecord::Base
 
   after_validation :reformat_uniqueness_validation
 
+  def self.from_funnel(funnel)
+    q = funnel.as_query
+
+    scope = Entity.scoped
+    scope.tagged_with(q[:tagged_with], :any => true) if q[:tagged_with] && not(q[:tagged_with].empty?)
+    scope.where(q[:where])
+  end
+
   def self.scoped_with_includes
     scope = Entity.scoped
     scope = scope.includes(:owners)
