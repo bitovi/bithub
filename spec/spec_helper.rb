b36d6f32f4f47b33a4e9d9a9f9a1f5aefb8b8eea
Nikica Jokic <neektza@gmail.com>
2014-05-26 15:29:33 +0200

    Test helpers + few factories.
    * spork boot in spec helper,
    * FunnelFactory
    * FeedConfigFactory
    * funnel_spec
    * methods from spec_helper to test_helper_methods.
    
    
    Former-commit-id: f1aec98e1c75a6c1e6f7221f3a47e5d32d1a85db

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,84 +1,21 @@
-# Spec helper for Rails-less tests
-
 ENV["RAILS_ENV"] ||= 'test'
+
+require 'rubygems'
+require 'spork'
 
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 require File.expand_path("#{PROJECT_ROOT}/config/environment", __FILE__)
 $:.unshift PROJECT_ROOT
 
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start if ENV['RAILS_ENV'] == 'test'
+require_relative 'test_helper_methods'
 
-require 'rspec/mocks'
-require 'rspec/rails'
-
-# ----------
-# VCR config
-# ----------
-
-# VCR.configure do |c|
-#   c.cassette_library_dir = 'fixtures/vcr_cassettes'
-#   c.hook_into :webmock
-# end
-
-# ---------------
-# Tag definitions
-# ---------------
-
-TAG_DEFINITIONS_PATH = File.join(PROJECT_ROOT, 'config', 'tag_definitions.yml')
-CATEGORY_RULES_PATH = File.join(PROJECT_ROOT, 'config', 'category_determination_rules.yml')
-SCORING_RULES_PATH = File.join(PROJECT_ROOT, 'config', 'scoring_rules.yml')
-
-def import_all
-  import_tags
-  import_category_rules
-  import_scoring_rules
+Spork.prefork do
+  require 'rspec/mocks'
+  require 'rspec/rails'
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start if ENV['RAILS_ENV'] == 'test'
 end
 
-def import_tags
-  tags = YAML::load_file(TAG_DEFINITIONS_PATH)
-  tags.each do |tag_name, opts|
-    t = Tag.new({
-      name: tag_name,
-      display_name: opts['display_name'],
-      aliases: opts['aliases'],
-      props: opts['props']
-    })
-    t.group_list = opts['group_list']
-    t.save
-  end
-end
-
-def import_category_rules
-  rules = YAML::load_file(CATEGORY_RULES_PATH)
-  rules.each do |category, scorings|
-    CategoryDeterminationRule.create({:name => category, :scorings => scorings})
-  end
-end
-
-def import_scoring_rules
-  rules = YAML::load_file(SCORING_RULES_PATH)
-  rules.each do |rule_config|
-    ScoringRule.create(rule_config)
-  end
-end
-
-
-# ----------------
-# Response loading
-# ----------------
-
-def load_and_parse(path)
-  ext_name = File.extname(path).gsub('.','').to_sym
-
-  loaders = {
-    json: lambda {|p| ActiveSupport::JSON.decode(File.read(path)) },
-    rss: lambda {|p| Nori.new(:parser => :nokogiri).parse(File.read(path)) }
-  }
-
-  loaders[ext_name].(path)
-end
-
-def raw_data(opts = {})
-  load_and_parse File.join('spec/support/responses', opts[:response_path])
+Spork.each_run do
+  # This code will be run each time you run your specs.
 end
---
+++ spec/test_helper_methods.rb
@@ -0,0 +1,60 @@
+# ---------------
+# Tag definitions
+# ---------------
+
+TAG_DEFINITIONS_PATH = File.join(PROJECT_ROOT, 'config', 'tag_definitions.yml')
+CATEGORY_RULES_PATH = File.join(PROJECT_ROOT, 'config', 'category_determination_rules.yml')
+SCORING_RULES_PATH = File.join(PROJECT_ROOT, 'config', 'scoring_rules.yml')
+
+def import_all
+  import_tags
+  import_category_rules
+  import_scoring_rules
+end
+
+def import_tags
+  tags = YAML::load_file(TAG_DEFINITIONS_PATH)
+  tags.each do |tag_name, opts|
+    t = Tag.new({
+      name: tag_name,
+      display_name: opts['display_name'],
+      aliases: opts['aliases'],
+      props: opts['props']
+    })
+    t.group_list = opts['group_list']
+    t.save
+  end
+end
+
+def import_category_rules
+  rules = YAML::load_file(CATEGORY_RULES_PATH)
+  rules.each do |category, scorings|
+    CategoryDeterminationRule.create({:name => category, :scorings => scorings})
+  end
+end
+
+def import_scoring_rules
+  rules = YAML::load_file(SCORING_RULES_PATH)
+  rules.each do |rule_config|
+    ScoringRule.create(rule_config)
+  end
+end
+
+# ----------------
+# Response loading
+# ----------------
+
+def load_and_parse(path)
+  ext_name = File.extname(path).gsub('.','').to_sym
+
+  loaders = {
+    json: lambda {|p| ActiveSupport::JSON.decode(File.read(path)) },
+    rss: lambda {|p| Nori.new(:parser => :nokogiri).parse(File.read(path)) }
+  }
+
+  loaders[ext_name].(path)
+end
+
+def raw_data(opts = {})
+  load_and_parse File.join('spec/support/responses', opts[:response_path])
+end
