99814165ed91770cc9d6421f2b87c627c0241978
Nikica Jokic <neektza@gmail.com>
2014-06-02 19:45:24 +0200

    Renamed funnels and funnel groups to funnel -> funnel_constraints

---
+++ app/models/funnel_constraint.rb
@@ -0,0 +1,9 @@
+class FunnelConstraint < ActiveRecord::Base
+  attr_accessible :feed_name, :type_name
+  validates_presence_of :feed_name, :type_name
+  has_and_belongs_to_many :funnels
+
+  def constraints
+    { :type_name => type_name, :feed_name => feed_name }
+  end
+end
--- db/migrate/20140527135455_create_funnels.rb
+++ db/migrate/20140527135455_create_funnels.rb
@@ -1,8 +1,8 @@
 class CreateFunnels < ActiveRecord::Migration
   def change
     create_table :funnels do |t|
-      t.string       :feed_name
-      t.string       :type_name
+      t.string       :name
+      t.string       :display_name
       t.string_array :tags
     end
   end
