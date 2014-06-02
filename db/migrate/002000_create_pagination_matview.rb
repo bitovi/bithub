55159ed5eae7329e08d91417473ef2a2fb9c47ca
Veljko Dragsic <veljko@kset.org>
2014-06-02 21:57:30 +0200

    Pagination matview uses cached_tag_list and funnels.
    Former-commit-id: cbc38992a8c95cad9495f6e347af5e203dceae2d

--- db/migrate/002000_create_pagination_matview.rb
+++ db/migrate/002000_create_pagination_matview.rb
@@ -2,17 +2,18 @@ class CreatePaginationMatview < ActiveRecord::Migration
   def up
     execute <<-SQL
 CREATE MATERIALIZED VIEW pagination AS
- SELECT e.thread_updated_ts AS ts,
+SELECT e.thread_updated_ts AS ts,
     e.id,
-    categories.name AS category,
-    ARRAY( SELECT t.name
-           FROM taggings tt,
-            tags t
-          WHERE tt.taggable_type::text = 'Entity'::text AND tt.tag_id = t.id AND tt.taggable_id = e.id) AS tags
-   FROM entities e
-   LEFT JOIN tags categories ON e.category_id = categories.id
-  WHERE e.parent_id IS NULL
-  ORDER BY e.thread_updated_ts DESC
+    string_to_array(e.cached_tag_list, ', ')::character varying[] AS tags,
+    funnels.name AS funnel
+    FROM entities e
+        LEFT JOIN ( SELECT f.name, fc.feed_name, fc.type_name, f.tags
+	   FROM funnels f
+	     LEFT JOIN funnel_constraints_funnels fcf ON fcf.funnel_id = f.id
+	     LEFT JOIN funnel_constraints fc ON fcf.funnel_constraint_id = fc.id
+	) AS funnels ON e.feed_name = funnels.feed_name AND e.type_name = funnels.type_name AND (funnels.tags = '{}' OR funnels.tags && string_to_array(e.cached_tag_list,', ')::character varying[])
+    WHERE e.parent_id IS NULL
+    ORDER BY e.thread_updated_ts DESC
 WITH DATA;
     SQL
 
