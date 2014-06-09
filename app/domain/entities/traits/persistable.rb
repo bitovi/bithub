bdc32cbf94caa1eaf186520743db61b566869a22
Nikica Jokic <neektza@gmail.com>
2014-06-09 11:27:43 +0200

    Bump thread on persisting of new entity.

--- app/domain/entities/traits/persistable.rb
+++ app/domain/entities/traits/persistable.rb
@@ -5,12 +5,14 @@ module Entities
       @instance.parent.save if @instance.parent
       @instance.children.each {|c| c.save} if @instance.children
       @instance.save
+      @instance.bump_thread
     end
 
     def persist!
       @instance.parent.save! if @instance.parent
       @instance.children.each {|c| c.save!} if @instance.children
       @instance.save!
+      @instance.bump_thread
     end
   end
 end
