de9754dbb4d7a555ce4cc521266c85319734994d
Nikica Jokic <neektza@gmail.com>
2014-06-14 00:12:25 +0200

    Solipsim contains methods such as #has_an_attr.
    Removed copy pasted code.

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,13 +1,6 @@
 class Country < ActiveRecord::Base
+  extend Solipsism
+
   attr_accessible :name, :display_name, :iso, :priority
-
   has_many :users
-
-  # Helpers
-  def self.has_an_attribute?(attr)
-    User.reflections.include?(attr) ||
-    User.reflections.include?(attr.to_s.pluralize.to_sym) ||
-    User.attribute_names.include?(attr) ||
-    User.attribute_names.include?(attr.to_s.pluralize.to_sym)
-  end
 end
