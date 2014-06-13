fae3cf22ba612d2d91e69d504624ca076968d397
Veljko Dragsic <veljko@kset.org>
2014-06-13 21:25:37 +0200

    #has_an_attribute? on Country model.

--- app/models/country.rb
+++ app/models/country.rb
@@ -3,4 +3,11 @@ class Country < ActiveRecord::Base
 
   has_many :users
 
+  # Helpers
+  def self.has_an_attribute?(attr)
+    User.reflections.include?(attr) ||
+    User.reflections.include?(attr.to_s.pluralize.to_sym) ||
+    User.attribute_names.include?(attr) ||
+    User.attribute_names.include?(attr.to_s.pluralize.to_sym)
+  end
 end
