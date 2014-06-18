e10095830be7f05a0aff7e0f17b783b17b622b89
Veljko Dragsic <veljko@kset.org>
2014-06-18 14:13:26 +0200

    Stackexchange oauth.
    Former-commit-id: 2cee323d3c0344ebd0414afd08201dec09604757

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,7 @@ Devise.setup do |config|
   config.omniauth :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET']
   config.omniauth :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
   config.omniauth :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  config.omniauth :stackexchange, ENV['STACKEXCHANGE_CLIENT_ID'], ENV['STACKEXCHANGE_CLIENT_SECRET'], public_key: ENV['STACKEXCHANGE_CLIENT_KEY'], site: 'stackoverflow'
 
   config.sign_out_via = :delete
 end
