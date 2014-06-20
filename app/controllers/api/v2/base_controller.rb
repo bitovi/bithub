a28ee7aeb7e41637398f92cda6d9c404a7577554
Veljko Dragsic <veljko@kset.org>
2014-06-20 16:59:07 +0200

    Defined basic abilities.
    Former-commit-id: 9155156486f3dc4f53d2a09351ba2b14f1712be2

--- app/controllers/api/v2/base_controller.rb
+++ app/controllers/api/v2/base_controller.rb
@@ -1,4 +1,8 @@
 class Api::V2::BaseController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :show_404
+  rescue_from ActiveRecord::RecordInvalid, with: :show_406
+  rescue_from CanCan::AccessDenied, with: :show_401
+
   respond_to :json
 
   include Api::V2::BaseHelpers
@@ -6,4 +10,20 @@ class Api::V2::BaseController < ActionController::Base
   def home
     render :text => "Bithub API v2"
   end
+
+  # CanCan override:
+  # https://github.com/ryanb/cancan/wiki/changing-defaults
+  def current_ability
+    if account_signed_in?
+      # Brand manager
+      @current_ability ||= AccountAbility.new current_account
+    elsif user_signed_in?
+      # Regular user
+      @current_ability ||= UserAbility.new current_user
+    else
+      # Anonymous
+      @current_ability ||= AnonAbility.new
+    end
+  end
+
 end
---
+++ app/models/user_ability.rb
@@ -0,0 +1,25 @@
+class UserAbility
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, :all
+
+    # if user.has_role? :admin
+    #   can :manage, :all
+    #   can :create_award, Award
+    #   can :manage_roles, User
+    #   can :read_sensitive_data, :user
+    # else
+    #   can :read, :all
+    #   can :create_upvote, Upvote
+    #   can :create_anteup, Anteup
+    #   can :destroy_upvote, Upvote
+    #   cannot :create_award, Award
+    #   cannot :read, Achievement
+    # end
+
+    # can :destroy_user, User do |subject_user|
+    #   user.has_role?(:admin) || user.id == subject_user.id
+    # end
+  end
+end
