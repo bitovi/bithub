1dd4f9b6e3e73ac036c99fd8e232ba4ac85e9e9c
Veljko Dragsic <veljko@kset.org>
2014-06-20 17:16:56 +0200

    Markdown decorator on stackexchange entities.
    Former-commit-id: efa0fab7930bf7ef691b36e80a48dc2e74c8feeb

--- app/domain/entities/feeds/stackexchange/types/answer.rb
+++ app/domain/entities/feeds/stackexchange/types/answer.rb
@@ -10,7 +10,7 @@ module Entities
       def build
         Entity.new({
           title: "answered ##{@payload.question_id}", # @payload.title
-          body: @payload.body_markdown || @payload.body,
+          body: @payload.body_markdown,
           url: @payload.link,
           origin_ts: @payload.creation_date,
           origin_id: @payload.answer_id.to_s,
