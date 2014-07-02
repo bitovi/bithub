a0ee6caca80e6402638d7550af189897fbef088d
Veljko Dragsic <veljko@kset.org>
2014-07-02 19:41:32 +0200

    Subdomain sessions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_filter :redirect_to_subdomain
 
   helper_method :crypter
-
-  def redirect_to_subdomain
-    if params[:acc].present?
-      account = Account.find(crypter.decrypt_and_verify(params[:acc]).to_i)
-      subdomain = account.brand.name
-      if subdomain == request.host.split('.').first
-        sign_in(:account, account)
-        redirect_to "/admin"
-      end
-    end
-  end
 
   def crypter
     key = 'some really long key that we will use for this'
