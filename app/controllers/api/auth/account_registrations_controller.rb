1c9296d27677065de8c51f0780cfd4d3c077a0be
Veljko Dragsic <veljko@kset.org>
2014-07-02 19:41:32 +0200

    Subdomain sessions.
    Former-commit-id: a0ee6caca80e6402638d7550af189897fbef088d

--- app/controllers/api/auth/account_registrations_controller.rb
+++ app/controllers/api/auth/account_registrations_controller.rb
@@ -4,8 +4,7 @@ class Api::Auth::AccountRegistrationsController < Devise::RegistrationsControlle
 
   def after_sign_up_path_for(resource)
     subdomain = resource.brand.name
-
-    "http://#{subdomain}.#{request.host}/login?acc=#{crypter.encrypt_and_sign(resource.id)}"
+    "http://#{subdomain}.#{request.domain}/admin"
   end
 
   def after_inactive_sign_up_path_for(resource)
