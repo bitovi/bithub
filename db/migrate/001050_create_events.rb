f960265395ca53cdc3dabd2da2b5b2024df347f4
Nikica Jokic <neektza@gmail.com>
2014-07-07 18:05:40 +0200

    Event->souce_data column is now of type json.

--- db/migrate/001050_create_events.rb
+++ db/migrate/001050_create_events.rb
@@ -3,9 +3,9 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :type_name, :null => true
       t.string :feed_name, :null => true
-      t.text   :source_data
       t.string :content_digest, :unique => true
       t.hstore :props, default: ''
+      t.json   :source_data
 
       ### one event can produce many 'parent' entities?!
       t.references :entity
