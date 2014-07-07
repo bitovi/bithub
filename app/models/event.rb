86bd1b35212f055a76afd3e8b524ff4ba1e84c7a
Nikica Jokic <neektza@gmail.com>
2014-07-07 18:05:40 +0200

    Event->souce_data column is now of type json.
    Former-commit-id: f960265395ca53cdc3dabd2da2b5b2024df347f4

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ActiveRecord::Base
   belongs_to :entity
 
+  store_accessor :props
+  store_accessor :source_data
+
   validates_presence_of :content_digest
   validates_uniqueness_of :content_digest
 end
