3b8b6c7c35c7c9d73ffdace383631e0105cffa0e
Veljko Dragsic <veljko@kset.org>
2014-07-07 17:45:03 +0200

    PG events.source_data as json type.
    Former-commit-id: 67593791d0d9f85c04610f6d4f12c83afd9a506b

--- db/migrate/001050_create_events.rb
+++ db/migrate/001050_create_events.rb
@@ -3,7 +3,7 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :type_name, :null => true
       t.string :feed_name, :null => true
-      t.text   :source_data
+      t.json   :source_data
       t.string :content_digest, :unique => true
       t.hstore :props, default: ''
 
