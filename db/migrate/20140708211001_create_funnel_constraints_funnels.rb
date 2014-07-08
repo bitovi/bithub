564df89fb8f8f1ac1696d4900f60116476422058
Veljko Dragsic <veljko@kset.org>
2014-07-08 21:13:52 +0200

    Funnel migrations rearranged.

--- db/migrate/20140708211001_create_funnel_constraints_funnels.rb
+++ db/migrate/20140708211001_create_funnel_constraints_funnels.rb
@@ -1,10 +1,8 @@
 class CreateFunnelConstraintsFunnels < ActiveRecord::Migration
-
   def change
     create_table :funnel_constraints_funnels, :id => false do |t|
       t.references :funnel
       t.references :funnel_constraint
     end
   end
-
 end
