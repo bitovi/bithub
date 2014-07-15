5244ee5725ca9d2cf7cca66a875d597f4f7cd870
Veljko Dragsic <veljko@kset.org>
2014-07-15 02:00:54 +0200

    Session keys in redis are namespaced with 'session'.
    Former-commit-id: 00dbf4abf02cecbaa76badc37911782b4487a8de

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,6 @@
 # Be sure to restart your server when you modify this file.
-Bithub::Application.config.session_store :redis_store, :expire_after => 1.month, :domain => ".#{ENV['DOMAIN']}"
+Bithub::Application.config.session_store \
+  :redis_store,
+  :redis_server => "#{ENV['REDIS_URL']}/session",
+  :expire_after => 1.month,
+  :domain => ".#{ENV['DOMAIN']}"
