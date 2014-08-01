34d87b97e54f1d090a8e37a568de18af31b8eb06
Nikica Jokic <neektza@gmail.com>
2014-08-01 17:23:23 +0200

    Digest calculation for follows includes the same class name in both cases.
    Former-commit-id: ef4a354c5a403e35fcfa4faa1c7c072b5b40fafc

--- app/domain/events/feeds/twitter/types/follow.rb
+++ app/domain/events/feeds/twitter/types/follow.rb
@@ -11,7 +11,7 @@ module Events
       def_delegator :@target, :screen_name, :target_screen_name
 
       def digest_seed
-        source_id.to_s + target_id.to_s
+        source_id.to_s + target_id.to_s + self.class.name
       end
 
       def created_at
