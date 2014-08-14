c8d9444093ceb1cf9ea9ecc7f660f036d04b29f7
Nikica Jokic <neektza@gmail.com>
2014-08-14 20:06:42 +0200

    Render template only if format == :html.
    In all other cases render a 404 becuase resource was not previously
    found.
    
    
    Former-commit-id: 8ba5dd0eb12621ef8396eac00d5152897d8693aa

--- app/controllers/kickstart_controller.rb
+++ app/controllers/kickstart_controller.rb
@@ -1,7 +1,13 @@
 class KickstartController < ApplicationController
+
   def admin
   end
 
   def frontend
+    respond_to do |format|
+      format.html { render 'kickstart/frontend' }
+      format.any  { head :not_found }
+    end
   end
+
 end
