88e293206fa1a0ae6b5f08eda274c358591233bb
Nikica Jokic <neektza@gmail.com>
2014-08-15 18:09:06 +0200

    Environment configuration updated.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -7,7 +7,7 @@ Bithub::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
