d2c73c1293ef820b49193d45a0b9eb2446c6d083
Nikica Jokic <neektza@gmail.com>
2014-09-15 10:59:34 +0200

    Tests for AccountManager that make sense written.
    Former-commit-id: bef52d65a9d4a95f5b041e1505d233f022e24d21

--- spec/domain/accounts/account_manager_spec.rb
+++ spec/domain/accounts/account_manager_spec.rb
@@ -1,29 +1,22 @@
+require 'domain/spec_helper'
+
 describe Accounts::AccountManager, :type => :domain do
+  describe "#only_logging_in?" do
+    it "says we're only logging in if there's no current_user" do
+      identity = FactoryGirl.build(:identity, :from_twitter)
+      am = Accounts::AccountManager.new(:twitter, identity, nil)
+      expect(am.only_logging_in?).to be_truthy
+    end
+  end
+  
 
   describe "#linking_or_merging?" do
-    it "determines if we're linking or merging identities/users"
-  end
-  
-  describe "#only_logging_in?" do
-    it "determines wheteher we're only logging (ie. not doing something more complex)"
-  end
-  
-  describe "#procure" do
-    context "when user is already logged_in (current_user exists)" do
-      it "just returns the current_user"
-    end
+    it "says we're linking_or_merging if the provided identity is already associated with the current_user" do
+      current_user = FactoryGirl.build(:user, :with_github_ident)
+      identity = FactoryGirl.build(:identity, :from_twitter)
 
-    context "when user isn't logged in, but exists in the system" do
-      it "looks up the user in the provided identity"
-    end
-
-    context "when the user doesn't exist" do
-      it "delegates creation of the user to AccountCreator"
+      am = Accounts::AccountManager.new(:twitter, identity, current_user)
+      expect(am.linking_or_merging?).to be_truthy
     end
   end
-
-  describe "#link_and_merge" do
-    it "delegates linking or merging of identities/users to AccountLinker"
-  end
-
 end
