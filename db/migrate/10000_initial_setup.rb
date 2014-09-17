c41c8ad91289cdd5c899e0375aa3d459456f70bd
Nikica Jokic <neektza@gmail.com>
2014-09-17 11:49:45 +0200

    Run initial setup for DB only if running on CI.
    Initial setup is adding extensions (hstore, intarray).

--- db/migrate/10000_initial_setup.rb
+++ db/migrate/10000_initial_setup.rb
@@ -1,3 +1,15 @@
 class InitialSetup < ActiveRecord::Migration
-  # postgresql extensions are now created by ansible
+  def up
+    unless ENV['VAGRANT'].present?
+      execute "CREATE EXTENSION IF NOT EXISTS hstore"
+      execute "CREATE EXTENSION IF NOT EXISTS intarray"
+    end
+  end
+
+  def down
+    unless ENV['VAGRANT'].present?
+      execute "DROP EXTENSION IF EXISTS hstore"
+      execute "DROP EXTENSION IF EXISTS intarray"
+    end
+  end
 end
