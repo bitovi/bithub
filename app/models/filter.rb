10ab398fc98fdfa7dbb2dcc94461cfc86b2e4c4f
Nikica Jokic <neektza@gmail.com>
2014-09-24 18:28:57 +0200

    Stripped down filter.
    Removed name, display_name, position and props::hstore.
    We won't need these anymore BAAS (Bithub as a product).

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,9 +1,6 @@
 class Filter < ActiveRecord::Base
 
-  include RankedModel
-  ranks :position
-
-  validates_presence_of :name, :is_conjunctive
+  validates_presence_of :is_conj
 
   has_one :embed_filter, :dependent => :destroy
   has_one :embed, :through => :embed_filter
@@ -12,20 +9,6 @@ class Filter < ActiveRecord::Base
     class_name: "NaturalLanguageQuery",
     foreign_key: "filter_id",
     association_foreign_key: "natural_language_query_id"
-
-  def disabled=(value)
-    self.props_will_change!
-    self.props['disabled'] = (!!value).to_s
-  end
-
-  def disabled
-    disabled = self.props['disabled']
-    if disabled == true or disabled == 'true'
-      return true
-    else
-      return false
-    end
-  end
 
   def covers?(entity)
     check = (constraints.map{|c| c.feed_name}.include?(entity.feed_name)) && (constraints.map {|c| c.type_name}.include?(entity.type_name))
@@ -38,15 +21,11 @@ class Filter < ActiveRecord::Base
   end
 
   def all?
-    is_conjunctive
+    is_con
   end
 
   def any?
-    not(is_conjunctive)
-  end
-
-  def weight
-    (constraints.count * 1) + (tags.count * 10)
+    not(is_conj)
   end
 
 end
--- db/migrate/20140924092534_modify_filters.rb
+++ db/migrate/20140924092534_modify_filters.rb
@@ -1,6 +1,10 @@
 class ModifyFilters < ActiveRecord::Migration
   def change
-    remove_column :filters, :tags, :string, array: true, default: '{}'
-    add_column :filters, :is_conjunctive, :boolean, :default => false
+    remove_column :filters , :props        , :hstore  , default: ''
+    remove_column :filters , :tags         , :string  , array: true , default: '{}'
+    remove_column :filters , :name         , :string  , default: ''
+    remove_column :filters , :display_name , :string  , default: ''
+    remove_column :filters , :position     , :integer , default: 0
+    add_column :filters, :is_conj, :boolean, :default => true
   end
 end
