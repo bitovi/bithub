652425ceb20f690f0ce928f090fa4582ce62e457
Nikica Jokic <neektza@gmail.com>
2014-09-24 18:48:47 +0200

    Removed Pagination completely.
    We won't have a big "you can see everything here" page any more so we
    don't need pagination statistics. We'll implement something else for
    each embed, but it will be much simpler as each embed knows how many
    entities it contains.
    
    
    Former-commit-id: 741e06332f87bc0eb209c0d85cf665e2e3885b74

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -81,9 +81,6 @@ class Entity < ActiveRecord::Base
   after_create :adopt_references_from_children
   after_destroy :decrease_score_in_author
 
-  after_save :update_pagination_table
-  after_destroy :update_pagination_table
-
   after_validation :reformat_uniqueness_validation
 
 
@@ -235,10 +232,6 @@ class Entity < ActiveRecord::Base
 
   def source_data
     last_modified_by.andand.source_data
-  end
-
-  def update_pagination_table
-    Pagination.refresh
   end
 
   def cache_key
