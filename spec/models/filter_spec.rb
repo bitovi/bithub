f17e3ff94d35a328b973ad3bcdbc36aefd174a79
Nikica Jokic <neektza@gmail.com>
2014-09-26 12:19:18 +0200

    Structure of Filter changed.
    It now delegates query combination to NatlangQueryQueryCombinator.
    Filter is only a persistence class.
    
    
    Former-commit-id: 0fd94393f438eafa3abfe7068ebc3588f42b6ba1

---
+++ spec/models/filter_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require 'models/filter'
+
+RSpec.describe Filter, :type => :model do
+
+  describe '#any?' do
+    it 'tells whether the queries are conjunctive' do
+      pending('todo')
+    end
+
+    it 'tells whether the queries are disjunctive' do
+      pending('todo')
+    end
+  end
+end
