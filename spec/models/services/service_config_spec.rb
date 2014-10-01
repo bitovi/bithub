ee7a93529c155f8ac78670b0bcb918c4667ae59b
Nikica Jokic <neektza@gmail.com>
2014-10-01 13:41:47 +0200

    Extracted ServiceConfigValidator from Service.
    It knows how to validate config from all feeds.

---
+++ spec/models/services/service_config_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'models/services/service_config_validator'
+
+describe ServiceConfigValidator do
+  
+  describe 'has?' do
+    it 'checks if the config is a Hash' do
+      scv = ServiceConfigValidator.new([:a_key, :a_value])
+      expect(scv.has?(:a_key)).to be_falsey
+    end
+
+    it 'checks if hash has the key' do
+      scv = ServiceConfigValidator.new({:no_key => :no_value})
+      expect(scv.has?(:a_key)).to be_falsey
+    end
+
+    it 'checks if the key is non-empty' do
+      scv = ServiceConfigValidator.new({:a_key => ""})
+      expect(scv.has?(:a_key)).to be_falsey
+    end
+  end
+
+end
