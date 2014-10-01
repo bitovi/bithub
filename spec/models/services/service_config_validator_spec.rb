ca06e69bcd42a7702cb2dd6f7f6ade4688f13779
Nikica Jokic <neektza@gmail.com>
2014-10-01 16:38:25 +0200

    Puts ServiceConfigValidator in Services module.
    Former-commit-id: 96304c7c4f46662f6feab314fcc486c09824e2ec

--- spec/models/services/service_config_validator_spec.rb
+++ spec/models/services/service_config_validator_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 require 'models/services/service_config_validator'
 
-describe ServiceConfigValidator do
+describe Services::ServiceConfigValidator do
   
   describe '#has?' do
     it 'checks if the config is a Hash' do
-      scv = ServiceConfigValidator.new([:a_key, :a_value], 'doesn_matter')
+      scv = Services::ServiceConfigValidator.new([:a_key, :a_value], 'doesn_matter')
       expect(scv.has?(:a_key)).to be_falsey
     end
 
     it 'checks if hash has the key' do
-      scv = ServiceConfigValidator.new({:no_key => :no_value}, 'doesn_matter')
+      scv = Services::ServiceConfigValidator.new({:no_key => :no_value}, 'doesn_matter')
       expect(scv.has?(:a_key)).to be_falsey
     end
 
     it 'checks if the key is non-empty' do
-      scv = ServiceConfigValidator.new({:a_key => ""}, 'doesn_matter')
+      scv = Services::ServiceConfigValidator.new({:a_key => ""}, 'doesn_matter')
       expect(scv.has?(:a_key)).to be_falsey
     end
   end
