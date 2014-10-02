41f6ceeb1092d300a82cb0c281a0def5f80b87ed
Veljko Dragsic <veljko@bitovi.com>
2014-10-02 02:33:19 +0200

    Tumblr domain code for wrapper and event.
    Former-commit-id: 82324ba601bbf17169c9956b1b361e4c49a8b60e

---
+++ app/models/events/tumblr/post.rb
@@ -0,0 +1,29 @@
+module Events
+  module Tumblr
+
+    class Post < Protocol
+      extend Forwardable
+
+      def_delegators :@post, :blog_name, :post_url, :type, :timestamp, :tags, :source_url, :source_title
+      alias_method :link, :post_url
+
+      def id
+        @post.id.to_s
+      end
+
+      def digest_seed
+        id.to_s + self.class.name
+      end
+
+      def created_at
+        Time.at timestamp.to_i
+      end
+
+      def wrap_response
+        @post ||= Wrappers::Tumblr::Post.new source_data
+        self
+      end
+    end
+
+  end
+end
