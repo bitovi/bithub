28a9e3651f13165bee15afe98fbffa61e2691d04
Veljko Dragsic <veljko@bitovi.com>
2014-10-07 15:40:03 +0200

    oAuth config cleanup.
    - id/secret namings
    - removed oauth callbacks for regular users
    
    
    Former-commit-id: 2711f6774151d1097f344d61882f044e891b128c

--- app/controllers/api/auth/omniauth_callbacks_controller.rb
+++ app/controllers/api/auth/omniauth_callbacks_controller.rb
@@ -3,71 +3,11 @@ class Api::Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksControll
   # rescue_from Exception, :with => :show_auth_error
   # rescue_from RuntimeError, :with => :show_auth_error
 
-  def github
-    oauthorize "github"
-  end
+  PROVIDERS = %w(github, twitter, meetup, stackexchange, facebook, disqus, foursquare, instagram, tumbler)
 
-  def github_brand
-    oauthorize_brand "github"
+  PROVIDERS.each do |provider|
+    define_method(provider) { oauthorize(provider) }
   end
-
-  def twitter
-    oauthorize "twitter"
-  end
-
-  def twitter_brand
-    oauthorize_brand "twitter"
-  end
-
-  def meetup
-    oauthorize "meetup"
-  end
-
-  def meetup_brand
-    oauthorize_brand "meetup"
-  end
-
-  def stackexchange
-    oauthorize "stackexchange"
-  end
-
-  def stackexchange_brand
-    oauthorize_brand "stackexchange"
-  end
-
-  def facebook
-    oauthorize "facebook"
-  end
-
-  def facebook_brand
-    oauthorize_brand "facebook"
-  end
-
-  def disqus
-    oauthorize "disqus"
-  end
-
-  def disqus_brand
-    oauthorize_brand "disqus"
-  end
-
-  def foursquare
-    oauthorize "foursquare"
-  end
-
-  def foursquare_brand
-    oauthorize_brand "foursquare"
-  end
-
-  def instagram_brand
-    oauthorize_brand "instagram"
-  end
-
-  def tumblr_brand
-    oauthorize_brand "tumblr"
-  end
-
-  ###
 
   def show_auth_error
     render :template => 'oauth/auth_error.html.erb'
@@ -77,64 +17,21 @@ class Api::Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksControll
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
   end
 
-  def link_identities
-    @identity = Identity.find_or_create_with_oauth_data(oauth_data)
-    Accounts::AccountLinker.new(current_user, @identity).determine_state.link
-    render :template => 'special/close_oauth_popup.html'
-  end
-
   private
 
-  def oauthorize(kind)
-    @sites = HashWithIndifferentAccess.new({
-      github: 'GitHub',
-      twitter: 'Twitter',
-      meetup: 'Meetup',
-      stackexchange: 'StackExchange',
-      facebook: 'Facebook',
-      disqus: 'Disqus',
-      foursquare: 'Foursquare',
-      instagram: 'Instagram',
-      tumblr: 'Tumblr'
-    })
-
-    @identity = Identity.find_or_init_with_oauth_data(oauth_data)
-    @manager = Accounts::AccountManager.new(kind, @identity, current_user)
-    @manager.linker.determine_state
-
-    if @manager.linking_or_merging?
-      session["devise.#{kind.downcase}_data"] = oauth_data
-      session["current_oauth_data"] = oauth_data
-      render :template => "oauth/account_linker.html.erb", :layout => false
-
-    elsif @manager.only_logging_in?
-      if (user = @manager.procure)
-        session["devise.#{kind.downcase}_data"] = oauth_data
-        sign_in user, :event => :authentication
-
-        # join user to current brand
-        user.join_brand(current_brand) if current_brand
-
-        render :template => 'special/close_oauth_popup.html'
-      else
-        render :json => { message: 'error' }, :status => 500
-      end
-    end
-  end
-
-  def oauthorize_brand(kind)
+  def oauthorize(provider)
     account = Account.find(current_account[:id])
     brand = account.brand
     uid = oauth_data[:uid].to_s
 
     # Build identity source_data
-    source_data = Identities::Builders.const_get(kind.camel_case).new({oauth: oauth_data}).build
+    source_data = Identities::Builders.const_get(provider.camel_case).new({oauth: oauth_data}).build
 
     # Find or update brand
-    if (identity = brand.identities.where(provider: kind, uid: uid).first)
+    if identity = brand.identities.where(provider: provider, uid: uid).first
       identity.source_data = source_data
     else
-      identity = brand.identities.build({provider: kind, uid: oauth_data[:uid], source_data: source_data})
+      identity = brand.identities.build({provider: provider, uid: oauth_data[:uid], source_data: source_data})
     end
 
     if identity.save
@@ -145,14 +42,8 @@ class Api::Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksControll
     end
   end
 
-  private
-
   def oauth_data
-    env["omniauth.auth"] || session["current_oauth_data"]
-  end
-
-  def current_brand
-    @brand ||= Brand.where({tenant_name: request.subdomain}).first
+    env["omniauth.auth"] # || session["current_oauth_data"]
   end
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,40 +9,45 @@ require 'omniauth-disqus'
 Devise.setup do |config|
   config.secret_key = '2aa637d41eb2b387b2bb19211702de5bafd653317bc8df921ffa1279c6f73c81418e49aa1fea745a22f322439311b9cd4b92320a12cadab54a6069b4763272f5'
 
-  # meetup
-  config.omniauth :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET']
-  config.omniauth :meetup_brand, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET']
+  config.omniauth :meetup,
+    ENV['MEETUP_CLIENT_ID'],
+    ENV['MEETUP_CLIENT_SECRET']
 
-  # github
-  config.omniauth :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
-  config.omniauth :github_brand, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: "user:email,read:org"
+  config.omniauth :github,
+    ENV['GITHUB_CLIENT_ID'],
+    ENV['GITHUB_CLIENT_SECRET'],
+    scope: "user:email,read:org"
 
-  # twitter
-  config.omniauth :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
-  config.omniauth :twitter_brand, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  config.omniauth :twitter,
+    ENV['TWITTER_CLIENT_ID'],
+    ENV['TWITTER_CLIENT_SECRET']
 
-  # stackexchange
-  config.omniauth :stackexchange, ENV['STACKEXCHANGE_CLIENT_ID'], ENV['STACKEXCHANGE_CLIENT_SECRET'], public_key: ENV['STACKEXCHANGE_CLIENT_KEY'], site: 'stackoverflow'
-  config.omniauth :stackexchange_brand, ENV['STACKEXCHANGE_CLIENT_ID'], ENV['STACKEXCHANGE_CLIENT_SECRET'], public_key: ENV['STACKEXCHANGE_CLIENT_KEY'], site: 'stackoverflow'
+  config.omniauth :stackexchange,
+    ENV['STACKEXCHANGE_CLIENT_ID'],
+    ENV['STACKEXCHANGE_CLIENT_SECRET'],
+    public_key: ENV['STACKEXCHANGE_CLIENT_KEY'],
+    site: 'stackoverflow'
 
-  # disqus
-  config.omniauth :disqus, ENV['DISQUS_KEY'], ENV['DISQUS_SECRET']
-  config.omniauth :disqus_brand, ENV['DISQUS_KEY'], ENV['DISQUS_SECRET']
+  config.omniauth :disqus,
+    ENV['DISQUS_CLIENT_ID'],
+    ENV['DISQUS_CLIENT_SECRET']
 
-  # facebook
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :scope => 'email,read_stream'
-  config.omniauth :facebook_brand, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :scope => 'email,manage_pages'
+  config.omniauth :facebook,
+    ENV['FACEBOOK_CLIENT_ID'],
+    ENV['FACEBOOK_CLIENT_SECRET'],
+    :scope => 'email,manage_pages'
 
-  # foursquare
-  config.omniauth :foursquare, ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET']
-  config.omniauth :foursquare_brand, ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_SECRET']
+  config.omniauth :foursquare,
+    ENV['FOURSQUARE_CLIENT_ID'],
+    ENV['FOURSQUARE_CLIENT_SECRET']
 
-  # instagram
-  config.omniauth :instagram, ENV['INSTAGRAM_CLIENT_ID'], ENV['INSTAGRAM_CLIENT_SECRET']
-  config.omniauth :instagram_brand, ENV['INSTAGRAM_CLIENT_ID'], ENV['INSTAGRAM_CLIENT_SECRET']
+  config.omniauth :instagram,
+    ENV['INSTAGRAM_CLIENT_ID'],
+    ENV['INSTAGRAM_CLIENT_SECRET']
 
-  # tumblr
-  config.omniauth :tumblr_brand, ENV['TUMBLR_CONSUMER_KEY'], ENV['TUMBLR_CONSUMER_SECRET']
+  config.omniauth :tumblr,
+    ENV['TUMBLR_CLIENT_ID'],
+    ENV['TUMBLR_CLIENT_SECRET']
 
   config.sign_out_via = [:delete, :get]
 end
