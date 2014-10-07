68b39ff9ff2b4e4299c69bb15d6e7fc60b9750ee
Nikica Jokic <neektza@gmail.com>
2014-10-07 13:30:37 +0200

    Removed service creating after-hook from brand-id.

--- app/models/brand_identity.rb
+++ app/models/brand_identity.rb
@@ -7,15 +7,4 @@ class BrandIdentity < ActiveRecord::Base
   end
   alias_attribute :provider_name, :provider
 
-  # TODO design is about INTENT, no domain code in after_hooks
-  # def create_feed_config
-  #   if self.brand && self.provider
-  #     feed_name = self.provider.gsub('_brand','')
-
-  #     self.brand.feed_configs.create({
-  #       feed_name: self.provider.gsub('_brand',''),
-  #       config: {}
-  #     }) unless self.brand.feed_configs.find_by_feed_name(feed_name)
-  #   end
-  # end
 end
