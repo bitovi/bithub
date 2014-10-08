6bd262c1066dccc983b1161966b76254f6fc1f2e
Nikica Jokic <neektza@gmail.com>
2014-10-08 13:39:19 +0200

    Revert "Removed AmqpHelper, CrawlerNotifer replaced it."
    This reverts commit bff6f131068251cfa667d08aa0fc571245956ac7 [formerly 4dc74d29efed939175146be71ac187489c84ff9f].
    
    
    Former-commit-id: 92768d8978e5ab576361da1852368ea606165218

---
+++ lib/amqp_helpers.rb
@@ -0,0 +1,35 @@
+require 'multi_json'
+
+module AmqpHelpers
+
+  def rabbit(args = {})
+    @conn = Bunny.new(rabbitmq_uri).start
+    ch = @conn.create_channel
+
+    exchange_type = args.fetch(:exchange_type) { 'direct' }
+    exchange_name = args.fetch(:exchange_name) { '' }
+    exchange_opts = args.fetch(:exchange_opts) { Hash.new }
+
+    @x = ch.send(exchange_type, *[exchange_name, exchange_opts])
+    self
+  end
+
+  def publish(msg, rk)
+    returning @x.publish(MultiJson.dump(msg), :routing_key => rk) do
+      @conn.close
+    end
+  end
+
+  def exchange
+    @x
+  end
+
+  def rabbitmq_uri
+    ENV.fetch('RABBITMQ_URI') { "amqp://bithub:Ei7PhaaH@localhost/bithub" }
+  end
+
+  def returning(exp)
+    yield
+    exp
+  end
+end
