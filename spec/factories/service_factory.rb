a37fb35e990acad9fb7860576cd22a31c1b76e60
Nikica Jokic <neektza@gmail.com>
2014-10-10 13:33:30 +0200

    Service FactoryGirl factory rubocop applied.
    Former-commit-id: dac467436f658b1e363a67944d882869d3af644c

--- spec/factories/service_factory.rb
+++ spec/factories/service_factory.rb
@@ -1,37 +1,48 @@
 FactoryGirl.define do
 
   factory :service do
-    brand_name 'neektza'
-    brand
-
     factory :twitter_service do
       feed_name 'twitter'
-      config Hash["terms", ["canjs", "javascriptmvc"]]
+      config Hash['terms', %w(canjs javascriptmvc)]
     end
 
     factory :facebook_service do
       feed_name 'facebook'
-      config Hash["pages", [{ "id"=>"487939194652299", "name"=>"Koryu Bujutsu Zagreb", "access_token"=>"SomeToken123"}]]
+      config Hash['pages', [
+        { 'id' => '487939194652299',
+          'name' => 'Koryu Bujutsu Zagreb',
+          'access_token' => 'SomeToken123'
+      }]]
     end
 
     factory :meetup_service do
-      feed_name "meetup"
-      config Hash["groups", [{"id"=>"10878382", "name"=>"ZgElixir"}, {"id"=>"13140052", "name"=>"rubyzg"}], "terms", ["neektza"]]
+      feed_name 'meetup'
+      config Hash['groups', [
+        { 'id' => '10878382', 'name' => 'ZgElixir' },
+        { 'id' => '13140052', 'name' => 'rubyzg' }
+      ], 'terms', ['neektza']]
     end
-    
+
     factory :disqus_service do
       feed_name 'disqus'
-      config Hash["forums", [{"id"=>"pltconfusion", "name"=>"PLT Confusion"}]]
+      config Hash['forums', [
+        { 'id' => 'pltconfusion', 'name' => 'PLT Confusion' }
+      ]]
     end
 
     factory :stackexchange_service do
-      feed_name "stackexchange"
-      config Hash["tags", ["neektza"]]
+      feed_name 'stackexchange'
+      config Hash['tags', ['neektza']]
     end
-    
+
     factory :github_service do
       feed_name 'github'
-      config Hash["repos", ["neektza/twitter", "neektza/rmeetup"], "orgs", ["KSET"]]
+      config Hash[
+        'repos',
+        %w(neektza/twitter neektza/rmeetup),
+        'orgs',
+        ['KSET']
+      ]
     end
 
   end
