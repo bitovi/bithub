40156f69ae0ff743936434a392bf568f1e98389c
Nikica Jokic <neektza@gmail.com>
2014-10-10 16:16:58 +0200

    Few template request spec to fill out added.
    Former-commit-id: c3a23d2202548244e0e4c58fc775647d08e55954

--- spec/requests/brand_registration_spec.rb
+++ spec/requests/brand_registration_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Account registration', type: :request do
-  it 'creates the brand if an account is being created without an invite'
-  it 'creates only the account if an account is being created through an invite'
+  it 'creates an account, a default embed and a template for a service'
+  it 'creates the brand along with the account if account is being registered'
+  it 'doesn\'t create the brand if the account is being invited'
 end
---
+++ spec/requests/embed_creation_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+Rspec.describe 'Embed creation', type: :request do
+  context 'creating an embed with a blocking and a moderating filter' do
+    it 'responds with the embed along with the filters the user built'
+  end
+end
