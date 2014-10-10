ab4598bf8de8121d887a9152acea0f45d903f73a
Veljko Dragsic <veljko@bitovi.com>
2014-10-10 21:01:03 +0200

    Updated abilities determination in BaseController.
    Removed references to current_user and UserAbility.

--- app/controllers/api/v2/base_controller.rb
+++ app/controllers/api/v2/base_controller.rb
@@ -14,18 +14,9 @@ class Api::V2::BaseController < ActionController::Base
   # CanCan override:
   # https://github.com/ryanb/cancan/wiki/changing-defaults
   def current_ability
-
-    # give user an admin role if account is already signed in
-    current_user.add_role :admin if account_signed_in? and user_signed_in?
-
-    if account_signed_in? && on_subdomain?
-      # Brand manager
-      @current_ability ||= AccountAbility.new current_account, current_user
-    elsif user_signed_in?
-      # Regular user
-      @current_ability ||= UserAbility.new current_user
+    if account_signed_in?
+      @current_ability ||= AccountAbility.new current_account
     else
-      # Anonymous
       @current_ability ||= AnonAbility.new
     end
   end
@@ -34,20 +25,6 @@ class Api::V2::BaseController < ActionController::Base
   def authenticate!
     if account_signed_in?
       :authenticate_account!
-    elsif user_signed_in?
-      :authenticate_user!
-    end
-  end
-
-  def is_admin?
-    current_account && current_account.has_role?(:admin)
-  end
-
-  def on_subdomain?
-    if is_admin?
-      true
-    else
-      current_account.brand && (current_account.brand.tenant_name == request.subdomain)
     end
   end
 
