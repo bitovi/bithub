cc18ae249f1860daa1ba58bb67f199e1fe0755de
Veljko Dragsic <veljko@bitovi.com>
2014-10-10 21:04:15 +0200

    New Brand is created during new registration.
    Overrided Devise #create method, upon registrating new account belonging
    brand is created as well.

--- app/controllers/api/auth/account_registrations_controller.rb
+++ app/controllers/api/auth/account_registrations_controller.rb
@@ -1,18 +1,19 @@
 class Api::Auth::AccountRegistrationsController < Devise::RegistrationsController
+
   def create
-    super do |acc|
-      Brands::BrandBuilder.new(acc).build.save
+    super do |account|
+      Brands::BrandBuilder.new(account).build.save
     end
   end
 
   protected
 
   def after_sign_up_path_for(resource)
-    subdomain = resource.brand.tenant_name
-    "http://#{subdomain}.#{request.domain}/admin"
+    '/admin'
   end
 
   def after_inactive_sign_up_path_for(resource)
     '/admin'
   end
+
 end
