d3bf809ac000f790fee787c89876ba5ddaf6e920
Nikica Jokic <neektza@gmail.com>
2014-10-10 12:29:17 +0200

    Create Brand after truncation in rails_helper.
    If other way around, then brand gets deleted.
    
    
    Former-commit-id: 0f1b859f6f3d8e58f7909e699d9a1ecfb7badfbe

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,10 +13,9 @@ RSpec.configure do |config|
     Celluloid.boot
 
     Apartment::Database.drop('testy') rescue nil
-    Brand.create name: 'testy', tenant_name: 'testy'
-
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with :truncation, except: %w(tags scoring_rules)
+    Brand.create name: 'testy', tenant_name: 'testy'
   end
 
   config.after(:suite) do
