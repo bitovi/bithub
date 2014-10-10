9fee78f4918921f44ac4c46e1beb14d57255d117
Nikica Jokic <neektza@gmail.com>
2014-10-10 17:47:35 +0200

    Typo in spec fixed.

--- spec/requests/embed_creation_spec.rb
+++ spec/requests/embed_creation_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rspec.describe 'Embed creation', type: :request do
+RSpec.describe 'Embed creation', type: :request do
   context 'creating an embed with a blocking and a moderating filter' do
     it 'responds with the embed along with the filters the user built'
   end
