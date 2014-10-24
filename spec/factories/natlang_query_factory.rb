a303d9168af43c311580729e86086d2d65521e27
Nikica Jokic <neektza@gmail.com>
2014-10-24 02:01:09 +0200

    Additional factory NatlangQuery fixture factories.
    Former-commit-id: d6d225c62d5422461b73647a30a7d0fb52aa813a

--- spec/factories/natlang_query_factory.rb
+++ spec/factories/natlang_query_factory.rb
@@ -1,9 +1,17 @@
 FactoryGirl.define do
 
   factory :natlang_query do
-    attr "content"
-    op "contains"
-    val "some bug"
+    trait :contains_canjs do
+      attr 'content'
+      op 'contains'
+      val 'canjs'
+    end
+
+    trait :is_from_github do
+      attr 'feed_name'
+      op 'is'
+      val 'github'
+    end
   end
 
 end
