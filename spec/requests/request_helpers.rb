d44d77bfed02573f827baf55052c45b5534f5c51
Nikica Jokic <neektza@gmail.com>
2014-10-27 14:05:45 +0100

    Support code for OAuth testing.

--- spec/requests/request_helpers.rb
+++ spec/requests/request_helpers.rb
@@ -1,16 +1,43 @@
-def account_registration_data
-  {
+OmniAuth.config.test_mode = true
+
+OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+  'provider' => 'twitter',
+  'uid' => '123545',
+  'user_info' => {
+    'name' => 'mockuser',
+    'image' => 'mock_user_thumbnail_url'
+  },
+  'credentials' => {
+    'token' => 'mock_token',
+    'secret' => 'mock_secret'
+  }
+})
+
+OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+  'provider' => 'github',
+  'uid' => '545123',
+  'user_info' => {
+    'name' => 'mockuser',
+    'image' => 'mock_user_thumbnail_url'
+  },
+  'credentials' => {
+    'token' => 'mock_token',
+    'secret' => 'mock_secret'
+  }
+})
+
+module AuthTestData
+  ACCOUNT_REGISTRATION_DATA = {
     name: 'neektza',
     email: 'neektza@gmail.com',
     password: 'foobar123',
     password_confirmation: 'foobar123'
   }
-end
 
-def account_login_data
-  {
+  ACCOUNT_LOGIN_DATA = {
     name: 'neektza',
     password: 'foobar123',
     remember_me: '0'
   }
 end
+
