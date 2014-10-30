52059574b48a786e68c3747ae74158f458d8c74f
Nikica Jokic <neektza@gmail.com>
2014-10-31 00:30:26 +0100

    Request specs for filter, service and embed endpoints.

--- spec/requests/embeds_endpoint_spec.rb
+++ spec/requests/embeds_endpoint_spec.rb
@@ -18,33 +18,43 @@ RSpec.describe 'Embed endpoints', type: :request do
     @current_brand = Brand.where(name: 'neektza').first
   end
 
-  describe 'GET /embeds' do
-    it 'responds with all embeds (for current brand)' do
-      Apartment::Database.switch('neektza')
-      embeds = FactoryGirl.create_list(:embed, 10, brand: @current_brand)
+  context 'given the account is logged in and the brand is determined' do
+    describe 'GET /embeds' do
+      it 'responds with all embeds' do
+        Apartment::Database.switch('neektza')
+        embeds = FactoryGirl.create_list(:embed, 10, brand: @current_brand)
 
-      get "/api/#{api_version}/embeds"
-      expect(response).to be_success
-      expect(json.length).to eq(embeds.length)
+        get "/api/#{api_version}/embeds"
+        expect(response).to be_success
+        expect(json.length).to eq(embeds.length)
+      end
     end
-  end
 
-  describe 'GET /embeds/:id' do
-    it 'responds with an embed' do
-      Apartment::Database.switch('neektza')
-      FactoryGirl.create(:embed, brand: @current_brand)
+    describe 'GET /embeds/:id' do
+      it 'responds with a specific embed' do
+        Apartment::Database.switch('neektza')
+        FactoryGirl.create(:embed, brand: @current_brand)
 
-      get "/api/#{api_version}/embeds/1"
-      expect(response).to be_success
-      expect(json).to have_keys(%w(name colorscheme layout))
+        get "/api/#{api_version}/embeds/1"
+        expect(response).to be_success
+        expect(json).to have_keys(%w(name colorscheme layout))
+      end
     end
-  end
 
-  describe 'POST /embeds' do
-    it 'creates a new embed for the current brand' do
-      post "/api/#{api_version}/embeds", { embed: embed_creation_data }
-      expect(response).to be_success
-      expect(json).to have_keys(%w(name colorscheme layout))
+    describe 'POST /embeds' do
+      it 'creates a new embed' do
+        post "/api/#{api_version}/embeds", { embed: embed_creation_data }
+        expect(response).to be_success
+        expect(json).to have_keys(%w(name colorscheme layout))
+      end
+    end
+
+    describe 'DELETE /embeds/:id' do
+      it 'destroys an existing embed' do
+        embed = FactoryGirl.create(:embed, brand: @current_brand)
+        delete "/api/v3/embeds/#{embed.id}"
+        expect(Service.count).to eq 0
+      end
     end
   end
 end
--- spec/requests/filters_endpoint_spec.rb
+++ spec/requests/filters_endpoint_spec.rb
@@ -2,18 +2,76 @@ require 'rails_helper'
 require_relative 'request_helpers'
 
 RSpec.describe 'Filter endpoints', type: :request do
+  let(:api_version) { 'v3' }
+
   before(:each) do
     post '/register', { account: AuthTestData::ACCOUNT_REGISTRATION_DATA }
     post '/login', { account: AuthTestData::ACCOUNT_LOGIN_DATA }
     @current_brand = Brand.where(name: 'neektza').first
   end
 
-  describe 'GET /filters' do
-    it 'gets all filters for current brand'
-  end
+  context 'given the account is logged in and the brand is determined' do
+    context 'and given a certain embed the filter belongs to' do
 
-  describe 'POST /filters' do
-    it 'creates a filter for the current brand'
+      describe 'GET /embed/1/filters' do
+        it 'gets all filters' do
+          embed = FactoryGirl.create(:embed, brand: @current_brand)
+          embed.filters.create!(classification: 'moderating')
+          embed.filters.create!(classification: 'blocking')
+
+          get "/api/#{api_version}/embeds/#{embed.id}/filters"
+          expect(response).to be_success
+          expect(json['data'].length).to eq(2)
+        end
+      end
+
+      describe 'GET /embeds/1/filters/1' do
+        it 'gets a specific filter' do
+          embed = FactoryGirl.create(:embed, brand: @current_brand)
+          embed.filters.create(classification: 'moderating')
+          embed.filters.create(classification: 'blocking')
+
+          get "/api/#{api_version}/embeds/#{embed.id}/filters/1"
+          expect(response).to be_success
+          expect(json.keys).to include('is_conj', 'classification')
+        end
+      end
+
+      describe 'POST /embeds/1/filters' do
+        it 'creates a filter' do
+          embed = FactoryGirl.create(:embed, brand: @current_brand)
+
+          post "/api/#{api_version}/embeds/#{embed.id}/filters", {
+            filter: {
+              is_conj: true,
+              classification: 'moderating',
+              natlang_queries: [{
+                is_negated: false,
+                attr: 'content',
+                op: 'contains',
+                val: 'canjs'
+              }, {
+                is_negated: true,
+                attr: '',
+                op: 'tagged_with',
+                val: 'canjs'
+              }]
+            }
+          }.to_json, AuthTestData::POST_HEADERS
+
+          expect(json.keys).to include('is_conj', 'classification', 'queries')
+        end
+      end
+
+      describe 'DELETE /filters/:id' do
+        it 'destroys an existing embed' do
+          embed = FactoryGirl.create(:embed, brand: @current_brand)
+          filter = embed.filters.create(classification: 'moderating')
+
+          delete "/api/#{api_version}/embeds/#{embed.id}/filters/#{filter.id}"
+          expect(Filter.count).to eq 0
+        end
+      end
+    end
   end
 end
-
