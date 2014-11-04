54f4fb9f55d6e4e4d5d0767b13d7eca847d47f14
Veljko Dragsic <veljko@bitovi.com>
2014-11-04 15:59:26 +0100

    Subscription model.
    Holds billing info for certain brand, like current plan, CC info and
    stripe subscription/customer ids. (1-1 rel. with brand).
    
    This is the place where it should be checked if brand is on end of
    trail period, active or past due.
    
    Upon creating new customer #new_from_customer methods should be used to
    create initial subscription.
    Also listens for stripe 'customer.subscription_updated' webhook to keep
    subscription status in sync.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,6 +4,50 @@ class Subscription < ActiveRecord::Base
   belongs_to :brand
 
   after_customer_subscription_updated! do |subscription, event|
-    # do something
+    self.update_from_subscription(subscription, event_id: event.id)
   end
+
+  def self.new_from_customer(customer, brand, opts)
+    card = customer.cards.data.first
+    subscription = customer.subscriptions.data.first
+    plan = subscription.plan
+
+    attrs = {
+      brand_id: brand.id,
+      plan_id: plan.id,
+
+      stripe_customer_id: customer.id,
+      stripe_subscription_id: subscription.id,
+      stripe_subscription_status: subscription.status,
+
+      # card_token: opts[:card_token], # do we need this?
+      card_exp_month: card.exp_month,
+      card_exp_year: card.exp_year,
+      card_type: card.brand,
+      card_last4: card.last4
+    }
+
+    self.new(attrs)
+  end
+
+  def self.update_from_subscription(subscription, opts={})
+    attrs = {
+      plan_id: subscription.plan.id,
+      stripe_event_id: opts[:event_id],
+      stripe_subscription_status: subscription.status,
+    }
+
+    if existing = self.find_by_customer_id(subscription.customer)
+      unless existing.update_attributes(attrs)
+        Rails.logger.warn "[Stripe Webhook] Updating subscription '#{subscription.id}' failed! \n#{existing.errors.messages}"
+      end
+    else
+      Rails.logger.warn "[Stripe Webhook] Subscription '#{subscription.id}' not found!"
+    end
+  end
+
+  def self.find_by_customer_id(id)
+    where(stripe_customer_id: id).order(:created_at).last
+  end
+
 end
