02fbb5849702434d8bb8318f36d241862125e477
Veljko Dragsic <veljko@bitovi.com>
2014-11-04 16:04:48 +0100

    Payment model.
    Listens for stripe's 'invoice.payment_succeeded' webhook.
    
    When new invoice is created and payment is successful, stripe sends
    notif via webhook. Invoice data (plan, date, ids, amount) are saved as
    Payment.
    
    Payment should be never updated, it kind-of copy of stripe's log so that
    we can display billing history to the brand manager.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,6 +4,34 @@ class Payment < ActiveRecord::Base
   belongs_to :brand
 
   after_invoice_payment_succeeded! do |invoice, event|
-    # do something
+    new_invoice =  self.new_from_invoice(invoice, event_id: event.id)
+
+    unless new_invoice.save
+      Rails.logger.warn "[Stripe Webhook] Saving invoice '#{invoice.id}' failed! \n#{new_invoice.errors.messages}"
+    end
+  end
+
+  def self.new_from_invoice(invoice, opts={})
+    brand = Subscription.find_by_customer_id(invoice.customer).brand
+    plan  = invoice.lines.data.first.plan
+
+    attrs = {
+      total: invoice.total,
+      currency: invoice.currency,
+
+      plan_id: plan.id,
+
+      period_start: Time.at(invoice.period_start),
+      period_end: Time.at(invoice.period_end),
+
+      stripe_event_id: opts[:event_id],
+      stripe_invoice_id: invoice.id,
+      stripe_customer_id: invoice.customer,
+      stripe_subscription_id: invoice.subscription,
+
+      brand_id: brand.id
+    }
+
+    self.new(attrs)
   end
 end
