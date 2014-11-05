39aec4a335fc10cb0981adfab12de67011f1af10
Veljko Dragsic <veljko@bitovi.com>
2014-11-06 00:11:30 +0100

    Subscription is created upon registration.
    New Subscription is created when building new brand via BrandBuilder.
    Subscription is refrenced to newly created brand and Stripe customer is created.

--- config/stripe/plans.rb
+++ config/stripe/plans.rb
@@ -1,10 +1,19 @@
 # Run `rake stripe:prepare` to update plans on stripe.com
 
-Stripe.plan :primo do |plan|
+Stripe.plan :starter do |plan|
   plan.name = 'Starter'
   plan.amount = 1000 # in cents
   plan.currency = 'usd'
   plan.interval = 'month'
   plan.interval_count = 1
-  plan.trial_period_days = 30
+  plan.trial_period_days = 45
+end
+
+Stripe.plan :advanced do |plan|
+  plan.name = 'Advanced'
+  plan.amount = 4500
+  plan.currency = 'usd'
+  plan.interval = 'month'
+  plan.interval_count = 1
+  plan.trial_period_days = 45
 end
