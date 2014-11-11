5bcbe78afccfaa96c40ec6f081d4a0348f4155a4
Nikica Jokic <neektza@gmail.com>
2014-11-12 00:21:13 +0100

    SQL injection fix for Fork entity.

--- app/models/entities/feeds/github/types/fork.rb
+++ app/models/entities/feeds/github/types/fork.rb
@@ -1,14 +1,12 @@
 module Entities
   module Github
-
     class Fork < Protocol
-
       def find
         @event.event_id && find_by_actor_and_repo_name.first
       end
 
       def build
-        Entity.new({
+        Entity.new(
           title: "forked #{@event.repo.name}",
           origin_ts: @event.created_at,
           origin_id: @event.fork_id.to_s,
@@ -16,27 +14,25 @@ module Entities
             origin_author_id: @event.actor.id,
             origin_author_name: @event.actor.login,
             origin_author_avatar_url: @event.actor.avatar_url,
-            repo_name: @event.repo.name,
+            repo_name: @event.repo.name
           }
-        })
+        )
       end
 
       def find_by_origin_id
         Entity
-        .feed('github')
-        .type('fork')
-        .where(origin_id: @event.fork_id.to_s)
+          .feed('github')
+          .type('fork')
+          .where(origin_id: @event.fork_id.to_s)
       end
 
       def find_by_actor_and_repo_name
         Entity
-        .feed('github')
-        .type('fork')
-        .where("props -> 'origin_author_id' = '#{@event.actor.id}'")
-        .where("props -> 'repo_name' = '#{@event.repo.name}'")
+          .feed('github')
+          .type('fork')
+          .where("props -> 'origin_author_id' = :actor_id", actor_id: @event.actor.id)
+          .where("props -> 'repo_name' = :repo_name", repo_name: @event.repo.name)
       end
-
     end
-
   end
 end
