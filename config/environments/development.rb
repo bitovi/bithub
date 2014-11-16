c2fe0836f0d910fb37e9d521e9d5803617ee9895
Veljko Dragsic <veljko@bitovi.com>
2014-11-16 13:04:01 +0100

    Plan model.
    Plans are defined in config/stripe/plans.yml and imported on
    db:recreate.
    
    Run `rake stripe:prepare` to update Stripe API with new plans.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,7 @@ Bithub::Application.configure do
 
 
   # Don't care if the mailer can't send
-  config.action_mailer.default_url_options = { host: "bithub.dev" }
+  config.action_mailer.default_url_options = { host: "bithub.loc" }
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
