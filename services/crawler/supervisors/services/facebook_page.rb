53e9246bdd4594682dea87a9f011c69ab21eb317
Nikica Jokic <neektza@gmail.com>
2014-11-21 00:38:48 +0100

    Converted Facebook feed to FacebookPage service.

---
+++ services/crawler/supervisors/services/facebook_page.rb
@@ -0,0 +1,29 @@
+require 'koala'
+
+module Supervisors
+  module Services
+    class FacebookPage < Supervisors::Service
+      def boot
+        @endpoints = SupervisionGroup.new
+
+        fetcher = Fetchers::Facebook::PageFeed.new(
+          ::Koala::Facebook::API.new(page_token))
+
+        @endpoints.supervise_as(
+          @path.child_actor_name(page_id),
+          Poller, *[
+            @path,
+            fetcher,
+            {interval: 60}
+          ]
+        )
+      end
+
+      private
+
+      def page_id
+        service_config.fetch(:id)
+      end
+    end
+  end
+end
