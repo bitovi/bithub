33de772df17a7a79dce49a4f7f4b3fe9a7ba7edc
Nikica Jokic <neektza@gmail.com>
2014-11-21 16:59:33 +0100

    Reorganization of some services.
    Former-commit-id: 202941c878a394b84f1dddf43bb71eb869060632

--- services/crawler/supervisors/services/disqus_forum.rb
+++ services/crawler/supervisors/services/disqus_forum.rb
@@ -1,31 +1,30 @@
 require 'supervisors/service'
 
-module Supervisors
-  module Services
-    class DisqusForum < Supervisors::Service
-      def boot
-        fetcher = Fetchers::Disqus::Comments.new(
-          api_key: api_key, forum: forum_url)
+module Supervisors::Services::Disqus
+  class Forum < Supervisors::Service
+    def boot
+      @endpoints = SupervisionGroup.new
 
-        @endpoints.supervise_as(
-          @path.child_actor_name(forum_url),
-          Poller, *[
-            @path,
-            fetcher,
-            {interval: 60}
-          ]
-        )
-      end
+      fetcher = Fetchers::Disqus::Comments.new(
+        api_key: api_key, forum: forum_url)
 
-      private
+      @endpoints.supervise_as(
+        @path.child_actor_name("forum_#{forum_url}"),
+        Poller, *[
+          @path,
+          fetcher,
+          {interval: 60}
+        ])
+    end
 
-      def forum_url
-        config.fetch(:url)
-      end
+    private
 
-      def api_key
-        static_config.fetch(:disqus).fetch(:api_key)
-      end
+    def forum_url
+      config.fetch(:url)
+    end
+
+    def api_key
+      static_config.fetch(:disqus).fetch(:api_key)
     end
   end
 end
---
+++ services/crawler/supervisors/services/github/org.rb
@@ -0,0 +1,26 @@
+module Supervisors::Services::Github
+  class Org < Supervisors::Service
+    include Supervisors::Services::Github::Common
+
+    def boot
+      @endpoints = SupervisionGroup.new
+
+      org_fetcher = Fetchers::Github::OrgActivity.new(
+        client, { org_name: org_name })
+
+      @endpoints.supervise_as(
+        @path.child_actor_name("org_#{org_name}"),
+        Poller, *[
+          @path,
+          org_fetcher,
+          {interval: 60}
+        ])
+    end
+
+    private
+
+    def org_name
+      service_config.fetch(:name)
+    end
+  end
+end
