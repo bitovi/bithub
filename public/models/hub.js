85d70ff334f05f998e27b0f8eaf5c8ef0824b502
Mihael Konjevic <konjevic@gmail.com>
2014-12-01 16:39:45 +0100

    Editting of the hub name
    Former-commit-id: b6be78317ded96b255e51eae6e24c80adb1ca078

--- public/models/hub.js
+++ public/models/hub.js
@@ -1,7 +1,11 @@
-steal('can/model', 'can/list/promise', function(Model){
+steal('can/model', 'can/list/promise', 'can/construct/super', 'can/map/backup', function(Model){
 	return Model.extend({
 		resource : '/api/v3/embeds'
 	}, {
-		
+		serialize : function(){
+			return {
+				embed : this._super.apply(this, arguments)
+			}
+		}
 	});
 })
