03fbf1fc3337610886f3b3a8bb2f323ee4f53dd3
Mihael Konjevic <konjevic@gmail.com>
2014-12-07 17:07:47 +0100

    Add expand / collapse services on the hub list page

--- public/models/hub.js
+++ public/models/hub.js
@@ -1,7 +1,21 @@
-steal('can/model', 'can/list/promise', 'can/construct/super', 'can/map/backup', function(Model){
+steal(
+'can/model',
+'./service.js',
+'can/list/promise',
+'can/construct/super',
+'can/map/backup',
+'can/map/define',
+function(Model, ServiceModel){
 	return Model.extend({
 		resource : '/api/v3/embeds'
 	}, {
+		define : {
+			services : {
+				get : function(){
+					return new ServiceModel.List({embed_id: this.attr('id')})
+				}
+			}
+		},
 		serialize : function(){
 			return {
 				embed : this._super.apply(this, arguments)
