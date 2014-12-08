353cb7a484539947bcc48065ee0de404627d48f3
Nikica Jokic <neektza@gmail.com>
2014-12-08 13:08:13 +0100

    Catching rate limiting Github exception in fetcher
    Former-commit-id: 390ac769eee1733d31af1c7bb71f8aef5bbebe7f

--- services/crawler/fetchers/github/repo_activity.rb
+++ services/crawler/fetchers/github/repo_activity.rb
@@ -1,3 +1,5 @@
+require 'github_api'
+
 module Fetchers
   module Github
 
@@ -11,6 +13,9 @@ module Fetchers
 
       def fetch
         @client.activity.events.repos @user, @repo
+      rescue ::Github::Error::Forbidden => e
+        Celluloid.logger.error "Github::RepoActivity fetcher error: #{e}"
+        nil
       end
     end
   end
