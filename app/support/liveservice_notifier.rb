379d2cda0f86db38fdeb09b236c7f4a7192251e3
Veljko Dragsic <veljko@bitovi.com>
2014-12-11 18:13:13 +0100

    Liveservice amqp autodelete set to true.
    Former-commit-id: d73c94124a76cc0e36f7f0ccb8ffd98e4c732943

--- app/support/liveservice_notifier.rb
+++ app/support/liveservice_notifier.rb
@@ -4,7 +4,7 @@ module Support
 
     def notif(msg, rk)
       Rails.logger.info "Publishing command #{msg}"
-      rabbit(exchange_name: 'x.liveservice').publish(msg, rk)
+      rabbit(exchange_name: 'x.liveservice', exchange_opts: {auto_delete: true}).publish(msg, rk)
     end
   end
 end
