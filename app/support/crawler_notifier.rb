3913a3e1b2c0b738ac5d2a8a9947caf56f102cdd
Veljko Dragsic <veljko@bitovi.com>
2014-12-11 17:22:37 +0100

    Liveservice notifier excluded to separate file.
    Former-commit-id: 708ed4868e601b8276c1a6771a3cfd2f5ab44a6a

--- app/support/crawler_notifier.rb
+++ app/support/crawler_notifier.rb
@@ -1,20 +1,10 @@
 module Support
-
   class CrawlerNotifier
     include AmqpHelpers
 
     def notif(msg)
       Rails.logger.info "Publishing command #{msg}"
       rabbit(exchange_name: 'x.crawler').publish(msg, :config)
-    end
-  end
-
-  class LiveserviceNotifier
-    include AmqpHelpers
-
-    def notif(msg, rk)
-      Rails.logger.info "Publishing command #{msg}"
-      rabbit(exchange_name: 'x.liveservice').publish(msg, rk)
     end
   end
 end
