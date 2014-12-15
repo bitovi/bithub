dd3f5011cb566a9880620ef8d754c15b43c0a580
Nikica Jokic <neektza@gmail.com>
2014-12-15 15:29:49 +0100

    Remember service for entities coming through.

--- app/models/embed_entity.rb
+++ app/models/embed_entity.rb
@@ -17,4 +17,3 @@ class EmbedEntity < ActiveRecord::Base
     destroy
   end
 end
-
--- services/crawler/publisher.rb
+++ services/crawler/publisher.rb
@@ -66,6 +66,7 @@ class Publisher
           type_name: dispatched.type_name.snake_case,
           brand_id: owner_data.brand.id,
           embed_id: owner_data.embed.id,
+          service_id: owner_data.service.id,
           brand_name: owner_data.brand.name,
           embed_name: owner_data.embed.name,
           feed_name: feed
