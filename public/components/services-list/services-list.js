df6ee5230f9c8a0f821a5c0f66e0abb3c1f0570e
Mihael Konjevic <konjevic@gmail.com>
2014-12-18 16:52:20 +0100

    Work on the services form and list

--- public/components/services-list/services-list.js
+++ public/components/services-list/services-list.js
@@ -6,26 +6,6 @@ steal(
 'can/map/define',
 function(Component, Models, initView){
 
-	var formatKey = function(key){
-		if(key === 'url') return 'URL';
-		return can.capitalize(key.replace(/_/g, ' '));
-	}
-
-	var formatConfig = function(config){
-		var res = ['<ul class="config">'];
-		for(var k in config){
-			res.push('<li><b>' + formatKey(k) + '</b>: ');
-			if(can.isPlainObject(config[k])){
-				res.push(formatConfig(config[k]));
-			} else {
-				res.push(config[k]);
-			}
-			res.push('</li>');
-		}
-		res.push('</ul>')
-		return res.join('');
-	}
-
 	return Component.extend({
 		tag : 'bh-services-list',
 		template : initView,
@@ -41,13 +21,8 @@ function(Component, Models, initView){
 		},
 		helpers : {
 			isCurrentService : function(service, opts){
-				console.log(this.attr())
 				service = can.isFunction(service) ? service() : service;
 				return service === this.attr('currentService') ? opts.fn(opts.scope.add(service)) : opts.inverse(opts.scope.add(service));
-			},
-			formatConfig : function(config){
-				config = can.isFunction(config) ? config() : config;
-				return formatConfig(config.attr());
 			}
 		}
 	});
