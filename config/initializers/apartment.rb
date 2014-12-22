5f8abf1e89e15d018f34beb8bc9dad0a066aac45
Veljko Dragsic <veljko@bitovi.com>
2014-12-22 17:51:13 +0100

    Apartmen elevator checks if tenant exists.
    Former-commit-id: f2484e88878e2721c947381b7cf50fdf7a8c69ef

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -2,8 +2,6 @@
 require 'rake'
 
 require 'apartment/elevators/generic'
-# require 'apartment/elevators/domain'
-# require 'apartment/elevators/subdomain'
 
 Apartment.configure do |config|
   config.excluded_models = %w(Brand BrandIdentity Account AccountsBrand AccountRole AccountsAccountRole User BrandsUser Country Subscription Payment StripeWebhooksLog Plan)
@@ -15,8 +13,7 @@ Apartment.configure do |config|
 end
 
 Rails.application.config.middleware.use 'Apartment::Elevators::Generic', lambda { |request|
-  request.session['tenant_name']
-}
+  tenant_name = request.session['tenant_name']
 
-# Rails.application.config.middleware.use 'Apartment::Elevators::Domain'
-# Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
+  Apartment.tenant_names.include?(tenant_name) ? tenant_name : nil
+}
