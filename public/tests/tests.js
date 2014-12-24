5f848710e3447fc4bf2d571bc36595421c08fe01
Mihael Konjevic <konjevic@gmail.com>
2014-12-24 16:44:39 +0100

    Show tumblr images

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -5,8 +5,15 @@ steal(
 'components/tag-list/tag-list-tests.js',
 './smoketest.js',
 function(){
+	window.location.hash = "";
 	can.route.ready(true);
 	can.fixture.on = true;
 	QUnit.config.reorder = false;
+
+
+	if(window.Testee) {
+		window.Testee.init();
+	}
+
 	QUnit.start();
 });
