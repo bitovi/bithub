1e75cc3f05950f0341164b31a23cf4092ce0a582
Nikica Jokic <neektza@gmail.com>
2015-01-08 17:21:59 +0100

    Revert to public schema if no schema found in initializer.
    Former-commit-id: 96cec1395820a1d82a3b1fe98062e4907c9bd6ce

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -3,17 +3,19 @@ require 'rake'
 
 require 'apartment/elevators/generic'
 
+DEFAULT_BITHUB_TENANT = 'public'
+
 Apartment.configure do |config|
   config.excluded_models = %w(Brand BrandIdentity Account AccountsBrand AccountRole AccountsAccountRole User BrandsUser Country Subscription Payment StripeWebhooksLog Plan)
   config.use_schemas = true
   config.use_sql = true
   config.tenant_names = -> { Brand.pluck :tenant_name }
-  config.persistent_schemas = %w( public )
-  config.default_schema = 'public'
+  config.persistent_schemas = [ DEFAULT_BITHUB_TENANT ]
+  config.default_schema = DEFAULT_BITHUB_TENANT
 end
 
 Rails.application.config.middleware.use 'Apartment::Elevators::Generic', lambda { |request|
   tenant_name = request.session['tenant_name']
 
-  Apartment.tenant_names.include?(tenant_name) ? tenant_name : nil
+  Apartment.tenant_names.include?(tenant_name) ? tenant_name : DEFAULT_BITHUB_TENANT
 }
