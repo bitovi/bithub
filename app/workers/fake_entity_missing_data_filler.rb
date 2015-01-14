2be8e9f5c12bd792ea604410ef60b5a1835bc9d6
Veljko Dragsic <veljko@bitovi.com>
2015-01-14 14:22:59 +0100

    Apartment #switch made deprecated, using #switch!

--- app/workers/fake_entity_missing_data_filler.rb
+++ app/workers/fake_entity_missing_data_filler.rb
@@ -7,16 +7,16 @@ module Workers
 
     def perform
       Brand.pluck(:name).each do |name|
-        Apartment::Tenant.switch name
-        fff = ::Entities::Services::FakeFollowFiller.new
+        Apartment::Tenant.switch name do
+          fff = ::Entities::Services::FakeFollowFiller.new
 
-        if fff.user_ids_with_missing_names.count >= 50
-          fff.fill_missing
-        else
-          Rails.logger.info "Not enough empty Follow entities to init processing"
+          if fff.user_ids_with_missing_names.count >= 50
+            fff.fill_missing
+          else
+            Rails.logger.info "Not enough empty Follow entities to init processing"
+          end
         end
       end
-      Apartment::Tenant.switch 'public'
     end
   end
 end
