629d2bc8276614e37740eec88484720ef62c44ac
Veljko Dragsic <veljko@bitovi.com>
2015-01-14 14:13:21 +0100

    Instagram preloading media.
    On supervisor boot.

--- services/crawler/supervisors/services/instagram/base.rb
+++ services/crawler/supervisors/services/instagram/base.rb
@@ -1,5 +1,6 @@
 require 'instagram'
 require 'active_support/core_ext/string'
+require 'supervisors/support/owner_data'
 
 module Supervisors::Services::Instagram
   class Base < Supervisors::Service
@@ -15,6 +16,10 @@ module Supervisors::Services::Instagram
       rescue ::Instagram::Error => e
         Celluloid.logger.info "Instagram subscription failed with #{e.message}"
       end
+
+      if self.respond_to? :preload, true
+        publish preload service_config
+      end
     end
 
     def token
@@ -22,6 +27,24 @@ module Supervisors::Services::Instagram
     end
 
     private
+
+    def subscribe; raise NotImplementedError; end
+    def preload; raise NotImplementedError; end
+
+    def publish(items)
+      Actor[:publisher].publish owner_data, items
+    end
+
+    def owner_data
+      OwnerData.new\
+        @path.brand.id,
+        @path.brand.name,
+        @path.embed.id,
+        @path.embed.name,
+        @path.service.id,
+        'instagram',
+        'media_event'
+    end
 
     def delete_subscriptions
       client.subscriptions.each do |sub|
--- services/crawler/supervisors/services/instagram/user.rb
+++ services/crawler/supervisors/services/instagram/user.rb
@@ -7,5 +7,9 @@ module Supervisors::Services::Instagram
       client.create_subscription object: "user", callback_url: callback_url, aspect: "media"
     end
 
+    def preload(params)
+      Fetchers::Instagram::UserRecentMedia.fetch params.fetch(:id), count: 100
+    end
+
   end
 end
