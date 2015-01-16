1898f1b67bec721cd6925039d9f683c8e3b87470
Mihael Konjevic <konjevic@gmail.com>
2015-01-16 16:36:47 +0100

    Bits are not columnized; Bits are paginated
    Former-commit-id: 605bd1f097f26ad740847c868c1d752300f78a89

--- public/bits/bits.js
+++ public/bits/bits.js
@@ -6,17 +6,78 @@ steal(
 'can/map/define',
 'components/service-loader',
 'bit',
+'can/construct/proxy',
 function(Component, initView, Models){
 	return Component.extend({
 		tag : 'bh-bits',
 		template : initView,
 		scope : {
+			loading : false,
+			hasNextPage : true,
 			init : function(){
 				var self = this;
-				Models.Bit.findAll({hubId: can.route.attr('hubId')}).then(function(data){
-					var bits = self.attr('bits');
-					bits.unshift.apply(bits, data);
+
+				this.attr('params', {
+					offset: 0,
+					limit: 50
 				});
+
+				this.loadData();
+			},
+			getParams : function(){
+				var hubId = can.route.attr('hubId');
+				var params = this.attr('params').attr();
+
+				params.hubId = hubId;
+
+				return params;
+			},
+			loadData : function(){
+				var self = this;
+
+				this.attr('loading', true);
+
+				Models.Bit.findAll(this.getParams()).then(function(data){
+					var bits = self.attr('bits');
+
+					can.batch.start();
+
+					bits.push.apply(bits, data);
+					self.attr('loading', false);
+
+					if(data.length < self.attr('params.limit')){
+						self.attr('hasNextPage', false);
+					}
+
+					can.batch.stop();
+				});
+			},
+			nextPage : function(){
+				var params;
+				if(!this.attr('loading') && this.attr('hasNextPage')){
+					params = this.attr('params');
+					params.attr('offset', params.attr('offset') + params.attr('limit'));
+					this.loadData();
+				}
+			}
+		},
+		events : {
+			inserted : function(){
+				this.element.on('scroll', this.proxy('appendContent'));
+			},
+			appendContent : function(){
+				var self = this;
+				clearTimeout(this.__appendContentTimeout);
+				this.__appendContentTimeout = setTimeout(function(){
+					var scrollTop = self.element.scrollTop();
+					var scrollHeight = self.element.prop('scrollHeight');
+					var height = self.element.height();
+
+					(scrollHeight - scrollTop - height < 300) && self.scope.nextPage();
+				}, 100);
+			},
+			removed : function(){
+				this.element.off('scroll');
 			}
 		}
 	})
