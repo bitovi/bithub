30e425b2ff2ba851b41912466cbd7fe83a1924f1
Veljko Dragsic <veljko@bitovi.com>
2015-01-18 18:51:44 +0100

    Preparation for systemd.
    - foreman updated to support export to systemd
    - capistrano deploy task updated
    - staging server is temporary using server_migration branch
    
    
    Former-commit-id: 1030d73cf47ed48a906c4c836d9fd44b21bb6aa2

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,4 @@
 server 'staging.bithub.com', user: fetch(:user), roles: %w{app db}
-set :branch, 'master'
+set :branch, 'server_migration'
 
 set :log_level, :debug
-
-set :rbenv_custom_path, '/opt/rbenv'
-set :rbenv_type, :system
-set :rbenv_ruby, '2.1.2'
