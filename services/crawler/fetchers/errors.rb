838d7a38c1e35c284f6525d166bf5f75192830cc
Nikica Jokic <neektza@gmail.com>
2015-01-26 20:34:11 +0100

    Fetchers updated:
    Github issues and pull requests fetching.
    Twitter followers fixed.

--- services/crawler/fetchers/errors.rb
+++
@@ -1,6 +0,0 @@
-class ServiceError < StandardError; end
-
-class ConfigError < ServiceError; end
-class AuthError < ServiceError; end
-class RemoteError < ServiceError; end
-class UnknownError < ServiceError; end
