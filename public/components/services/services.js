6605a404b52bce3c45c8cec28345976c5c97458c
Mihael Konjevic <konjevic@gmail.com>
2015-02-03 16:10:12 +0100

    Redesign the sidebar and services panel

--- public/components/services/services.js
+++ public/components/services/services.js
@@ -40,41 +40,6 @@ function(Component, Models, initView){
 				this.attr('currentService', Models.Service.createEmptyService(feed));
 			}
 		},
-		events : {
-			'.scroll-left mouseenter' : function(){
-				this.__scrollDirection = 'left';
-				this.scrollPicker();
-			},
-			'.scroll-right mouseenter' : function(){
-				this.__scrollDirection = 'right';
-				this.scrollPicker();
-			},
-			scrollPicker : function(){
-				var servicesEl = this.element.find('.new-service-picker'),
-					self = this,
-					marginLeft = parseInt(servicesEl.css('margin-left'), 10),
-					containerWidth = this.element.find('.service-slider-wrap').innerWidth() - 1,
-					width = servicesEl.outerWidth() - 2;
-
-				if(this.__scrollDirection === 'left' && marginLeft < 0){
-					this.__scrollTimeout = setTimeout(function(){
-						servicesEl.css('margin-left', marginLeft + 2);
-						self.scrollPicker();
-					}, 4);
-				}
-
-				if(this.__scrollDirection === 'right' && Math.abs(marginLeft) + containerWidth < width){
-					this.__scrollTimeout = setTimeout(function(){
-						servicesEl.css('margin-left', marginLeft - 2);
-						self.scrollPicker();
-					}, 4);
-				}
-			},
-			'.scroll-left, .scroll-right mouseleave' : function(){
-				clearTimeout(this.__scrollTimeout);
-				delete this.__scrollDirection;
-			}
-		},
 		helpers : {
 			currentServiceIsNewAndHasFeedName : function(feedName, opts){
 				var currentService = this.attr('currentService');
