dfcfd0d5da0154d132f5eadbfe3a3a12a2af3f96
Nikica Jokic <neektza@gmail.com>
2015-02-05 16:52:18 +0100

    Publish clear_service_errors COMMAND to x.web.
    Published when fetching in Poller succeeds.

--- services/crawler/publishers/notification_publisher.rb
+++ services/crawler/publishers/notification_publisher.rb
@@ -8,11 +8,21 @@ class NotificationPublisher
     Celluloid.logger.info 'Initializing Notification publisher'
 
     rf = RabbitFactory.new(ConnectionManager.instance.rabbit)
-    @x = rf.x('x.liveservice', :direct)
-    @q = rf.q('q.liveservice.services').bind(@x, routing_key: 'services')
+    @x_frontend = rf.x('x.liveservice', :direct)
+    @q_frontend = rf.q('q.liveservice.services').bind(@x_frontend, routing_key: 'services')
+
+    @x_backend = rf.x('x.web', :direct)
+    @q_backend = rf.q('q.web.commands').bind(@x_backend, routing_key: 'commands')
   end
 
-  def publish(notif)
-    @x.publish(notif.to_json, routing_key: 'services')
+  def publish_to_frontend(notif)
+    Celluloid.logger.info "Publishing COMMAND #{notif.fetch(:payload)} to frontend"
+    @x_frontend.publish(notif.to_json, routing_key: 'services')
+  end
+  alias_method :publish, :publish_to_frontend
+
+  def publish_to_backend(notif)
+    Celluloid.logger.info "Publishing COMMAND #{notif.fetch(:payload)} to backend"
+    @x_backend.publish(notif.to_json, routing_key: 'commands')
   end
 end
