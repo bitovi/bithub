4926331b65da706799065ae9b929d5b0f0f5c677
Nikica Jokic <neektza@gmail.com>
2015-02-06 15:28:12 +0100

    Initial fetch feature:
    Initial fetch fetches with pagination (for Github 10 pages back).
    Every other fetch fetches 1 page.

--- services/crawler/poller.rb
+++ services/crawler/poller.rb
@@ -14,26 +14,38 @@ class Poller
 
     @lock_ttl = opts.fetch(:interval) { 3600 }
     @timer = every(HEARTBEAT_INTERVAL) { fetch }
+
     fetch
   end
   attr_reader :fetcher, :lock_ttl
 
   def fetch
-    if !locker.nil? && !locker.locked?(LockInfo.new(lock_name, nil))
+    # Initial fetch crawls back X pages
+    if (@fetcher.respond_to?(:initial_fetch)) && (!locker.nil? && !locker.locked?(LockInfo.new(initial_fetch_lock_name)))
+      locker.lock(LockInfo.new(initial_fetch_lock_name, :infinity))
+      locker.lock(LockInfo.new(lock_name, lock_ttl))
+      if (events = @fetcher.initial_fetch)
+        handle_new_events(events)
+      end
+    elsif !locker.nil? && !locker.locked?(LockInfo.new lock_name)
       locker.lock(LockInfo.new(lock_name, lock_ttl))
       if (events = @fetcher.fetch)
-        Celluloid.logger.info "#{fetcher_name} for brand '#{@path.brand.name}', fetched #{events.count} events"
-        if events.count > 0
-          publish events
-        else
-          notification_publisher.publish(empty_response_notif)
-        end
-        notification_publisher.publish_to_backend(clear_service_errors_notif)
-        notification_publisher.publish_to_frontend(clear_service_errors_notif)
+        handle_new_events(events)
       end
     end
   rescue => e
     error_publisher.publish(e, @path)
+  end
+
+  def handle_new_events(events)
+    Celluloid.logger.info "#{fetcher_name} for brand '#{@path.brand.name}', fetched #{events.count} Events"
+    if events.count > 0
+      publish events
+    else
+      notification_publisher.publish(empty_response_notif)
+    end
+    notification_publisher.publish_to_backend(clear_service_errors_notif)
+    notification_publisher.publish_to_frontend(clear_service_errors_notif)
   end
 
   def publish(data)
@@ -78,6 +90,10 @@ class Poller
   
   def lock_name
     "lock:polling:brand/#{@path.brand.id}:embed/#{@path.embed.id}:service/#{@path.service.id}"
+  end
+
+  def initial_fetch_lock_name
+    "lock:polling:initial_fetch:brand/#{@path.brand.id}:embed/#{@path.embed.id}:service/#{@path.service.id}"
   end
 
   def fetcher_name
