1c0a6bf54f85c3c65b7b3905d179d9b89213025b
Nikica Jokic <neektza@gmail.com>
2015-02-10 16:55:42 +0100

    EmbedEntity#is_pinned is false by default.

--- db/migrate/20150203150337_add_is_pinned_to_embed_entities.rb
+++ db/migrate/20150203150337_add_is_pinned_to_embed_entities.rb
@@ -1,5 +1,5 @@
 class AddIsPinnedToEmbedEntities < ActiveRecord::Migration
   def change
-    add_column :embed_entities, :is_pinned, :boolean, default: false
+    add_column :embed_entities, :is_pinned, :boolean, null: false, default: false
   end
 end
