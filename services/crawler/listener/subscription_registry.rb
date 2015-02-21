b7bd682ffb7fcc2f82b72aa0fc610db8271a9e42
Veljko Dragsic <veljko@bitovi.com>
2015-02-21 20:31:12 +0100

    Removed #handle_msg from subscriptions registry.
    Registrating will be handled from service supervisors.

--- services/crawler/listener/subscription_registry.rb
+++ services/crawler/listener/subscription_registry.rb
@@ -25,29 +25,4 @@ class SubscriptionRegistry
     key = SubscriptionKey.new feed, type, id
     @subscriptions[key]
   end
-
-  def handle_message(msg)
-    action     = msg.fetch :action
-    service    = msg.fetch :service
-    owner_data = build_owner_data_from_message msg
-
-    if action == :start
-      subscribe service[:feed_name], service[:type_name], service[:config][:id], owner_data
-    elsif action == :stop
-      unsubscribe service[:feed_name], service[:type_name], service[:config][:id], owner_data
-    else
-      # handle restart :/
-    end
-  end
-
-  private
-
-  def build_owner_data_from_message(msg)
-    b = msg.fetch :brand
-    e = msg.fetch :embed
-    s = msg.fetch :service
-
-    OwnerData.new b[:id], b[:name], e[:id], e[:name], s[:id], s[:feed_name], s[:type_name]
-  end
-
 end
