0e0b817d53f8caf20c609ce5249711b93539e843
Veljko Dragsic <veljko@bitovi.com>
2015-02-26 12:32:51 +0100

    Mandrill as mailing service.
    Former-commit-id: efed6a3425f95803c8dda96a8c6839eca05b1c2b

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,5 @@
+require './lib/logger_factory'
+
 Bithub::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -21,9 +23,6 @@ Bithub::Application.configure do
   # Use a different cache store
   config.cache_store   = :redis_store, "#{ENV['REDIS_URL']}/cache", { expires_in: 7.days }
 
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
-
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
@@ -31,24 +30,25 @@ Bithub::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
+  # Logging with log4r
+  lf = LoggerFactory.new 'rails', :environment => Rails.env
+  config.logger = lf.component_logger
+  config.action_controller.logger = lf.ac_logger
+  config.active_record.logger = lf.ar_logger
+  config.log_level = :info
 
-  # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  # Mandrill as default mailer
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :enable_starttls_auto => true,
+    :user_name => ENV['MANDRILL_USERNAME'],
+    :password  => ENV['MANDRILL_API_KEY'],
+    :authentication => 'login',
+    :domain => 'staging.bithub.com'
+  }
+  config.action_mailer.default_url_options = { host: "staging.bithub.com" }
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
@@ -63,9 +63,6 @@ Bithub::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  # required by heroku: http://guides.rubyonrails.org/asset_pipeline.html#precompiling-assets
-  config.assets.initialize_on_precompile = false
 
   config.eager_load = true
 end
