efff06f428018bf103e462964df21db85b4fa90f
Nikica Jokic <neektza@gmail.com>
2015-03-02 15:21:39 +0100

    OwnerData promoted from Struct to Class.
    Former-commit-id: db8831f5f6cba0b022222cc00c4e071b8243e519

--- services/crawler/supervisors/owner_data.rb
+++ services/crawler/supervisors/owner_data.rb
@@ -1,17 +1,24 @@
 require 'supervisors/node_types'
 
-class OwnerData < Struct.new(:brand, :embed, :service)
+class OwnerData
 
-  def initialize(brand_id, brand_name, embed_id, embed_name, service_id, service_feed, service_type)
+  def initialize(
+    brand_id,
+    brand_name,
+    embed_id,
+    embed_name,
+    service_id,
+    service_feed,
+    service_type
+  )
 
-    brand   = BrandInfo.new brand_id.to_i, brand_name
-    embed   = EmbedInfo.new embed_id.to_i, embed_name
-    service = ServiceInfo.new service_id.to_i, service_feed, service_type
-
-    super brand, embed, service
+    @brand   = BrandInfo.new brand_id.to_i, brand_name
+    @embed   = EmbedInfo.new embed_id.to_i, embed_name
+    @service = ServiceInfo.new service_id.to_i, service_feed, service_type
   end
+  attr_reader :brand, :embed, :service
 
   def to_s
-    "#{brand} / #{embed} / #{service}"
+    "#{@brand} / #{@embed} / #{@service}"
   end
 end
