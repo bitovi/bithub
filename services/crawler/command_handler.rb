954a06b930684f2bdee7a362a07cb4bc5e912283
Nikica Jokic <neektza@gmail.com>
2015-03-03 19:26:47 +0100

    Reworked command handling.
    CommandHandler now only handles commands if the Supervisor::Main is
    started (if there are interested parties).
    
    Otherwise, some messages end up in /dev/null.
    
    
    Former-commit-id: 58386f7319b4bff7c8a3f317c328978f3eb1d5aa

--- services/crawler/command_handler.rb
+++ services/crawler/command_handler.rb
@@ -5,30 +5,49 @@ class CommandHandler
   include Celluloid
   include Celluloid::Logger
 
+  RETRY_INTERVAL = 5
+
   def initialize(opts={})
+    @consumer_name = opts.fetch(:consumer_name)
     @receiver_name = opts.fetch(:receiver_name) { :main }
 
-    rf = RabbitFactory.new(ConnectionManager.instance.rabbit)
+    rf = RabbitFactory.new(@chan = ConnectionManager.instance.rabbit)
     @x = rf.x('x.crawler', :direct)
-    @q = rf.q('q.poller.commands').bind(@x, :routing_key => 'config')
+    @q = rf.q("q.crawler.#{@consumer_name}.commands").bind(@x, :routing_key => 'config')
 
-    listen
+    wait_for_receiver
+  end
+
+  def wait_for_receiver
+    if receiver && receiver.booted?
+      @q.purge # Main supervisor just booted, so we have a fresh state of the world.
+      listen
+    else
+      after(RETRY_INTERVAL) { wait_for_receiver }
+    end
+  end
+
+  def cancel_consumer_and_wait
+    info "CommandHandler unsubscribing, waiting for receiver '#{shard_name}'"
+    @consumer.cancel
+    wait_for_receiver
   end
 
   def listen
-    info "Initializing Commander"
-    @q.subscribe do |delivery_info, properties, payload|
+    info "CommandHandler now listening for commands"
+    @consumer = @q.subscribe do |delivery_info, properties, payload|
       msg = JSON.parse(payload).symbolize_keys
-      dispatch_command msg
+
+      if receiver && receiver.booted?
+        receiver.handle_msg(msg)
+      else
+        cancel_consumer_and_wait
+      end
     end
   end
 
-  def dispatch_command(msg)
-    if receiver
-      receiver.handle_message msg
-    else
-      error "Unable to dispatch command #{msg}"
-    end
+  def shard_name
+    "#{@consumer_name}>#{@receiver_name}"
   end
 
   def receiver
