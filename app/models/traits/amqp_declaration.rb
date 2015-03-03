660eb637c079ca9b109e87d5cd887c57f8610b8d
Nikica Jokic <neektza@gmail.com>
2015-03-03 19:41:07 +0100

    ConnectionManager supports channel sharing.
    When using AMQP declaration trait in app/models, that shared channel
    is used because otherwise, each publish action creates a new channel.
    
    
    Former-commit-id: b0219cee607ad2da19c18ba709e19598a30449c2

--- app/models/traits/amqp_declaration.rb
+++ app/models/traits/amqp_declaration.rb
@@ -2,11 +2,11 @@ module Traits
   module AmqpDeclaration
 
     def x(x_name)
-      @x ||= RabbitFactory.new(ConnectionManager.instance.rabbit).x(x_name)
+      @x ||= RabbitFactory.new(ConnectionManager.instance.shared_rabbit).x(x_name)
     end
 
     def q(q_name)
-      @q ||= RabbitFactory.new(ConnectionManager.instance.rabbit).q(q_name)
+      @q ||= RabbitFactory.new(ConnectionManager.instance.shared_rabbit).q(q_name)
     end
 
   end
