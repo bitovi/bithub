0d93b2ff404bf5dac900387a7c65c4cc4a0f2151
Nikica Jokic <neektza@gmail.com>
2015-03-03 19:26:04 +0100

    Most of CommandHandler is now unnecessary.
    Only needs to know how to fetch the entire tree, instead of specific
    subtrees (brands, embeds, services).

--- services/crawler/configuration_fetcher.rb
+++ services/crawler/configuration_fetcher.rb
@@ -1,73 +1,41 @@
 require 'httparty'
-require 'supervisors/node_types'
+require 'supervisors/node_types/node_types'
 
 class ConfigurationFetcher
   include Celluloid
   include CoreHelpers
+  
+  RETRY_INTERVAL = 3
 
   def static_config
     @static_config ||= env_config
   end
 
-  def brand(bi)
-    config.fetch(:brands).select{|b| b.fetch(:id) == bi.id}.first
-  end
-  alias_method :brand_config, :brand
-
-  def embed(bi, ei)
-    brand(bi).fetch(:embeds).select{|e| e.fetch(:id) == ei.id}.first
-  end
-  alias_method :embed_config, :embed
-
-  def service(bi, ei, si)
-    embed(bi, ei).fetch(:services).select{|s| s.fetch(:id) == si.id}.first
-  end
-  alias_method :service_config, :service
-
   def config
-    c = Condition.new
-    fetch_until_ok(c)
-    c.wait
+    fetch_until_available(is_available)
+    is_available.wait
   end
 
-  def fetch_until_ok(c)
-    val = actually_fetch
+  def is_available
+    @condition ||= Condition.new
+  end
+
+  private
+
+  def fetch_until_available(c)
+    val = fetch_and_parse
     after(0) { c.broadcast(val) }
   rescue => e
-    Celluloid.logger.error "Web unresponsive, trying again in 3 seconds"
-    after(3) { fetch_until_ok(c) }
+    Celluloid.logger.error "Web unresponsive, trying again in #{RETRY_INTERVAL} seconds"
+    after(RETRY_INTERVAL) { fetch_until_available(c) }
   end
 
-  def actually_fetch
+  def fetch_and_parse
     resp = HTTParty.get url
     hash = JSON.parse resp.body
     val = symbolize_keys hash
     val
   end
-
-  def traverse(feed_name, type_name, attr_name, attr_value)
-    acc = []
-
-    config.fetch(:brands).each do |b|
-      b.fetch(:embeds).each do |e|
-        e.fetch(:services).each do |s|
-          if s[:feed_name] == feed_name && s[:type_name] == type_name && s[:config][attr_name.to_sym] == attr_value
-            acc.push({
-              brand_id: b[:id],
-              brand_name: b[:name],
-              embed_id: e[:id],
-              embed_name: e[:name],
-              service: s
-            })
-          end
-        end
-      end
-    end
-
-    acc
-  end
-
-  private
 
   def env_config
     {
@@ -95,5 +63,4 @@ class ConfigurationFetcher
   def url
     ENV['CRAWLER_CONFIG'] + '?secret=' + ENV['CRAWLER_SECRET_KEY']
   end
-
 end
