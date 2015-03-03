b0219cee607ad2da19c18ba709e19598a30449c2
Nikica Jokic <neektza@gmail.com>
2015-03-03 19:41:07 +0100

    ConnectionManager supports channel sharing.
    When using AMQP declaration trait in app/models, that shared channel
    is used because otherwise, each publish action creates a new channel.

--- lib/connection_manager.rb
+++ lib/connection_manager.rb
@@ -15,6 +15,11 @@ class ConnectionManager
   end
   alias_method :rabbitmq, :rabbit
 
+  def shared_rabbit
+    @rabbit_chan ||= @rabbit_conn.create_channel
+  end
+  alias_method :shared_rabbitmq, :shared_rabbit
+
   def redis
     @redis_conn
   end
