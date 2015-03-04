60a10a15c7394712e40d21da316b05e2741dc2d5
Veljko Dragsic <veljko@bitovi.com>
2015-03-04 16:34:17 +0100

    404 for routing and template missing in app controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
 
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from ActionView::MissingTemplate, with: :render_404
+
+  def render_404
+    render plain: '404 not found', status: 404
+  end
+
   helper_method :crypter
 
   def crypter
