b8bcfeca3f51a58f53be3bba27d971b0460c2a4c
Nikica Jokic <neektza@gmail.com>
2015-03-05 17:20:19 +0100

    When deleting services or embeds, clear links too.
    1.
    When deleting an embed, delete all entities that became orphaned
    (belong to no embed).
    
    2.
    When deleting a service, delete connections between entities
    belonging to the service we're currently deleting and the embed
    that service belongs to.
    
    Afterwards, delete connections between entities and the service we're
    currently deleting.
    
    Finally, delete all entities that belong to no service (orphaned
    entities).
    
    
    Former-commit-id: 87052a589f3d8050a00ed4a6d9b86745a49a7b32 [formerly cfb90e1b3095515a92249b720814c574168d4959]
    Former-commit-id: 7b400a474a8a8c9779889d8f9ea33f23d4fdbf0e

--- app/controllers/api/v3/embeds_controller.rb
+++ app/controllers/api/v3/embeds_controller.rb
@@ -28,7 +28,7 @@ class Api::V3::EmbedsController < Api::V3::BaseController
   end
 
   def destroy
-    if owner_embed.destroy
+    if owner_embed.delete_and_clear_relations
       render :json => msg_hash(@filter, 'destroy', 'success')
     else
       render :json => msg_hash(@filter, 'destroy'), :status => 406
--- app/controllers/api/v3/services_controller.rb
+++ app/controllers/api/v3/services_controller.rb
@@ -49,9 +49,8 @@ class Api::V3::ServicesController < Api::V3::BaseController
 
   def destroy
     @service = Service.find(service_id)
-    @service.clear_linked_entities if clear_linked_entities?
 
-    if @service.destroy
+    if @service.delete_and_clear_relations
       render :json => msg_hash(@service, 'destroy', 'success')
     else
       render :json => msg_hash(@service, 'destroy'), :status => 406
@@ -129,10 +128,6 @@ class Api::V3::ServicesController < Api::V3::BaseController
 
   def api_adapter
     Support::ThirdPartyApiAdapter.new
-  end
-
-  def clear_linked_entities?
-    params[:clear_rels].present? && params[:clear_rels] == 'true'
   end
 
   def create_new_service
