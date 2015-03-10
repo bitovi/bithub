2fbc2ce8ee0e2e24b6f7e76eca24bd9e652d7f13
Veljko Dragsic <veljko@bitovi.com>
2015-03-10 16:47:58 +0100

    Possibly fixed migration.

--- db/migrate/20150309174442_add_invite_code_reference_to_account.rb
+++ db/migrate/20150309174442_add_invite_code_reference_to_account.rb
@@ -1,7 +1,7 @@
 class AddInviteCodeReferenceToAccount < ActiveRecord::Migration
   def change
-    change_table :accounts do |t|
-      t.references :invite_code unless t.column_exists? :invite_code
+    unless column_exists? 'public.accounts', 'invite_code_id'
+      add_column :accounts, :invite_code_id, :integer
     end
   end
 end
