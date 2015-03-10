1ec5a153c12a30ac6920d5afa384b64f900be697
Veljko Dragsic <veljko@bitovi.com>
2015-03-10 16:41:51 +0100

    Last migration checks if column already exists.

--- db/migrate/20150309174442_add_invite_code_reference_to_account.rb
+++ db/migrate/20150309174442_add_invite_code_reference_to_account.rb
@@ -1,7 +1,7 @@
 class AddInviteCodeReferenceToAccount < ActiveRecord::Migration
   def change
     change_table :accounts do |t|
-      t.references :invite_code
+      t.references :invite_code unless t.column_exists? :invite_code
     end
   end
 end
