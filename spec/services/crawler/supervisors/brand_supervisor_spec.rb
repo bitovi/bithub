89129ea99d9892f0e69e141cb029131aa26cc4d0
Nikica Jokic <neektza@gmail.com>
2015-03-13 13:37:42 +0100

    New tests for supervisors and of course, refactoring.
    Former-commit-id: 17b4731501ef7cec25cc729cdd26e011800f9ab9 [formerly 556fdd870a673767ae8385e67c83d43324f739f0]
    Former-commit-id: 3b37d96ee4d20139b792a6c55f9a335ed2fdaa45

---
+++ spec/services/crawler/supervisors/brand_supervisor_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+require 'supervisors/brand'
+require 'supervisors/node_types/node_types'
+require_relative 'support/service_supervisor_mock'
+
+describe Supervisors::Brand do
+  before { Celluloid.boot }
+  after { Celluloid.shutdown }
+  
+  let(:config) do
+    JSON.parse(File.read('spec/support/responses/configurator/test_account_config.json'), symbolize_names: true)
+  end
+
+  let(:brand_sup_parent_node) do
+    SupervisionNode.from_message({
+      main: { wassap: 'notin' }
+    })
+  end
+
+  let(:target) do
+    SupervisionNode.from_message({
+      main: { qua: 'wat' },
+      brand: { id: 42, name: 'my_brand' },
+      embed: { id: 17, name: 'my_embed' }
+    })
+  end
+
+  describe '#stop_embed_supervisor' do
+    it 'starts the designated embed supervisor' do
+      brand_sup = Supervisors::Brand.new(brand_sup_parent_node, NodeTypes::BrandInfo.new(42, 'foo'))
+
+      expect do
+        brand_sup.handle_cmd(target, :start)
+      end.to change { brand_sup.children.size }.from(0).to(1)
+    end
+
+    it 'stops the designated embed supervisor' do
+      brand_sup = Supervisors::Brand.new(brand_sup_parent_node, NodeTypes::BrandInfo.new(42, 'foo'))
+      brand_sup.boot(config.fetch(:brands).first)
+
+      expect do
+        brand_sup.handle_cmd(target, :stop)
+      end.to change { brand_sup.children.size }.from(2).to(1)
+    end
+  end
+end
