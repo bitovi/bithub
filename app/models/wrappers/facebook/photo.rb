d8f20c2aa5cfdd1a1a9a1965e3765f7744c052ef
Veljko Dragsic <veljko@bitovi.com>
2015-03-16 16:01:00 +0100

    Fix in facebook photo wrapper.

--- app/models/wrappers/facebook/photo.rb
+++ app/models/wrappers/facebook/photo.rb
@@ -49,13 +49,11 @@ module Wrappers
         types = opts[:types] || nil
 
         data.reduce([]) do |acc, el|
-
-          puts "=== #{el[:type]}, #{types}"
           if types && types.include?(el[:type])
             acc.push el
           elsif sa_data = el[:subattachments].andand[:data]
             # handle subattachments
-            acc.push *extract_from_attachments_by_type(sa_data, type)
+            acc.push *extract_from_attachments_by_type(sa_data, opts)
           end
 
           acc
