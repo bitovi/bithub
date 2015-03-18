14637f17b38a0b479589d6c74b8d2dd090feb6f8
Veljko Dragsic <veljko@bitovi.com>
2015-03-18 13:01:22 +0100

    Add limits migration checks columns existance.

--- db/migrate/20150305180607_add_limits.rb
+++ db/migrate/20150305180607_add_limits.rb
@@ -1,7 +1,10 @@
 class AddLimits < ActiveRecord::Migration
   def change
-    add_column :plans, :grace_period, :integer, null: false, default: 15
-    add_column :plans, :limits,       :json,    null: false, default: '{}'
-    add_column :plans, :features,     :json,    null: false, default: '{}'
+    add_column :plans, :grace_period, :integer, null: false, default: 15 \
+      unless column_exists? 'public.plans', 'grace_period'
+    add_column :plans, :limits,       :json,    null: false, default: '{}' \
+      unless column_exists? 'public.plans', 'limits'
+    add_column :plans, :features,     :json,    null: false, default: '{}' \
+      unless column_exists? 'public.plans', 'features'
   end
 end
