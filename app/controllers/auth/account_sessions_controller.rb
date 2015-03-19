b3e017b1e3aa577a3c6038f5a1f736be3d95b617
Veljko Dragsic <veljko@bitovi.com>
2015-03-19 11:27:06 +0100

    Fixed sign in path after login.

--- app/controllers/auth/account_sessions_controller.rb
+++ app/controllers/auth/account_sessions_controller.rb
@@ -3,14 +3,10 @@ class Auth::AccountSessionsController < Devise::SessionsController
   protected
 
   def after_sign_in_path_for(account)
-    # find brand, if there is more than one display options
-    if account.brands.count == 1
-      session['tenant_name'] = account.brands.first.tenant_name
+    # TODO: choose organization and choose brand
+    session['tenant_name'] = account.organizations.first.brands.first.tenant_name
 
-      admin_index_path
-    else
-      choose_brand_admin_index_path
-    end
+    admin_index_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
