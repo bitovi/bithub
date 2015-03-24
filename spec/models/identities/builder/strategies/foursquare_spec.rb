2dea6a84900a9966d14074b8be49526251fa26e7
Nikica Jokic <neektza@gmail.com>
2015-03-24 18:11:18 -0500

    Foursquare Builder strategy rewritten.
    Tests added.
    
    
    Former-commit-id: 14f4343f59e6e811dfe8ec4830ef4519b84f844d [formerly 3dc6d569c373dfb12ad65a3ada063ec0506bcfee] [formerly ac91349bb66ea7148a37a8510a950fd86e26cc3c [formerly 365e8185d20da9bfd8636137811cc57184e017d9]]
    Former-commit-id: 55a1159c8ab1146c6df0897c981074d677181cb2 [formerly ffa1d2fb84bae4df9afdaf134440a085f76b70c7]
    Former-commit-id: db5e30385a06a150920103fc6ebf55cc77bb0c02

---
+++ spec/models/identities/builder/strategies/foursquare_spec.rb
@@ -0,0 +1,37 @@
+require 'models/identities/builder_spec_helper'
+
+describe Identities::Builder::Strategies::Foursquare do
+  let(:oauth_data) do
+    {
+      "credentials" => {
+        "token"=>"22PI511OSAJQDCUZT1JUAH3MIXCHCA24PJL04WPAB0HODUVM",
+        "expires"=>false
+      }
+    }
+  end
+
+  describe '#extract_credentials' do
+    it 'extracts the tokens from the OAuth response' do
+      b = Identities::Builder::Strategies::Foursquare.new(oauth_data)
+      b.extract_credentials
+
+      expect(b.result).to eq({
+        credentials: {
+          access_token: oauth_data.fetch('credentials').fetch('token'),
+        }
+      })
+    end
+  end
+  
+  describe '#fetch_venues' do
+    it 'fetches the user\'s managed venues from Foursquare\'s API' do
+      b = Identities::Builder::Strategies::Foursquare.new(oauth_data)
+      VCR.use_cassette('builder_foursquare_venues') do
+        b.fetch_venues
+      end
+
+      expect(b.result).to include(:venues)
+      expect(b.result[:venues].first).to include('id', 'name')
+    end
+  end
+end
