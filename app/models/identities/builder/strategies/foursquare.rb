365e8185d20da9bfd8636137811cc57184e017d9
Nikica Jokic <neektza@gmail.com>
2015-03-24 18:11:18 -0500

    Foursquare Builder strategy rewritten.
    Tests added.

---
+++ app/models/identities/builder/strategies/foursquare.rb
@@ -0,0 +1,32 @@
+module Identities
+  class Builder
+    module Strategies
+      class Foursquare < Identities::Builder::Protocol
+
+        def extract_credentials
+          @result[:credentials] = {
+            access_token: @source_data.fetch(:credentials).fetch(:token)
+          }
+        end
+
+        def fetch_venues
+          if (venues = venues_over_http)
+            @result[:venues] = venues
+          end
+        end
+
+        private
+        def venues_over_http
+          client.managed_venues.items
+        end
+
+        def client
+          Foursquare2::Client.new(
+            api_version: '20141111',
+            oauth_token: @source_data.fetch(:credentials).fetch(:token)
+          )
+        end
+      end
+    end
+  end
+end
