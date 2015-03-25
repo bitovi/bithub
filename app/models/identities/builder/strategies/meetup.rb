29f9e3f1bd90298d47e3f377f0ac5716ea08ed56
Nikica Jokic <neektza@gmail.com>
2015-03-25 10:06:59 -0500

    Meetup Builder strategy rewritten.
    Locked down with tests.
    
    
    Former-commit-id: a6d090926c9d5b931ed6c7888b684abb8b7c6284 [formerly 0eb5c7859959d44766dc614e7192604d9eb19d7a] [formerly 8e454385d67b83b7074c8925048dfebb08b2939f [formerly 142c9cdd12f9843e7b5fae91a75bcabf83be6489]]
    Former-commit-id: 23f7f5e0efa777ffa2e1633d8488646bf13f3cea [formerly ac4f05c1224b89997ceb45331813279b42b853b5]
    Former-commit-id: cf68d97892807bbf4ec09ca851d723dc2b05fd25

---
+++ app/models/identities/builder/strategies/meetup.rb
@@ -0,0 +1,77 @@
+require 'rmeetup'
+
+module Identities
+  class Builder
+    module Strategies
+      class Meetup < Identities::Builder::Protocol
+
+        def extract_credentials
+          @result[:credentials] = super.merge({
+            refresh_token: @source_data.fetch(:credentials).fetch(:refresh_token),
+            expires_at: @source_data.fetch(:credentials).fetch(:expires_at)
+          })
+        end
+
+        def fetch_groups
+          if (groups = groups_via_http)
+            @result[:groups] = groups_to_hashes(groups)
+          end
+        end
+
+        def refresh_credentials
+          if (creds = credentials_over_http)
+            @data[:credentials] = {
+              access_token: creds[:access_token],
+              refresh_token: creds[:refresh_token],
+              expires_at: Time.now.to_i + creds['expires_in']
+            }
+          end
+        end
+
+        private
+        def groups_via_http
+          client = RMeetup::Client.new :access_token => @source_data.fetch(:credentials).fetch(:token)
+          client.fetch :groups, :member_id => @source_data.fetch(:uid)
+        end
+
+
+        def groups_to_hashes(groups)
+          groups.map do |g|
+            {
+              id: g.id,
+              name: g.name,
+              link: g.link,
+              urlname: g.urlname,
+              timezone: g.timezone
+            }
+          end
+        end
+
+        def credentials_over_http
+          params = {
+            grant_type: 'refresh_token',
+            client_id: ENV['MEETUP_KEY'],
+            client_secret: ENV['MEETUP_SECRET'],
+            refresh_token: @source_data.fetch(:credentials).fetch(:refresh_token)
+          }
+
+          response = https_client.post "/oauth2/access", URI.encode_www_form(params)
+
+          if response.code == "200"
+            HashWithIndifferentAccess.new(JSON.parse(response.body))
+          else
+            Rails.logger.error "Meetup, refreshing access tokens failed with #{response.code} #{response.body.inspect}"
+            nil
+          end
+        end
+        
+        MEETUP_API_DOMAIN = 'secure.meetup.com'
+        def https_client
+          client = Net::HTTP.new MEETUP_API_DOMAIN, 443
+          client.use_ssl = true
+          client
+        end
+      end
+    end
+  end
+end
