cb8cc39511aa743c1b19e4045d82b5442bbdbb37
Nikica Jokic <neektza@gmail.com>
2015-03-25 17:00:02 -0500

    BrandIdent fixture factory was causing tests to fail.

--- spec/factories/brand_identity_factory.rb
+++ spec/factories/brand_identity_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :brand_identity do
     provider 'facebook'
-    source_data {{}}
-    extracted {{}}
+    source_data Hash.new
+    extracted_data Hash.new
   end
 end
