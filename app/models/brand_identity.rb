f9f0831086f0618791b1df271ec67024720ad9c0
Nikica Jokic <neektza@gmail.com>
2015-03-25 16:14:43 -0500

    BrandIdentity has methods for delegating.
    It delegates to Builders and Facades.
    ServiceConfig#property_id added. Used in plucking credentials and
    suggestions from brand identities.

--- app/models/brand_identity.rb
+++ app/models/brand_identity.rb
@@ -1,13 +1,31 @@
 class BrandIdentity < ActiveRecord::Base
-
   belongs_to :brand
-
-  def config
-    Identities::BrandIdentityConfig.new(source_data, provider_name)
-  end
+  has_many :services
   alias_attribute :provider_name, :provider
 
   def credentials(property_id = nil)
-    config.credentials(property_id)
+    facade.credentials(property_id)
+  end
+
+  def property_name_for_id(property_id)
+    facade.property_name_for_id(property_id)
+  end
+
+  def property_id_name_pairs
+    facade.property_id_name_pairs
+  end
+  
+  def builder
+    Identities::Builder.new(
+      Identities::Builders.const_get(
+        provider_name.camel_case, false),
+      source_data)
+  end
+
+  def facade
+    Identities::Facade.new(
+      Identities::Facades.const_get(
+        provider_name.camel_case, false),
+      source_data, extracted_data)
   end
 end
