57de39be7cb7a6e361258cafd9a608b901baecbb
Nikica Jokic <neektza@gmail.com>
2015-03-25 17:00:29 -0500

    Schema update.
    Former-commit-id: 7feb9cc78468d73222666330568174ee40303627 [formerly 781e8feb2aa2a0b4265d06ece124e6f169d1e73f] [formerly 162c9bf666936e289563705eb296b0cc18851f3f [formerly 17da4382552acb228e78714664a133338963ac43]]
    Former-commit-id: e2d6f74129778462e302158853122c4eb2ad62d4 [formerly 945a5ab1dcd4ddea89e69bf684b99c87ab13646d]
    Former-commit-id: a7fee7790417e89bd879a062083e864dfd00c577

--- db/schema.rb
+++ db/schema.rb
@@ -83,10 +83,11 @@ ActiveRecord::Schema.define(version: 20150324150405) do
   create_table "brand_identities", force: true do |t|
     t.string   "provider"
     t.string   "uid"
-    t.json     "source_data", default: {}
+    t.json     "source_data",    default: {}
     t.integer  "brand_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.json     "extracted_data"
   end
 
   add_index "brand_identities", ["brand_id"], :name => "index_brand_identities_on_brand_id"
