ce21fc2d0ed73f5c3d6ab404e975e45bbb217bae
Nikica Jokic <neektza@gmail.com>
2015-03-25 13:28:38 -0500

    Removed token refreshing code strategies.
    Doesn't work. Will need to be implemented at a later time. For now it's
    only confusing.

--- app/models/identities/builder_strategies/meetup.rb
+++ app/models/identities/builder_strategies/meetup.rb
@@ -2,7 +2,7 @@ require 'rmeetup'
 
 module Identities
   module BuilderStrategies
-    class Meetup < Protocol
+    class Meetup < Builder::StrategyProtocol
       MEETUP_API_DOMAIN = 'secure.meetup.com'
 
       def run
@@ -23,16 +23,6 @@ module Identities
         end
       end
 
-      def refresh_credentials
-        if (creds = credentials_over_http)
-          @data[:credentials] = {
-            access_token: creds[:access_token],
-            refresh_token: creds[:refresh_token],
-            expires_at: Time.now.to_i + creds['expires_in']
-          }
-        end
-      end
-
       private
       def groups_via_http
         client = RMeetup::Client.new :access_token => @source_data.fetch(:credentials).fetch(:token)
@@ -49,24 +39,6 @@ module Identities
             urlname: g.urlname,
             timezone: g.timezone
           }
-        end
-      end
-
-      def credentials_over_http
-        params = {
-          grant_type: 'refresh_token',
-          client_id: ENV['MEETUP_KEY'],
-          client_secret: ENV['MEETUP_SECRET'],
-          refresh_token: @source_data.fetch(:credentials).fetch(:refresh_token)
-        }
-
-        response = https_client(MEETUP_API_DOMAIN).post "/oauth2/access", URI.encode_www_form(params)
-
-        if response.code == "200"
-          HashWithIndifferentAccess.new(JSON.parse(response.body))
-        else
-          fail "Meetup Identity Builder, refreshing access tokens failed with #{response.code} #{response.body.inspect}"
-          nil
         end
       end
     end
