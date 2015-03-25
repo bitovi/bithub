fedabd52ee9dfcad9ecc1bee9144c29bc873b494
Nikica Jokic <neektza@gmail.com>
2015-03-25 16:14:43 -0500

    BrandIdentity has methods for delegating.
    It delegates to Builders and Facades.
    ServiceConfig#property_id added. Used in plucking credentials and
    suggestions from brand identities.
    
    
    Former-commit-id: 5fa3c6abdbafd9f822506b5f26c8c523e7c437ff [formerly b25c6eaaa9e4745345d089738a6c2fa83e6d5fbf] [formerly 02c2931c0f8506730e3d4678ee5e2f96fb3740b1 [formerly f9f0831086f0618791b1df271ec67024720ad9c0]]
    Former-commit-id: dc73af0dc7677a78d493fba2ed25bd580a560ac2 [formerly 5fad205365bda9148b1828dbdd4b7da25817e959]
    Former-commit-id: bde286de68f7baaf569d6af6c95ac45504058b8c

--- app/models/service.rb
+++ app/models/service.rb
@@ -65,8 +65,8 @@ class Service < ActiveRecord::Base
     service_errors.present?
   end
 
-  def entity_count
-    entities.count
+  def property_id
+    service_config.property_id
   end
 
   def service_config
@@ -82,9 +82,7 @@ class Service < ActiveRecord::Base
   end
 
   def config_with_credentials
-    service_config.data.merge(
-      brand_identity.credentials(
-        config['id']))
+    service_config.data.merge(brand_identity.credentials)
   end
 
   # private
@@ -123,4 +121,11 @@ class Service < ActiveRecord::Base
       action: action
     }
   end
+
+  ['disqus', 'facebook', 'foursquare', 'github', 'instagram', 'meetup', 'rss', 'stackexchange', 'tumblr', 'twitter'].each do |fn|
+    define_method "is_#{fn}?" do
+      fn ==  feed_name
+    end
+  end
+
 end
