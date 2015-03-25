f27ce42152d092dbebe26c3e324e58729c01dd3c
Nikica Jokic <neektza@gmail.com>
2015-03-25 10:49:29 -0500

    Instagram Builder strategy rewritten.
    an d locked down with tests.
    
    
    Former-commit-id: 16533ee56d5f2724f711418b9cbb1882912cd405 [formerly df0231ce9058cd1d1976088fabf2e39b8fcd6a75] [formerly 3a374c1ab11811f68fe0eede69b263ea81adffda [formerly 7251d5039f3f81d1be59c371480e04b566f9615a]]
    Former-commit-id: 2a28f3303f004eaa431ba5ed4912fc92b85978f1 [formerly 831443aa2cd6a1d0a57e70a6036fe31afd425734]
    Former-commit-id: 51492d13014ea5aabe431d7e15bbf3e4a5110709

---
+++ app/models/identities/builder/strategies/instagram.rb
@@ -0,0 +1,9 @@
+module Identities
+  class Builder
+    module Strategies
+      class Instagram < Identities::Builder::Protocol
+        # Everything needed for Instagram is defined in the Protocol
+      end
+    end
+  end
+end
---
+++ spec/models/identities/builder/strategies/instagram_spec.rb
@@ -0,0 +1,26 @@
+require 'models/identities/builder_spec_helper'
+
+describe Identities::Builder::Strategies::Instagram do
+  let(:oauth_data) do
+    {
+      "uid"=>"532573905",
+      "credentials" => {
+        "token"=>"532573905.045f29a.2553800ce44c4d2dbcfbc639eef56682",
+        "expires"=>false
+      }
+    }
+  end
+
+  describe '#extract_credentials' do
+    it 'extracts the tokens from the OAuth response' do
+      b = Identities::Builder::Strategies::Instagram.new(oauth_data)
+      b.extract_credentials
+
+      expect(b.result).to eq({
+        credentials: {
+          access_token: oauth_data.fetch('credentials').fetch('token')
+        }
+      })
+    end
+  end
+end
