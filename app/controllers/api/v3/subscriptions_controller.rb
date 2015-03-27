e76cc0992d90a2b31278c33e9b31f1917a758148
Veljko Dragsic <veljko@bitovi.com>
2015-03-27 11:15:44 -0500

    Subscriptions #current endpoint.

--- app/controllers/api/v3/subscriptions_controller.rb
+++ app/controllers/api/v3/subscriptions_controller.rb
@@ -3,8 +3,19 @@ class Api::V3::SubscriptionsController < Api::V3::BaseController
   load_and_authorize_resource
 
   def show
+    @subscription = Subscription.find subscription_id
+    render :show
+  end
+
+  def current
     @subscription = Subscription.first
     render :show
+  end
+
+  private
+
+  def subscription_id
+    params.require(:id)
   end
 
 end
