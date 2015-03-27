3cd3bdb763b62fa96ad29a9c5cd5a0dae5b92554
Nikica Jokic <neektza@gmail.com>
2015-03-27 11:57:39 -0500

    Transaction instrumentation for handler and publishers.
    Former-commit-id: 7690d356f8568844eab5ea11554779826cb47c87 [formerly 2c03cff12941a7b6ba0df2b13ccaea42f9672b9a] [formerly 67bbcbe28e293b1ecbe6328ea2b6df461ec4a43b [formerly bf4a500c23b70148e22b038235854bf766febb03]]
    Former-commit-id: 4b2a65c5a3a4914b970ae132f539f745bd0e1207 [formerly c8306bb4f84aefd4449aaa378d6896769ce2be35]
    Former-commit-id: 852cf6dc177c55ddd44c4dbe4cf2fe954496bf19

--- services/crawler/publishers/notification_publisher.rb
+++ services/crawler/publishers/notification_publisher.rb
@@ -4,6 +4,8 @@ require 'rabbit_factory'
 class NotificationPublisher
   include Celluloid
   include Celluloid::Logger
+  include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
+  add_transaction_tracer :publish, :category => 'OtherTransaction/Publishers'
 
   def initialize
     Celluloid.logger.info 'Initializing Notification publisher'
