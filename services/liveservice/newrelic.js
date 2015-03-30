814736fcdae15544f527da4916f6188b2fc53861
Nikica Jokic <neektza@gmail.com>
2015-03-30 18:02:17 -0400

    Newrelic app names normalized.
    All names are in the form of Bithub-{component_name}.

--- services/liveservice/newrelic.js
+++ services/liveservice/newrelic.js
@@ -1,9 +1,7 @@
 var env = process.env.ENV,
-	agent_enabled = (env != 'development'),
-	app_name = 'Bithub' + ' (' + process.env.ENV + ')';
+	agent_enabled = (env != 'development');
 
 exports.config = {
-	app_name:      [app_name],
 	agent_enabled: agent_enabled,
 	license_key:   'fceb2642ac34bfe1de22a2a43bb9502a0ac48d8a',
 	logging: {
