4183fff9f57eb09c293e54ba40fcfa59e68a10e0
Nikica Jokic <neektza@gmail.com>
2015-04-14 18:00:32 +0200

    False is the default for :is_approved_manually
    Former-commit-id: 86adeeb097c9c9c927c9f437bf4bd6048e270d64 [formerly be85ca129ba90ffd836b597f1385facffedecf59] [formerly f5ae799e288231eb37f549fea4623ca10a0faa43 [formerly 0bfad234712e76b0c7a9a693d0b2004e9e161e7f]]
    Former-commit-id: cbcb1bdde94ffe67ba6631383a2e9a8046572bbd [formerly f04d2df3149334288707e6fab75a04a8f6b60130]
    Former-commit-id: a92afb7c2971e6d75a969937858dfdec195f0e4a

--- db/migrate/20150409172247_update_manual_moderation.rb
+++ db/migrate/20150409172247_update_manual_moderation.rb
@@ -1,7 +1,15 @@
 class UpdateManualModeration < ActiveRecord::Migration
+
   def up
+    Brand.pluck(:name).each do |n|
+      Apartment::Tenant.switch(n) do
+        EmbedEntity.where(is_approved_manually: nil).update_all(is_approved_manually: false)
+      end
+    end
+
     change_column_default :embed_entities, :is_approved_manually, false
     change_column_null :embed_entities, :is_approved_manually, false
+
   end
   
   def down
