441456bc56bd754a47a60c9c8552aaa0133d235a
Nikica Jokic <neektza@gmail.com>
2015-04-22 15:10:07 +0200

    Invite code removed from tests.
    Was breaking the build.
    
    
    Former-commit-id: 7ce6904924ea749bab8aac5288e2b53405a8dc9b [formerly 85a8a14f92a9b7a1751a9cbd2a36fe7b25027e16] [formerly c2e72407a4c9f5b9f36f3604c0b041a718599b96 [formerly 002c3fd1f5b3dfa064d3e28c71ca6f1c5ac32a54]]
    Former-commit-id: 2c77ea08e3d70889dece02f613d4e8917f0a1459 [formerly efdff7f72092cba5fc642800b6dc891b026ff84a]
    Former-commit-id: 76ec5888b332225fbdaa5b83f52347dae1853000

--- spec/factories/account_factory.rb
+++ spec/factories/account_factory.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
     email "kikica@gmail.com"
     password "coobar123"
     password_confirmation "coobar123"
-    association :invite_code, factory: [:invite_code, :surely_doesnt_exist]
   end
 end
 
