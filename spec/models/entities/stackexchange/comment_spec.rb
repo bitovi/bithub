fcd34718f9967f6e6f65ce410181f46d5635728f
Nikica Jokic <neektza@gmail.com>
2015-05-04 12:20:33 +0200

    Tests for StackExchange Entity builders.
    Former-commit-id: 39435e5d2f413b5fe2abe5926e2a7ba7b7d396d4 [formerly 8fab9185f91ce0c8c3829bd42323b6a86874617f] [formerly cb3c5a76c83bc019cc6cd68aee8f1e55f1ba79fb [formerly 71bdfe5b1ecb2b1261455cc634aa55f155016ce1]] [formerly ce14133fc03062d8426c9483a5473e2f6ced3b9b [formerly 81ca2fbd039c48d538cef7e1e2e9104dcb30c6cf] [formerly d539e150b4a378dcc8d565849a328b4de98adbe4 [formerly 8e7570e40ec7995f1abc0553a37936a90c9915d4]]]
    Former-commit-id: 0d20dbda887f9a46fdb35965474baed65e38c2db [formerly 0f9ec7518334e485192f4f1a682f64b1cbc9b69e] [formerly 204b55a1dd5b336db7cabd636d3fa4bbc84ffa10 [formerly 470785998e0904bf40f0b9f2aa118a886a5ce0a7]]
    Former-commit-id: 9b5b0a949364bd00004de65c0c58ea97873df10e [formerly 1fd178b0f5cd67d82624dc1b6021c9d895ffa78a]
    Former-commit-id: 4ccc394935e894b6d1213a31b2b6e00604d1893b

--- spec/models/entities/stackexchange/comment_spec.rb
+++ spec/models/entities/stackexchange/comment_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 describe Entities::Stackexchange::Comment do
   describe 'data' do
-    it 'prepares the data for building/updating'
+    it 'prepares the data for building/updating' do
+      comment_event = Events::Stackexchange::CommentEvent.new(
+        raw_data(response_path: 'stackexchange/response.json')['items'][0]['answers'][0]['comments'][0])
+
+      entity_wrapper = Entities::Stackexchange::Comment.new(comment_event)
+      instance = entity_wrapper.procure.instance
+
+      expect(instance.attributes.keys).to include('title', 'body', 'url', 'origin_id', 'origin_ts')
+      expect(instance.props.keys).to include('origin_author_id', 'origin_author_name', 'origin_author_avatar_url', 'score', 'post_id', 'post_type')
+    end
   end
 end
