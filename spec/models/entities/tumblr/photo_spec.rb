597f10c014151ab7806c06bf341591a8c63e8f62
Nikica Jokic <neektza@gmail.com>
2015-05-04 11:03:17 +0200

    Tests for Tumblr entity builders.

---
+++ spec/models/entities/tumblr/photo_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Entities::Tumblr::Photo do
+  describe 'data' do
+    it 'prepares the data for building/updating' do
+      post_event = Events::Tumblr::Post.new(
+        raw_data(response_path: 'tumblr/response.json')['response'][1])
+
+      entity_wrapper = Entities::Tumblr::Photo.new(post_event)
+      instance = entity_wrapper.procure.instance
+
+      expect(instance.attributes.keys).to include('title', 'body', 'url', 'origin_id', 'origin_ts')
+      expect(instance.props.keys).to include('tags', 'origin_author_name', 'photos')
+    end
+  end
+end
