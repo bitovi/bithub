303dad54f166605860a467cfc17f28d1e8e6806a
Veljko Dragsic <veljko@bitovi.com>
2015-05-05 17:59:54 +0200

    Liveservice debugging.
    Former-commit-id: 9a90da46bf9e8f31d5e2b1536cd96bdb4e4c5b5f [formerly 7de5dc24fbab2f9693e539c0e675db1775dfb24c] [formerly 0fa4f89ea78196d23a275de5b1a04223acc58603 [formerly 126e9e22700d356439f42b20c8551c95951879d4]] [formerly c862f4357b901f11951cfdf6114a89afd202925c [formerly 9d61e4bb7f23c2e0486efa869737d2fc345246b9] [formerly a0c9c971c758a82ee2259eaffdb9c08b5d683400 [formerly 9ade962a1a24d1e0142ca1f7cc30fb6638801649]]]
    Former-commit-id: cba72a74ba2ca5fa0869d2ce934a054ed8bb47e8 [formerly 658b4c2f73a5566aab55f9ceb9e27c008422b6af] [formerly 0a5e431b40bd122a6c78643ac73a5f765c554f2e [formerly e1e7c2d6b2d453543daff7587b7c6eefadec0c0f]]
    Former-commit-id: a32b1238be14e972b78179bb3ded46e75e1b3e69 [formerly 727c3bf0345e94f97b4fdcb52c5f26e185534067]
    Former-commit-id: 1ef90d85d1598b2ca42e8cd9475f06167392fdcc

--- public/connect-liveservice.js
+++ public/connect-liveservice.js
@@ -32,6 +32,10 @@ steal(function(){
 				console.log('DISCONNECTION!', msg);
 			});
 
+			currentSocket.on('entities', function( entity ) {
+				console.log('NEW ENTITY!', entity.id );
+			});
+
 			return currentSocket;
 		}
 	}
