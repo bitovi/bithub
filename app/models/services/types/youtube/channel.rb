fecfbc071036472b014843d6b999d45111cf9d49
Veljko Dragsic <veljko@bitovi.com>
2015-05-13 19:33:50 +0200

    Youtube service config supports urls.
    Former-commit-id: 2acc8f75a650c013cef307bbe01c00a79e854ac1 [formerly f62792fdb4f27147d4ecd504855f28956030bf3a] [formerly 327c3d594effeb71802d8419b9f7eae3ad0d4165 [formerly 9610b077626743d60e68073d9142875e03340c07]] [formerly d46bae78e7f29c433d025477058df4120eb1b9bf [formerly e3563c803cd471f3192bae505ea75f1cb4865d10] [formerly 7e8d10ab9765d8559d0b513ccae01df1be3ceecf [formerly c182d5600d43b7875af81ff4c71542c5bf2cfa1f]]]
    Former-commit-id: 1aea72704a516c4836575b09fd019b63582f586c [formerly 48c675c32562501f61c55187943da856fb3e105c] [formerly aa8bfe8ab5b0a0b55b1081dfe0fa8acefadd6ad0 [formerly ec04ca2330b9543c665e1765ac4a3913b28f1b27]]
    Former-commit-id: ff4c86f6c86ba5c66f9ad14b9ac00d70426c4253 [formerly b31a949c5372c7b40ea0b2339bfc53f18e425429]
    Former-commit-id: c7c9583c49368285d2bf787d6ebaad48077d862c

--- app/models/services/types/youtube/channel.rb
+++ app/models/services/types/youtube/channel.rb
@@ -5,6 +5,21 @@ module Services
         include Virtus.model(:strict => true)
         attribute :id, String
         attribute :display_name, String, :default => ''
+
+        def url=(url)
+          if channel_id = channel_id_from_url(url)
+            self.id = channel_id
+          end
+
+          self.display_name = url
+        end
+
+        def channel_id_from_url(url)
+          if res = /^http.?:\/\/.*youtube.com\/channel\/([^?\/]*)/.match(url)
+            res[1]
+          end
+        end
+
       end
     end
   end
--- app/models/services/types/youtube/playlist.rb
+++ app/models/services/types/youtube/playlist.rb
@@ -5,6 +5,25 @@ module Services
         include Virtus.model(:strict => true)
         attribute :id, String
         attribute :display_name, String, :default => ''
+
+        def url=(url)
+          if playlist_id = playlist_id_from_url(url)
+            self.id = playlist_id
+          end
+
+          self.display_name = url
+        end
+
+        def playlist_id_from_url(url)
+          uri = URI(url)
+
+          if /.*youtube.com/.match(uri.host)
+            if params = (uri.query && CGI::parse(uri.query))
+              params['list'] && params['list'].first
+            end
+          end
+        end
+
       end
     end
   end
