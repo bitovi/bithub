69faf9f96752ac70b2f820fa7cde84a0307491fa
Mihael Konjevic <konjevic@gmail.com>
2015-05-14 18:35:40 +0200

    Work on the embed rendering

--- public/stealconfig.js
+++ public/stealconfig.js
@@ -1,4 +1,25 @@
 (function () {
+
+	// Console-polyfill. MIT license.
+	// https://github.com/paulmillr/console-polyfill
+	// Make it safe to do console.log() always.
+	(function(global) {
+		'use strict';
+		global.console = global.console || {};
+		var con = global.console;
+		var prop, method;
+		var empty = {};
+		var dummy = function() {};
+		var properties = 'memory'.split(',');
+		var methods = ('assert,clear,count,debug,dir,dirxml,error,exception,group,' +
+									 'groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles,profileEnd,' +
+									 'show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn').split(',');
+		while (prop = properties.pop()) if (!con[prop]) con[prop] = empty;
+		while (method = methods.pop()) if (!con[method]) con[method] = dummy;
+	})(typeof window === 'undefined' ? this : window);
+	// Using `this` for web workers while maintaining compatibility with browser
+	// targeted script loaders such as Browserify or Webpack where the only way to
+	// get to the global object is via `window`.
 	// taking from HTML5 Shiv v3.6.2 | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 Licensed
 	var supportsUnknownElements = false;
 
@@ -15,7 +36,7 @@
 					typeof frag.cloneNode == 'undefined' ||
 						typeof frag.createDocumentFragment == 'undefined' ||
 						typeof frag.createElement == 'undefined'
-					);
+				);
 			}());
 		} catch (e) {
 			// assign a false positive if detection fails => unable to shiv
