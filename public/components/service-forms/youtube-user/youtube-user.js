62aefff4f966439a4c6b5252d0bd72a0cd847e73
Mihael Konjevic <konjevic@gmail.com>
2015-05-15 15:48:56 +0200

    Add youtube embed to cards
    Former-commit-id: 7ba441f08ab4139adcb2eb741cb5c42d766fa675 [formerly b6025da92a3500b0dc00b4e0279bd74b0ff9b66f] [formerly 91328766cacfc7bd2fcb231e753db366f46366ee [formerly c26f2daab308a642a56507db40ea48a103a86eca]] [formerly 252dcf405daf2ed0e788440c78e1bcc02fc5f21d [formerly 34e21ae2a1091cfa7d4587a043b8309a557ae4f3] [formerly bf42f86ccb2f3aac2637593e1fe25cfc0ac08d8e [formerly d1a07c4d9a4417a524678feac92df1a5c2543752]]]
    Former-commit-id: 708be0a450f0012275e66ff49f52aba95192b726 [formerly 8cdf36d471e60dc9f619514bf2b8f894197cb970] [formerly 3796eca9ce0ba01d6866e6d01d55c2dcbb78d263 [formerly a1778cbfd10e32ceac3f5102d23d92811de824db]]
    Former-commit-id: 914e24cf31d1bf153dc73f52fdb9b0db5134e7ee [formerly f3a028ae62464b4ca43e0d6cf9fe14e86372f562]
    Former-commit-id: bc6339a78ef7d31d0e0fc5c71b56d16c5118cdf6

--- public/components/service-forms/youtube-user/youtube-user.js
+++ public/components/service-forms/youtube-user/youtube-user.js
@@ -6,8 +6,8 @@ function(Component, initView){
   return Component.extend({
     tag : 'bh-youtube-user-service',
     template : initView,
-    scope : {
-
-    }
+    scope : function(){
+			console.log(arguments);
+		}
   });
 });
