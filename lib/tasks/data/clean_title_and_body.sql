c4bdf44aa23984bb6099cdb591ac45c794f7adff
Nikica Jokic <neektza@gmail.com>
2015-05-16 11:48:13 +0200

    A better (and working) task for filling searchable fields.

--- lib/tasks/data/clean_title_and_body.sql
+++ lib/tasks/data/clean_title_and_body.sql
@@ -1,2 +1,18 @@
-select trim(regexp_replace(regexp_replace(body, E'<.*?>', '', 'g' ), '[\s]+', ' ', 'g'))
-from entities;
+update entities
+set searchable_body = (select trim(regexp_replace(regexp_replace(body, E'<.*?>', '', 'g' ), '[\s]+', ' ', 'g'))
+	from entities as inner_entities
+	where inner_entities.id = entities.id
+);
+
+update entities
+set searchable_title = (select trim(regexp_replace(regexp_replace(title, E'<.*?>', '', 'g' ), '[\s]+', ' ', 'g'))
+	from entities as inner_entities
+	where inner_entities.id = entities.id
+);
+
+update entities
+set searchable_title = (
+	select trim(regexp_replace(regexp_replace(title, E'<.*?>', '', 'g' ), '[\s]+', ' ', 'g')) || ' ' || trim(regexp_replace(regexp_replace(body, E'<.*?>', '', 'g' ), '[\s]+', ' ', 'g')) || ' ' || url
+	from entities as inner_entities
+	where inner_entities.id = entities.id
+);
