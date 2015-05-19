9b8cba73dae4c722636812f649a08e33723c456a
Mihael Konjevic <konjevic@gmail.com>
2015-05-19 22:27:18 +0200

    Convert bit code to es6 syntax
    Former-commit-id: 0d58a558e36d97c51b9a53d16257efc9e7d42558 [formerly 3759163b51caf889c2dff61b04cc7a37e547f565] [formerly f0caaa6022befc17af273452e40ddfbc51252460 [formerly 350fc4638fc456ab1d0ab32fe5e0b076e22f0bb7]] [formerly 7673b5e717b9f8b8a76bf03b8711c09e573c06e0 [formerly 3a6b5aa2f12915cf0cad3e3a8d44a53300ba2158] [formerly da5283ac08a31654f46fdc3996817a5befb923e8 [formerly 50e2b7e28feea192961ea20ee075f5f6ba4c1e63]]]
    Former-commit-id: 4294be5fff002e164bae5994b2921a4ebf4e7963 [formerly 5a0c527533e359e4bd60ab8ddf609702b5b075d4] [formerly ae5d4584d7a0b02787e4965a55b29234604a4099 [formerly 4140bddf90aeab02efd17f237dcfb9336ab14251]]
    Former-commit-id: 0bd6e66c96428b708a38b204e14d4c6589a9bf06 [formerly 9e3fa96364d81c6a8d815257ed7ee3c896cf9105]
    Former-commit-id: 8f87276aba0afdef0ecec219e33ee5ae7740b77d

--- public/bit/image-gallery/image-gallery.js
+++ public/bit/image-gallery/image-gallery.js
@@ -1,29 +1,28 @@
-steal(
-'can/component',
-'./image-gallery.stache!',
-'./image-gallery.less!', function(Component, initView){
-	return Component.extend({
-		tag: 'bh-image-gallery',
-		template: initView,
-		scope: {
-			init : function(){
-				this.attr('currentImage', this.attr('images.0'));
-			},
-			setCurrent : function(img){
-				this.attr('currentImage', img);
-			},
-			hasGallery : function(){
-				var images = this.attr('images');
-				return images && images.attr('length') > 1;
-			}
+import can from "can/";
+import initView from "./image-gallery.stache!";
+import "./image-gallery.less!";
+
+can.Component.extend({
+	tag: 'bh-image-gallery',
+	template: initView,
+	scope: {
+		init : function(){
+			this.attr('currentImage', this.attr('images.0'));
 		},
-		helpers : {
-			isCurrent : function(img, opts){
-				img = can.isFunction(img) ? img() : img;
-				if(img === this.attr('currentImage')){
-					return opts.fn();
-				}
+		setCurrent : function(img){
+			this.attr('currentImage', img);
+		},
+		hasGallery : function(){
+			var images = this.attr('images');
+			return images && images.attr('length') > 1;
+		}
+	},
+	helpers : {
+		isCurrent : function(img, opts){
+			img = can.isFunction(img) ? img() : img;
+			if(img === this.attr('currentImage')){
+				return opts.fn();
 			}
 		}
-	});
+	}
 });
