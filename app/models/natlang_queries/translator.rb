a5ad3bdf3f3edf45ceda20f796d371051396cd53
Nikica Jokic <neektza@gmail.com>
2015-05-21 00:22:50 +0200

    attr_name translation happens where it's supposed to.
    NatlangQuery::Translator translates the attr_name (instead of NLQ
    itself) to it's searchable variant when it needs to.

--- app/models/natlang_queries/translator.rb
+++ app/models/natlang_queries/translator.rb
@@ -1,7 +1,10 @@
 module NatlangQueries
 
   VALID_OPS = %w(contains starts_with ends_with contains_phrase contains_any contains_all is)
-  SEARCHABLE_ATTRIBUTES = %w(title body searchable_title searchable_body searchable_content searchable_author)
+  SEARCHABLE_ATTRIBUTES = %w(searchable_title searchable_body searchable_content searchable_author)
+
+  OPS_TRANSLATED_TO_LIKE = %w(contains_phrase like starts_with ends_with)
+  OPS_TRANSLATED_TO_FULL_TEXT_SEARCH = %w(contains contains_all contains_any)
 
   class Translator
     def initialize(query, klass = Entity)
@@ -13,26 +16,10 @@ module NatlangQueries
       { :method => tmethod, :arg => targuments }
     end
 
-    def op_translated_to_full_text_search?
-      %w(contains contains_all contains_any).include? @q.op
-    end
-
-    def op_translated_to_like?
-      %w(contains_phrase like starts_with ends_with).include?(@q.op) || (@q.op == 'is' && @q.raw_attr_name == 'author')
-    end
-
-    def attribute_defined?
-      @klass.has_an_attribute?(@q.attr_name)
-    end
-
-    def attribute_is_string?
-      %i(string text).include?(@klass.columns_hash[@q.attr_name].type)
-    end
-
     def tmethod
       if op_translated_to_full_text_search?
         :advanced_search
-      elsif @q.op == 'is' || op_translated_to_like?
+      elsif op == 'is' || op_translated_to_like?
         :where
       end
     end
@@ -40,14 +27,14 @@ module NatlangQueries
     def targuments
       if op_translated_to_full_text_search?
         search_arguments
-      elsif @q.op == 'is' || op_translated_to_like?
+      elsif op == 'is' || op_translated_to_like?
         where_arguments
       end
     end
 
     def search_arguments
-      if SEARCHABLE_ATTRIBUTES.include? @q.attr_name
-        Hash[@q.attr_name, search_value]
+      if attr_is_searchable?
+        Hash[attr, search_value]
       else
         search_value
       end
@@ -58,44 +45,81 @@ module NatlangQueries
     end
     
     def where_column
-      # "Full text search" doesn't care about attr_name so it doesn't matter what it's value is
+      # "Full text search" doesn't care about attr so it doesn't matter what it's value is
       # In case of "ILIKE search" we translate 'content' to 'body'
       if attribute_defined?
-        @q.attr_name
+        attr
       end
     end
 
     def where_op
       if op_translated_to_like?
         @q.negated? ? 'NOT ILIKE' : 'ILIKE'
-      elsif @q.op == 'is'
+      elsif op == 'is'
         @q.negated? ? '<>' : '='
       end
     end
 
     def where_value
       if attribute_defined? && attribute_is_string? && op_translated_to_like?
-        if @q.op == 'like' || @q.op == 'contains_phrase'
-          '%' + @q.val + '%'
-        elsif @q.op == 'is' && @q.attr_name =~ /author/
-          '%' + @q.val + '%'
-        elsif @q.op == 'starts_with'
-          @q.val + '%'
-        elsif @q.op == 'ends_with'
-          '%' + @q.val
+        if op == 'like' || op == 'contains_phrase'
+          '%' + val + '%'
+        elsif op == 'is' && attr =~ /author/
+          '%' + val + '%'
+        elsif op == 'starts_with'
+          val + '%'
+        elsif op == 'ends_with'
+          '%' + val
         end
       else
-        @q.val
+        val
       end
     end
 
     def search_value
-      if @q.op == 'contains_all' || @q.op == 'contains'
-        @q.val.gsub(',','&')
-      elsif @q.op == 'contains_any'
-        @q.val.gsub(',','|')
+      if op == 'contains_all' || op == 'contains'
+        val.gsub(',','&')
+      elsif op == 'contains_any'
+        val.gsub(',','|')
       end
     end
 
+    private
+
+    def attr
+      if %w(content title body author).include?(@q.attr_name)
+        "searchable_#{@q.attr_name}"
+      else
+        @q.attr_name
+      end
+    end
+
+    def op
+      @q.op
+    end
+
+    def val
+      @q.val
+    end
+
+    def attribute_defined?
+      @klass.has_an_attribute?(attr)
+    end
+
+    def attribute_is_string?
+      %i(string text).include?(@klass.columns_hash[attr.to_s].type)
+    end
+
+    def attr_is_searchable?
+      SEARCHABLE_ATTRIBUTES.include? attr
+    end
+    
+    def op_translated_to_full_text_search?
+      OPS_TRANSLATED_TO_FULL_TEXT_SEARCH.include? @q.op
+    end
+    
+    def op_translated_to_like?
+      OPS_TRANSLATED_TO_LIKE.include?(@q.op) || (op == 'is' && attr =~ /author/)
+    end
   end
 end
