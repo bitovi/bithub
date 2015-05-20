9b375e78ae558071d0e584a53769af16fd8a19e3
Mihael Konjevic <konjevic@gmail.com>
2015-05-20 19:19:41 +0200

    Start analytics integration

--- public/components/analytics/analytics.js
+++ public/components/analytics/analytics.js
@@ -1,76 +1,78 @@
 steal(
-'can/component',
-'./analytics.stache!',
-'chart',
-'models',
-'./analytics.less!',
-'components/service-config-formatter',
-function(Component, initView, Chart, Models){
+	'can/component',
+	'./analytics.stache!',
+	'chart',
+	'models',
+	'./analytics.less!',
+	'components/service-config-formatter',
+	function(Component, initView, Chart, Models){
 
-	return Component.extend({
-		tag : 'bh-analytics',
-		template: initView,
-		scope : {
-			resolution: 'hour',
-			init : function(){
-				var self = this;
-				var hubId = this.attr('state.hubId');
-
-				
-
-				$.when(
-					Models.Analytics.findAll({hubId: hubId, resolution: this.attr('resolution')}),
-					Models.Hub.findOne({id: hubId})
-				).then(function(analytics, hub){
-					self.attr({
-						analytics: analytics,
-						hub: hub
+		return Component.extend({
+			tag : 'bh-analytics',
+			template: initView,
+			scope : {
+				resolution: 'hour',
+				init : function(){
+					var self = this;
+					$.when(
+						this.analyticsReq(),
+						Models.Hub.findOne({id: this.attr('state.hubId')})
+					).then(function(analytics, hub){
+						self.attr({
+							analytics: analytics,
+							hub: hub
+						});
 					});
-				});
-			},
-			reloadAnalytics : function(){
-				var self = this;
+				},
+				analyticsReq : function(){
+					var hubId = this.attr('state.hubId');
+					return Models.Analytics.findAll({
+						ownerId: hubId,
+						sourceType: 'services',
+						resolution: this.attr('resolution')
+					});
+				},
+				reloadAnalytics : function(){
+					var self = this;
 
-				Models.Analytics.findAll({
-					hubId: this.attr('state.hubId'),
-					resolution: this.attr('resolution')
-				}).then(function(data){
-					self.attr('analytics', data);
-				});
-			}
-		},
-		helpers : {
-			renderGraph : function(type){
-				var self = this;
-
-				type = can.isFunction(type) ? type() : type;
-
-				return function(el){
-					var ctx = el.getContext('2d');
-					var chart = new Chart(ctx).Line(self.attr('analytics').graphData(type), {
-						bezierCurveTension: 0.1,
-						pointDotRadius: 2,
-						datasetStrokeWidth: 2,
-						multiTooltipTemplate: "<%=datasetLabel%> - <%= value %> items",
-					})
+					this.analyticsReq().then(function(data){
+						self.attr('analytics', data);
+					});
 				}
 			},
-			finalCount : function(timepoints){
-				timepoints = can.isFunction(timepoints) ? timepoints() : timepoints;
-				return (timepoints[timepoints.length - 1] || {volume: '0'}).volume;
-			},
-			hasEnoughData : function(opts){
-				var analytics = this.attr('analytics');
-				if(analytics && analytics.graphData('volume').labels.length > 1){
-					return opts.fn();
+			helpers : {
+				renderGraph : function(type){
+					var self = this;
+					type = can.isFunction(type) ? type() : type;
+					return function(el){
+						setTimeout(function(){
+							var ctx = el.getContext('2d');
+							var chart = new Chart(ctx).Line(self.attr('analytics').graphData(type), {
+								bezierCurveTension: 0.1,
+								pointDotRadius: 2,
+								datasetStrokeWidth: 2,
+								multiTooltipTemplate: "<%=datasetLabel%> - <%= value %> items",
+							});
+						}, 4);
+					}
+				},
+				finalCount : function(timepoints){
+					timepoints = can.isFunction(timepoints) ? timepoints() : timepoints;
+					return (timepoints[timepoints.length - 1] || {volume: '0'}).volume;
+				},
+				hasEnoughData : function(opts){
+					var analytics = this.attr('analytics');
+					console.log(analytics.graphData('volume'))
+					if(analytics && analytics.graphData('volume').labels.length > 1){
+						return opts.fn();
+					}
+					return opts.inverse();
 				}
-				return opts.inverse();
+			},
+			events : {
+				'{scope} resolution' : function(){
+					this.scope.reloadAnalytics();
+				}
 			}
-		},
-		events : {
-			'{scope} resolution' : function(){
-				this.scope.reloadAnalytics();
-			}
-		}
+		})
 	})
-})
