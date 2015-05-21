d6d16326d9dd0f528bc4dfa1a54511817eb9b7dd
Nikica Jokic <neektza@gmail.com>
2015-05-21 18:25:31 +0200

    Interaction analytics.

--- app/controllers/api/v3/interactions_controller.rb
+++ app/controllers/api/v3/interactions_controller.rb
@@ -1,18 +1,13 @@
 class Api::V3::InteractionsController < Api::V3::BaseController
 
   before_filter :authenticate_account!
-  before_action :set_interaction, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
 
   def index
-    if (@source = interacted_with)
-      @data = Interaction.stats(resolution, source_type, source_id).all
-    else
-      @data = Interaction.stats(resolution).all
-    end
-      
-    render :json => { interactions: @data }
+    @zoom = zoom
+    @data = Interaction.stats(resolution, zoom, composed_filter).all
+    render :index
   end
 
   def create
@@ -29,40 +24,34 @@ class Api::V3::InteractionsController < Api::V3::BaseController
   private
 
   def interaction_params
-    params.require(:interaction).permit(:source_id, :source_type, :created_at)
+    params.require(:interaction).permit(:primary_source_id, :primary_source_type, :secondary_source_id, :secondary_source_type, :event_type, :event_subtype, :created_at)
   end
 
   def interaction_type
     params.permit(:type)
   end
 
-  def interacted_with
-    if source_type
-      if source_type == 'embeds' && source_id
-        Embed.find(1)
-      elsif source_type == 'cards' && source_id
-        Entity.find(1)
-      end
-    else
-      fail ArgumentError.new("Missing params.")
-    end
-  end
-
   private
-
+  
   def resolution
     params['resolution'] || 'hour'
   end
 
-  def source_type
-    params[:source_type]
+  def composed_filter
+    attrs = %i(primary_source_id secondary_source_id primary_source_type secondary_source_type event_type event_subtype)
+
+    Hash[attrs.select do |a|
+      params[a].present?
+    end.map do |a|
+      [a, params[a]]
+    end]
   end
 
-  def source_id
-    params[:source_id]
+  def zoom
+    params[:zoom] || 'detailed'
   end
 
-  def owner_id
-    params[:owner_id]
+  def set_zoom
+    @zoom = zoom
   end
 end
