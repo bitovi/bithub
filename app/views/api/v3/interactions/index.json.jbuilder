2436b85f387b74caccf1ee0506bb674191e3d325
Mihael Konjevic <konjevic@gmail.com>
2015-05-22 16:19:55 +0200

    Analytics are working
    Former-commit-id: 22c809797da805ddb258891e52f1feb81259cd71 [formerly db2a407069b821e66d72a2e1fe9e4df8a42308d7] [formerly 86cd74e1e77bfdf356bf0f66ab17f45128315d6b [formerly 42f318c054ea4be913c0a5373abc175c094fe1f7]] [formerly a05d874f9d864d83314f9ed126229d20f811680d [formerly d4382a1b06af9bad404e27558053a72d09b4a438] [formerly 802e906df79aebec456c8d61797abc76b3acbad3 [formerly b3ac040224d44f9cd2e44c75475fee5c817b9cd9]]]
    Former-commit-id: 86fdbbc1e514e041e5e0787e7f442fd86aeb0a58 [formerly 0d8211921a1ee56a47705435956ac824f2106f18] [formerly 9cebe19fd8e13bb6b6716407a96a19ad1f9a1f29 [formerly b363d9b68515b8c05a5b9ce5f5d30acf41ef1f37]]
    Former-commit-id: 64918de6602d6f904be1f14b78545673dbc15f53 [formerly 4f17859c0772a43e75fe6d7f2863643cb3d8ab28]
    Former-commit-id: a188d8d059d75469d23eac7fb297cc1250e348d5

--- app/views/api/v3/interactions/index.json.jbuilder
+++ app/views/api/v3/interactions/index.json.jbuilder
@@ -1,6 +1,6 @@
 json.array!(@data) do |interaction|
   if @zoom == 'detailed'
-    json.extract! interaction, :created_at, :primary_source_id, :secondary_source_id, :event_type, :event_subtype, :volume
+    json.extract! interaction, :created_at, :primary_source_id,  :event_type, :event_subtype, :volume
   elsif @zoom == 'rough'
     json.extract! interaction, :created_at, :primary_source_id, :event_type, :volume
   end
--- public/models/interaction_event.js
+++ public/models/interaction_event.js
@@ -1,12 +1,15 @@
-steal('can/model', 'can/construct/super', function(Model){
+steal('can/model', 'moment', 'can/construct/super', function(Model, moment){
+
 	var wrapCreate = function(data){
 		return {
 			interaction: data
 		};
 	};
 
-	return Model.extend({
+	var InteractionEvent = Model.extend({
 		create : 'POST /api/v3/interactions',
+		findAll : 'GET /api/v3/interactions',
+		
 		createScrollInteraction : function(hubId){
 			return this.create(wrapCreate({
 				primary_source_id: hubId,
@@ -34,4 +37,88 @@ steal('can/model', 'can/construct/super', function(Model){
 			}));
 		}
 	}, {});
+
+	var fillZeroes = function(data, length){
+		while(data.length < length){
+			data.unshift(0);
+		}
+		return data;
+	};
+
+	InteractionEvent.List = InteractionEvent.List.extend({
+		totalShareInteractionsGraphData: function(){
+			var length = this.attr('length');
+			var labels = [];
+			var data = [];
+			var i;
+			for(i = 0; i < length; i++){
+				labels.push(moment(this.attr(i + '.created_at')).fromNow());
+				data.push(this.attr(i + '.volume'));
+			}
+
+			return {
+				datasets: [{
+					data: data,
+					label: "Share count",
+					strokeColor: '#f30',
+					fillColor: '#fff'
+				}],
+				labels: labels
+			};
+		},
+		linkInteractionsGraphData : function(){
+			return this.totalShareInteractionsGraphData();
+		},
+		scrollInteractionsGraphData : function(){
+			return this.totalShareInteractionsGraphData();
+		},
+		perNetworkShareInteractionsGraphData : function(){
+			var length = this.attr('length');
+			var labels = {};
+			var data = {};
+			var longestLabels = [];
+			var datasets = [];
+			var i, k, current, currentSubtype;
+			
+			for(i = 0; i < length; i++){
+				current = this.attr(i);
+				currentSubtype = current.attr('event_subtype');
+				if(!data[currentSubtype]){
+					data[currentSubtype] = [];
+				}
+				if(!labels[currentSubtype]){
+					labels[currentSubtype] = [];
+				}
+				data[currentSubtype].push(current.attr('volume'));
+				labels[currentSubtype].push(moment(current.attr('created_at')).fromNow());
+			}
+
+			for(k in labels){
+				if(labels[k].length > longestLabels.length){
+					longestLabels = labels[k];
+				}
+			}
+
+			for(k in data){
+				if(data[k].length < longestLabels.length){
+					data[k] = fillZeroes(data[k], longestLabels.length);
+				}
+				datasets.push({
+					label: k,
+					data: data[k],
+					strokeColor: '#f30',
+					fillColor: 'rgba(255,255,255,0)'
+				});
+			}
+
+			return {
+				labels: longestLabels,
+				datasets: datasets
+			};
+			
+		}
+	});
+
+	return InteractionEvent;
+
 });
