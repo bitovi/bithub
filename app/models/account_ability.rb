070040423d0e5c68dc8e5cbfd9e1e2339b9faddf
Nikica Jokic <neektza@gmail.com>
2015-05-22 13:07:24 +0200

    New roles:
    * bithub_admin -> can manage whole Bithub,
    * organization_admin -> can manage his own organization,
    * brand_admin -> can manage only one brand under an organizaion.

--- app/models/account_ability.rb
+++ app/models/account_ability.rb
@@ -2,22 +2,30 @@ class AccountAbility
   include CanCan::Ability
 
   def initialize(account)
-    if (account.has_role? :admin) || (ENV['RAILS_ENV'] == 'test')
+    if (account.has_role? :bithub_admin) || (ENV['RAILS_ENV'] == 'test')
       can :manage, :all
+
+    elsif (account.has_role? :organization_admin)
+      can :manage, Organization, id: account.organization_ids
+
+    elsif (account.roles_name & %w(brand_admin organization_admin)).length > 0
+      can :manage, Brand, organization_id: account.organization_ids
+
     else
-      # manage it's own account
+      # can manage its own account
       can :manage, Account, id: account.id
 
-      # update and leave organization
-      can [:read, :update], Organization, id: account.organization_ids
-      can [:read, :destroy], AccountOrganization, account_id: account.id
+      # can read its organization information
+      can [:create, :read], Organization, id: account.organization_ids
 
-      # create new a brand or read/update/delete owned brands
-      can :create, Brand, organization_id: account.organization_ids
-      can [:read, :update, :destroy], Brand, organization_id: account.organization_ids
+      # can read and destroy it's own organization membership
+      can [:create, :read, :destroy], AccountOrganization, account_id: account.id
+
+      # can create/read/update/delete a brand
+      can [:create, :read], Brand, organization_id: account.organization_ids
 
       # read/destroy owned brand identities
-      can [:read, :destroy], BrandIdentity, brand_id: account.brand_ids
+      can [:create, :read], BrandIdentity, brand_id: account.brand_ids
 
       # countries list
       can :read, Country
