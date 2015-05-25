f10a8acc938606f9a7d1947ce864c593348a58b8
Nikica Jokic <neektza@gmail.com>
2015-05-25 11:48:41 +0200

    Logs-as-data applied to publishers.
    Log whenever something significant happens.
    
    
    Former-commit-id: fb68cbd0ea224cfe3bfee605d056e0e55cbab84b [formerly 887b4b7f9542adb46773862bfb11882cc8fea47f] [formerly 7ac7b4b073c19c8218f07b2c760ebf19aa97e987 [formerly 773553deeafa06eb6386e776e09a8017fc3162d3]] [formerly 38cfbc71bfd0113a53ec60ba21ace6a1626662bf [formerly dc3a5fb6eb2a549f1ad504160a494a4a1fb87026] [formerly 23ca075a02ee020b0524f88d0a9b05e28dab1fc5 [formerly e5eba7e846e02f03c24173593f8410a1b7abc0cf]]]
    Former-commit-id: e832e43cd39346141df92b37c9536e3a229e3bba [formerly 33b489672a6e05b8a74c9b69a27eead24ee28fd3] [formerly 0bf571b9941ad775c7f8cf36494b2842d600f6a8 [formerly 096ba273cfb453b77b4fa903482f9514cca92f98]]
    Former-commit-id: 04ff07683afebc56178a555371699325cb79983b [formerly dcfe79b31409bba14655031af2d1a2394fbebfcc]
    Former-commit-id: 49d59d699acfb4113edcb111898fc8569b8e5ae9

--- services/crawler/publishers/error_publisher.rb
+++ services/crawler/publishers/error_publisher.rb
@@ -4,10 +4,10 @@ require 'newrelic_rpm'
 
 class ErrorPublisher
   include Celluloid
-  include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
+  include Celluloid::Logger
 
   def initialize
-    Celluloid.logger.info 'Initializing Error publisher'
+    info 'Initializing Error publisher'
 
     rf = RabbitFactory.new(ConnectionManager.instance.rabbit)
 
@@ -15,29 +15,20 @@ class ErrorPublisher
     @q = rf.q('q.web.errors').bind(@x, routing_key: 'errors')
   end
 
-  def publish(error, owner_info)
-    Celluloid.logger.info "#{owner_info.to_log_format} Publishing Error #{error.class.name}"
-    @x.publish(msg(error, owner_info).to_json, routing_key: 'errors')
+  def publish(error, owner_data)
+    info "[#{owner_data.to_log_format}][ERROR_PUBLISHER] Publishing error #{error.class.name}"
+    @x.publish(msg(error, owner_data).to_json, routing_key: 'errors')
   end
-  add_transaction_tracer :publish, :category => 'OtherTransaction/Publishers'
 
-  def msg(error, owner_info)
+  def msg(error, owner_data)
     {
       error: {
         klass: error.class.name,
         message: error.message,
         backtrace: error.backtrace.join("\n"),
-        service_id: owner_info.service.id # TODO: remove, already contained in meta
+        service_id: owner_data.service.id # TODO: remove, already contained in meta
       },
-      meta: {
-        type_name: owner_info.service.type_name,
-        brand_id: owner_info.brand.id,
-        embed_id: owner_info.embed.id,
-        service_id: owner_info.service.id,
-        brand_name: owner_info.brand.name,
-        embed_name: owner_info.embed.name,
-        feed_name: owner_info.service.feed_name
-      }
+      meta: owner_data.to_h
     }
   end
 
