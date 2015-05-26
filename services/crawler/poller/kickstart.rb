4685e413b9874bd59bca9abd1e969b606d223e09
Veljko Dragsic <veljko@bitovi.com>
2015-05-26 16:12:17 +0200

    rbtrace for debugging on testing.

--- services/crawler/poller/kickstart.rb
+++ services/crawler/poller/kickstart.rb
@@ -8,6 +8,8 @@ $:.unshift(ROOT_DIR)
 $:.unshift(File.join(ROOT_DIR, 'lib'))
 $:.unshift(File.join(ROOT_DIR, 'app', 'models'))
 
+require 'rbtrace'
+
 # theirs
 require 'bundler/setup'
 require 'rubygems'
