0cdd65a37c373459dc728c57dd397997a0a4fadb
Mihael Konjevic <konjevic@gmail.com>
2015-05-29 14:47:09 +0200

    Finish the implementation of the infinite vertial bit component
    Former-commit-id: d7296dab4643a19415675fca826c27d3d75b07f6 [formerly 56d38bbb4d38f1d720b750b46f507a79766423a7] [formerly 2ec007a955470bad24ce99df4ae357226236b65b [formerly aa925886804e6e530071fbd11489439359dae77a]] [formerly 2b0ea7e2da1bb09a5d0d0c482717131cf81fe011 [formerly 7598e5ac7fcee93b06d53feb6a0abb28c7dea476] [formerly 7d691b0871a3fa14026c00a4fb866c73835347ac [formerly 28de89431bbe4c4c20a4ee5b3ad98e3f6d502de5]]]
    Former-commit-id: 8c302eccd202529f54843b40bd11326369c83582 [formerly c819c4b5b749c600912b784086a3b10089e366ac] [formerly 7ca2488597354946952b46fe29c38f04a96b5dd7 [formerly 1461b20361f1a62f4212329e94610e11d3b8d74e]]
    Former-commit-id: 222c3c706c015e285fc8d6c92ad11322d23d1e53 [formerly a7236cf4da7b0028ab326f56329b8fb97126e61e]
    Former-commit-id: 476b438b692ad9b54d4684dafe15c61724616d2c

--- public/bits_vertical_infinite/bits_vertical_infinite.js
+++ public/bits_vertical_infinite/bits_vertical_infinite.js
@@ -1,15 +1,12 @@
 import can from "can/";
 import initView from "./bits_vertical_infinite.stache!";
-import _map from "lodash/collections/map";
-import _reduce from "lodash/collections/reduce";
 import _throttle from "lodash/functions/throttle";
+import PartitionedColumnList from "./partitioned_column_list";
 
 import "./bits_vertical_infinite.less!";
 import "can/construct/proxy/";
 
 var CARD_MIN_WIDTH = 300;
-var PER_PAGE = 50;
-
 
 var calculateColumnCount = function(el){
 	var width = el && el.width() || 0;
@@ -18,247 +15,6 @@ var calculateColumnCount = function(el){
 	}
 	return Math.min(5, Math.floor(width / CARD_MIN_WIDTH));
 };
-
-var makeColumns = function(count){
-	return _map(Array(count), () => { return []; });
-};
-
-var makeCalculateCurrent = function(currentColumn, columnCount){
-	return function(){
-		currentColumn++;
-		if(currentColumn === columnCount){
-			currentColumn = 0;
-		}
-		return currentColumn;
-	};
-};
-
-var makePerColumnAmount = function(columnCount, limit){
-	var columns = _map(Array(columnCount), () => { return 0; });
-	var currentColumn = 0;
-	var calculateCurrent = makeCalculateCurrent(0, columnCount);
-
-	for(var i = 0; i < limit; i++){
-		columns[currentColumn]++;
-		currentColumn = calculateCurrent();
-	}
-
-	return columns;
-};
-
-var totalCount = function(columns){
-	return _reduce(columns, (acc, c) => { return acc + c.length; }, 0);
-};
-
-export var PartitionedColumnList = can.Map.extend({
-	init : function(source){
-		this.attr({
-			__allData : source || [],
-			__columns : [],
-			__currentColumn : 0,
-			__currentPrependColumn : 0,
-			__columnCount : 0,
-			__limit : Infinity,
-			__prependPaused: false,
-		});
-
-		if(source){
-			source.on('add', this.proxy('replicateChangesFromSource'));
-			source.on('remove', this.proxy('replicateChangesFromSource'));
-		}
-	},
-	replicateChangesFromSource : function(ev, elements, index){
-		var isPrependPaused = this.attr('__prependPaused');
-		var what = ev.type;
-		var isTail;
-
-		if(what === 'add'){
-			isTail = this.__allData.length - elements.length === index;
-			if(isTail){
-				this.append(elements);
-			} else if(!isPrependPaused){
-				if(index === 0){
-					this.prependImmediately(elements);
-				} else {
-					can.batch.start();
-					this.clearColumns();
-					this.resetCurrentColumn();
-					this.append();
-					can.batch.stop();
-				}
-			} else if(isPrependPaused){
-				this.attr('__dataAddedWhilePrependPaused', true);
-			}
-		} else if(what === 'remove'){
-			this.removeItems(elements);
-		}
-	},
-	removeItems : function(items){
-		var columns = this.attr('__columns');
-		var index;
-		can.batch.start();
-		for(var i = 0; i < items.length; i++){
-			for(var j = 0; j < columns.length; j++){
-				index = columns[j].indexOf(items[i]);
-				if(index > -1){
-					columns[j].splice(index, 1);
-				}
-			}
-		}
-		can.batch.stop();
-	},
-	clearColumns : function(){
-		var columns = this.attr('__columns');
-		for(var i = 0; i < columns.length; i++){
-			columns[i].splice(0);
-		}
-	},
-	resetCurrentColumn : function(){
-		this.attr('__currentColumn', 0);
-	},
-	prependPaused : function(val){
-		this.attr('__prependPaused', val);
-		if(!val){
-			this.attr({
-				__currentPrependColumn: 0,
-				__dataAddedWhilePrependPaused: false
-			});
-		}
-	},
-	columns : function(){
-		return this.attr('__columns');
-	},
-	columnCount : function(){
-		return this.attr('__columnCount');
-	},
-	limit : function(){
-		return this.attr('__limit');
-	},
-	hasPending : function(){
-		return !this.__pendingItems || this.__pendingItems.length === 0;
-	},
-	source : function(){
-		return this.attr('__allData');
-	},
-	addPending : function(item){
-		return item;
-	},
-	append : function(newData){
-		var currentColumn = this.attr('__currentColumn');
-		var columnCount = this.attr('__columnCount');
-		var allData = this.attr('__allData');
-		var columns = this.columns();
-		var limit = this.attr('__limit');
-		var calculateCurrent = makeCalculateCurrent(currentColumn, columnCount);
-		var appendingData = newData ? newData : allData;
-
-		can.batch.start();
-
-		for(var i = 0; i < appendingData.length; i++){
-			if(limit === Infinity || totalCount(columns) < limit){
-				columns[currentColumn].push(this.addPending(appendingData[i]));
-				currentColumn = calculateCurrent();
-			}
-		}
-
-		this.attr('__currentColumn', currentColumn);
-		can.batch.stop();
-	},
-	prependImmediately : function(newData){
-		var columnCount = this.attr('__columnCount');
-		var columns = this.columns();
-		var currentPrependColumn = this.attr('__currentPrependColumn');
-		var calculateCurrent = makeCalculateCurrent(currentPrependColumn, columnCount);
-		var currentLimit = this.attr('__limit');
-		can.batch.start();
-		for(var i = 0; i < newData.length; i++){
-			columns[currentPrependColumn].unshift(this.addPending(newData[i], true));
-			currentPrependColumn = calculateCurrent();
-		}
-		this.attr('__currentPrependColumn', currentPrependColumn);
-		this.attr('__limit', currentLimit + newData.length);
-		can.batch.stop();
-	},
-	appendCb : function(){
-		return (data) => { this.append(data); };
-	},
-	resetColumns : function(newColumnCount){
-		var currentColumnCount = this.attr('__columnCount');
-		newColumnCount = newColumnCount || this.attr('__columnCount');
-		
-		can.batch.start();
-		this.attr({
-			__currentColumn : 0,
-			__columnCount : newColumnCount
-		});
-		
-		if(currentColumnCount !== newColumnCount){
-			this.attr('__columns').replace(makeColumns(newColumnCount));
-		} else {
-			this.clearColumns();
-		}
-		this.append();
-		// We need to append current data to new columns
-		can.batch.stop();
-	},
-	resetColumnsAndAppend : function(newColumnCount, data){
-		can.batch.start();
-		this.resetColumns(newColumnCount);
-		this.append(data);
-		can.batch.stop();
-	},
-	setLimit : function(limit){
-		var columns = this.columns();
-		var columnCount = this.attr('__columnCount');
-		var currentLimit = this.attr('__limit');
-		var currentColumn = this.attr('__currentColumn');
-		var calculateCurrent = makeCalculateCurrent(currentColumn, columnCount);
-		var allData = this.attr('__allData');
-		var appendUntil = limit > allData.length ? allData.length : limit;
-		var perColumnAmount, i;
-		
-		can.batch.start();
-		if(appendUntil >= currentLimit){
-			for(i = currentLimit; i < appendUntil; i++){
-				columns[currentColumn].push(this.addPending(allData[i]));
-				currentColumn = calculateCurrent();
-			}
-			this.attr('__currentColumn', currentColumn);
-		} else if(limit !== Infinity) {
-			// Mutate column lists in place so we wouldn't trigger
-			// CanJS live binding for columns. This way items that stay
-			// in page won't be removed and then inserted again.
-			perColumnAmount = makePerColumnAmount(columnCount, appendUntil);
-			for(i = 0; i < columnCount; i++){
-				columns[i].splice(perColumnAmount[i], columns[i].length);
-			}
-			
-			currentColumn = (appendUntil % columnCount);
-			if(currentColumn === columnCount){
-				currentColumn = 0;
-			}
-
-			this.attr('__currentColumn', currentColumn);
-		}
-		this.attr('__limit', limit);
-		can.batch.stop();
-	},
-	hasDataAfterLimit : function(){
-		var allData = this.attr('__allData');
-		var firstIndex = allData.indexOf(this.columns()[0][0]);
-		var length = allData.length;
-		var limit = this.attr('__limit');
-		
-		return (length - firstIndex - limit > 0);
-	},
-	resetFromTopIfNeeded : function(){
-		this.attr('__limit', PER_PAGE);
-		if(this.attr('__dataAddedWhilePrependPaused')){
-			this.resetColumns();
-		}
-		this.prependPaused(false);
-	}
-});
 
 var PartitionedColumnListWithDeferredRendering = PartitionedColumnList.extend({
 	addPending : function(item, shouldUnshift){
@@ -322,14 +78,14 @@ can.Component.extend({
 				var newColumnCount =  calculateColumnCount(this.element);
 				
 				if(currentColumnCount !== newColumnCount){
-					partitionedList.resetColumns(newColumnCount);
-					partitionedList.append();
+					partitionedList.resetColumns(newColumnCount, true);
 				}
 			}, 1);
 		},
 		nextPage : function(){
 			var partitionedList = this.scope.attr('partitionedList');
 			
+			// If we already made a request at this scroll height
 			if(this.__minHeight === this.__minHeightTriggeredReq){
 				return;
 			}
@@ -337,20 +93,22 @@ can.Component.extend({
 			this.__minHeightTriggeredReq = this.__minHeight;
 			
 			if(partitionedList.hasDataAfterLimit()){
-				partitionedList.setLimit(partitionedList.limit() + PER_PAGE);
+				partitionedList.nextPage();
 			} else {
-				partitionedList.setLimit(Infinity);
+				partitionedList.setLimitAndFillColumns(Infinity);
 				this.element.trigger('bits:nextPage');
 			}
 		},
 		scrollHandler : function(){
 			var scrollTop = this.element.scrollTop();
 			var scrollHeight = (this.__minHeight || this.element.prop('scrollHeight'));
-			var height =  this.element.height();
+			var height = this.element.height();
 			var partitionedList = this.scope.attr('partitionedList');
 			var onBottom = scrollHeight - scrollTop - height < 400;
 			var isLoading = this.scope.attr('isLoading');
 			
+			this.element.trigger('interaction:scroll', [this.scope.attr('state.hubId')]);
+
 			if(scrollTop === 0){
 				partitionedList.resetFromTopIfNeeded();
 				setTimeout(this.proxy('calculateMinHeight'), 1);
