59e1debf9af38f73e3ff575648f6d552a58882de
Mihael Konjevic <konjevic@gmail.com>
2015-05-29 14:47:09 +0200

    Finish the implementation of the infinite vertial bit component
    Former-commit-id: d82d2397f587656c1a39794853a21ed1ee3a5cba [formerly 2829d03fee90551b4e01dfbc16b2c0cdac269e39] [formerly b7304aa265943cd76d90c83691d968740c53b984 [formerly 5527cb9e93cde8e732efb04ce40fc280151c381a]] [formerly 68c01bad9460622cebf96c4314cfa4ca1edf352e [formerly 9033ac01df7ddc26bdc1801bb3c777623954e8c9] [formerly a9250774427206542340da44bd9a64df3e7fd7f0 [formerly 2c9997461daef1d490d824d850200eaa00ec31ee]]]
    Former-commit-id: 5d6dd21e504507f9ae9ebaecdc8a28bae318b23a [formerly ac416e9e26161b154490c880965668913bf385a9] [formerly cb9c73d8271646a4fb353ba3a02d831ebe9bc3ee [formerly ca4b4a6c23130fe3df6991640466d1bb22a02b0a]]
    Former-commit-id: 22a6aa2be805513cdf950252f1791aecbdddde68 [formerly 11779c5ce749a91bda1e25b69f9543ef91c84590]
    Former-commit-id: 958f35b835df791e731e6ba531343a6183ef6ee1

--- public/bit-list/bit-list.js
+++ public/bit-list/bit-list.js
@@ -9,23 +9,6 @@ import "can/construct/proxy/";
 import "bit/";
 import "bits_vertical_infinite/";
 
-var CARD_MIN_WIDTH = 300;
-
-var calculateColumnCount = function(el){
-	var width = el.width();
-	if(width < CARD_MIN_WIDTH) {
-		return 1;
-	}
-	return Math.min(5, Math.floor(width / CARD_MIN_WIDTH));
-};
-
-var makeColumns = function(count){
-	return _map(new Array(count), function(){
-		return new can.List();
-	});
-};
-
-var WINDOW_COUNT = 50;
 
 export default can.Control.extend({
 	pluginName : 'bh-bits',
@@ -34,7 +17,6 @@ export default can.Control.extend({
 		opts = opts || {};
 		opts.isLoading   = can.compute(false);
 		opts.hasNextPage = can.compute(true);
-		opts.currentScrollTop = can.compute(0);
 		return this._super(el, opts);
 	},
 	init : function(){
@@ -86,244 +68,17 @@ export default can.Control.extend({
 		
 		this.load();
 	},
-	/* =
-=======
-
-import "can/construct/super/";
-import "can/construct/proxy/";
-import "bit/";
-
-var CARD_MIN_WIDTH = 300;
-
-var calculateColumnCount = function(el){
-	var width = el.width();
-	if(width < CARD_MIN_WIDTH) {
-		return 1;
-	}
-	return Math.min(5, Math.floor(width / CARD_MIN_WIDTH));
-};
-
-var makeColumns = function(count){
-	return _map(new Array(count), function(){
-		return new can.List();
-	});
-};
-
-var WINDOW_COUNT = 50;
-
-export default can.Control.extend({
-	pluginName : 'bh-bits',
-}, {
-	setup : function(el, opts){
-		opts = opts || {};
-		opts.columnCount = can.compute(0);
-		opts.isLoading   = can.compute(false);
-		opts.hasNextPage = can.compute(true);
-		opts.currentScrollTop = can.compute(0);
-		return this._super(el, opts);
-	},
-	init : function(){
-		this.__timeouts = {};
-		this.__minHeight = 0;
-
-		this.columns = new can.List();
-
-		this.element.html(initView({
-			isLoading : this.options.isLoading,
-			columnCount : this.options.columnCount,
-			state : this.options.state,
-			columns : this.columns
-		}));
-
-		this.__hasItemsOnTop = false;
-
-		this.updateColumnCount();
-		this.load();
-	},
-	load : function(){
-		var self = this;
-		this.options.isLoading(true);
-
-		this.__pendingReq = Bit.findAll(this.options.state.getParams()).then(function(data){
-			var bits = self.options.state.attr('bits');
-
-			can.batch.start();
-
-			bits.push.apply(bits, data);
-			self.options.isLoading(false);
-
-			if(data.length < self.options.state.attr('params.limit')){
-				self.options.hasNextPage(false);
-			}
-
-			self.currentLimit = self.currentLimit + data.length;
-			self.partition(data);
-
-			delete self.__pendingReq;
-
-			can.batch.stop();
-		});
-	},
->>>>>>> 1202d983204d8a64199f6cfe235d45a46b689d20
-	updateColumnCount : function(){
-		this.options.columnCount(calculateColumnCount(this.element));
-	},
-	'{window} resize' : function(){
-		this.clearTimeout('windowResize');
-		this.setTimeout('windowResize', 100, 'updateColumnCount');
-	},
-	'{columnCount} change' : function(compute, ev, newVal){
-		this.resetColumns(newVal);
-	},
-	"{state.bits} partition" : function(){
-		if(this.options.currentScrollTop() === 0){
-			this.resetColumns(this.options.columnCount());
-		}
-	},
-	"{currentScrollTop} change" : function(currentScrollTop, ev, newVal){
-		if(newVal !== 0){
-			return;
-		}
-		var columnCount = this.options.columnCount();
-		var perColumns = can.map(new Array(columnCount), function(){ return 0; });
-		var currentCount = 0;
-		var i;
-		
-		this.currentColumn = 0;
-
-		for(i = 0; i < WINDOW_COUNT; i++){
-			perColumns[this.currentColumn]++;
-			this.currentColumn++;
-			currentCount++;
-			if(this.currentColumn === columnCount){
-				this.currentColumn = 0;
-			}
-		}
-
-		can.batch.start();
-		for(i = 0; i < columnCount; i++){
-			this.columns[i].splice(perColumns[i], this.columns[i].length);
-		}
-		can.batch.stop();
-		
-		this.currentLimit = currentCount;
-		setTimeout(this.proxy('calculateMinHeight'), 1);
-	},
-	resetColumns : function(columnCount){
-		this.columns.replace(makeColumns(columnCount));
-		this.currentColumn = 0;
-		this.partitionFromTop(this.options.state.attr('bits'));
-	},
-	partitionFromTop : function(bits){
-		this.currentLimit = 0;
-		this.partitionPart(bits);
-	},
-	partitionPart : function(bits){
-		this.partition(bits.slice(this.currentLimit, this.currentLimit + WINDOW_COUNT));
-		this.currentLimit = this.currentLimit + WINDOW_COUNT;
-	},
-	partition : function(bits){
-		var columnLength = this.columns.length;
-		var self = this;
-		var start = 0;
-		var partitionFn = function(){
-			var end = start + 5;
-
-			if(bits.length < end){
-				end = bits.length;
-			}
-
-			can.batch.start();
-			for(var i = start; i < end; i++){
-				console.log(self.currentColumn)
-				self.columns[self.currentColumn].push(bits[i]);
-				self.currentColumn++;
-				if(self.currentColumn >= columnLength){
-					self.currentColumn = 0;
-				}
-			}
-			can.batch.stop();
-			
-			if(end < bits.length){
-				start = end;
-				setTimeout(partitionFn, 1);
-			}
-			self.calculateMinHeight();
-		};
-<<<<<<< HEAD
-
-		partitionFn();
-
-	},
-	nextPage : function(){
-		var params;
-		var bits = this.options.state.attr('bits');
-
-		if(this.currentLimit < bits.length){
-			this.partitionPart(bits);
-		} else if(!this.options.isLoading() && this.options.hasNextPage()){
-			params = this.options.state.attr('params');
-			params.attr('offset', this.options.state.attr('bits.length'));
-			this.load();
-		}
-	},
-	appendContent : function(){
-		var self = this;
-		this.clearTimeout('appendContent');
-
-		if(!this.__scrollInteractionRecorded && !this.options.state.isAdmin()){
-			this.__scrollInteractionRecorded = true;
-			InteractionEvent.createScrollInteraction(this.options.state.attr('hubId'));
-		}
-		
-		this.setTimeout('appendContent', 100, function(){
-			var scrollTop = self.element.scrollTop();
-			var scrollHeight = self.__minHeight || self.element.prop('scrollHeight');
-			var height =  self.element.height();
-
-			self.options.currentScrollTop(scrollTop);
-
-			if(scrollHeight - scrollTop - height < 500){
-				self.nextPage();
-			}
-		});
-	},
-	scroll : 'appendContent',*/
-	clearTimeout : function(name){
-		clearTimeout(this.__timeouts[name]);
-		delete this.__timeouts[name];
-	},
-	setTimeout : function(name, timeout, fn){
-		if(typeof fn === 'string'){
-			fn = this.proxy(fn);
-		}
-		this.__timeouts[name] = setTimeout(fn, timeout);
-	},
-	clearAllTimeouts : function(){
-		for(var k in this.__timeouts){
-			this.clearTimeout(k);
-		}
-	},
-	clearPendingReq : function(){
+	destroy : function(){
 		if(this.__pendingReq){
 			this.__pendingReq.abort();
 		}
-	},
-	destroy : function(){
-		this.clearAllTimeouts();
 		return this._super.apply(this, arguments);
 	},
-	"bit:loaded" : 'calculateMinHeight',
-	calculateMinHeight : function(){
-		if(!this.element){
-			return;
+	'interaction:scroll' : function(el, ev, hubId){
+		if(!this.__savedScrollInteraction){
+			InteractionEvent.createScrollInteraction(hubId);
+			this.__savedScrollInteraction = true;
 		}
-
-		var heights = can.map(this.element.find('.column'), function(c){
-			return $(c).height();
-		});
-		var minHeight = Math.min.apply(Math, heights);
-		this.__minHeight = minHeight;
 	},
 	'interaction:link' : function(el, ev, hubId, entityId){
 		if(!this.options.state.isAdmin()){
