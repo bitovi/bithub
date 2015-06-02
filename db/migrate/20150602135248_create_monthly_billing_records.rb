11204a202bff2d512d50ae11b7525787b954fac1
Veljko Dragsic <veljko@bitovi.com>
2015-06-02 22:51:53 +0200

    Monthly billing tables, models and manager.
    Former-commit-id: cda0d60259be14db3d6d5999520018a5c41afed4 [formerly 49fc0b229779b90d5383794e3306ad9cc1bccdba] [formerly 95704d469e1214995ea64bede50df8a6186e9efa [formerly c336d3efd2ce23bdc6d5ff3a26ef535fac6a347a]] [formerly 1215319ac575813dc5d5c81e969bfd1a239dd89e [formerly c9fb12974e6f59920b614674fb06b792544fc4a6] [formerly 572fd7efaa0b516a2a377fba60218e453f7ef3e5 [formerly bc8c82126d7c732b3955947f339eb6dd9074eaeb]]]
    Former-commit-id: 7538ce7656a2e3144e202639ed13e5f3459efece [formerly 50e9786ff0330350d33b3f88022a1e2ebc87c966] [formerly fe3593be6c963a2573b934f17cc22f665e8bb954 [formerly 4d8df88045d094b93fe8a7ad84d02e4d1a4afcbb]]
    Former-commit-id: f1518bd64b4f64af298950b315f913362f5193c2 [formerly 37a1954954ca0285b85b18b6f2b27d650db13eb0]
    Former-commit-id: ec6520d48f33e3c11f53fb783d7df51770020589

---
+++ db/migrate/20150602135248_create_monthly_billing_records.rb
@@ -0,0 +1,18 @@
+class CreateMonthlyBillingRecords < ActiveRecord::Migration
+  def change
+    create_table :monthly_billing_records do |t|
+
+      t.references :monthly_billings
+
+      t.string  :description
+      t.integer :amount,      default: 0
+      t.integer :price,       default: 0
+      t.string  :currency,    default: 'USD'
+
+      t.hstore  :props,       default: ''
+
+      t.timestamps
+
+    end if Apartment::Tenant.current == 'public'
+  end
+end
