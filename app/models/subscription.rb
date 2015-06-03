d2bc832b8549a36fa40eb1509661386c0650c004
Veljko Dragsic <veljko@bitovi.com>
2015-06-03 15:47:02 +0200

    Commented code related to plan based billing.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,13 +5,13 @@ class Subscription < ActiveRecord::Base
   belongs_to :plan
   has_many :payments
 
-  validates :plan_id, :presence => true
+  # validates :plan_id, :presence => true
 
   before_destroy :delete_stripe_customer
 
-  after_customer_subscription_updated! do |subscription, event|
-    self.update_from_subscription(subscription, event_id: event.id)
-  end
+  # after_customer_subscription_updated! do |subscription, event|
+  #   self.update_from_subscription(subscription, event_id: event.id)
+  # end
 
   def update_card(stripe_token)
     stripe_customer = Stripe::Customer.retrieve self.stripe_customer_id
@@ -29,40 +29,40 @@ class Subscription < ActiveRecord::Base
     end
   end
 
-  def update_plan(stripe_plan_id)
-    stripe_customer = Stripe::Customer.retrieve self.stripe_customer_id
-    stripe_subscription = stripe_customer.subscriptions.retrieve self.stripe_subscription_id
-    stripe_subscription.plan = stripe_plan_id
+  # def update_plan(stripe_plan_id)
+  #   stripe_customer = Stripe::Customer.retrieve self.stripe_customer_id
+  #   stripe_subscription = stripe_customer.subscriptions.retrieve self.stripe_subscription_id
+  #   stripe_subscription.plan = stripe_plan_id
 
-    if stripe_subscription.save
-      if plan = Plan.find_by_stripe_id(stripe_plan_id)
-        update_attributes!({plan: plan})
-      end
-    end
-  end
+  #   if stripe_subscription.save
+  #     if plan = Plan.find_by_stripe_id(stripe_plan_id)
+  #       update_attributes!({plan: plan})
+  #     end
+  #   end
+  # end
 
-  def self.update_from_subscription(subscription, opts={})
-    attrs = {
-      stripe_event_id: opts[:event_id],
-      stripe_subscription_status: subscription.status,
-    }
+  # def self.update_from_subscription(subscription, opts={})
+  #   attrs = {
+  #     stripe_event_id: opts[:event_id],
+  #     stripe_subscription_status: subscription.status,
+  #   }
 
-    if existing = self.find_by_customer_id(subscription.customer)
-      unless existing.update_attributes(attrs)
-        Rails.logger.warn "[Stripe Webhook] Updating subscription '#{subscription.id}' failed! \n#{existing.errors.messages}"
-      end
-    else
-      Rails.logger.warn "[Stripe Webhook] Subscription '#{subscription.id}' not found!"
-    end
-  end
+  #   if existing = self.find_by_customer_id(subscription.customer)
+  #     unless existing.update_attributes(attrs)
+  #       Rails.logger.warn "[Stripe Webhook] Updating subscription '#{subscription.id}' failed! \n#{existing.errors.messages}"
+  #     end
+  #   else
+  #     Rails.logger.warn "[Stripe Webhook] Subscription '#{subscription.id}' not found!"
+  #   end
+  # end
 
   def self.find_by_customer_id(id)
     where(stripe_customer_id: id).order(:created_at).last
   end
 
-  def self.available_plans
-    Stripe::Plans.constants.map {|p| p.to_s.downcase}.reject {|p| p == 'configuration'}
-  end
+  # def self.available_plans
+  #   Stripe::Plans.constants.map {|p| p.to_s.downcase}.reject {|p| p == 'configuration'}
+  # end
 
   def self.current
     # TODO: determine through organization
@@ -75,7 +75,7 @@ class Subscription < ActiveRecord::Base
       Rails.logger.info "Stripe customer #{customer.id} already exists for org #{organization.name}"
     else
       Rails.logger.info "Creating Stripe customer for org #{organization.name}"
-      customer = Stripe::Customer.create plan: plan.stripe_id
+      customer = Stripe::Customer.create ### plan: plan.stripe_id
     end
 
     subscription = customer.subscriptions.data.first
