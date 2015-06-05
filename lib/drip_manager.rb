f967c846238c066621a4662889fc3c9551f4149f
Veljko Dragsic <veljko@bitovi.com>
2015-06-05 15:30:02 +0200

    New accounts subscribed to Drip.

---
+++ lib/drip_manager.rb
@@ -0,0 +1,17 @@
+class DripManager
+
+  def create_or_update_subscriber(email, opts={})
+    force = opts.fetch(:force) { false }
+
+    if force || Rails.env.production?
+      client.create_or_update_subscriber email
+    end
+  end
+
+  def client
+    @client ||= Drip::Client.new do |c|
+      c.api_key = ENV['DRIP_API_KEY']
+      c.account_id = ENV['DRIP_ACCOUNT_ID']
+    end
+  end
+end
