a70dd552c99ea54a1fb8d77b6a931fd90958a61e
Nikica Jokic <neektza@gmail.com>
2015-06-05 14:33:34 +0200

    Production env logger changed.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,9 +32,9 @@ Bithub::Application.configure do
 
   # Logging with log4r
   lf = LoggerFactory.new 'rails', :environment => Rails.env
-  config.logger = lf.component_logger
-  config.action_controller.logger = lf.ac_logger
-  config.active_record.logger = lf.ar_logger
+  config.logger = lf.logger
+  config.action_controller.logger = lf.loggers('action_controller')
+  config.active_record.logger = lf.loggers('active_record')
   config.lograge.enabled = true
   config.log_level = :info
 
