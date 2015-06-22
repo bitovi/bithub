eea1b6354f64e0b56a3b316c486cdb7d9e171be1
Nikica Jokic <neektza@gmail.com>
2015-06-22 19:37:38 +0200

    Call new Type determination code from Github events/entities properly.
    Former-commit-id: 829916c8c9617b3de3247ce63051931259513306 [formerly b67b299a0ea0376b2f81a767f017c764e36f2d3a] [formerly a5147269506c28ca2ad6ca109c7ea559b932aa52 [formerly f7697501a9ea4622e58c29272c903dd435fd8a52]] [formerly 6efaa22c8b0695593014b4fbbdda1787699117ea [formerly e83893b72086731ad70b418f92226c903e276059] [formerly a6a9f4388fe7f0f40069863859dbb2a673bf7e0a [formerly 7dcbd148d2a078db6cb04e2b33054ef4730213e1]]]
    Former-commit-id: c4f1e9b626455beb07344acfc6326e8b2ab41d86 [formerly f7493c1c4775144f50382b8628e4cab186ed79a5] [formerly 3ea483211fce43c698a636d73557de007a3165de [formerly 48100104173848271fbfeb0566085ab015903c8e]]
    Former-commit-id: 820de1e86e960759b384c053632dea93f3bad844 [formerly 048c8bda41fbdf3920c4b9fe83ab02cdddef43ef]
    Former-commit-id: 92d40d8dda93a11091d208ffeacbdbb92a3c054e

--- app/models/entities/github/issue.rb
+++ app/models/entities/github/issue.rb
@@ -47,7 +47,7 @@ module Entities
           most_recent_child.source_data.symbolize_keys!
 
           data = most_recent_child.last_modified_by.source_data
-          event = Events::Dispatcher.dispatch(data, 'github')
+          event = Events::Github::TypeClassDeterminator.new(data).type_class.new(data)
 
           # IssueComment, has an embedded Issue or PullRequest that have labels etc
           if event.respond_to? :ipr
