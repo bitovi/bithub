ab74dcfcd6ec64657e4cd982868c941223f9c7aa
Nikica Jokic <neektza@gmail.com>
2015-06-22 20:44:07 +0200

    Specs fixed after moving stuff around.
    Former-commit-id: 5c54c301a918bcb19aaf09736f46550a77ec4667 [formerly e81bf6ca88595c72afaa457ef0e60955f1192712] [formerly 579e01c4e64bc2388014e8c2abbdc186c69ce3a5 [formerly 49517e38d931bf7ba534590e5b23916ac398da22]] [formerly e3e93e6289700504b6d877e3af5c362d29d2da28 [formerly f49ef9fd00f4a11119d4c76f34431a6e2dcef1ea] [formerly 9e1b939c58a5a6d591fd3091a8db82d4ec44876f [formerly b23a8ee4708abb0c57b0d0f015bdfa4700d1eeee]]]
    Former-commit-id: e5053d3da895f66568efd94eeae08aa0d46fa908 [formerly 960399df3106c36ee5d7344b866514269db950fb] [formerly f03c6ef0fc59b0fe35ffb9978dbea7c8e2dba8b2 [formerly 08b1b31f4f87d3b87f121ef55e5c47050faccab6]]
    Former-commit-id: 4dafc4a5be5caccf3bd72e276306fef7a6328ef8 [formerly 60bf53db034b18d6232886e11ed657337484705c]
    Former-commit-id: 2532512c07a3dbb452a025bda2a49c591be9ac7f

--- spec/models/entities/foursquare/checkin_spec.rb
+++ spec/models/entities/foursquare/checkin_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 describe Entities::Foursquare::Checkin do
   describe 'data' do
-    it 'prepares the data for building/updating' #do
-      # checkin_event = Events::Foursquare::CheckinEvent.new(
-      #   raw_data(response_path: 'foursquare/checkin_event')['response'][1])
+    it 'prepares the data for building/updating' do
+      raw = CGI.parse(File.read('spec/support/responses/foursquare/checkin_postback'))
+      checkin_event = Events::Foursquare::CheckinEvent.new(raw.merge({'venue' => {}}))
 
-      # entity_wrapper = Entities::Disqus::Post.new(checkin_event)
+      entity_wrapper = Entities::Disqus::Post.new(checkin_event)
 
-      # expect(entity_wrapper.data.keys).to include(
-      #   :title, :body, :url, :origin_id, :origin_ts)
+      expect(entity_wrapper.data.keys).to include(
+        :title, :body, :url, :origin_id, :origin_ts)
 
-      # expect(entity_wrapper.data[:props].keys).to include(
-      #   :origin_author_id, :origin_author_name,
-      #   :venue_id, :venue_name)
-    # end
+      expect(entity_wrapper.data[:props].keys).to include(
+        :origin_author_id, :origin_author_name,
+        :venue_id, :venue_name)
+    end
   end
 end
