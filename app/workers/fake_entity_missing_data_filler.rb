42d68ec1b21f88baa5bbc0132932661f56ae3cfd
Nikica Jokic <neektza@gmail.com>
2015-06-22 22:36:59 +0200

    Moved worker model under workers/
    Former-commit-id: d94c21dbc56484a8ec7b26ab425f5e976cf5f896 [formerly 400dc9fde2e6d41af8272597bd6a19e0b50ea052] [formerly 7ac6160300b4ba5626ff6e5cf3d976b18fda5af2 [formerly 730e81d1306dcbd5b9679983f2ee09056459bcd6]] [formerly 965258a3f890c0565cac0a49de1da39fe62d66a9 [formerly bed0ebbf4c39b0583064a25b73b7395ef16ede12] [formerly b47acefb11722d914fd00f9186d0b81fe12843f7 [formerly 279f0208f8294bc1b7148f5f3daf244fccb0311c]]]
    Former-commit-id: 8e41ac856020a036632aead2f0c6a2fa91a6ffa7 [formerly 1eda012f283614511f90615fb3b0ac58f7034658] [formerly 082b763065c0c1de89b9a11316d4f486df4f7a34 [formerly 673b63c51fdd34771670fc09c67d3dc2ac8f3074]]
    Former-commit-id: 08590a38709e3ed7808e45542064ce03277f1f60 [formerly bf331a73a52263f90816170cb497cfdbc95eece5]
    Former-commit-id: 887aa87952ad1bd0507802802b6e33e534b4b404

--- app/workers/fake_entity_missing_data_filler.rb
+++ app/workers/fake_entity_missing_data_filler.rb
@@ -9,7 +9,7 @@ module Workers
       Brand.pluck(:name).each do |name|
         Apartment::Tenant.switch name do
           Sidekiq.redis do |conn|
-            fff = ::Entities::Services::FakeFollowFiller.new(conn)
+            fff = Workers::Support::FakeFollowFiller.new(conn)
             fff.fill_missing
           end
         end
