dacdb153ee0aa08acb57a2dba3750cd9e7c0d84b
Veljko Dragsic <veljko@bitovi.com>
2015-06-22 15:17:15 +0200

    Revert "Facebook service from url."
    This reverts commit fc902210dd09466a97c27439c576272231c091e8 [formerly 792bdb6330373f0fe198d68fe6a83df4dc923588] [formerly e172cf0374a646ae572837cf1bf41f54cd7b38a0 [formerly 5e8bf99ce6cbc70bcbd304394ac524c8e3d4c60e]] [formerly de26943c5c04c17188cb039c64834bc8c1cc4fd0 [formerly 20377a39d883e18293acf5dc9c453ac06ae0e897] [formerly a2249a1a6864226e5f863b2617bc4a4de3a7beb1 [formerly 112d3de5cd0c27fbf42fb651b2695e6a08238c0d]]] [formerly 326cf1a6678107b2d50ae95ab4c7e99331eca173 [formerly d59699ed38dff5e565e111c59aac4e6f8eff3834] [formerly a77fbd58a41f148b9f03524023323de97f4ce1a2 [formerly 13803a3b9545d21da6a6f09b25a9bcab4f722388]] [formerly 822bdb3fe3c044a942811a12166806552cc20b18 [formerly 7bc5f91dfb2aa1b1b26285301c2127f469df5316] [formerly 009d4dddd51a202400b2be7a55939720a214e9e7 [formerly 3c0cf1d5aca6e98cd770f8aa64328f6ff66f3da6]]]].
    
    
    Former-commit-id: ecf08dddbcc516cb14b6189eaec57f61aa017d5e [formerly df7e70808af9ab4840e93199ee1789c6267ed103] [formerly 2aacc67edab88b2db6f0168b3591f7bce0f3a231 [formerly 6e98c6da744d408306dbe7482ab7e489b05f7b5b]] [formerly 1fc6aaa9b18d5bf7796a05372025ef84af9de458 [formerly 9c790e0dbbde02191ce187dc399de32377440984] [formerly 5c521d06bfe755c909a274014bf91076512120a1 [formerly f85b6e1b7ef2ae2994618cefd3896b79e9590624]]]
    Former-commit-id: 51ab6c1ace701c4422b6ff040aa1f146dd6fd39a [formerly 017604da0c4b64212cdf85e0c303a0aedce1c742] [formerly f5b6bf06e7ae1e85ed029f074fe8be6dac0d18d2 [formerly 00adbf7e0674dd7334f1dbca8a15f7744ca4631d]]
    Former-commit-id: 746944d37ce9b8646b4220cec90e989987a3635f [formerly e2269a079c1965041bc167cca00ac307e24e3bb1]
    Former-commit-id: b601a2db0f425a5d2b8d16e46af06a315cb8c126

--- app/models/services/types/facebook/page.rb
+++ app/models/services/types/facebook/page.rb
@@ -4,42 +4,10 @@ module Services
       class Page
         include Virtus.model(:strict => true)
         attribute :id, String
-        attribute :url, String
         attribute :display_name, String, :default => ''
-
-        def url=(url)
-          if page = page_from_url(url)
-            self.id = page['id']
-            self.display_name = page['name']
-          end
-
-          super url
+        def humanized_name=(name)
+          self.display_name = name
         end
-
-        private
-
-        def page_from_url(url)
-          if page_name_or_id = page_name_or_id_from_url(url)
-            graph_client.graph_call "v2.2/#{page_name_or_id}"
-          end
-        end
-
-        def page_name_or_id_from_url(url)
-          if res = /^http.?:\/\/.*facebook.com\/pages\/[^\/]*\/([^?\/]*)/.match(url)
-            res[1]
-          elsif res = /^http.?:\/\/.*facebook.com\/([^?\/]*)/.match(url)
-            res[1]
-          end
-        end
-
-        def graph_client
-          @client ||= Koala::Facebook::API.new app_token
-        end
-
-        def app_token
-          ENV['FACEBOOK_CLIENT_ID'] + '|' + ENV['FACEBOOK_CLIENT_SECRET']
-        end
-
       end
     end
   end
