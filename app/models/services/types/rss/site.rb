3d61d0523c7da6f78bf35c84ae10c04c374c146a
Veljko Dragsic <veljko@bitovi.com>
2015-07-15 23:18:01 +0200

    Rss service config extracts rss link from page.

--- app/models/services/types/rss/site.rb
+++ app/models/services/types/rss/site.rb
@@ -1,10 +1,27 @@
+require 'nokogiri'
+require 'open-uri'
+
 module Services
   module Types
     module Rss
+
       class Site
         include Virtus.model(:strict => true)
         attribute :url, String
         attribute :tag_with, String, required: false, default: lambda { |obj, attr| obj.url_as_tag(obj.url) }
+
+        def url=(url)
+          response = open(url)
+
+          if response.meta['content-type'].starts_with? 'text/html'
+            page = Nokogiri::HTML response
+            if rss_url = page.css("link[type='application/rss+xml']").first
+              url = rss_url['href'].starts_with?('/') ? File.join(url, rss_url['href']) : rss_url['href']
+            end
+          end
+
+          super url
+        end
 
         def url_as_tag(url)
           url = url.sub(/^https?\:\/\//, '').sub(/^www./,'')
@@ -12,6 +29,7 @@ module Services
             slug.chop! if slug.last == '-'
           end
         end
+
       end
     end
   end
