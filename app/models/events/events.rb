7b9dd67c7b126b277abcaf7157fa82d49f38e9d8
Nikica Jokic <neektza@gmail.com>
2015-07-16 13:56:02 +0200

    Respect you APIs.

--- app/models/events/events.rb
+++ app/models/events/events.rb
@@ -32,7 +32,7 @@ require 'events/type_determinator'
 
 module Events
   def self.event_instance(packet, hint = nil)
-    event_class(packet, hint).new(packet)
+    event_class(packet, hint).new(packet[:source_data], packet[:meta])
   end
 
   def self.event_class(packet, hint = nil)
