9f728cc8027183d14345f04b17d99156a944e1a8
Nikica Jokic <neektza@gmail.com>
2015-07-20 15:53:57 +0200

    Start Updater in listener/kickstart.
    Former-commit-id: ba94c6870b895a8f8b4e622439e259ce282367ac [formerly c19819dd7218682b83ff6e81f88501552d1d564b] [formerly 75909422cee84833ce7c38fbda6de8e4939df1c7 [formerly e69f49e458133c524b9b0acb392ba919e8bf370a]] [formerly f6264740c07f8f1bc4597d35edf59cacd7bce077 [formerly ca705db8278300c0cc3afbd5dadca71a0666b9f1] [formerly 7b20547b12647118eef10e600af918b43b36da17 [formerly 265ee06aa728144eb418e01e046be2d1b0388af6]]]
    Former-commit-id: 753e4b5f1303d7ed286dd84b45b0baa55abbfc4b [formerly 0fc6539d61dfb1b54de83d7fbf44dc3b63d48730] [formerly 85cf30fd84150ab196f83b4307809b2809435ded [formerly 7cba2d4c36b3a1d6a508652ec5d50bdc71f813fa]]
    Former-commit-id: 677b486e82ac8a90dbb88ab065bee2e271e63dc6 [formerly d11d58f05f84ce4695bc16a0dd31219cdcd6508e]
    Former-commit-id: 71c23d23f5657cecf57a5dc9b3f4ce823386f3ad

--- services/listener/kickstart.rb
+++ services/listener/kickstart.rb
@@ -41,6 +41,7 @@ Celluloid.logger = logger
 
 class WebWorkers < Celluloid::SupervisionGroup
   supervise(Persistor, as: :entity_persistor, args: [])
+  supervise(Updater, as: :entity_updater, args: [])
   supervise(Listener, as: :error_listener, args: ['q.web.errors', 'errors', ErrorHandler])
   supervise(Listener, as: :event_listener, args: ['q.web.events', 'events', EventHandler])
   supervise(Listener, as: :command_listener, args: ['q.web.commands', 'commands', CommandHandler])
