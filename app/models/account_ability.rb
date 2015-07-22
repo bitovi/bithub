f60c0e629ad003167963c8d224721ed63aed3326
Nikica Jokic <neektza@gmail.com>
2015-07-22 13:19:01 +0200

    CanCan abilities for Organization admins.

--- app/models/account_ability.rb
+++ app/models/account_ability.rb
@@ -4,57 +4,60 @@ class AccountAbility
   def initialize(account)
     if (account.has_role? :bithub_admin) || (ENV['RAILS_ENV'] == 'test')
       can :manage, :all
-
-    elsif (account.has_role? :organization_admin)
-      can :manage, Organization, id: account.organization_ids
-
-    elsif (account.roles_name & %w(brand_admin organization_admin)).length > 0
-      can :manage, Brand, organization_id: account.organization_ids
-
-    else
-      # can manage its own account
-      can :manage, Account, id: account.id
-
-      # can read its organization information
-      can [:create, :read], Organization, id: account.organization_ids
-
-      # can read and destroy it's own organization membership
-      can [:create, :read, :destroy], AccountOrganization, account_id: account.id
-
-      # can create/read/update/delete a brand
-      can [:create, :read], Brand, organization_id: account.organization_ids
-
-      # read/destroy owned brand identities
-      can [:create, :read], BrandIdentity, brand_id: account.brand_ids
-
-      # countries list
-      can :read, Country
-
-      # read plans, owned subscriptions with payments
-      can :read, Plan
-      can :manage, Subscription, organization_id: account.organization_ids # [:read, :current]
-      can :read, Payment, subscription: {organization_id: account.organization_ids}
-
-      can :block   , EmbedEntity
-      can :approve , EmbedEntity
-      can :pin     , EmbedEntity
-      can :unpin   , EmbedEntity
-
-      # models locked inside tenants
-      can :manage, Embed
-      can :moderate, Embed
-      
-      can :manage, Service
-      can :suggest, Service
-
-      can :manage, EmbedEntity
-      can :manage, EmbedPreset
-      can :manage, Entity
-      can :manage, Filter
-      can :manage, Grouping
-      can :manage, Histogram
-      can :manage, ServiceEntity
-      can :manage, User
     end
+
+    # Admin of an organization
+    if (account.has_role? :organization_admin)
+      can :manage, Organization, id: account.organization_ids
+    end
+
+    # Admin of a brand within an organization
+    if (account.roles_name & %w(brand_admin organization_admin)).length > 0
+      can :manage, Brand, organization_id: account.organization_ids
+    end
+
+    # can manage its own account
+    can :manage, Account, id: account.id
+
+    # can read its organization information
+    can [:create, :read], Organization, id: account.organization_ids
+
+    # can read and destroy it's own organization membership
+    can [:create, :read, :destroy], AccountOrganization, account_id: account.id
+
+    # can create/read/update/delete a brand
+    can [:create, :read], Brand, organization_id: account.organization_ids
+
+    # read/destroy owned brand identities
+    can [:create, :read], BrandIdentity, brand_id: account.brand_ids
+
+    # countries list
+    can :read, Country
+
+    # read plans, owned subscriptions with payments
+    can :read, Plan
+    can :manage, Subscription, organization_id: account.organization_ids # [:read, :current]
+    can :read, Payment, subscription: {organization_id: account.organization_ids}
+
+    can :block   , EmbedEntity
+    can :approve , EmbedEntity
+    can :pin     , EmbedEntity
+    can :unpin   , EmbedEntity
+
+    # models locked inside tenants
+    can :manage, Embed
+    can :moderate, Embed
+
+    can :manage, Service
+    can :suggest, Service
+
+    can :manage, EmbedEntity
+    can :manage, EmbedPreset
+    can :manage, Entity
+    can :manage, Filter
+    can :manage, Grouping
+    can :manage, Histogram
+    can :manage, ServiceEntity
+    can :manage, User
   end
 end
