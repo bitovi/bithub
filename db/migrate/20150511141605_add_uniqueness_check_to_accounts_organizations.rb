b4f85dc169bab1ae2faff481581b5a9cb6e00a59
Nikica Jokic <neektza@gmail.com>
2015-07-31 11:17:30 +0200

    After-merge cleanup and preparations migrations and data cleanup tasks

--- db/migrate/20150511141605_add_uniqueness_check_to_accounts_organizations.rb
+++ db/migrate/20150511141605_add_uniqueness_check_to_accounts_organizations.rb
@@ -1,9 +1,13 @@
 class AddUniquenessCheckToAccountsOrganizations < ActiveRecord::Migration
   def up
-    execute "alter table accounts_organizations add constraint no_double_links unique (account_id, organization_id);"
+    if Apartment::Tenant.current == 'public'
+      execute "alter table accounts_organizations add constraint no_double_links unique (account_id, organization_id);"
+    end
   end
 
   def down
-    execute "alter table accounts_organizations drop constraint no_double_links;"
+    if Apartment::Tenant.current == 'public'
+      execute "alter table accounts_organizations drop constraint no_double_links;"
+    end
   end
 end
