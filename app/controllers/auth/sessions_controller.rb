50b499e3323356e80d75d1ba9c834a23640214f7
Nikica Jokic <neektza@gmail.com>
2015-08-04 17:16:14 +0200

    Store tenant_name and organization_id in session after sign_in.
    Former-commit-id: 62f7a58a14d565313e3d3ec8e8a2eb14a8089b67 [formerly 0e48e4d5c08da17368ddeae8957065febec2c5ef] [formerly f23b3c0ee3db3b0e40b64a9f356b934c8c58261f [formerly 29aeae237bd416798cb7bffda408e9f0a565db11]] [formerly c41ee93395e4fedd266f3c33e78162a64dab9fe0 [formerly 3c3f8eefeabc6d75912d292c45415b3593fb5196] [formerly 9043ed9fef35c327e793d975e75f81ae81c1206d [formerly 8accadfc527a8dffbbf53d7ac992bcbaab45b451]]]
    Former-commit-id: fcae8e4bf6d2ff0cb2fda589748612e9afb573bf [formerly 3002e7bfe296d2cf44b9b28c5e082af51e661590] [formerly 4866840cbd3a93e7148a1bfda823ca35ff103b87 [formerly 11ced266fd733fb750ab021c77b7dfd7954cdb0a]]
    Former-commit-id: a9bd338b6af259bf5d8d72b36c5bda823caa9083 [formerly 83fe1e05e4941a84669c896ecaf7cd954bbb3ba0]
    Former-commit-id: 76605d4bed3461133af64749945745aa07857d77

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -1,5 +1,13 @@
 class Auth::SessionsController < Devise::SessionsController
 
+  def create
+    super do |account|
+      current_organization = account.organizations.first
+      session['organization_id'] = current_organization.id
+      session['tenant_name'] = current_organization.brands.first.tenant_name
+    end
+  end
+
   protected
 
   def after_sign_in_path_for(account)
