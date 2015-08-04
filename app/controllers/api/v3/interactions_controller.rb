58c300b94732e63e9dbce888a6f4f148944a05ab
Nikica Jokic <neektza@gmail.com>
2015-08-04 18:19:54 +0200

    Interaction analytics fixed.

--- app/controllers/api/v3/interactions_controller.rb
+++ app/controllers/api/v3/interactions_controller.rb
@@ -1,19 +1,21 @@
 class Api::V3::InteractionsController < Api::V3::BaseController
-
-  before_filter :authenticate_account!
-
   respond_to :html
+  before_action :switch_tenant
 
   def index
     @zoom = zoom
     @data = Interaction.stats(resolution, zoom, composed_filter).all
     render :index
+  ensure
+    Apartment::Tenant.switch!
   end
 
   def create
     @interaction = Interaction.new(interaction_params)
-    @interaction.save
+    @interaction.save!
     render json: @interaction
+  ensure
+    Apartment::Tenant.switch!
   end
 
   def destroy
@@ -24,7 +26,16 @@ class Api::V3::InteractionsController < Api::V3::BaseController
   private
 
   def interaction_params
-    params.require(:interaction).permit(:primary_source_id, :primary_source_type, :secondary_source_id, :secondary_source_type, :event_type, :event_subtype, :created_at)
+    params.require(:interaction).permit(
+      :primary_source_id,
+      :primary_source_type,
+      :secondary_source_id,
+      :secondary_source_type,
+      :event_type,
+      :event_subtype,
+      :created_at,
+      :tenant_name
+    )
   end
 
   def interaction_type
@@ -32,6 +43,17 @@ class Api::V3::InteractionsController < Api::V3::BaseController
   end
 
   private
+
+  def switch_tenant
+    fail 'Tenant must be provided in the session or as param' if !tenant_name
+    Apartment::Tenant.switch!(tenant_name)
+  end
+
+  def tenant_name
+    tenant_name = session[:tenant_name] || params[:interaction][:tenant_name]
+    params[:interaction].delete(:tenant_name)
+    tenant_name
+  end
   
   def resolution
     params['resolution'] || 'hour'
