8accadfc527a8dffbbf53d7ac992bcbaab45b451
Nikica Jokic <neektza@gmail.com>
2015-08-04 17:16:14 +0200

    Store tenant_name and organization_id in session after sign_in.

--- app/controllers/api/v3/current/organizations_controller.rb
+++ app/controllers/api/v3/current/organizations_controller.rb
@@ -4,6 +4,7 @@ class Api::V3::Current::OrganizationsController < Api::V3::Current::AbstractCont
   def choose
     if @organization = current_account.organizations.find_by_id(organization_id)
       session['organization_id'] = @organization.id
+      session['tenant_name']     = @organization.brands.first.tenant_name
       render :show
     else
       render json: { msg: 'You don\'t belong to that organization' }, status: 422
