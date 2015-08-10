bb401df8e49bd7b105e0c73614384ac55949828e
Nikica Jokic <neektza@gmail.com>
2015-08-10 20:39:40 +0200

    Prepared for deployment to staging.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,45 @@
 # config valid only for Capistrano 3.1
 #lock '3.1.0'
 
-set :application, 'web'
+set :application, 'bithub'
+set :user, 'bithub'
+
+role :db, "master"
+role :db, "slave"
 
 set :scm, :git
 set :repo_url, 'git@github.com:bitovi/bithub.git'
 ask :branch, 'master'
-set :deploy_to, '/home/bithub/web'
-set(:ssh_options, { forward_agent: true })
+set :deploy_to, '/home/bithub/bithub'
+set :ssh_options, { forward_agent: true }
 
-# Default value for :format is :pretty
 set :format, :pretty
-
-# Default value for :log_level is :debug
 set :log_level, :info
-
-# Default value for :pty is false
 set :pty, true
 
-# Default value for default_env is {}
-set :default_env, {
-  env: fetch(:stage),
-  rails_env: fetch(:stage)
-}
+set :linked_files, fetch(:linked_files, []).push(
+  'config/database.yml',
+)
 
-# Default value for keep_releases is 5
+set :linked_dirs, fetch(:linked_dirs, []).push(
+  'log',
+  'tmp/pids',
+  'tmp/cache',
+  'tmp/sockets',
+  'vendor/bundle',
+  'public/system'
+)
+
+set :default_env, { env: fetch(:stage), rails_env: fetch(:stage) }
+
 set :keep_releases, 10
 
 # Custom variables
-set :backup_path, "/backups/dbsnapshots/"
-set :backup_ext, ".backup"
-set :log_path, "/home/bithub/web/shared/log/"
+set :backup_path, "/pg_dumps/"
+set :backup_ext, ".dump"
 set :current_path, File.join([fetch(:deploy_to), 'current'])
 set :shared_path, File.join([fetch(:deploy_to), 'shared'])
-set :user, "bithub"
+set :log_path, File.join([fetch(:shared_path), 'log'])
 
 # 'foreman' command should be prefixed with 'rbenv exec' and 'bundle exec'
 set :rbenv_map_bins, fetch(:rbenv_map_bins, []).push('foreman')
