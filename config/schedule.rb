18f365f7aaf1b8e4da9f228e07359688ca0236ef
Nikica Jokic <neektza@gmail.com>
2015-08-24 16:02:46 +0200

    Defined schedules for new recurring tasks.

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,18 @@ every '10,20,30,40,50 * * * *' do
   rake 'recurring:fill_fake_follows'
 end
 
-every :day, at: '06:15am' do
+every :day, at: '05:30am' do
+  rake 'recurring:flag_inactive'
+end
+
+every :day, at: '06:30am' do
+  rake 'recurring:age_out_entities'
+end
+
+every :day, at: '07:30am' do
+  rake 'recurring:clean_orphaned_entities'
+end
+
+every :day, at: '09:30am' do
   command '/usr/bin/psql -c \'VACUUM FULL ANALYZE;\''
 end
