d6953db09efef0d2f85b44e1acae3efcc10a6ff4
Nikica Jokic <neektza@gmail.com>
2015-09-11 18:18:38 +0200

    Public<>Admin decision code made more explicit.
    Former-commit-id: fdadf66b6d588faf33e0bee1c6be019d960ac344 [formerly db027fbdba3a78d991e8f246913ac347645679a9] [formerly 14133f0495b49fdf8c441410ca4c56828001af56 [formerly 073456b0adf015087dde6188f619d05cf5bdab65]] [formerly ae955e3665d0d6b62bdbdd702d145ea9ea286b9b [formerly 514de6116384882a6aef406df1de2203df92ca11] [formerly f291c3dea6cc36b5eec7b8d8a9ce5a6298d0b979 [formerly 8bdf72e5cf2d0e29f8e9a57c0e2e84de870f2057]]]
    Former-commit-id: e808a30ba00e65983cccdf8a17a0ded28fb8715b [formerly a64c3eba88aba1e7820fbd6f3c827aca18352a3b] [formerly fdc45089d6cfab9ca56814501b5b1eb4de52644e [formerly 762f8477861c53c826770870cbb6ff416b37c5a1]]
    Former-commit-id: 17bdca93972565e32c4bd7616364fc8bb596c575 [formerly 70d7d60ef60524a266cd58981c8b85004a37cef1]
    Former-commit-id: d2917ab22b4565efa19398a5d6b920d424fe34a1

--- app/controllers/api/v3/embed_entities_controller.rb
+++ app/controllers/api/v3/embed_entities_controller.rb
@@ -9,14 +9,20 @@ class Api::V3::EmbedEntitiesController < Api::V3::ApiController
   helper_method :list_cache_key
 
   def index
-    unless (tn = params[:tenant_name]) && Apartment.tenant_names.include?(tn)
+    if account_signed_in? 
+      @tenant_name = Apartment::Tenant.current
+      @visibility = params[:view] || 'public'
+    else
+      @tenant_name = params[:tenant_name]
+      @visibility = 'public'
+    end
+
+    if @tenant_name.blank? || !Apartment.tenant_names.include?(@tenant_name)
       show_406('Valid tenant name must be provided')
       return 
     end
 
-    @visibility = current_account ? (params[:view] || 'public') : 'public'
-
-    Apartment::Tenant.switch(tn) do
+    Apartment::Tenant.switch(@tenant_name) do
       scope = build_scope
       @entities = EntityDecorator.decorate_collection(scope.all, context: { embed: owner_embed })
       render :index
