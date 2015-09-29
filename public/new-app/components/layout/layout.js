a7cd33c6880bd273451ce1ff26fd9bde50a86dd4
Mihael Konjevic <konjevic@gmail.com>
2015-09-29 20:51:27 +0200

    Work on the new frontend

--- public/new-app/components/layout/layout.js
+++ public/new-app/components/layout/layout.js
@@ -1,6 +1,7 @@
 import can from "can";
 import initView from "./layout.stache!";
 
+import appState from "models/app-state";
 import HubModel from "models/hub";
 
 import "./layout.less!";
@@ -15,7 +16,7 @@ export default can.Component.extend({
 	scope : {
 		showServicesPanel: false,
 		init(){
-			this.attr('hub', new HubModel({name: "outrageous-darkness-1337"}));
+			this.attr('appState', appState);
 		},
 		toggleServicesPanel(){
 			this.attr('showServicesPanel', !this.attr('showServicesPanel'));
