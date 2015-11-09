cdd12570397bf8dbb2c03e6c1d6481702d7febee
Mihael Konjevic <konjevic@gmail.com>
2015-11-09 16:08:09 +0100

    Work on the mobile app

---
+++ public/new-bithub-client/src/models/brand.js
@@ -0,0 +1,5 @@
+import can from "can";
+
+export default can.Model.extend({
+	findOne : '/api/v3/brands/current'
+}, {});
---
+++ public/new-bithub-client/src/moderation-tabs/moderation-tabs.js
@@ -0,0 +1,30 @@
+import can from "can";
+import template from "./moderation-tabs.stache!";
+
+import "./moderation-tabs.less!";
+import "can/route/";
+
+const ICON_MAPPINGS = {
+	approved: 'thumbs-up',
+	starred: 'star',
+	deleted: 'trash'
+};
+
+
+export default can.Component.extend({
+	tag: 'bh-moderation-tabs',
+	template: template,
+	scope : {
+		icon : function(icon){
+			var color = this.attr('activeTab') === icon ? "white" : "grey";
+			return "images/app-resources-icons-" + (ICON_MAPPINGS[icon] || icon) + "-" + color + ".svg"	
+		}
+	},
+	helpers : {
+		moderationUrl : function(tab){
+			tab = can.isFunction(tab) ? tab() : tab;
+			return can.route.url({moderationTab: tab}, true);
+		}
+	}
+});
+
