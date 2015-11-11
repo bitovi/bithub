ca8c0fdf0032821e6c0b53878a6b25b8f9158bed
Mihael Konjevic <konjevic@gmail.com>
2015-11-11 12:44:14 +0100

    Implement first cut of the moderation flow
    Former-commit-id: a5a62b634edb01007ffa0a3e3463cc39bfaedbee [formerly 7a404d1e51f8134d12c45b21ea2324e9b7d47c90] [formerly 84de2d2e7e2b893dbff181f3b9ac3e8cfffdaec6 [formerly 9917eff87113f0eaffe100749469a8cab8f83605]] [formerly 3bbbe9d5cbf42abeb80f9e3d2f26825b2c7a016d [formerly 983055e9d7b352b167133ca1219c309b7c2b2268] [formerly 9e5007df82b06c78142538e6ea2dc517253f021d [formerly 03ea0a630328e4d6a504f82ccbfe06048e56007f]]]
    Former-commit-id: 0f018f0c7b8d43fcbed142cf43eb577b2489c8f7 [formerly ae38d3873e75f708f012705e5e3fe318d9d1de48] [formerly 95e37bcf7557d0453516609a57ef09d36f398bf1 [formerly 23dbfe80c63ad1083f08e57f114e85bde9f385bb]]
    Former-commit-id: 4cd328918f7112a1373b5b27347f9631a609d910 [formerly d3ac1895403d6a86047dbd0505de4d8c44a29646]
    Former-commit-id: c0145e9c666b50e957a6c467e5a9ad2e58feb27a

--- public/new-bithub-client/src/models/bit.js
+++ public/new-bithub-client/src/models/bit.js
@@ -5,7 +5,7 @@ import "can/list/promise/";
 import "can/map/define/";
 
 var Bit = can.Model.extend({
-	resource : '/api/v3/embeds/{hubId}/entities',
+	resource : '/api/v4/embeds/{hubId}/entities',
 }, {
 	define : {
 		thread_updated_at: {
@@ -42,27 +42,38 @@ var Bit = can.Model.extend({
 	}
 });
 
-Bit.ACTIONS = ['pin', 'unpin', 'approve', 'disapprove'];
 
-var makeBitAction = function(action){
-	var templateUrl = '/api/v3/embeds/{hubId}/entities/{id}/' + action;
+Bit.DECISIONS = ['pending', 'starred', 'deleted', 'approved'];
+
+var makeBitAction = function(decision){
+	var templateUrl = '/api/v4/embeds/{hubId}/entities/{id}/decide/?decision={decision}';
 	return function(hubId){
+		var realDecision = decision;
+		var currentDecision = this.attr('decision');
+		
+		if(decision === currentDecision){
+			realDecision = 'pending';
+		}
+
 		var url = can.sub(templateUrl, {
 			hubId : hubId,
-			id : this.attr('id')
+			id : this.attr('id'),
+			decision: realDecision
 		});
 
 		return $.ajax(url, {
 			dataType: 'json',
 			type: 'PUT'
 		}).then(function(data){
-			Bit.model(data);
+			can.trigger(Bit, 'decision', [currentDecision, realDecision]);
+			var bit = Bit.model(data);
+			bit.updated();
 		});
 	};
 };
 
-for(var i = 0; i < Bit.ACTIONS.length; i++){
-	Bit.prototype[Bit.ACTIONS[i]] = makeBitAction(Bit.ACTIONS[i]);
+for(var i = 0; i < Bit.DECISIONS.length; i++){
+	Bit.prototype['decide' + can.capitalize(Bit.DECISIONS[i])] = makeBitAction(Bit.DECISIONS[i]);
 }
 
 var checkIfBitIsBelowCurrentBit = function(bit, currentBit){
@@ -140,6 +151,12 @@ Bit.List = Bit.List.extend({
 		}
 
 		this.splice(index, 0, bit);
+	},
+	remove : function(bit){
+		var index = this.indexOf(bit);
+		if(index > -1){
+			this.splice(index, 1);
+		}
 	}
 });
 
--- public/new-bithub-client/src/moderation-panel/moderation-panel.js
+++ public/new-bithub-client/src/moderation-panel/moderation-panel.js
@@ -1,9 +1,29 @@
 import can from "can";
 import template from "./moderation-panel.stache!";
+import Bit from "src/models/bit";
 
 import "./moderation-panel.less!";
 
 export default can.Component.extend({
 	tag: 'bh-moderation-panel',
-	template: template
+	template: template,
+	scope : {
+		Bit: Bit
+	},
+	events : {
+		"{scope.Bit} updated" : function(Bit, ev, bit){
+			var bitDecision = bit.attr('decision');
+			var activeDecision = this.scope.attr('activeDecision');
+			var bits = this.scope.attr('bits');
+			var bitElement = this.element.find('[data-bit-id=' + bit.id + ']');
+			if(bitDecision !== activeDecision){
+				setTimeout(function(){
+					bitElement.slideUp(300, function(){
+						bits.remove(bit);
+					});
+				}, 1000);
+				
+			}
+		}
+	}
 });
