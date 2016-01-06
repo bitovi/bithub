3144b7a7ce8fbf71f76c119e1feaa2642739fd7c
Nikica Jokic <neektza@gmail.com>
2016-01-06 18:00:02 +0100

    Initial DB setup is different on Heroku.

--- db/migrate/10000_initial_setup.rb
+++ db/migrate/10000_initial_setup.rb
@@ -1,12 +1,5 @@
 class InitialSetup < ActiveRecord::Migration
   def change
-    if ENV['VAGRANT'].nil?
-      ActiveRecord::Base.connection.execute "CREATE EXTENSION IF NOT EXISTS hstore WITH VERSION '1.2';"
-      ActiveRecord::Base.connection.execute "CREATE EXTENSION IF NOT EXISTS intarray WITH VERSION '1.0'"
-      ActiveRecord::Base.connection.execute "CREATE EXTENSION IF NOT EXISTS btree_gin"
-      ActiveRecord::Base.connection.execute "CREATE EXTENSION IF NOT EXISTS plpgsql;"
-    end
-
     ActiveRecord::Base.connection.execute "CREATE EXTENSION IF NOT EXISTS hstore;"
     ActiveRecord::Base.connection.execute "CREATE EXTENSION IF NOT EXISTS intarray;"
     ActiveRecord::Base.connection.execute "CREATE EXTENSION IF NOT EXISTS btree_gin"
