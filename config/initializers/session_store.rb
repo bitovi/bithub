e476c8ad4aa464eb696731dc947850c6e0315abf
Nikica Jokic <neektza@gmail.com>
2016-01-15 13:39:26 +0100

    Session store uses REDIS_URL.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-host, port, db = /^redis:\/\/([\d\.]+):(\d+)\/(\d+)/.match( ENV['REDIS_URL'] ).captures
-
 Bithub::Application.config.session_store :redis_session_store, {
   key: '_session_id',
-  redis: {
-    host: host,
-    port: port,
-    db: db,
-    expire_after: 1.month,
-    key_prefix: 'session:'
-  },
+  url: ENV['REDIS_URL'],
   serializer: :json
 }
