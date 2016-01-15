36620e0c23f1087d1069a718a45616306ad7e8f9
Mihael Konjevic <konjevic@gmail.com>
2016-01-15 18:05:23 +0100

    Make unicorn socket location compatible with new bithub

--- config/unicorn_local.rb
+++ config/unicorn_local.rb
@@ -7,7 +7,7 @@ timeout 30
 preload_app true
 
 # Unix socket
-listen "unix:/tmp/bithub_unicorn.sock", :backlog => 64
+listen "unix:/tmp/bithub.sock", :backlog => 64
 
 # PID
 pid "./tmp/pids/unicorn.pid"
