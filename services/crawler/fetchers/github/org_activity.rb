9ca59a2f404dedf99256ac24dd0f7e4034b2b356
Nikica Jokic <neektza@gmail.com>
2016-01-21 16:53:32 +0100

    Handle redirects in Fetchers::Github responses.
    Sometimes the Github client responds with a 30x. We now follow that
    redirect where before we used to just ignore it.

--- services/crawler/fetchers/github/org_activity.rb
+++ services/crawler/fetchers/github/org_activity.rb
@@ -1,3 +1,4 @@
+require_relative 'common'
 require 'github_api'
 
 module Fetchers
@@ -5,6 +6,7 @@ module Fetchers
 
     class OrgActivity
       include Protocol
+      include Github::UnknownGithubErrorHandler
 
       def initialize(client, opts)
         @client = client
@@ -16,8 +18,20 @@ module Fetchers
 
         handle_errors do
           @client.activity.events.auto_pagination = false
-          @client.activity.events.org(@org)
+          resp = @client.activity.events.org(@org)
+
+          handle_unknown_response(resp) do
+            @client.activity.events.org(@org)
+          end
+
         end
+      end
+      
+      def reset_settings_from_redirect(resp)
+        @org = HTTParty.get(resp['url'])[0]['org']['login']
+      rescue => e
+        Celluloid.logger.error "[FETCHER] Error in Github/OrgActivity while trying to reset @user and @repo"
+        [ ]
       end
 
       def initial_fetch
--- services/crawler/fetchers/github/repo_issues.rb
+++ services/crawler/fetchers/github/repo_issues.rb
@@ -1,8 +1,11 @@
+require_relative 'common'
+
 module Fetchers
   module Github
 
     class RepoIssues
       include Protocol
+      include Github::UnknownGithubErrorHandler
 
       def initialize(client, opts)
         @client = client
@@ -13,8 +16,18 @@ module Fetchers
         Celluloid.logger.info "[FETCHER] Fetching Github/RepoIssues"
 
         handle_errors do
-          @client.issues.list(user: @user, repo: @repo)
+          resp = @client.issues.list(user: @user, repo: @repo)
+          handle_unknown_response(resp) do
+            @client.issues.list(user: @user, repo: @repo)
+          end
         end
+      end
+
+      def reset_settings_from_redirect(resp)
+        @user, @repo = HTTParty.get(resp['url'])[0]['url'].match(/repos\/(.*)\/issues/)[1].split('/')
+      rescue => e
+        Celluloid.logger.error "[FETCHER] Error in Github/RepoIssues while trying to reset @user and @repo"
+        [ ]
       end
     end
   end
