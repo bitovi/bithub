cbd9758522d9d06336a057f671afe4f3dfab2899
Nikica Jokic <neektza@gmail.com>
2016-01-27 17:40:00 +0100

    Forgot to commit BaseFetcher.

---
+++ services/guzzler/fetchers/facebook/base_fetcher.rb
@@ -0,0 +1,22 @@
+require 'koala'
+require 'guzzler/fetchers/protocol'
+
+module Guzzler::Fetchers
+
+  module Facebook
+    class BaseFetcher
+      include Protocol
+
+      LIMIT  = 250
+      FIELDS = 'attachments,from,message,picture,link,object_id,updated_time,type,status_type'
+
+      def initialize(service = nil)
+        @service = (block_given?) ? yield : service
+      end
+        
+      def client
+        Koala::Facebook::API.new(@service.config.fetch(:access_token))
+      end
+    end
+  end
+end
