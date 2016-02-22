d3db45d06af3c673f70169c6c2f9429e53faca45
Mark Stahl <mjstahl@gmail.com>
2016-02-22 11:25:58 -0500

    Modify log, socket, and pid locations for Puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,17 +1,22 @@
 if defined?(Puma)
-  workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-  threads_count = Integer(ENV['MAX_THREADS'] || 5)
-  threads threads_count, threads_count
-  
-  environment ENV['RACK_ENV'] || 'development'
+  workers Integer(ENV["WEB_CONCURRENCY"] || 2)
+  threads_count = Integer(ENV["MAX_THREADS"] || 5)
+  threads 1, threads_count
 
-  if ENV['ENV'] == 'production'
-    preload_app!
-    port ENV['PORT']
-  elsif ENV['ENV'] == 'development'
-    bind "unix:///tmp/bithub.sock"
-  end
+  app_dir = File.expand_path("../../..", File.dirname(__FILE__))
+  shared_dir = "#{app_dir}/shared"
 
+  rails_env = ENV["RACK_ENV"] || "production"
+  environment rails_env
+
+  bind "unix://#{shared_dir}/sockets/puma.sock"
+
+  stdout_redirect "#{shared_dir}/logs/puma.stdout.log", "#{shared_dir}/logs/puma.stderr.log", true
+
+  pidfile "#{shared_dir}/pids/puma.pid"
+  state_path "#{shared_dir}/pids/puma.state"
+
+  preload_app!
   rackup DefaultRackup
 
   on_worker_boot do
