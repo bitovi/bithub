89fca85ff97e3185cfc38b9e1c037f8dee0dc98b
Mark Stahl <mjstahl@gmail.com>
2016-03-30 16:44:38 -0400

    Refactor tests to not fail, allowing us to uncomment them

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Api::SessionsController, type: :controller do
+	well_formed = { "email": "hello@example.com", "password": "UDontKnowJack" }
+	
 	describe "POST /session" do
 		it "should return 400 when no email property is present" do
 			post :create, { "password": "UDontKnowJack" }
@@ -11,33 +13,42 @@ RSpec.describe Api::SessionsController, type: :controller do
 			post :create, { "email": "hello@example.com" }
 			response.should have_http_status 400
 		end
-		
-		# it "should log a user in given an email and password" do
-		# 	well_formed = { "email": "hello@example.com", "password": "UDontKnowJack" }
-		# 	Account.new(well_formed).save!
-			
-		# 	post :create, well_formed
-		# 	response.should have_http_status 201
-		# 	assigns[:current_account][:email].should eq("hello@example.com")
-		# end
 	end
 	
-	# describe "GET /session" do
-	# 	it "should return a session when requested by an authenticated user" do
-	# 		account = FactoryGirl.create(:account)
-	# 		sign_in :account, account
-	# 		get :show
-	# 		response.should have_http_status 200
-	# 		assigns[:current_account].should eq(account)
-	# 	end
-	# end
+	context "the user exists in the system" do
+		before(:example) do
+			account = Account.new(well_formed)
+			account.save!
+			
+			organization = Organizations::OrganizationBuilder.new account, {}
+			organization.build.save!
+		end
+		
+		describe "POST /session" do
+			it "should log a user in given an email and password" do
+				post :create, well_formed
+				response.should have_http_status 201
+				assigns[:current_account][:email].should eq("hello@example.com")
+			end
+		end
+		
+		describe "GET /session" do
+			it "should return a session when requested by an authenticated user" do
+				post :create, well_formed
+				get :show
+				response.should have_http_status 200
+				assigns[:current_account].email.should eq(well_formed[:email])
+			end
+		end
 	
-	describe "DELETE /session" do
-		it "should log the user out when requested by an authenticated user" do
-			sign_in :account, FactoryGirl.create(:account)
-			delete :destroy
-			response.should have_http_status 200
-			assigns[:current_account].should be_nil
+		describe "DELETE /session" do
+			it "should log the user out when requested by an authenticated user" do
+				sign_in :account, FactoryGirl.create(:account)
+				
+				delete :destroy
+				response.should have_http_status 200
+				assigns[:current_account].should be_nil
+			end
 		end
 	end
 end
