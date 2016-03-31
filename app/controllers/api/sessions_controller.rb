f04d8264c2b4bd0aebee0a00369790f904af73be
Mark Stahl <mjstahl@gmail.com>
2016-03-31 09:25:16 -0400

    Fixes #375. Adds organization_id and tenant_name to the session object

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,32 +3,31 @@ class Api::SessionsController < Api::BaseController
 	before_action :ensure_current_account,      only: [ :show, :destroy ]
 
     def create
-		return user_session if current_account
+		return render json: user_session if current_account
     	
 		resource = Account.find_for_database_authentication email: params[:email]
 		return invalid_login_attempt unless resource
 		
 		if resource.valid_password? params[:password]
 			sign_in :account, resource
-			return user_session :created
+			return render json: user_session, status: :created
 		end
 		invalid_login_attempt
 	end
 	
 	def show
-		return user_session
+		return render json: user_session, status: :ok
 	end
 
     def destroy
 		sign_out :account
 		render json: { }, status: :ok
     end
-
-    protected
 	
-	def user_session(status = :ok)
-		user = {}.merge(current_account.as_json)
-		user[:tenant_name] = current_account.organizations.first.brands.first.tenant_name
-		render json: user, status: status
+	def user_session
+		organization = current_account.organizations.first
+		session["organization_id"] = organization.id
+		session["tenant_name"] = organization.brands.first.tenant_name
+		return session
 	end
 end
