1ceaed22b1ea3c35ed6379ad8c3a98591909f68a
Mark Stahl <mjstahl@gmail.com>
2016-03-31 09:30:25 -0400

    Refactoring of error messages on base controller

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -9,22 +9,14 @@ class Api::BaseController < ActionController::Base
 	
 	def ensure_current_account
 		return if current_account
-		return must_be_authenticated
-	end
-	
-	def ensure_auth_params_exists
-		return unless params[:email].blank? || params[:password].blank?
-		return invalid_login_attempt
-	end
-	
-	def must_be_authenticated
-		render json: {
+		return render json: {
 			message: "You must authenticate prior to requesting this resource"
 		}, status: :unauthorized
 	end
 	
-	def invalid_login_attempt
-		render json: { 
+	def ensure_auth_params_exists
+		return unless params[:email].blank? || params[:password].blank?
+		return render json: { 
 			message: "We were unable to log you in. Please double-check your email and password."
 		}, status: :bad_request
 	end
