c85261e370a75d8004764477a60be52755340715
Mark Stahl <mjstahl@gmail.com>
2016-06-29 15:20:48 -0400

    Instagram compliance

--- services/guzzler/fetchers/instagram/base.rb
+++ services/guzzler/fetchers/instagram/base.rb
@@ -61,9 +61,9 @@ module Guzzler::Fetchers
 
       def create_client
         if @access_token
-          ::Instagram.client access_token: @access_token, scope: "public_content"
+          ::Instagram.client access_token: @access_token
         else
-          ::Instagram.client client_id: ENV['INSTAGRAM_CLIENT_ID'], client_secret: ENV['INSTAGRAM_CLIENT_SECRET'], scope: "public_content"
+          ::Instagram.client client_id: ENV['INSTAGRAM_CLIENT_ID'], client_secret: ENV['INSTAGRAM_CLIENT_SECRET']
         end
       end
 
--- services/guzzler/listener/subscribers/instagram.rb
+++ services/guzzler/listener/subscribers/instagram.rb
@@ -28,7 +28,7 @@ module Guzzler
         end
 
         def client
-          @client ||= ::Instagram.client client_id: ENV['INSTAGRAM_CLIENT_ID'], client_secret: ENV['INSTAGRAM_CLIENT_SECRET'], scope: "public_content"
+          @client ||= ::Instagram.client client_id: ENV['INSTAGRAM_CLIENT_ID'], client_secret: ENV['INSTAGRAM_CLIENT_SECRET']
         end
 
         def callback_url
